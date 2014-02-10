aefb0b4bd8546588cc712396409c989c1f176f97
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-02-10 13:46:21 -0800

    Fixed title

--- services/queue/routes/index.js
+++ services/queue/routes/index.js
@@ -4,7 +4,7 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: "Task Cluste Queue - Deploy Test" });
+  res.render('index', { title: null });
 };
 
 // Include all modules
