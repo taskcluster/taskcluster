1d7fd5e9c2348b50959eaba23b5e26a014b4d6de
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-02-25 18:27:44 -0800

    Allow Content-Type for CORS request, it's basically required :)

--- services/queue/routes/api/utils.js
+++ services/queue/routes/api/utils.js
@@ -129,7 +129,7 @@ API.prototype.mount = function(app, mountpoint) {
   // Allow CORS requests to the API
   app.use(mountpoint, function(req, res, next) {
     res.header('Access-Control-Allow-Origin',   '*');
-    res.header('Access-Control-Allow-Headers',  'X-Requested-With');
+    res.header('Access-Control-Allow-Headers',  'X-Requested-With,Content-Type');
     next();
   });
 
