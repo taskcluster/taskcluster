d3abd48fa4c976db5d2d8f0925c17b99b037f140
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-04-02 19:30:00 -0700

    Revert the removal of tar-stream, docker copy still supplies a tar-stream

--- workers/docker-worker/docker_worker/middleware/artifact_extractor.js
+++ workers/docker-worker/docker_worker/middleware/artifact_extractor.js
@@ -1,45 +1,115 @@
 var mime          = require('mime');
 var Promise       = require('promise');
 var debug         = require('debug')('taskcluster-docker-worker:ArtifactExtractor');
+var fs            = require('fs');
+var tar           = require('tar-stream');
 var request       = require('superagent');
 
+
 /**
-
-Upload file given by path from within container to a putUrl,
-Return a promise for reason the file is missing, or `null` if the file
-uploaded successfully. The promise will only fail in case on unexpected
-errors such as network, etc.
-
-*/
+ * Upload file given by path from within container to a putUrl,
+ * Return a promise for reason the file is missing, or `null` if the file
+ * uploaded successfully. The promise will only fail in case on unexpected
+ * errors such as network, etc.
+ */
 var putFileFromContainer = function(container, path, putUrl, contentType) {
-  var copy = Promise.denodeify(container.copy.bind(container));
-  /**
-  Copy will return the raw resource if asked for a single file OR a .tar.gz (uncompressed) of the requested
-  folder.
-  */
-  return copy({ Resource: path }).then(function(res) {
-    return new Promise(function(accept, reject) {
-      // pipe docker file into the response...
-      var req = request
-        .put(putUrl)
-        .set('Content-Type',    contentType)
-        .set('Content-Length',  res.headers['content-length']);
+  return new Promise(function(accept, reject) {
+    // Copy resource from docker container, this operation returns a TAR-stream
+    container.copy({Resource: path}, function(err, res) {
+      // Handle errors such as file that doesn't exist
+      if (err) {
+        debug("Failed to fetch artifact: ", err, err.stack);
+        return accept("Failed to extract artifact from '" + path +
+                      "' most likely it doesn't exist");
+      }
 
-      res.pipe(req);
-      req.once('error', reject);
-      req.once('response', function(res) {
-        if (res.error) return reject(res.error);
-        accept();
+      // Create a tar extraction stream
+      var extract = tar.extract();
+
+      // We'll track resolution as we decode the stream. This is just for
+      // robustness as the API isn't well-documented and we don't like
+      // undefined behavior
+      var resolution = null;
+
+      // Handle entries from the tar-stream as they occur, hopefully we'll only
+      // get one entry! We can get more than one, if path is a folder. We don't
+      // want to support the folder extraction use-case because we can't upload
+      // to S3 before we have the size of the file we're uploading. Hence, we
+      // would have to hit the host file system, which is bad karma, in that
+      // case might as well tar it up inside the worker first.
+      extract.on('entry', function(header, stream, next) {
+        // If we've extract files before we have been given a folder, we don't
+        // like that at all...
+        if (resolution !== null) {
+          debug("container.copy from docker gave us more than entry!");
+
+          // This is unexpected behavior, so it's best to assume that the
+          // artifact uploaded before is undefined
+          resolution = {
+            action:   accept,
+            retval:   "Failed to extract artifact, '" + path + "' is a " +
+                      "folder, please tar it up first. We only support one " +
+                      "file per artifact"
+          };
+
+          // Drain the stream
+          stream.resume();
+          stream.on('end', function() {
+            // Whatever just happened we have to keep going
+            next();
+          });
+          return;
+        }
+        // Set a resolution, so that we can come past the check above. Note that
+        // this resolution is also true... if suddenly the tar-stream finishes
+        // right now. But this should NEVER happen
+        resolution = {
+          action:     reject,
+          retval:     new Error("CRITICAL: Artifact upload didn't finish")
+        };
+
+        // Make a request to the
+        var req = request
+                    .put(putUrl)
+                    .set('Content-Type',    contentType)
+                    .set('Content-Length',  header.size);
+        //TODO: Investigate the feasibility of setting additional custom headers
+        //      such as file permissions, executable bit as these are present in
+        //      the tar `header`.
+
+        // Pipe the file stream to the PUT request
+        stream.pipe(req, {end: false});
+
+        // End PUT request and handle response
+        req.end(function(res) {
+          // If put result failed
+          if (!res.ok) {
+            debug("Failed to pipe the artifact file stream to S3");
+            resolution = {
+              action:     reject,
+              retval:     res.text
+            };
+          } else {
+            debug("Artifact successfully uploaded!");
+            resolution = {
+              action:     accept,
+              retval:     null
+            };
+          }
+          // Whatever happened we have to finish tar extraction
+          next();
+        });
       });
 
+      // Wait for TAR extraction to finish
+      extract.on('finish', function() {
+        // Make the resolution whatever it was take effect
+        resolution.action(resolution.retval);
+      });
+
+      // Pipe the data-stream from docker for tar-extraction
+      res.pipe(extract);
     });
-  }).catch(function(err) {
-    debug("Failed to put artifact: ", err, err.stack);
-    // XXX: Only S3 artifact upload failure or some terrible docker bug should
-    //      cause this error.
-    throw err;
-  }).then(function() {
-    debug("Artifact successfully uploaded!", path);
   });
 };
 
--- workers/docker-worker/docker_worker/test/integration/artifact_extractor_test.js
+++ workers/docker-worker/docker_worker/test/integration/artifact_extractor_test.js
@@ -19,14 +19,11 @@ suite('artifact extration tests', function() {
     }).then(function(data) {
       // Get task specific results
       var result = data.result.result;
-      console.log(data.result);
       assert.equal(result.exitCode, 0);
       assert.ok(data.result.artifacts['username.txt'] !== undefined);
       assert.ok(data.result.artifacts['passwd.txt'] !== undefined);
-      // XXX: As of docker version 0.9.1 we don't know when files are missing
-      // this is likely a bug as it reports it as an error in docker.log.
-      //assert.ok(data.result.artifacts['my-missing.txt'] === undefined);
-      //assert.ok(result.missingArtifacts['my-missing.txt'] !== undefined);
+      assert.ok(data.result.artifacts['my-missing.txt'] === undefined);
+      assert.ok(result.missingArtifacts['my-missing.txt'] !== undefined);
     });
   });
 });
