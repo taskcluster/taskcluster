adc982157d586127659f5ba7ea3bf1ce1b1efb33
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-04-22 15:25:27 -0700

    Initial commit

---
+++ libraries/client/client_test.js
@@ -0,0 +1,12 @@
+var queue = require('./client').queue;
+
+exports.test_getAMQPConnectionString = function(test) {
+  test.expect(1);
+  queue.getAMQPConnectionString().then(function(result) {
+    test.ok(result.url);
+    test.done();
+  }).catch(function() {
+    test.ok(false);
+    test.done();
+  });
+};
