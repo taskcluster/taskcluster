ce830aed6d40a42804ce21c80304d7f1249ce93a
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-04-22 17:37:29 -0700

    Forgot to remove ./client

--- services/treeherder/worker.js
+++ services/treeherder/worker.js
@@ -1,7 +1,6 @@
 var debug     = require('debug')('worker');
 var config    = require('./config');
 var nconf     = require('nconf');
-var Client    = require('./client');
 var events    = require('./events');
 
 config.load();
