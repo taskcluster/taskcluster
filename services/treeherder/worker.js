817faf981e1afcc6370755d0eb7c9bc494c9b37e
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-04-23 10:08:10 -0700

    Reorder config loading

--- services/treeherder/worker.js
+++ services/treeherder/worker.js
@@ -1,9 +1,9 @@
-var debug     = require('debug')('worker');
 var config    = require('./config');
+config.load();
+
+var debug     = require('debug')('worker');
 var nconf     = require('nconf');
 var events    = require('./events');
-
-config.load();
 
 events.setup().then(function() {
   console.log("Now running...");
