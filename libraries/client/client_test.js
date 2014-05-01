5b8f71be36482fc37760e11956a23da9260cbce8
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-05-01 16:33:09 -0700

    New version bugs fixed

--- libraries/client/client_test.js
+++ libraries/client/client_test.js
@@ -10,3 +10,14 @@ exports.test_getAMQPConnectionString = function(test) {
     test.done();
   });
 };
+
+exports.test_getAMQPConnectionString = function(test) {
+  test.expect(1);
+  queue.getAMQPConnectionString().then(function(result) {
+    test.ok(result.url);
+    test.done();
+  }).catch(function() {
+    test.ok(false);
+    test.done();
+  });
+};
