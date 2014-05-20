f0ca10da01b7e4478196024e0c6c0d0df8847ca9
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-05-20 16:57:43 -0700

    Add schemas and tests for schemas

--- services/auth/test/validate_test.js
+++ services/auth/test/validate_test.js
@@ -1,7 +1,3 @@
-var validate    = require('../utils/validate');
-var fs          = require('fs');
-var path        = require('path');
-
 /**
  * Test cases each defined with relative path, schema identifier and success as
  * either true or false depending on whether or not validation should succeed
@@ -10,46 +6,81 @@ var path        = require('path');
  */
 var testCases = [
   {
-    path:     'schemas/task-graph-example.json',
-    schema:   'http://schemas.taskcluster.net/scheduler/v1/task-graph.json#',
-    success:  true,
+    path:     'schemas/client-scopes-ok1.json',
+    schema:   'http://schemas.taskcluster.net/auth/v1/client-scopes-response.json#',
+    success:  true
   }, {
-    path:     'schemas/invalid-task-graph-example.json',
-    schema:   'http://schemas.taskcluster.net/scheduler/v1/task-graph.json#',
-    success:  false,
+    path:     'schemas/client-scopes-ok2.json',
+    schema:   'http://schemas.taskcluster.net/auth/v1/client-scopes-response.json#',
+    success:  true
+  }, {
+    path:     'schemas/client-scopes-ok3.json',
+    schema:   'http://schemas.taskcluster.net/auth/v1/client-scopes-response.json#',
+    success:  true
+  }, {
+    path:     'schemas/client-scopes-fail1.json',
+    schema:   'http://schemas.taskcluster.net/auth/v1/client-scopes-response.json#',
+    success:  false
+  }, {
+    path:     'schemas/client-scopes-fail2.json',
+    schema:   'http://schemas.taskcluster.net/auth/v1/client-scopes-response.json#',
+    success:  false
+  }, {
+    path:     'schemas/client-credentials-ok.json',
+    schema:   'http://schemas.taskcluster.net/auth/v1/client-credentials-response.json#',
+    success:  true
+  }, {
+    path:     'schemas/client-credentials-fail.json',
+    schema:   'http://schemas.taskcluster.net/auth/v1/client-credentials-response.json#',
+    success:  false
   }
 ];
 
-// define a test for each test file
-testCases.forEach(function(testCase) {
-  exports[testCase.path] = function(test) {
-    test.expect(1);
+suite("validate", function() {
+  var fs          = require('fs');
+  var path        = require('path');
+  var assert      = require('assert');
+  var base        = require('taskcluster-base');
+  var validator = null;
 
-    // Setup validate if needed, before we continue
-    validate.setup().then(function() {
+  // Setup validator
+  setup(function() {
+    return base.validator({
+      folder:           path.join(__dirname, '..', 'schemas'),
+      constants:        require('../schemas/constants'),
+    }).then(function(validator_) {
+      validator = validator_;
+    });
+  });
+
+  // Create a test for each test case
+  testCases.forEach(function(testCase) {
+    test(testCase.path, function() {
       // Load test data
       var filePath = path.join(__dirname, testCase.path);
       var data = fs.readFileSync(filePath, {encoding: 'utf-8'});
       var json = JSON.parse(data);
 
       // Validate json
-      var errors = validate(json, testCase.schema);
+      var errors = validator.check(json, testCase.schema);
 
       // Test errors
       if(testCase.success) {
-        test.ok(errors === null, "Schema doesn't match test for " + testCase.path);
         if (errors !== null) {
           console.log("Errors:");
           errors.forEach(function(error) {
             console.log(error);
           });
         }
+        assert(errors === null, "Schema doesn't match test for " + testCase.path);
       } else {
-        test.ok(errors !== null, "Schema matches unexpectedly test for " + testCase.path);
+        assert(errors !== null, "Schema matches unexpectedly test for " + testCase.path);
       }
-
-      // okay tests are done
-      test.done();
     });
-  };
+  }, this);
+
+  // Release validator
+  teardown(function() {
+    validator = null;
+  })
 });
