1be99e9b3a6b35b7a333dd5297f0fde8b36f3455
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-05-24 00:49:12 -0700

    Added most of the things needed for new reference.json support...

--- libraries/client/utils/update-apis.js
+++ libraries/client/utils/update-apis.js
@@ -7,9 +7,6 @@ var program     = require('commander');
 var _           = require('lodash');
 var Promise     = require('promise');
 
-// Default API version to load
-var DEFAULT_VERSION = 1;
-
 // Path to apis.json file
 var apis_json   = path.join(__dirname, '..', 'apis.json');
 
@@ -20,22 +17,18 @@ var loadApis = function() {
 
 /** Save APIs to apis.json */
 var saveApis = function(apis) {
-  fs.writeFileSync(apis_json, JSON.stringify(apis), {encoding: 'utf-8'});
+  fs.writeFileSync(apis_json, JSON.stringify(apis, null, 2), {encoding: 'utf-8'});
 };
-
-// Track if an action is running
-var ran_action = false;
 
 program
   .command('list')
   .description("List API references and names stored")
   .action(function() {
-    ran_action = true;
     var apis = loadApis();
     var rows = [
-      ['Name', 'baseUrl']
+      ['Name', 'referenceUrl']
     ].concat(_.keys(apis).map(function(name) {
-      return [name, apis[name].baseUrl];
+      return [name, apis[name].referenceUrl];
     }));
     console.log(cliff.stringifyRows(rows));
   });
@@ -44,7 +37,6 @@ program
   .command('show <name>')
   .description("Show references for a specific API")
   .action(function(name, options) {
-    ran_action = true;
     var apis  = loadApis();
     var api   = apis[name];
     if (api === undefined) {
@@ -55,23 +47,13 @@ program
   });
 
 program
-  .command('add <name> <baseUrl>')
-  .option(
-    '-v, --api-version [version]',
-    "API version to load",
-    DEFAULT_VERSION
-  )
+  .command('add <name> <reference-url>')
   .option(
     '-f, --force',
     "Overwrite existing API entry with same name"
   )
-  .option(
-    '-r, --reference [file]',
-    "Reference to load from file, if not loaded from <baseUrl>"
-  )
-  .description("Add API reference with <name> with <baseUrl>")
-  .action(function(name, baseUrl, options) {
-    ran_action = true;
+  .description("Add API reference with <name> with <referenceUrl>")
+  .action(function(name, referenceUrl, options) {
     var apis = loadApis();
     // Check that we don't overwrite unless there is a force
     if (apis[name] !== undefined && !options.force) {
@@ -80,35 +62,22 @@ program
       process.exit(1);
     }
 
-    // Load reference
-    var loaded_reference = null;
-    if (options.reference) {
-      // Load reference from file
-      loaded_reference = new Promise(function(accept, reject) {
-        var data = fs.readFileSync(options.reference, {encoding: 'utf-8'});
-        accept(JSON.parse(data));
-      });
-    } else {
-      // Load reference using baseUrl and version
-      var url = baseUrl + '/v' + options.apiVersion + '/reference';
-      console.log("Fetching reference from: " + url);
-      loaded_reference = request.get(url).end().then(function(res) {
-        if (!res.ok) {
-          console.log("Failed to fetch reference from: " + baseUrl)
-          console.log("Error: " + res.text);
-          process.exit(2);
-        }
-        return res.body;
-      });
-    }
+    // Load reference from referenceUrl
+    console.log("Fetching reference from: " + referenceUrl);
+    var loaded_reference = request.get(referenceUrl).end().then(function(res) {
+      if (!res.ok) {
+        console.log("Failed to fetch reference from: " + referenceUrl)
+        console.log("Error: " + res.text);
+        process.exit(2);
+      }
+      return res.body;
+    });
 
     // Save reference when loaded
     loaded_reference.then(function(reference) {
       apis[name] = {
-        baseUrl:      baseUrl,
-        reference:    reference,
-        autoUpdate:   (options.reference == undefined),
-        version:      options.apiVersion
+        referenceUrl: referenceUrl,
+        reference:    reference
       };
       saveApis(apis);
       console.log("Add reference: " + name);
@@ -119,35 +88,28 @@ program
 
 program
   .command('update')
-  .description("Update all auto-updatable API references")
+  .description("Update all API references")
   .action(function() {
-    ran_action = true;
     var apis = loadApis();
 
-    // Print APIs that can't be auto-updated
-    _.keys(apis).filter(function(name) {
-      return !apis[name].autoUpdate;
-    }).forEach(function(name) {
-      console.log("Cannot auto-update: " + name);
-    });
-
     // Update remaining references
-    _.keys(apis).filter(function(name) {
-      return apis[name].autoUpdate;
-    }).map(function(name) {
+    Promise.all(_.keys(apis).map(function(name) {
       var api = apis[name];
-      // Load reference using baseUrl and version
-      var url = api.baseUrl + '/v' + api.version + '/reference';
-      console.log("Fetching reference from: " + url);
-      loaded_reference = request.get(url).end().then(function(res) {
+      // Load reference from referenceUrl
+      console.log("Fetching reference from: " + api.referenceUrl);
+      loaded_reference = request
+                            .get(api.referenceUrl)
+                            .end().then(function(res) {
         if (!res.ok) {
-          console.log("Failed to fetch reference from: " + baseUrl)
+          console.log("Failed to fetch reference from: " + api.referenceUrl);
           console.log("Error: " + res.text);
           process.exit(2);
         }
         console.log("Updated: " + name);
         api.reference = res.body;
       });
+    })).then(function() {
+      saveApis(apis);
     });
   });
 
@@ -155,20 +117,26 @@ program
   .command('remove <name>')
   .description('Remove API with a name')
   .action(function(name, options) {
-    ran_action = true;
     var apis = loadApis();
     if (apis[name] === undefined) {
       console.log("No API named: " + name);
       process.exit(1);
     }
     delete apis[name];
+    saveApis(apis);
     console.log("Removed: " + name);
   });
+
+// Show help on unknown action
+program.on('*', function() {
+  program.outputHelp();
+});
 
 program
   .version(require('../package.json').version)
   .parse(process.argv);
 
-if (!ran_action) {
+// Show help if no action
+if (program.args.length < 1 ) {
   program.outputHelp();
 }
