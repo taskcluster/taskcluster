5957ad609bf85f6a4c45d9404317ef0c49f0492f
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-06-02 17:47:18 -0700

    Refactored config files

--- services/queue/config/production.js
+++ services/queue/config/production.js
@@ -1,40 +1,34 @@
-var Provider = require('nconf').Provider;
+module.exports = {
+  queue: {
+    // Should be overwritten by environment variable
+    publishMetaData:              'false',
+    exchangePrefix:               'queue/v1/',
+    tasks: {
+      bucket:                     'tasks.taskcluster.net',
+      publicBaseUrl:              'http://tasks.taskcluster.net'
+    }
+  },
 
-module.exports = function config() {
-  var nconf = new Provider();
+  server: {
+    publicUrl:                      'http://queue.taskcluster.net',
+    port:                           80
+  }
 
-  // Load configuration from command line arguments, if requested
-  nconf.argv();
+  database: {
+    // Provided by environment variable
+    connectionString:               undefined
+  },
 
-  // Configurations elements loaded from commandline, these are the only
-  // values we should ever really need to change.
-  nconf.env({
-    separator:  '__',
-    whitelist:  [
-      'queue__taskBucket',
-      'queue__taskBucketIsCNAME',
-      'queue__publishSchemas',
-      'server__hostname',
-      'server__port',
-      'server__cookieSecret',
-      'database__connectionString',
-      'amqp__url',
-      'aws__accessKeyId',
-      'aws__secretAccessKey'
-    ]
-  });
 
-  // Config from current working folder if present
-  nconf.file('local', 'taskcluster-queue.conf.json');
+  amqp: {
+    // Provided by environment variable
+    url:                            undefined
+  },
 
-  // User configuration
-  nconf.file('user', '~/.taskcluster-queue.conf.json');
-
-  // Global configuration
-  nconf.file('global', '/etc/taskcluster-queue.conf.json');
-
-  // defaults
-  nconf.defaults(require('./defaults'));
-
-  return nconf;
+  // Credentials are given by environment variables
+  aws: {
+    accessKeyId:                    undefined,
+    secretAccessKey:                undefined,
+    region:                         'us-west-2'
+  }
 };
