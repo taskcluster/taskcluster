0b69a5f8b6fe197ee6f21f589fe263084d8cab1e
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-06-03 14:23:22 -0700

    Added test utility for testing schemas

--- libraries/testing/testing.js
+++ libraries/testing/testing.js
@@ -5,6 +5,9 @@ var Promise       = require('promise');
 var debug         = require('debug')('base:LocalApp');
 var events        = require('events');
 var util          = require('util');
+var base          = require('./');
+var fs            = require('fs');
+var path          = require('path');
 
 
 /**
@@ -106,3 +109,63 @@ LocalApp.prototype.terminate = function() {
 
 // Export LocalApp
 exports.LocalApp = LocalApp;
+
+/**
+ * Test schemas with a positive and negative test cases. This will run call
+ * `setuo` and `test` which is assumed to exist in global scope.
+ * Basically, it only makes sense to use from inside `suite` in a mocha test.
+ *
+ * options:{
+ *   validator: {}  // options for base.validator
+ *   cases: [
+ *     {
+ *       schema:    'http://...'      // JSON schema identifier to test against
+ *       path:      'test-file.json', // Path to test file
+ *       success:   true || false     // Is test expected to fail
+ *     }
+ *   ],
+ *   basePath:      path.join(__dirname, 'validate')  // basePath test cases
+ * }
+ */
+var schemas = function(options) {
+  assert(options.validator, "Options must be given for validator");
+  assert(options.cases instanceof Array, "Array of cases must be given");
+
+  var validator = null;
+  setup(function() {
+    return base.validator(options.validator).then(function(validator_) {
+      validator = validator_;
+    });
+  });
+
+  options.cases.forEach(function(testCase) {
+    test(testCase.path, function() {
+      // Load test data
+      var filePath = testCase.path;
+      // Prefix with basePath if a basePath is given
+      if (options.basePath) {
+        filePath = path.join(options.basePath, filePath);
+      }
+      var data = fs.readFileSync(filePath, {encoding: 'utf-8'});
+      var json = JSON.parse(data);
+
+      // Validate json
+      var errors = validator.check(json, testCase.schema);
+
+      // Test errors
+      if(testCase.success) {
+        if (errors !== null) {
+          debug("Errors: %j", errors);
+        }
+        assert(errors === null,
+               "Schema doesn't match test for " + testCase.path);
+      } else {
+        assert(errors !== null,
+               "Schema matches unexpectedly test for " + testCase.path);
+      }
+    });
+  });
+};
+
+// Export schemas
+exports.schemas = schemas;
