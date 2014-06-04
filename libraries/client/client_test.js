958752f3b644e1c1c17f2b8b115e57c907e900c8
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-06-04 13:28:32 -0700

    New taskcluster client works

--- libraries/client/client_test.js
+++ libraries/client/client_test.js
@@ -1,23 +1,15 @@
-var queue = require('./client').queue;
-
-exports.test_getAMQPConnectionString = function(test) {
-  test.expect(1);
-  queue.getAMQPConnectionString().then(function(result) {
-    test.ok(result.url);
-    test.done();
-  }).catch(function() {
-    test.ok(false);
-    test.done();
+suite('auth', function() {
+  var taskcluster = require('./');
+  test('inspect', function() {
+    var auth = new taskcluster.Auth({
+      credentials: {
+        clientId:     'zIroSzcvRPezmco_cecVmA',
+        accessToken:  'Rj7z_GDMSaKWGadgs-je1Am7z5Q4zuQgOj6AZa0wzSSgAIkKQpB1RTG328W1Ls6WTA'
+      }
+    });
+    // Inspect the credentials
+    return auth.inspect('zIroSzcvRPezmco_cecVmA').then(function(client) {
+      assert(client.clientId === 'zIroSzcvRPezmco_cecVmA', "Expected clientId");
+    });
   });
-};
-
-exports.test_getAMQPConnectionString = function(test) {
-  test.expect(1);
-  queue.getAMQPConnectionString().then(function(result) {
-    test.ok(result.url);
-    test.done();
-  }).catch(function() {
-    test.ok(false);
-    test.done();
-  });
-};
+});
