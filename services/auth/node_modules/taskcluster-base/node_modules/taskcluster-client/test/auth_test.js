6fbd2f061405d961dc468f88f7a355d69c95c3b5
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-06-04 14:50:02 -0700

    New taskcluster-base release

---
+++ services/auth/node_modules/taskcluster-base/node_modules/taskcluster-client/test/auth_test.js
@@ -0,0 +1,16 @@
+suite('auth', function() {
+  var taskcluster = require('../');
+  var assert      = require('assert');
+  test('inspect', function() {
+    var auth = new taskcluster.Auth({
+      credentials: {
+        clientId:     'zIroSzcvRPezmco_cecVmA',
+        accessToken:  'Rj7z_GDMSaKWGadgs-je1Am7z5Q4zuQgOj6AZa0wzSSgAIkKQpB1RTG328W1Ls6WTA'
+      }
+    });
+    // Inspect the credentials
+    return auth.inspect('zIroSzcvRPezmco_cecVmA').then(function(client) {
+      assert(client.clientId === 'zIroSzcvRPezmco_cecVmA', "Expected clientId");
+    });
+  });
+});
