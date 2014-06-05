33d914368f8f99a7ac66df82c213fb2e4379d98f
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-06-05 15:47:42 -0700

    Various tests fixed

--- services/queue/test/runtests.sh
+++ services/queue/test/runtests.sh
@@ -1,18 +1,15 @@
 #!/bin/bash -ve
 # USAGE: Run this file using `npm test` (must run from repository root)
 
-mocha                             \
-  test/validate_test.js
-  #test/queue/events_test.js       \
-  #test/queue/tasks_store_test.js  \
+mocha                               \
+  test/validate_test.js             \
+  test/queue/tasks_test.js          \
+  test/queue/taskstore_test.js      \
+  test/api/artifact_urls.js         \
+  test/api/claim_timeout.js         \
+  test/api/claim.js                 \
+  test/api/define_schedule_task.js  \
+  test/api/pending_tasks.js         \
+  test/api/post_task.js             \
+  test/api/rerun_test.js            \
   ;
-
-
-if [ "$AWS_ACCESS_KEY_ID" == '' ] ;
-then
-  echo "Skipping running test that require s3";
-else
-  echo "Running tests which require s3 credentials"
-  #./node_modules/.bin/mocha test/api/*.js; test/queue/bucket_test.js
-fi
-
