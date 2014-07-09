7713499595e2100c928451f941588a350a78b52f
James Lal <james@lightsofapollo.com>
2014-07-09 00:45:22 -0700

    Always run all api tests

--- services/queue/test/runtests.sh
+++ services/queue/test/runtests.sh
@@ -5,11 +5,5 @@ mocha                               \
   test/validate_test.js             \
   test/queue/tasks_test.js          \
   test/queue/taskstore_test.js      \
-  test/api/artifact_urls.js         \
-  test/api/claim_timeout.js         \
-  test/api/claim.js                 \
-  test/api/define_schedule_task.js  \
-  test/api/pending_tasks.js         \
-  test/api/post_task.js             \
-  test/api/rerun_test.js            \
+  test/api/*.js                     \
   ;
