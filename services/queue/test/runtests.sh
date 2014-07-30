5d4c2074a61178cae07587e96d3f4f18a6a95d2c
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-07-30 13:07:45 -0700

    More tests and a fix for poor scope parameterization

--- services/queue/test/runtests.sh
+++ services/queue/test/runtests.sh
@@ -1,17 +1,19 @@
 #!/bin/bash -ve
 # USAGE: Run this file using `npm test` (must run from repository root)
 
+
 mocha                                 \
   test/validate_test.js               \
   test/queue/task_test.js             \
   test/queue/bucket_test.js           \
   test/queue/blobstore_test.js        \
-  test/api/artifact_test.js           \
-  test/api/claimtask_test.js          \
   test/api/createtask_test.js         \
-  test/api/helper.js                  \
+  test/api/gettask_test.js            \
+  test/api/claimtask_test.js          \
+  test/api/reportcompleted_test.js    \
+  test/api/artifact_test.js           \
   test/api/querytasks_test.js         \
   test/api/reaper_test.js             \
   test/api/reruntask_test.js          \
-  test/api/gettask_test.js            \
   ;
+
