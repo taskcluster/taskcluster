e1402b88b00b5578fe38580c97b3951b27be62eb
James Lal <james@lightsofapollo.com>
2014-08-11 10:12:34 -0700

    Do not submit stats during tests

--- workers/docker-worker/config/test.js
+++ workers/docker-worker/config/test.js
@@ -1,6 +1,12 @@
 module.exports = {
   testMode: true,
 
+  // Disable statsd by default...
+  statsd: {
+    prefix: '',
+    url: 'tcp://localhost:8125'
+  },
+
   logging: {
     // Expires one hour from now so test logs don't live too long...
     liveLogExpires: 3600,
