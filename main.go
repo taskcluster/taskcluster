872f0a4a334577bb3c18465d9ddb31fd53b85295
James Lal <james@lightsofapollo.com>
2014-09-02 09:55:01 -0700

    Talk over http instead of unix socket directly

--- main.go
+++ main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"log"
-	"net"
 	"net/http"
-	"net/http/pprof"
 	"os"
-	"os/signal"
 	"sync"
-	"syscall"
 
 	stream "github.com/lightsofapollo/continuous-log-serve/writer"
 	. "github.com/visionmedia/go-debug"
@@ -18,17 +13,7 @@ import (
 
 var debug = Debug("continuous-log-serve")
 
-type Routes struct {
-	stream *stream.Stream
-}
-
-func NewRoutes(stream *stream.Stream) *Routes {
-	return &Routes{
-		stream: stream,
-	}
-}
-
-func Abort(writer http.ResponseWriter) error {
+func abort(writer http.ResponseWriter) error {
 	// We need to hijack and abort the request...
 	conn, _, err := writer.(http.Hijacker).Hijack()
 
@@ -42,17 +27,38 @@ func Abort(writer http.ResponseWriter) error {
 	return nil
 }
 
-func (self *Routes) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
-	handle := self.stream.Observe()
+func startLogServe(stream *stream.Stream) {
+	routes := http.NewServeMux()
+	routes.HandleFunc("/log", func(w http.ResponseWriter, r *http.Request) {
+		// TODO: Add method context switching here...
+		getLog(stream, w, r)
+	})
+
+	server := http.Server{
+		Addr:    ":60023",
+		Handler: routes,
+	}
+
+	debug("output server listening... %s", server.Addr)
+	server.ListenAndServe()
+}
+
+// HTTP logic for serving the contents of a stream...
+func getLog(
+	stream *stream.Stream,
+	writer http.ResponseWriter,
+	req *http.Request,
+) {
+	handle := stream.Observe()
 
 	defer func() {
 		// Ensure we close our file handle...
 		// Ensure the stream is cleaned up after errors, etc...
-		self.stream.Unobserve(handle)
+		stream.Unobserve(handle)
 		debug("send connection close...")
 	}()
 
-	file, err := os.Open(self.stream.File.Name())
+	file, err := os.Open(stream.File.Name())
 
 	req.Header.Set("Content-Type", "text/plain; charset=utf-8")
 	req.Header.Set("Content-Encoding", "chunked")
@@ -69,15 +75,15 @@ func (self *Routes) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
 
 	// Keep a record of the starting offset to verify we copied the right
 	// things...
-	startingOffset := self.stream.Offset
+	startingOffset := stream.Offset
 
 	// Begin by reading data from the file sink.
-	io.CopyN(writer, file, int64(self.stream.Offset))
+	io.CopyN(writer, file, int64(stream.Offset))
 
-	if self.stream.Ended {
+	if stream.Ended {
 		// Handle the edge case where the file has ended while we where coping bytes
 		// over above.
-		if startingOffset != self.stream.Offset {
+		if startingOffset != stream.Offset {
 			io.Copy(writer, file) // copy drains entire file until EOF
 		}
 		return
@@ -92,101 +98,79 @@ func (self *Routes) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
 	_, writeToErr := handle.WriteTo(writer)
 	if writeToErr != nil {
 		log.Println("Error during write...", writeToErr)
-		Abort(writer)
+		abort(writer)
 	}
-}
-
-func inputHandler(conn net.Conn) {
-	defer conn.Close()
-	// Create the stream server...
-	listener, err := net.Listen("tcp", ":60023")
-	if err != nil {
-		log.Fatal("Could not create listener...")
-	}
-
-	stream, err := stream.NewStream(conn)
-	if err != nil {
-		log.Fatal("Failed to open writer stream")
-	}
-
-	// Begin consuming data from the underlying stream...
-	go func() {
-		debug("consuming from stream...")
-		err := stream.Consume()
-		if err != nil {
-			log.Fatal("Consume error", err)
-		}
-	}()
-
-	mux := http.NewServeMux()
-	mux.Handle("/log", NewRoutes(stream))
-	mux.HandleFunc("/debug/pprof/", pprof.Index)
-	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-	mux.HandleFunc("/debug/pprof/heap", pprof.Handler("heap").ServeHTTP)
-	mux.HandleFunc("/debug/pprof/goroutine", pprof.Handler("goroutine").ServeHTTP)
-	mux.HandleFunc("/debug/pprof/threadcreate", pprof.Handler("threadcreate").ServeHTTP)
-
-	server := &http.Server{
-		Handler: mux,
-	}
-
-	_, port, err := net.SplitHostPort(listener.Addr().String())
-	debug("http server listening on %s", port)
-	if err != nil {
-		log.Fatal("Failed to aquire port")
-	}
-
-	log.Printf("http://localhost:%s/", port)
-
-	// Begin serving ...
-	wait := sync.WaitGroup{}
-	wait.Add(1)
-	go func() {
-		err = server.Serve(listener)
-		if err != nil {
-			log.Fatal("Error serving request", err)
-		}
-		wait.Done()
-	}()
-	wait.Wait()
 }
 
 func main() {
-	dir, err := os.Getwd()
-	if err != nil {
-		log.Fatal("getwd error", err)
+	// TODO: Right now this is a collection of hacks until we build out something
+	// nice which can handle multiple log connections. Right now the intent is to
+	// use this as a process per task (which has overhead) but should be fairly
+	// clean (memory wise) in the long run as we will terminate the process
+	// frequently per task run.
+
+	handlingPut := false
+	mutex := sync.Mutex{}
+
+	routes := http.NewServeMux()
+	server := http.Server{
+		// Main put server listens on the public root for the worker.
+		Addr:    ":60022",
+		Handler: routes,
 	}
 
-	socket := fmt.Sprintf("%s/input.sock", dir)
-	signalChan := make(chan os.Signal)
-	signal.Notify(signalChan, syscall.SIGINT)
-	debug("open socket %s", socket)
+	// The "main" http server is for the PUT side which should not be exposed
+	// publicly but via links in the docker container... In the future we can
+	// handle something fancier.
+	routes.HandleFunc("/log", func(w http.ResponseWriter, r *http.Request) {
+		debug("input %s %s", r.Method, r.URL.String())
 
-	// Cleanup on SIGINT
-	go func() {
-		<-signalChan
-		os.Remove(socket)
-		debug("SIGINT")
-		os.Exit(1)
-	}()
-
-	server, err := net.Listen("unix", socket)
-	defer os.Remove(socket)
-
-	if err != nil {
-		log.Fatal("Failed to open input socket ", socket)
-	}
-
-	defer server.Close()
-
-	for {
-		conn, err := server.Accept()
-		if err != nil {
-			log.Println("Error accepting connection...", err)
-			continue
+		if r.Method != "PUT" {
+			debug("input not put")
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("This endpoint can only handle PUT requests"))
+			return
 		}
-		go inputHandler(conn)
-	}
+
+		// Threadsafe checking of the `handlingPut` flag
+		mutex.Lock()
+		if handlingPut {
+			debug("Attempt to put when in progress")
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("This endpoint can only process one http PUT at a time"))
+			mutex.Unlock() // used instead of defer so we don't block other rejections
+			return
+		}
+		mutex.Unlock() // So we don't block other rejections...
+
+		stream, streamErr := stream.NewStream(r.Body)
+
+		if streamErr != nil {
+			debug("input stream open err", streamErr)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Could not open stream for body"))
+
+			// Allow for retries of the initial put if something goes wrong...
+			mutex.Lock()
+			handlingPut = false
+			mutex.Unlock()
+		}
+
+		// Signal initial success...
+		w.WriteHeader(http.StatusCreated)
+
+		// Initialize the sub server in another go routine...
+		debug("Begin consuming...")
+		go startLogServe(stream)
+		consumeErr := stream.Consume()
+		if consumeErr != nil {
+			log.Println("Error finalizing consume of stream", consumeErr)
+			abort(w)
+			return
+		}
+	})
+
+	// Listen forever on the PUT side...
+	debug("input server listening... %s", server.Addr)
+	server.ListenAndServe()
 }
--- utils/write.js
+++ utils/write.js
@@ -1,11 +1,16 @@
 var net = require('net');
+var http = require('http');
+var url = require('url');
 
 var unix = __dirname + '/../input.sock';
 var idx = 0;
-var client = net.connect({ path: unix }, function() {
-	setInterval(function() {
-		client.write("### CHUNK" + (++idx) + " ###\r\n")
-		client.write("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?")
-		client.write("\r\n### CHUNK END " + (idx) + " ###\r\n")
-	}, 10);
-});
+
+var opts = url.parse('http://localhost:60022/log');
+opts.method = 'PUT'
+var client = http.request(opts);
+
+setInterval(function() {
+	client.write("### CHUNK" + (++idx) + " ###\r\n")
+	client.write("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?")
+	client.write("\r\n### CHUNK END " + (idx) + " ###\r\n")
+}, 10);
