9d0871289a8af054b92a4d467cb059817abda452
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-09-09 12:10:25 -0700

    Fixed a bunch of small things... refactored api to allow slashes... not that I like it

--- services/index/test/api_test.js
+++ services/index/test/api_test.js
@@ -15,34 +15,51 @@ suite("API", function() {
     var myns    = slugid.v4();
     var taskId  = slugid.v4();
     var taskId2  = slugid.v4();
-    return subject.index.insert(myns + '.my-task', {
+    return subject.index.insertTask(myns + '.my-task', {
       taskId:     taskId,
       rank:       41,
       data:       {hello: "world"},
       expires:    expiry.toJSON()
     }).then(function() {
-      return subject.index.find(myns + '.my-task').then(function(result) {
+      return subject.index.findTask(myns + '.my-task').then(function(result) {
         assert(result.taskId === taskId, "Wrong taskId");
       });
     }).then(function() {
-      return subject.index.insert(myns + '.my-task', {
+      return subject.index.insertTask(myns + '.my-task', {
         taskId:     taskId2,
         rank:       42,
         data:       {hello: "world - again"},
         expires:    expiry.toJSON()
       });
     }).then(function() {
-      return subject.index.find(myns + '.my-task').then(function(result) {
+      return subject.index.findTask(myns + '.my-task').then(function(result) {
         assert(result.taskId === taskId2, "Wrong taskId");
       });
     });
   });
 
   test('find (non-existing', function() {
-    return subject.index.find(slugid.v4() + '.' + slugid.v4()).then(function() {
+    var ns = slugid.v4() + '.' + slugid.v4();
+    return subject.index.findTask(ns).then(function() {
       assert(false, "This shouldn't have worked");
     }, function(err) {
       assert(err.statusCode === 404, "Should have returned 404");
+    });
+  });
+
+  test('list namespaces (top-level)', function() {
+    return subject.index.listNamespaces('', {}).then(function(result) {
+      result.namespaces.forEach(function(ns) {
+        assert(ns.namespace.indexOf('.') === -1, "shouldn't have any dots");
+      });
+    });
+  });
+
+  test('list tasks (top-level)', function() {
+    return subject.index.listTasks('', {}).then(function(result) {
+      result.tasks.forEach(function(task) {
+        assert(task.namespace.indexOf('.') === -1, "shouldn't have any dots");
+      });
     });
   });
 });
