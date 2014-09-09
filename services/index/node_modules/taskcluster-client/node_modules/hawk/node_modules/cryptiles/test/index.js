b15f8dfa377a0b14bb6887cac9b730351fcdac89
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-09-09 12:03:38 -0700

    Upgraded taskcluster-base

--- services/index/node_modules/taskcluster-client/node_modules/hawk/node_modules/cryptiles/test/index.js
+++ services/index/node_modules/taskcluster-client/node_modules/hawk/node_modules/cryptiles/test/index.js
@@ -11,11 +11,12 @@ var internals = {};
 
 // Test shortcuts
 
+var lab = exports.lab = Lab.script();
+var before = lab.before;
+var after = lab.after;
+var describe = lab.experiment;
+var it = lab.test;
 var expect = Lab.expect;
-var before = Lab.before;
-var after = Lab.after;
-var describe = Lab.experiment;
-var it = Lab.test;
 
 
 describe('Cryptiles', function () {
