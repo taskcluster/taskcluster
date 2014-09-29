4693cba051226e379fd9cb3c5c1f31223628d670
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-09-29 15:20:44 -0700

    Removed node_modules

--- services/index/node_modules/taskcluster-base/node_modules/aws-sdk/lib/browser.js
+++
@@ -1,9 +0,0 @@
-var AWS = require('./core');
-
-// Load the DOMParser XML parser
-AWS.XML.Parser = require('./xml/browser_parser');
-
-// Load the XHR HttpClient
-require('./http/xhr');
-
-if (typeof window !== 'undefined') window.AWS = AWS;
--- services/index/node_modules/taskcluster-base/node_modules/usage/build/Release/.deps/Release/sysinfo.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/sysinfo.node := rm -rf "Release/sysinfo.node" && cp -af "Release/obj.target/sysinfo.node" "Release/sysinfo.node"
