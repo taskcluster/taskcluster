2d3cfb56adb59e96f38f0d51258bcb4baa647d88
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-10-17 18:24:33 -0700

    Messed up workaround for 0.11

--- libraries/client/index.js
+++ libraries/client/index.js
@@ -2,8 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var _       = require('lodash');
-var SockJS  = require('sockjs-client-node');
+var _ = require('lodash');
 
 // Export methods and classes from lib/
 _.defaults(exports,
@@ -14,4 +13,11 @@ _.defaults(exports,
 );
 
 // Provide a SockJS client implementation for the WebListener
-exports.WebListener.SockJS = SockJS;
+Object.defineProperty(exports.WebListener, 'SockJS', {
+  enumerable: true,
+  get:        function() {
+    // Load it on demand to keep things working under node 0.11 where binary
+    // modules doesn't work well; at least have seen issues with this component
+    return require('sockjs-client-node');
+  }
+});
