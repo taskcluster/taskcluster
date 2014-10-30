7cff64311d4427bb236af751234e275c03993cbb
John Ford <john@johnford.info>
2014-10-30 17:21:32 +0100

    Changes to the build system

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 .PHONY: test
 test:
-	#pylint taskcluster --indent-string='  '
-	pep8 --ignore=E111 taskcluster
+	pyflakes taskcluster
+	pyflakes test
 	./bin/python setup.py test
 
 JS_CLIENT_BRANCH=master
 APIS_JSON=$(PWD)/taskcluster/apis.json
 APIS_JS_HREF=https://raw.githubusercontent.com/taskcluster/taskcluster-client/master/lib/apis.js
+
+PYTHON := python
+VENV := env-$(basename $(PYTHON))
 
 .PHONY: apis.json
 apis.json:
@@ -14,9 +17,16 @@ apis.json:
 	curl -L -o $(APIS_JSON) $(APIS_JS_HREF)
 	OUTPUT=$(APIS_JSON) node translateApis.js
 	@python -mjson.tool $(APIS_JSON) > /dev/null || echo "apis.json cannot be parsed by python's JSON"
-	
+
 .PHONY: dev-env
 dev-env:
-	virtualenv .
-	./bin/pip install -r requirements.txt
-	./bin/pip install -r devel-requirements.txt
+	[ -d $(VENV) ] || $(PYTHON) -m virtualenv $(VENV)
+	[ -d .pyhawk ] || git clone 'https://github.com/jhford/pyhawk' .pyhawk
+	(cd .pyhawk && ../$(VENV)/bin/python setup.py install)
+	for x in "$(shell $(VENV)/bin/python devDep.py)" ; do $(VENV)/bin/python -m pip install $$x || true ; done 
+	$(VENV)/bin/python setup.py develop
+
+.PHONY: clean
+clean:
+	find . -name "*.py?" -exec rm {} +
+	rm -rf env-*
--- setup.py
+++ setup.py
@@ -2,27 +2,32 @@
 
 from setuptools import setup
 
-setup(
-  name='taskcluster',
-  version='0.0.1',
-  description='Python client for Taskcluster',
-  author='John Ford',
-  author_email='jhford@mozilla.com',
-  url='taskcluster.github.io/taskcluster-client.py',
-  packages=['taskcluster'],
-  package_data={
-    'taskcluster.client': ['apis.json']
-  },
-  install_requires=[
-    'requests==2.4.3',
-    'PyHawk==0.1.3',
-  ],
-  test_suite="nose.collector",
-  tests_require=[
-    'nose==1.3.4',
-    'httmock==1.2.2',
-    'rednose==0.4.1',
-    'mock==1.0.1',
-    'setuptools-lint==0.3',
-  ]
-)
+tests_require = [
+  'nose==1.3.4',
+  'httmock==1.2.2',
+  'rednose==0.4.1',
+  'mock==1.0.1',
+  'setuptools-lint==0.3',
+]
+
+install_requires = [
+  'requests==2.4.3',
+  'PyHawk==0.1.4', # Note: this version is from gh.com/jhford/PyHawk
+]
+
+if __name__ == '__main__':
+  setup(
+    name='taskcluster',
+    version='0.0.1',
+    description='Python client for Taskcluster',
+    author='John Ford',
+    author_email='jhford@mozilla.com',
+    url='taskcluster.github.io/taskcluster-client.py',
+    packages=['taskcluster'],
+    package_data={
+      'taskcluster.client': ['apis.json']
+    },
+    install_requires=install_requires,
+    test_suite="nose.collector",
+    tests_require=tests_require
+  )
