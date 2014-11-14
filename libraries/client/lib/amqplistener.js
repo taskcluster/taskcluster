c6a24c8a193419d43bf25aa855fdeff97ee69803
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-11-14 15:47:10 -0800

    Fixed bug 1098153 and added test for PulseListener

--- libraries/client/lib/amqplistener.js
+++ libraries/client/lib/amqplistener.js
@@ -321,7 +321,7 @@ AMQPListener.prototype._handle = function(msg) {
   // Process handlers
   Promise.all(this.listeners('message').map(function(handler) {
     return Promise.resolve(null).then(function() {
-      return handler(message);
+      return handler.call(that, message);
     });
   })).then(function() {
     return that._channel.ack(msg);
