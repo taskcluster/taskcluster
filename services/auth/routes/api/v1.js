6a0a25b95fcd0f64f58c3e40acc8fb9e4414ee2b
Jonas Finnemann Jensen <jopsen@gmail.com>
2015-01-06 15:02:30 -0800

    Changed how we configure azureAccounts

--- services/auth/routes/api/v1.js
+++ services/auth/routes/api/v1.js
@@ -379,15 +379,10 @@ api.declare({
     return;
   }
 
-  debug("ctx.azureAccounts: %s", account);
-  debug(Object.keys(ctx.azureAccounts));
-  debug("typeof(ctx.azureAccounts[account]): %j", typeof(ctx.azureAccounts[account]));
-  debug("ctx.azureAccounts: %j", ctx.azureAccounts);
-
   // Check that the account exists
   if (!ctx.azureAccounts[account]) {
     return res.status(404).json({
-      message:    "Account '" + account + "'' not found, can't delegate access"
+      message:    "Account '" + account + "' not found, can't delegate access"
     });
   }
 
