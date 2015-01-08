cbc0de091a5e9b7fbbff33931bccb101651e8bd8
Jonas Finnemann Jensen <jopsen@gmail.com>
2015-01-08 15:37:27 -0800

    Merge pull request #3 from taskcluster/entities-migration
    Entities migration

---
+++ libraries/api/test/entity/remove_test.js
@@ -0,0 +1,119 @@
+suite("Entity (remove)", function() {
+  var assert  = require('assert');
+  var slugid  = require('slugid');
+  var _       = require('lodash');
+  var Promise = require('promise');
+  var base    = require('../../');
+
+  var helper  = require('./helper');
+  var cfg = helper.loadConfig();
+
+  var Item = base.Entity.configure({
+    version:          1,
+    partitionKey:     base.Entity.keys.StringKey('id'),
+    rowKey:           base.Entity.keys.StringKey('name'),
+    properties: {
+      id:             base.Entity.types.String,
+      name:           base.Entity.types.String,
+      count:          base.Entity.types.Number
+    }
+  }).setup({
+    credentials:  cfg.get('azure'),
+    table:        cfg.get('azureTestTableName')
+  });
+
+
+  test("Item.create, item.remove", function() {
+    var id = slugid.v4();
+    return Item.create({
+      id:     id,
+      name:   'my-test-item',
+      count:  1
+    }).then(function(item) {
+      assert(item instanceof Item);
+      assert(item.id === id);
+      assert(item.count === 1);
+      return item.remove();
+    }).then(function() {
+      return Item.load({
+        id:     id,
+        name:   'my-test-item'
+      });
+    }).catch(function(err) {
+      assert(err.code === 'ResourceNotFound');
+    });
+  });
+
+
+  test("Item.create, Item.remove", function() {
+    var id = slugid.v4();
+    return Item.create({
+      id:     id,
+      name:   'my-test-item',
+      count:  1
+    }).then(function(item) {
+      return Item.remove({
+        id:     id,
+        name:   'my-test-item'
+      });
+    }).then(function() {
+      return Item.load({
+        id:     id,
+        name:   'my-test-item'
+      });
+    }).catch(function(err) {
+      assert(err.code === 'ResourceNotFound');
+    });
+  });
+
+  test("Item.remove (error when doesn't exist)", function() {
+    return Item.remove({
+      id:     slugid.v4(),
+      name:   'my-test-item'
+    }).catch(function(err) {
+      assert(err.code === 'ResourceNotFound');
+    });
+  });
+
+  test("Item.create, item.remove (abort if changed)", function() {
+    var id = slugid.v4();
+    return Item.create({
+      id:     id,
+      name:   'my-test-item',
+      count:  1
+    }).then(function(itemA) {
+      return Item.load({
+        id:     id,
+        name:   'my-test-item'
+      }).then(function(itemB) {
+        return itemB.modify(function() {
+          this.count += 1;
+        });
+      }).then(function() {
+        return itemA.remove();
+      });
+    }).catch(function(err) {
+      assert(err.code === 'UpdateConditionNotSatisfied');
+    });
+  });
+
+  test("Item.create, item.remove (ignore changes)", function() {
+    var id = slugid.v4();
+    return Item.create({
+      id:     id,
+      name:   'my-test-item',
+      count:  1
+    }).then(function(itemA) {
+      return Item.load({
+        id:     id,
+        name:   'my-test-item'
+      }).then(function(itemB) {
+        return itemB.modify(function() {
+          this.count += 1;
+        });
+      }).then(function() {
+        return itemA.remove(true);
+      });
+    });
+  });
+});
