a2c190a9873fea707d1e2207be6f8a259c698f95
Pete Moore <pmoore@mozilla.com>
2015-01-20 17:54:00 +0100

    work in progress

--- generate-api.go
+++ generate-api.go
@@ -7,17 +7,19 @@ import (
 	// tcClient "github.com/petemoore/taskcluster-client.go/lib"
 )
 
-type topLevel struct {
-	referenceUrl string
-	reference    interface{}
-}
-
-func (t topLevel) String() string {
-	return fmt.Sprintf("referenceUrl: '%v', reference: '%v'", t.referenceUrl, t.reference)
+type Entry struct {
+	Type        string
+	Method      string
+	Route       string
+	Args        []string
+	Name        string
+	Title       string
+	Description [][]string
+	Output      string
 }
 
 func main() {
-	var f map[string]topLevel = make(map[string]topLevel)
+	var f map[string]interface{}
 	bytes, err := ioutil.ReadFile("apis.json")
 	if err != nil {
 		fmt.Println(err)
@@ -27,6 +29,36 @@ func main() {
 		fmt.Println(err)
 	}
 	for k, v := range f {
-		fmt.Printf("%v = '%v'\n", k, v)
+		m := v.(map[string]interface{})
+		reference := m["reference"].(map[string]interface{})
+		version := reference["version"]
+		title := reference["title"]
+		description := reference["description"]
+		baseUrl := reference["baseUrl"]
+		entries := reference["entries"]
+		fmt.Printf("API: %v\n", k)
+		fmt.Printf("  referenceUrl = '%v'\n", m["referenceUrl"])
+		fmt.Printf("    version = '%v'\n", version)
+		fmt.Printf("    title = '%v'\n", title)
+		fmt.Printf("    description = '%v'\n", description)
+		fmt.Printf("    baseUrl = '%v'\n", baseUrl)
+		fmt.Printf("    entries type is %T\n", entries)
+		e := entries.([]interface{})
+		for _, entry := range e {
+			Type := entry.(map[string]interface{})["type"]
+			method := entry.(map[string]interface{})["method"]
+			route := entry.(map[string]interface{})["route"]
+			name := entry.(map[string]interface{})["name"]
+			title := entry.(map[string]interface{})["title"]
+			description := entry.(map[string]interface{})["description"]
+			output := entry.(map[string]interface{})["output"]
+			fmt.Printf("      entry type = '%v'\n", Type)
+			fmt.Printf("      entry method = '%v'\n", method)
+			fmt.Printf("      entry route = '%v'\n", route)
+			fmt.Printf("      entry name = '%v'\n", name)
+			fmt.Printf("      entry title = '%v'\n", title)
+			fmt.Printf("      entry description = '%v'\n", description)
+			fmt.Printf("      entry output = '%v'\n", output)
+		}
 	}
 }
