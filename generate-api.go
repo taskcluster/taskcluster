5bc100d117e923eb1cac74cab7c404718c33f995
Pete Moore <pmoore@mozilla.com>
2015-01-20 16:05:18 +0100

    go fmt

--- generate-api.go
+++ generate-api.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-    "encoding/json"
-    "io/ioutil"
-    "fmt"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 )
 
 func main() {
-    var f interface{}
-    bytes, err := ioutil.ReadFile("apis.json")
-    if err != nil {
-        fmt.Println(err)
-    }
-    err = json.Unmarshal(bytes, &f)
-    fmt.Printf("Result: %v", f)
+	var f interface{}
+	bytes, err := ioutil.ReadFile("apis.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = json.Unmarshal(bytes, &f)
+	fmt.Printf("Result: %v", f)
 }
