7238f6c9196335e71d899f8c9386755754e557cd
Pete Moore <pmoore@mozilla.com>
2015-01-23 14:52:05 +0100

    work in progress

--- model/generated-code.go
+++ model/generated-code.go
@@ -1,6 +1,28 @@
 package model
 
 type Auth struct {
-	referenceUrl string
-	reference    interface{}
+}
+
+type ScopesResponse struct {
+	ClientId    string
+	AccessToken string
+	Scopes      []string
+	Expires     string
+}
+
+type GetCredentialsResponse struct {
+	ClientId    string
+	AccessToken string
+	Scopes      []string
+	Expires     string
+	Name        string
+	Description string
+}
+
+func (auth Auth) Scopes(clientId string) ScopesResponse {
+	return ScopesResponse{}
+}
+
+func (auth Auth) GetCredentials(clientId string) GetCredentialsResponse {
+	return GetCredentialsResponse{}
 }
