d5f2af7428d662c911294aea31a907616d9970b8
Pete Moore <pmoore@mozilla.com>
2015-01-23 19:21:29 +0100

    work in progress

---
+++ client/client.go
@@ -0,0 +1,5 @@
+package client
+
+import (
+// "github.com/lightsofapollo/taskcluster-proxy/taskcluster"
+)
--- model/generated-code.go
+++ model/generated-code.go
@@ -1,6 +1,19 @@
 package model
 
+import (
+	"crypto/sha256"
+	"fmt"
+	hawk "github.com/tent/hawk-go"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
 type Auth struct {
+	// Client ID required by Hawk
+	ClientId string
+	// Access Token required by Hawk
+	AccessToken string
 }
 
 type ScopesResponse struct {
@@ -20,6 +33,26 @@ type GetCredentialsResponse struct {
 }
 
 func (auth Auth) Scopes(clientId string) ScopesResponse {
+	credentials := &hawk.Credentials{
+		ID:   auth.ClientId,
+		Key:  auth.AccessToken,
+		Hash: sha256.New,
+	}
+	httpRequest, _ := http.NewRequest("GET", fmt.Sprintf("https://auth.taskcluster.net/v1/client/%v/scopes", auth.ClientId), nil)
+	reqAuth := hawk.NewRequestAuth(httpRequest, credentials, 0).RequestHeader()
+	httpRequest.Header.Set("Authorization", reqAuth)
+	httpClient := &http.Client{}
+	response, err := httpClient.Do(httpRequest)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		defer response.Body.Close()
+		contents, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s\n", string(contents))
+	}
 	return ScopesResponse{}
 }
 
