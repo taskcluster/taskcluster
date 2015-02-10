7137812f47ea8e3b23ce30f535eec4d55655aa8e
Jonas Finnemann Jensen <jopsen@gmail.com>
2015-02-09 16:59:30 -0800

    Extended fromNow with year, month and second support

--- libraries/client/test/utils_test.js
+++ libraries/client/test/utils_test.js
@@ -2,6 +2,28 @@ suite('taskcluster.utils', function() {
   var taskcluster       = require('../');
   var assert            = require('assert');
 
+  test('parseTime 1 year', function() {
+    assert.equal(taskcluster.utils.parseTime('1y').years, 1);
+    assert.equal(taskcluster.utils.parseTime('1 yr').years, 1);
+    assert.equal(taskcluster.utils.parseTime('1 year').years, 1);
+    assert.equal(taskcluster.utils.parseTime('1 years').years, 1);
+    assert.equal(taskcluster.utils.parseTime('1year').years, 1);
+    assert.equal(taskcluster.utils.parseTime('1    yr').years, 1);
+    assert.equal(taskcluster.utils.parseTime('  1    year   ').years, 1);
+    assert.equal(taskcluster.utils.parseTime('  1 years   ').years, 1);
+  });
+
+  test('parseTime 1 month', function() {
+    assert.equal(taskcluster.utils.parseTime('1mo').months, 1);
+    assert.equal(taskcluster.utils.parseTime('1 mo').months, 1);
+    assert.equal(taskcluster.utils.parseTime('1 month').months, 1);
+    assert.equal(taskcluster.utils.parseTime('1 months').months, 1);
+    assert.equal(taskcluster.utils.parseTime('1month').months, 1);
+    assert.equal(taskcluster.utils.parseTime('1    mo').months, 1);
+    assert.equal(taskcluster.utils.parseTime('  1    month   ').months, 1);
+    assert.equal(taskcluster.utils.parseTime('  1 months   ').months, 1);
+  });
+
   test('parseTime 1 day', function() {
     assert.equal(taskcluster.utils.parseTime('1d').days, 1);
     assert.equal(taskcluster.utils.parseTime('1 d').days, 1);
@@ -39,6 +61,7 @@ suite('taskcluster.utils', function() {
     assert.equal(taskcluster.utils.parseTime('45m').minutes, 45);
     assert.equal(taskcluster.utils.parseTime('45 m').minutes, 45);
     assert.equal(taskcluster.utils.parseTime('45 min').minutes, 45);
+    assert.equal(taskcluster.utils.parseTime('45 minute').minutes, 45);
     assert.equal(taskcluster.utils.parseTime('45 minutes').minutes, 45);
     assert.equal(taskcluster.utils.parseTime('45minutes').minutes, 45);
     assert.equal(taskcluster.utils.parseTime('45    m').minutes, 45);
@@ -46,10 +69,25 @@ suite('taskcluster.utils', function() {
     assert.equal(taskcluster.utils.parseTime('  45 minutes   ').minutes, 45);
   });
 
-  test('parseTime 2d3h6m', function() {
-    assert.equal(taskcluster.utils.parseTime('2d3h6m').days, 2);
-    assert.equal(taskcluster.utils.parseTime('2d3h6m').hours, 3);
-    assert.equal(taskcluster.utils.parseTime('2d3h6m').minutes, 6);
+  test('parseTime 45 seconds', function() {
+    assert.equal(taskcluster.utils.parseTime('45 s').seconds, 45);
+    assert.equal(taskcluster.utils.parseTime('45 s').seconds, 45);
+    assert.equal(taskcluster.utils.parseTime('45 sec').seconds, 45);
+    assert.equal(taskcluster.utils.parseTime('45 second').seconds, 45);
+    assert.equal(taskcluster.utils.parseTime('45 seconds').seconds, 45);
+    assert.equal(taskcluster.utils.parseTime('45seconds').seconds, 45);
+    assert.equal(taskcluster.utils.parseTime('45    s').seconds, 45);
+    assert.equal(taskcluster.utils.parseTime('  45    sec   ').seconds, 45);
+    assert.equal(taskcluster.utils.parseTime('  45 seconds   ').seconds, 45);
+  });
+
+  test('parseTime 1yr2mo3d4h5m6s', function() {
+    assert.equal(taskcluster.utils.parseTime('1yr2mo3d4h5m6s').years, 1);
+    assert.equal(taskcluster.utils.parseTime('1yr2mo3d4h5m6s').months, 2);
+    assert.equal(taskcluster.utils.parseTime('1yr2mo3d4h5m6s').days, 3);
+    assert.equal(taskcluster.utils.parseTime('1yr2mo3d4h5m6s').hours, 4);
+    assert.equal(taskcluster.utils.parseTime('1yr2mo3d4h5m6s').minutes, 5);
+    assert.equal(taskcluster.utils.parseTime('1yr2mo3d4h5m6s').seconds, 6);
     assert.equal(taskcluster.utils.parseTime('2d3h').minutes, 0);
     assert.equal(taskcluster.utils.parseTime('2d0h').hours, 0);
   });
