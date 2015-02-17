f453476a2fa5198a94dbd61b47f48b75dbf9d1b4
John Ford <john@johnford.info>
2015-02-17 16:19:04 +0100

    Use manifest.json from references.tc.net instead of local apis.json
    When I run this locally, I got the following for git status.
      modified:   bin/update-apis.js
      modified:   lib/apis.js
      modified:   taskcluster-client.js
    The differences in lib/apis.js look like normal updates of the api and
    the browserified client is a little different

--- libraries/client/bin/update-apis.js
+++ libraries/client/bin/update-apis.js
@@ -145,76 +145,41 @@ program
   });
 
 program
-  .command('add <name> <reference-url>')
-  .option(
-    '-f, --force',
-    "Overwrite existing API entry with same name"
-  )
-  .description("Add API reference with <name> with <referenceUrl>")
-  .action(function(name, referenceUrl, options) {
-    // Check that we don't overwrite unless there is a force
-    if (apis[name] !== undefined && !options.force) {
-      console.log("API named: " + name + " already exists");
-      console.log("Use --force to overwrite it");
-      process.exit(1);
-    }
-
-    // Load reference from referenceUrl
-    console.log("Fetching reference from: " + referenceUrl);
-    var loaded_reference = request.get(referenceUrl).end().then(function(res) {
-      if (!res.ok) {
-        console.log("Failed to fetch reference from: " + referenceUrl)
-        console.log("Error: " + res.text);
-        process.exit(2);
-      }
-      return res.body;
-    });
-
-    // Save reference when loaded
-    loaded_reference.then(function(reference) {
-      apis[name] = {
-        referenceUrl: referenceUrl,
-        reference:    reference
-      };
-      updateDocs();
-      saveApis();
-      browserifyModule();
-      console.log("Add reference: " + name);
-    }).catch(function(err) {
-      console.log("Error adding reference: " + err);
-    });
-  });
-
-program
   .command('update')
   .description("Update all API references")
   .action(function() {
-    // Update remaining references
-    Promise.all(Object.keys(apis).map(function(name) {
-      var api = apis[name];
-      // Load reference from referenceUrl
-      console.log("Fetching reference from: " + api.referenceUrl);
+    // Fetch the Reference Manifest
+    var manifestUrl = 'http://references.taskcluster.net/manifest.json';
+    console.log('Fetching manifest reference from %s', manifestUrl);
+    var p = request.get(manifestUrl).end();
 
-      return request.get(api.referenceUrl).end().
-        then(function(res) {
-          if (!res.ok) {
-            console.log("Failed to fetch reference from: " + api.referenceUrl);
-            console.log("Error: " + res.text);
-            process.exit(2);
-          }
-          if (_.isEqual(api.reference, res.body)) {
-            console.log("No changes from: " + api.referenceUrl);
-          } else {
-            console.log("Received changes from: " + api.referenceUrl);
-          }
-          api.reference = res.body;
+    p = p.then(function(res) {
+      var manifest = res.body;
+      return manifest;
+    });
+
+    p = p.then(function(manifest) {
+      return Promise.all(Object.keys(manifest).map(function(name) {
+        console.log('Fetching %s reference', name); 
+        return request.get(manifest[name]).end().then(function(res) {
+          console.log('Fetched ' + name);
+          apis[name] = {
+            referenceUrl: manifest[name],
+            reference: res.body,
+          };
         });
-    })).then(function() {
+      }));
+    });
+
+    p = p.then(function() {
       updateDocs();
       saveApis();
       browserifyModule();
     });
+
+    p.done();
   });
+
 
 program
   .command('remove <name>')
