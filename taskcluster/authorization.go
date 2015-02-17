55ec0f2c30b58bdf855976f2eaaf8e3fc9d9182d
James Lal <james@lightsofapollo.com>
2015-02-17 15:11:21 -0800

    Add bewit helper

--- taskcluster/authorization.go
+++ taskcluster/authorization.go
@@ -4,12 +4,17 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/json"
-	hawk "github.com/tent/hawk-go"
+	"fmt"
 	"net/http"
+	"time"
+
+	hawk "github.com/tent/hawk-go"
 )
 
+const BEWIT_EXPIRES = time.Hour
+
 type delegationOptions struct {
-	Scopes     []string `json:"authorizedScopes"`
+	Scopes []string `json:"authorizedScopes"`
 }
 
 func getAuth(clientId string, accessToken string, http *http.Request) *hawk.Auth {
@@ -20,6 +25,22 @@ func getAuth(clientId string, accessToken string, http *http.Request) *hawk.Auth
 		Hash: sha256.New,
 	}
 	return hawk.NewRequestAuth(http, credentials, 0)
+}
+
+func Bewit(clientId string, accessToken string, uri string) (string, error) {
+	credentials := &hawk.Credentials{
+		ID:   clientId,
+		Key:  accessToken,
+		Hash: sha256.New,
+	}
+
+	auth, err := hawk.NewURLAuth(uri, credentials, BEWIT_EXPIRES)
+	if err != nil {
+		return "", err
+	}
+
+	bewit := auth.Bewit()
+	return fmt.Sprintf("%s?bewit=%s", uri, bewit), nil
 }
 
 func Authorization(clientId string, accessToken string, http *http.Request) string {
