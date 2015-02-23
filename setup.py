ec060467ead64549478bb4d38ba8eae698d9eea2
John Ford <john@johnford.info>
2015-02-23 15:57:31 +0100

    0.0.9

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ install_requires = [
 if __name__ == '__main__':
   setup(
     name='taskcluster',
-    version='0.0.8',
+    version='0.0.9',
     description='Python client for Taskcluster',
     author='John Ford',
     author_email='jhford@mozilla.com',
