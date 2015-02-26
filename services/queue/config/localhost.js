731246ce351cf4209907c487f92e99f2f24a6ee4
Jonas Finnemann Jensen <jopsen@gmail.com>
2015-02-26 13:03:57 -0800

    Updated localhost to make it not interfere with test queues

--- services/queue/config/localhost.js
+++ services/queue/config/localhost.js
@@ -1,12 +1,32 @@
 module.exports = {
   queue: {
     publishMetaData:              'false',
-    // For testing purposes we let claims expire very fast
-    claimTimeout:                 30,
+    claimTimeout:                 20 * 60,
     publicArtifactBucket:         'test-bucket-for-any-garbage',
     privateArtifactBucket:        'test-bucket-for-any-garbage',
     artifactContainer:            'artifacts',
-    statsComponent:               'test-queue'
+    statsComponent:               'test-queue',
+    queuePrefix:                  'hacks',
+    taskTableName:                'LocalTasks',
+    artifactTableName:            'LocalArtifacts',
+    claimQueue:                   'local-claim-queue',
+    deadlineQueue:                'local-deadline-queue',
+    deadlineDelay:                1 * 60 * 1000,
+    // Positive, means we expire artifacts 4 days ahead of time, useful for
+    // testing. In production this should be "- x hours" or so...
+    artifactExpirationDelay:      '- 1 min',
+    // Positive, means that expire tasks 4 days ahead of time, useful for
+    // testing. In production this should be "- x hours" or so...
+    taskExpirationDelay:          '- 1 min',
+    deadline: {
+      pollingDelay:               1000,
+      parallelism:                1
+    },
+
+    claim: {
+      pollingDelay:               1000,
+      parallelism:                1
+    }
   },
 
   server: {
