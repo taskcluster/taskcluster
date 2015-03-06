c3c29bfa4efa43948ced0240b25ddd118ed67234
Jonas Finnemann Jensen <jopsen@gmail.com>
2015-03-06 11:54:36 -0800

    Merge pull request #15 from taskcluster/bug-1139279-connection-uri-2
    Bug 1139279 connection uri 2

--- libraries/client/test/pulselistener_test.js
+++ libraries/client/test/pulselistener_test.js
@@ -89,6 +89,19 @@ suite('PulseListener', function() {
   });
 
 
+  test('bind via connection string', function() {
+    var listener = new taskcluster.PulseListener({
+      credentials: {
+        namespace:            cfg.get('pulse:username'),
+        connectionString:     connectionString
+      }
+    });
+
+    return listener.resume().then(function() {
+      return listener.close();
+    });
+  });
+
   // Bind and listen with listener
   test('bind and listen', function() {
     // Create listener
