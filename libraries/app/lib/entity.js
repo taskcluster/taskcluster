9bccbbcc9a7871f4f5cbc281bbf26d045a0c102b
Jonas Finnemann Jensen <jopsen@gmail.com>
2015-03-20 14:26:12 -0700

    more request in parallel

--- libraries/app/lib/entity.js
+++ libraries/app/lib/entity.js
@@ -72,8 +72,8 @@ var AZURE_TABLE_TIMEOUT     = 30 * 1000;
 /** Azure table agent used for all instances of the table client */
 var globalAzureTableAgent = new https.Agent({
   keepAlive:      true,
-  maxSockets:     100,
-  maxFreeSockets: 10
+  maxSockets:     1000,
+  maxFreeSockets: 500
 });
 
 /**
