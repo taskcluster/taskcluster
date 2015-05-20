db5f4c0a48e4377eaaaa23ffa3c5e8bf530cd874
Jonas Finnemann Jensen <jopsen@gmail.com>
2015-05-20 13:32:49 -0700

    Merge pull request #6 from taskcluster/redirect-artifacts
    Added findArtifactFromTask + tests

--- services/index/test/api_test.js
+++ services/index/test/api_test.js
@@ -6,6 +6,12 @@ suite("API", function() {
   var slugid      = require('slugid');
   var _           = require('lodash');
   var subject     = helper.setup({title: "api test"});
+  var taskcluster = require('taskcluster-client');
+  var request     = require('superagent-promise');
+
+  // Artifact names that we have assigned scopes to testing credentials for.
+  var publicArtifactName = 'public/dummy-test-provisioner.log';
+  var privateArtifactName = 'private/dummy-test-provisioner.log';
 
   // Create expiration
   var expiry = new Date();
@@ -60,6 +66,109 @@ suite("API", function() {
       result.tasks.forEach(function(task) {
         assert(task.namespace.indexOf('.') === -1, "shouldn't have any dots");
       });
+    });
+  });
+
+  test('access public and private artifact', function() {
+    var taskId = slugid.v4();
+    return subject.queue.createTask(taskId, {
+      provisionerId:    "dummy-test-provisioner",
+      workerType:       "dummy-test-worker-type",
+      retries:          3,
+      created:          taskcluster.fromNowJSON(),
+      deadline:         taskcluster.fromNowJSON('30 min'),
+      payload: {
+        desiredResolution:  "success"
+      },
+      metadata: {
+        name:           "Print `'Hello World'` Once",
+        description:    "This task will prìnt `'Hello World'` **once**!",
+        owner:          "jojensen@mozilla.com",
+        source:         "https://github.com/taskcluster/taskcluster-index"
+      },
+      tags: {
+        objective:      "Test task indexing"
+      }
+    }).then(function() {
+      debug("### Claim task");
+      return subject.queue.claimTask(taskId, 0, {
+        workerGroup:  'dummy-test-workergroup',
+        workerId:     'dummy-test-worker-id'
+      });
+    }).then(function() {
+      debug("### Create public artifact");
+      return subject.queue.createArtifact(taskId, 0, publicArtifactName, {
+        storageType:  'error',
+        expires:      taskcluster.fromNowJSON('24 hours'),
+        reason:       'invalid-resource-on-worker',
+        message:      "Testing that we can access public artifacts from index"
+      });
+    }).then(function() {
+      debug("### Create private artifact");
+      return subject.queue.createArtifact(taskId, 0, privateArtifactName, {
+        storageType:  'error',
+        expires:      taskcluster.fromNowJSON('24 hours'),
+        reason:       'file-missing-on-worker',
+        message:      "Testing that we can access private artifacts from index"
+      });
+    }).then(function() {
+      debug("### Report task completed");
+      return subject.queue.reportCompleted(taskId, 0);
+    }).then(function() {
+      debug("### Insert task into index");
+      return subject.index.insertTask(taskId + '.my-task', {
+        taskId:     taskId,
+        rank:       41,
+        data:       {hello: "world"},
+        expires:    taskcluster.fromNowJSON('24 hours')
+      });
+    }).then(function() {
+      debug("### Download public artifact using index");
+      var url = subject.index.buildUrl(
+        subject.index.findArtifactFromTask,
+        taskId + '.my-task',
+        publicArtifactName
+      );
+      return request.get(url).redirects(0).end().catch(function(err) {
+        return err.response;
+      });
+    }).then(function(res) {
+      assert(res.statusCode === 303, "Expected 303 redirect");
+      return request.get(res.headers.location).end().then(function() {
+        assert(false, "Expected 403 response");
+      }, function(err) {
+        assert(err.response.statusCode === 403, "Expected 403");
+        assert(err.response.body.reason === 'invalid-resource-on-worker');
+      });
+    }).then(function() {
+      debug("### Download private artifact using index (no auth)");
+      var url = subject.index.buildUrl(
+        subject.index.findArtifactFromTask,
+        taskId + '.my-task',
+        privateArtifactName
+      );
+      return request.get(url).redirects(0).end().catch(function(err) {
+        return err.response;
+      });
+    }).then(function(res) {
+      assert(res.statusCode === 401, "Expected 401 access denied");
+    }).then(function() {
+      debug("### Download private artifact using index (with auth)");
+      var url = subject.index.buildSignedUrl(
+        subject.index.findArtifactFromTask,
+        taskId + '.my-task',
+        privateArtifactName, {
+        expiration:     15 * 60
+      });
+      return request.get(url).redirects(0).end().catch(function(err) {
+        return err.response;
+      });
+    }).then(function(res) {
+      assert(res.statusCode === 303, "Expected 303 redirect");
+      // We can't call the URL because server isn't configured with real
+      // credentials in the test.
+      assert(res.headers.location.indexOf('bewit') !== -1,
+             "Expected redirect to signed url");
     });
   });
 });
