ca5ab911ec8fd47295fe03422540566c4984688a
Pete Moore <pmoore@mozilla.com>
2015-06-04 15:01:23 +0200

    Work in progress...

--- plat_windows.go
+++ plat_windows.go
@@ -4,7 +4,19 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
+	"time"
+)
+
+type WindowsUser struct {
+	HomeDir  string
+	Name     string
+	Password string
+}
+
+var (
+	User WindowsUser
 )
 
 func processCommandOutput(callback func(line string), prog string, options ...string) error {
@@ -24,15 +36,67 @@ func startup() error {
 	// var lastError error = nil
 	fmt.Println("Detected Windows platform...")
 	fmt.Println("Looking for existing task users...")
-	processCommandOutput(removeOSUser, "wmic", "useraccount", "get", "name")
+	deleteExistingWindowsUsers()
+	createNewWindowsUser()
+	return nil
+}
+
+func createNewWindowsUser() {
+	// username can only be 20 chars, uuids are too long, therefore
+	// use prefix (5 chars) plus seconds since epoch (10 chars)
+	userName := "Task_" + strconv.Itoa((int)(time.Now().Unix()))
+	password := generatePassword()
+	User := WindowsUser{
+		HomeDir:  "C:\\Users\\" + userName,
+		Name:     userName,
+		Password: password,
+	}
+	commandsToRun := [][]string{
+		{"mkdir", User.HomeDir},
+		{"icacls", User.HomeDir, "/remote:g", "Users"},
+		{"icacls", User.HomeDir, "/remote:g", "Everyone"},
+		{"icacls", User.HomeDir, "/inheritance:r"},
+		{"icacls", User.HomeDir, "/grant:r", User.Name, ":(CI)F", "SYSTEM:(CI)F", "Administrators:(CI)F"},
+		{"net", "user", User.Name, User.Password, "/add", "/expires:never", "/passwordchg:no", "/homedir:" + User.HomeDir},
+		{"net", "localgroup", "Remote Desktop Users", "/add", User.Name},
+	}
+	for _, command := range commandsToRun {
+		fmt.Println("Running command: '" + strings.Join(command, "' '") + "'")
+		out, err := exec.Command(command[0], command[1:]...).Output()
+		if err != nil {
+			fmt.Printf("%v\n", err)
+		} else {
+			fmt.Println(string(out))
+		}
+	}
+}
+
+func generatePassword() string {
+	return "mon123!@#"
+}
+
+func deleteExistingWindowsUsers() {
+	err := processCommandOutput(deleteWindowsUserAccount, "wmic", "useraccount", "get", "name")
+	if err != nil {
+		fmt.Println("WARNING: could not list existing Windows user accounts")
+		fmt.Println("%v\n", err)
+	}
+	deleteHomeDirs()
+}
+
+func deleteHomeDirs() {
 	homeDirsParent, err := os.Open("C:\\Users")
 	if err != nil {
-		return err
+		fmt.Println("WARNING: Could not open C:\\Users directory to find old home directories to delete")
+		fmt.Println("%v\n", err)
+		return
 	}
 	defer homeDirsParent.Close()
 	fi, err := homeDirsParent.Readdir(-1)
 	if err != nil {
-		return err
+		fmt.Println("WARNING: Could not read complete directory listing to find old home directories to delete")
+		fmt.Println("%v\n", err)
+		// don't return, since we may have partial listings
 	}
 	for _, file := range fi {
 		if file.IsDir() {
@@ -42,19 +106,20 @@ func startup() error {
 				err = os.RemoveAll(path)
 				if err != nil {
 					fmt.Println("WARNING: could not delete directory '" + path + "'")
+					fmt.Println("%v\n", err)
 				}
 			}
 		}
 	}
-	return nil
 }
 
-func removeOSUser(line string) {
+func deleteWindowsUserAccount(line string) {
 	if strings.HasPrefix(line, "Task_") {
 		user := line
-		fmt.Println("Attempting to remove OS user " + user + "...")
+		fmt.Println("Attempting to remove Windows user " + user + "...")
 		out, err := exec.Command("net", "user", user, "/delete").Output()
 		if err != nil {
+			fmt.Println("WARNING: Could not remove Windows user account " + user)
 			fmt.Printf("%v\n", err)
 		}
 		fmt.Println(string(out))
