dea186311109f7c6579c7f947d201e5ada34f7b7
Pete Moore <pmoore@mozilla.com>
2015-06-04 14:07:26 +0200

    Work in progress...

--- plat_windows.go
+++ plat_windows.go
@@ -7,35 +7,58 @@ import (
 	"strings"
 )
 
-func startup() error {
-	fmt.Println("Detected Windows platform...")
-	fmt.Println("Looking for existing task users...")
-	out, err := exec.Command("C:\\Windows\\System32\\wbem\\WMIC.exe", "useraccount", "get", "name").Output()
+func processCommandOutput(callback func(line string), prog string, options ...string) error {
+	out, err := exec.Command(prog, options...).Output()
 	if err != nil {
 		fmt.Printf("%v\n", err)
 		return err
 	}
 	for _, line := range strings.Split(string(out), "\r\n") {
 		trimmedLine := strings.Trim(line, "\r\n ")
-		if strings.HasPrefix(trimmedLine, "Task_") {
-			removeOSUser(trimmedLine)
+		callback(trimmedLine)
+	}
+	return nil
+}
+
+func startup() error {
+	// var lastError error = nil
+	fmt.Println("Detected Windows platform...")
+	fmt.Println("Looking for existing task users...")
+	processCommandOutput(removeOSUser, "wmic", "useraccount", "get", "name")
+	homeDirsParent, err := os.Open("C:\\Users")
+	if err != nil {
+		return err
+	}
+	defer homeDirsParent.Close()
+	fi, err := homeDirsParent.Readdir(-1)
+	if err != nil {
+		return err
+	}
+	for _, file := range fi {
+		if file.IsDir() {
+			if fileName := file.Name(); strings.HasPrefix(fileName, "Task_") {
+				path := "C:\\Users\\" + fileName
+				fmt.Println("Removing home directory '" + path + "'...")
+				err = os.RemoveAll(path)
+				if err != nil {
+					fmt.Println("WARNING: could not delete directory '" + path + "'")
+				}
+			}
 		}
 	}
 	return nil
 }
 
-func removeOSUser(user string) {
-	fmt.Println("Attempting to remove OS user " + user + "...")
-	out, err := exec.Command("net", "user", user, "/delete").Output()
-	if err != nil {
-		fmt.Printf("%v\n", err)
+func removeOSUser(line string) {
+	if strings.HasPrefix(line, "Task_") {
+		user := line
+		fmt.Println("Attempting to remove OS user " + user + "...")
+		out, err := exec.Command("net", "user", user, "/delete").Output()
+		if err != nil {
+			fmt.Printf("%v\n", err)
+		}
+		fmt.Println(string(out))
 	}
-	fmt.Println(string(out))
-	out, err = exec.Command("C:\\Windows\\System32\\wbem\\WMIC.exe", "path", "win32_userprofile", "where", "name=\""+user+"\"", "delete").Output()
-	if err != nil {
-		fmt.Printf("%v\n", err)
-	}
-	fmt.Println(string(out))
 }
 
 func (task *TaskRun) generateCommand() *exec.Cmd {
