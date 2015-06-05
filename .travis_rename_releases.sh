9558df63aab9270ad6e66a5dd796750d4c061829
Pete Moore <pmoore@mozilla.com>
2015-06-05 22:29:02 +0200

    hopefully fixed environment variable RELEASE_FILE scope

--- .travis_rename_releases.sh
+++ .travis_rename_releases.sh
@@ -21,5 +21,5 @@ BINARY_NAME="generic-worker"
 # let's rename the release file because it has a 1:1 mapping with what it is called on
 # github releases, and therefore the name for each platform needs to be unique so that
 # they don't overwrite each other. Set a variable that can be used in .travis.yml
-RELEASE_FILE="${TRAVIS_BUILD_DIR}/generic-worker-${GIMME_OS}-${GIMME_ARCH}${EXT}"
+export RELEASE_FILE="${TRAVIS_BUILD_DIR}/generic-worker-${GIMME_OS}-${GIMME_ARCH}${EXT}"
 mv "${GOPATH}/bin/${GIMME_OS}_${GIMME_ARCH}/${BINARY_NAME}" "${RELEASE_FILE}"
