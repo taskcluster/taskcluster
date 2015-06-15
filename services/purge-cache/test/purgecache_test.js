0de2d269ab88801fb1987e465ca2a3cd0b0eff80
Jonas Finnemann Jensen <jopsen@gmail.com>
2015-06-15 11:44:43 -0700

    Initial commit

---
+++ services/purge-cache/test/purgecache_test.js
@@ -0,0 +1,23 @@
+suite("Purge-Cache", () => {
+  var helper = require('./helper');
+  var assume = require('assume');
+
+  test("Publish Purge-Cache Message", async () => {
+    // Start listening for message
+    await helper.events.listenFor('cache-purged',
+      helper.purgeCacheEvents.purgeCache({
+        provisionerId:  'dummy-provisioner',
+        workerType:     'dummy-worker'
+      })
+    );
+
+    // Request a purge-cache message
+    await helper.purgeCache.purgeCache('dummy-provisioner', 'dummy-worker', {
+      cacheName: 'my-test-cache'
+    });
+
+    // Wait for message and validate cacheName
+    var m = await helper.events.waitFor('cache-purged');
+    assume(m.payload.cacheName).is.equal('my-test-cache');
+  });
+});
