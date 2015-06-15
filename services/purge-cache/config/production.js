5d2aba9386af016541f55fb447689a0484d165ba
Jonas Finnemann Jensen <jopsen@gmail.com>
2015-06-15 11:59:53 -0700

    Production config

---
+++ services/purge-cache/config/production.js
@@ -0,0 +1,40 @@
+module.exports = {
+  purgeCache: {
+    // Should be overwritten by environment variable
+    publishMetaData:              'false',
+    exchangePrefix:               'v1/',
+  },
+
+  taskcluster: {
+    authBaseUrl:                  'https://auth.taskcluster.net/v1',
+    credentials: {
+      // Provided by environment variable
+      clientId:                   undefined,
+      accessToken:                undefined
+    }
+  },
+
+  server: {
+    publicUrl:                    'https://taskcluster-purge-cache.herokuapp.com',
+    port:                         80,
+    env:                          'production',
+    forceSSL:                     true,
+    // We trust the proxy on heroku, as the SSL end-point provided by heroku
+    // is a proxy, so we have to trust it.
+    trustProxy:                   true
+  },
+
+  // Pulse credentials
+  pulse: {
+    username:                     'taskcluster-purge-cache',
+    // Provided by environment variable
+    password:                     undefined
+  },
+
+  // Credentials are given by environment variables
+  aws: {
+    accessKeyId:                  undefined,
+    secretAccessKey:              undefined,
+    region:                       'us-west-2'
+  }
+};
