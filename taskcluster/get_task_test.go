0b0c77d5df025bf0cbff36260414b4178211070d
Pete Moore <pmoore@mozilla.com>
2015-07-24 18:19:53 +0200

    bandaged broken test, won't last for long though

--- taskcluster/get_task_test.go
+++ taskcluster/get_task_test.go
@@ -9,13 +9,13 @@ import (
 // XXX: This is a terrible test since it will eventually fail but the docker
 // worker tests will ensure this will work...
 func TestGetTask(t *testing.T) {
-	task, err := tc.GetTask("2szAy1JzSr6pyjVCdiTcoQ")
+	task, err := tc.GetTask("6dS50KOBRjCZX2L-ql3atQ")
 
 	if err != nil {
 		t.Fatalf("Error fetching task: %s", err)
 	}
 
-	if task.ProvisonerId != "aws-provisioner" {
+	if task.Scopes[0] != "docker-worker:cache:tc-vcs" {
 		t.Errorf("Could not fetch task...")
 	}
 }
