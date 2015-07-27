29046436875e5ecd6dc25f7e7b9781b290b41b55
Greg Arndt <arndt.greg@gmail.com>
2015-07-27 17:43:08 -0500

    Merge pull request #148 from EggyLv999/docker-save-test-fix
    Fixes imtermittent chunk 2 test failures

--- workers/docker-worker/test/integration/docker_save_test.js
+++ workers/docker-worker/test/integration/docker_save_test.js
@@ -7,7 +7,7 @@ import fs from 'mz/fs';
 import https from 'https';
 import request from 'superagent-promise';
 import TestWorker from '../testworker';
-import zlib from 'zlib';
+import waitForEvent from '../../lib/wait_for_event';
 // import Debug from 'debug';
 
 // let debug = Debug('docker-worker:test:docker-save-test');
@@ -47,51 +47,37 @@ suite('use docker-save', () => {
 
     let url = `https://queue.taskcluster.net/v1/task/${taskId}/runs/${runId}/artifacts/public/dockerImage.tar`;
 
-    try {
-      //superagent was only downlading 16K of data
-      await new Promise((accept, reject) => {
-        https.request(url, (res) => { //take the redirect
-          https.request(res.headers.location, (res) => {
-            let unzipStream = zlib.Gunzip();
-            res.pipe(unzipStream).pipe(fs.createWriteStream('/tmp/dockerload.tar'));
-            unzipStream.on('end', accept);
-            res.on('error', (err) => reject(err));
-          }).end();
-          res.on('error', (err) => reject(err));
-        }).end();
-      });
+    //superagent means no unzipping required
+    let res = await request.get(url).end();
+    res.pipe(fs.createWriteStream('/tmp/dockerload.tar'));
+    await waitForEvent(res, 'end');
 
-      let docker = new Docker(dockerOpts());
-      let imageName = 'task/' + taskId + '/' + runId + ':latest';
-      await docker.loadImage('/tmp/dockerload.tar');
-      let opts = {
-        AttachStdin: true,
-        AttachStdout: true,
-        AttachStderr: true,
-        Cmd: ['cat', '/tmp/test.log'],
-        Image: imageName
-      };
-      let streamOpts = {
-        logs: true,
-        stdout: true,
-      };
-      let container = await docker.createContainer(opts);
-      await container.start();
-      let stream = await container.attach(streamOpts);
-      let finished = false;
-      stream.on('data', (data) => {
-        assert(data.compare(new Buffer(0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x0b, //header
-          0x74,0x65,0x73,0x74,0x53,0x74,0x72,0x69,0x6e,0x67,0x0a))); //testString\n
-        finished = true;
-      });
-      await base.testing.sleep(5000);
-      assert(finished, 'did not receive any data back');
-      await Promise.all([container.remove(), fs.unlink('/tmp/dockerload.tar')]);
-      await docker.getImage(imageName).remove();
-    } catch (e) {
-      console.log(e);
-      if(e.stack) console.log(e.stack);
-      throw e;
-    }
+    let docker = new Docker(dockerOpts());
+    let imageName = 'task/' + taskId + '/' + runId + ':latest';
+    await docker.loadImage('/tmp/dockerload.tar');
+    let opts = {
+      AttachStdin: true,
+      AttachStdout: true,
+      AttachStderr: true,
+      Cmd: ['cat', '/tmp/test.log'],
+      Image: imageName
+    };
+    let streamOpts = {
+      logs: true,
+      stdout: true,
+    };
+    let container = await docker.createContainer(opts);
+    await container.start();
+    let stream = await container.attach(streamOpts);
+    let finished = false;
+    stream.on('data', (data) => {
+      assert(data.compare(new Buffer(0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x0b, //header
+        0x74,0x65,0x73,0x74,0x53,0x74,0x72,0x69,0x6e,0x67,0x0a))); //testString\n
+      finished = true;
+    });
+    await base.testing.sleep(5000);
+    assert(finished, 'did not receive any data back');
+    await Promise.all([container.remove(), fs.unlink('/tmp/dockerload.tar')]);
+    await docker.getImage(imageName).remove();
   });
 });
