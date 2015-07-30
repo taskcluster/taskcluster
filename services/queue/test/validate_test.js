fde0a8037474821906a9a8838d524ffe1d99f721
Jonas Finnemann Jensen <jopsen@gmail.com>
2015-07-30 10:33:37 -0700

    Merge pull request #46 from taskcluster/update-tc-base
    Update tc base

--- services/queue/test/validate_test.js
+++ services/queue/test/validate_test.js
@@ -5,8 +5,9 @@ suite('validate', function() {
   // Run test cases using schemas testing utility from taskcluster-base
   base.testing.schemas({
     validator: {
-      folder:       path.join(__dirname, '..', 'schemas'),
-      constants:    require('../schemas/constants')
+      folder:         path.join(__dirname, '..', 'schemas'),
+      constants:      require('../schemas/constants'),
+      schemaPrefix:   'queue/v1/'
     },
     basePath:       path.join(__dirname, 'validate_test'),
     schemaPrefix:   'http://schemas.taskcluster.net/',
