dbf94cd1289801eba862432db556d068220a3344
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2015-08-24 15:32:24 -0700

    Add implementation for createHook
    This changes the azure sign in from SAS back to the azure credentials,
    because Entity.ensureTable isn't supported with SAS. This also changes
    the hook definition to include a metadata section, which is more similar
    to the queue json definition.
    
    You can test the endpoint using the following curl command:
    `curl -X PUT -H "content-type:application/json" --binary-data
    @test/validate_test/test.json <endpoint url>`

--- services/hooks/config/test.js
+++ services/hooks/config/test.js
@@ -8,14 +8,5 @@ module.exports = {
   {
     publicUrl: 'http://localhost:60003',
     port:       60003
-  },
-  taskcluster:
-  {
-    authBaseUrl: 'http://localhost:60004',
-    credentials:
-    {
-      clientId: "test-server",
-      accessToken: "none"
-    }
   }
 }
