6c278f6f12ff2e5d44d4d771fc4696c807444e7a
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2015-08-26 13:05:01 -0700

    Add schedule-hook to bin; Add more methods to Scheduler

---
+++ services/hooks/bin/schedule-hooks.js
@@ -0,0 +1,92 @@
+#!/usr/bin/env node
+var base        = require('taskcluster-base');
+var data        = require('../hooks/data');
+var debug       = require('debug')('hooks:bin:server');
+var path        = require('path');
+var Promise     = require('promise');
+var Scheduler   = require('../hooks/scheduler');
+var taskcluster = require('taskcluster-client');
+
+/* Launch schedule-hooks */
+var launch = async function(profile) {
+  debug("Launching with profile: %s", profile);
+
+  // Load configuration
+  var cfg = base.config({
+    defaults:  require('../config/defaults'),
+    profile:   require('../config/' + profile),
+    envs: [
+      'pulse_username',
+      'pulse_password',
+      'taskcluster_credentials_clientId',
+      'taskcluster_credentials_accessToken',
+      'azure_accountName',
+      'azure_accountKey'
+    ],
+    filename:  'taskcluster-hooks'
+  });
+
+  // Create Hooks table
+  var Hook = data.Hook.setup({
+    table:        cfg.get('hooks:hookTableName'),
+    credentials:  cfg.get('azure'),
+    process:      'schedule-hooks'
+  });
+
+  // Create a validator
+  debug("Waiting for resources to be created");
+  var validator;
+  await Promise.all([
+      (async () => {
+        validator = await base.validator({
+          folder:        path.join(__dirname, '..', 'schemas'),
+          constants:     require('../schemas/constants'),
+          publish:       cfg.get('hooks:publishMetaData') == 'true',
+          schemaPrefix:  'hooks/v1/',
+          preload: [
+            'http://schemas.taskcluster.net/queue/v1/create-task-request.json',
+            'http://schemas.taskcluster.net/queue/v1/task-status.json'
+          ]
+        });
+      })(),
+      Hook.ensureTable(),
+  ]);
+
+  // Create scheduler
+  var scheduler = new Scheduler({
+    Hook:           Hook,
+    queue:          new taskcluster.Queue({
+      credentials:  cfg.get('taskcluster:credentials'),
+      baseUrl:      cfg.get('taskcluster:queueBaseUrl')
+    }),
+    pollingDelay:   cfg.get('hooks:schedule:pollingDelay')
+  });
+
+  // Start scheduler
+  scheduler.start();
+
+  // Notify the parent process, so this worker can run using LocalApp
+  base.app.notifyLocalAppInParentProcess();
+
+  return resolver;
+};
+
+// If schedule-hooks.js is executed run launch
+if (!module.parent) {
+  // Find configuration profile
+  var profile = process.argv[2];
+  if (!profile) {
+    console.log("Usage: schedule-hooks.js [profile]");
+    console.error("ERROR: No configuration profile is provided");
+  }
+  // Launch with given profile
+  launch(profile).then(function() {
+    debug("Launched schedule-hooks sucessfully");
+  }).catch(function(err) {
+    debug("Failed to start schedule-hooks, err: %s, as JSON: %j",
+          err, err, err.stack);
+    process.exit(1);
+  });
+}
+
+module.exports = launch;
--- services/hooks/hooks/scheduler.js
+++ services/hooks/hooks/scheduler.js
@@ -1,11 +1,26 @@
-var assert = require('assert');
-var Promise = require('promise');
-var data = require('data');
-var base = require('taskcluster-base');
-var slugid = require('slugid');
+var assert  = require('assert');
+var base    = require('taskcluster-base');
+var data    = require('data');
 var datejs  = require('date.js');
+var Promise = require('promise');
+var slugid  = require('slugid');
 
+/**
+ * The Scheduler will periodically check for tasks in azure storage that are
+ * in need of scheduling, by polling at some periodic rate. Hooks that have
+ * a defined schedule will be run if it's schedule is valid and the next
+ * scheduled date is in the past.
+ */
 class Scheduler {
+  /** Create a Scheduler instance.
+   *
+   * options:
+   * {
+   *   Hook:          // instance of data.Hook
+   *   queue:         // instance of taskcluster-client.Queue
+   *   pollingDelay:  // number of ms to sleep between polling
+   * }
+   * */
   constructor(options) {
     assert(options, "options must be given");
     assert(options.Hook.prototype instanceof data.Hook,
@@ -15,19 +30,25 @@ class Scheduler {
     assert(typeof(options.pollingDelay) == 'number',
         "Expected pollingDelay to be a number");
     // Store options on this for use in event handlers
-    this.Hook = options.Hook;
-    this.queue = options.queue;
+    this.Hook         = options.Hook;
+    this.queue        = options.queue;
+    this.pollingDelay = options.pollingDelay;
 
-    this.done = null;
-    this.stopping = false;
+    // Promise that the polling is done
+    this.done         = null;
+
+    // Boolean that the polling should stop
+    this.stopping     = false;
   }
 
+  /** Start polling */
   start() {
     if (this.done) {
       return;
     }
     this.stopping = false;
 
+    // Create a promise that we're done looping
     this.done = poll().catch((err) => {
       debug("Error: %s, as JSON: %j", err, err, err.stack);
       throw err;
@@ -36,28 +57,49 @@ class Scheduler {
     });
   }
 
+  /** Terminate iteration, returns a promise that polling is stopped */
+  terminate() {
+    this.stopping = true;
+    return this.done;
+  }
+
+  /** Polls for hooks that need to be scheduled and handles them in a loop */
   async poll() {
     while(!this.stopping) {
+      // Get all hooks that have a schedule defined, and a scheduled date
+      // that is less than the current time
       var hooks = await this.Hook.scan({
-        schedule: base.Entity.Op.notEqual(''),
-        nextScheduledDate: base.Entity.Op.lessThan(new Date())
+        schedule:           base.Entity.Op.notEqual(''),
+        nextScheduledDate:  base.Entity.Op.lessThan(new Date())
       }, {});
 
-      await Promise.all(hooks.entries.map(function(hook) {
-        let task = hook.taskPayload();
-        queue.createTask(hook.nextTaskId, task).then(function(resp) {
-          hook.modify(hook => {
-            hook.nextTaskId = slugid();
-            hook.nextScheduledDate = datejs(hook.schedule);
-          });
-        }
-        }));
+      await Promise.all(hooks.entries.map((hook) => {
+        // Don't let a single error break the loop, since it'll be retried later
+        return this.handleHook(hook).catch((err) => {
+          debug("Failed to handle hook: %j" +
+              ", with err: %s, as JSON: %j", hook, err, err, err.stack);
+        });
+      }));
 
       await this.sleep(this.pollingDelay);
     }
   }
 
+  /** Sleep for `delay` ms, returns a promise */
   sleep(delay) {
     return new Promise((accept) => { setTimeout(accept, delay); });
   }
+
+  /** Handle spawning a new task for a given hook that needs to be scheduled */
+  async handleHook(hook) {
+    let task = await hook.taskPayload();
+    let resp = await queue.createTask(hook.nextTaskId, task);
+    await hook.modify((hook) => {
+      hook.nextTaskId        = slugid();
+      hook.nextScheduledDate = datejs(hook.schedule);
+    });
+  }
 }
+
+// Export Scheduler
+module.exports = Scheduler
