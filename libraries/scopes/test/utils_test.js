150507d13c99ad872529bad54fe2225b4d4f62c0
Jonas Finnemann Jensen <jopsen@gmail.com>
2015-10-08 09:03:36 -0700

    Merge pull request #36 from djmitche/bug1193607
    Bug 1193607: validate scopes are from a limited charset

--- libraries/scopes/test/utils_test.js
+++ libraries/scopes/test/utils_test.js
@@ -1,3 +1,36 @@
+suite("validScope", function() {
+  var assert  = require('assert');
+  var utils   = require('../utils.js');
+
+  test("Normal-looking scopes are OK", function() {
+    assert(utils.validScope("auth:credentials"));
+  });
+
+  test("Star scopes are OK", function() {
+    assert(utils.validScope("queue:*"));
+  });
+
+  test("Scopes with spaces are OK", function() {
+    assert(utils.validScope("secrets:garbage:foo bar"));
+  });
+
+  test("Scopes with newlines are not OK", function() {
+    assert(!utils.validScope("some:garbage\nauth:credentials"));
+  });
+
+  test("Scopes with nulls are not OK", function() {
+    assert(!utils.validScope("some:garbage\0auth:credentials"));
+  });
+
+  test("Scopes with unicode characters are not OK", function() {
+    assert(!utils.validScope("halt:\u{1f6c7}"));
+  });
+
+  test("Empty scopes are allowed", function() {
+    assert(utils.validScope(""));
+  });
+});
+
 suite("scopeMatch", function() {
   var assert  = require('assert');
   var utils   = require('../utils.js');
