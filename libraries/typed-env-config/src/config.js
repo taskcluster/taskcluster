e095ff61158f9b7f87080cb8012752a4661a091e
Jonas Finnemann Jensen <jopsen@gmail.com>
2015-10-12 10:41:06 -0700

    Stuff from tc-base PR

--- libraries/typed-env-config/src/config.js
+++ libraries/typed-env-config/src/config.js
@@ -1,82 +1,179 @@
-"use strict";
-
-var assert      = require('assert');
-var _           = require('lodash');
-var nconf       = require('nconf');
+let _ = require('lodash');
+let yaml = require('js-yaml');
+let fs = require('fs');
+let debug = require('debug')('taskcluster-lib-config');
+let assert = require('assert');
 
 /**
- * Load and return a configuration object (nconf, not global)
- * Access properties using `config.get('object:key')`.
+ * Load configuration from files and environment variables.
  *
  * options:
+ * ```js
  * {
- *   defaults:     {},   // Default configuration values
- *   profile:      {},   // Profile configuration values (overrides defaults)
- *   envs:         [],   // Values to load from env, using `_` as separator
- *   filename:     null  // Filename to load config from at multiple levels
+ *   files: [               // Files to load configuration from
+ *     'config.yml',        // Defaults are relative to process.cwd
+ *     'user-config.yml'
+ *   ]
+ *   profile:  undefined,   // Profile to apply
+ *   env:      process.env  // Environment variables (mapping string to strings)
  * }
+ * ```
  *
- * Configuration values are searched in the following order:
- *  1. Environment variables
- *  2. Current working director (`./<filename>.conf.json`),
- *  3. Home folder (`~/.<filename>.conf.json`), and
- *  4. System config (`/etc/<filename>.conf.json`)
- *  5. Default value from `profile`
- *  6. Default value from `defaults`
+ * Configuration Format:
+ * ```yaml
+ * Defaults:
+ *   hostname:     localhost
+ *   port:         8080
+ * Profiles:
+ *   production:
+ *     hostname:   example.com
+ *     port:       !env:number PORT
+ * ```
+ *
+ * The following special YAML types can be used to load from environment
+ * variables:
+ * ```
+ * !env        <NAME>  Load string from env var <NAME>
+ * !env:string <NAME>  Load string from env var <NAME>
+ * !env:number <NAME>  Load number from env var <NAME>
+ * !env:flag   <NAME>  Load true if env var <NAME> is defined
+ * !env:bool   <NAME>  Load boolean as /true/i or /false/i from env var <NAME>
+ * !env:json   <NAME>  Load JSON object from env var <NAME>
+ * !env:list   <NAME>  Load list of space separated strings from env var <NAME>
+ * ```
+ *
+ * If the environment variable in question isn't defined, the value will be
+ * `undefined`, so it can fall-back to defaults from previous config file.
  */
-var config = function(options) {
-  // Set default options
+let config = (options) => {
   options = _.defaults({}, options, {
-    defaults:         {},
-    profile:          {},
-    envs:             [],
-    filename:         null
+    files: [
+      'config.yml',
+      'user-config.yml'
+    ],
+    profile:  undefined,
+    env:      process.env
   });
-  assert(options.envs instanceof Array, "'envs' must be an array");
+  assert(options.files instanceof Array, "Expected an array of files");
+  assert(typeof(options.env) === 'object', "Expected env to be an object");
 
-  // Create config provider
-  var cfg = new nconf.Provider();
-
-  // Load whitelisted configuration keys from environment variables
-  if (options.envs.length > 0) {
-    cfg.env({
-      separator:  '_',
-      whitelist:  options.envs
+  // Create a YAML type that loads from environment variable
+  let createType = (name, typeName, deserialize) => {
+    // Create new YAML type
+    return new yaml.Type(name, {
+      // Takes a string as input
+      kind: 'scalar',
+      // Accepts any string on the form [A-Z0-9_]+
+      resolve(data) {
+        return typeof(data) === 'string' && /^[A-Z0-9_]+$/.test(data);
+      },
+      // Deserialize the data, in the case we read the environment variable
+      construct(data) {
+        let value = options.env[data];
+        try {
+          return deserialize(value);
+        } catch (err) {
+          // Print a warning, if the environment variable is present
+          if (value !== undefined) {
+            console.log("base.config: Warning failed to load %s from " +
+                        "environment variable '%s'", typeName, data);
+          }
+          return undefined;
+        }
+      }
     });
+  };
+
+  // Construct YAML schema
+  const YAML_SCHEMA = yaml.Schema.create(yaml.JSON_SCHEMA, [
+    createType('!env', 'string', val => {
+      assert(typeof(val) === 'string');
+      return val;
+    }),
+    createType('!env:string', 'string', val => {
+      assert(typeof(val) === 'string');
+      return val;
+    }),
+    createType('!env:number', 'string', val => {
+      assert(typeof(val) === 'string');
+      return parseFloat(val);
+    }),
+    createType('!env:flag', 'string', val => {
+      return typeof(val) === 'string';
+    }),
+    createType('!env:bool', 'string', val => {
+      assert(typeof(val) === 'string');
+      if (/^true$/i.test(val)) {
+        return true;
+      }
+      if (/^false$/i.test(val)) {
+        return false;
+      }
+      return undefined;
+    }),
+    createType('!env:json', 'string', val => {
+      assert(typeof(val) === 'string');
+      return JSON.parse(val);
+    }),
+    createType('!env:list', 'string', val => {
+      assert(typeof(val) === 'string');
+      return (val.match(/'[^']*'|"[^"]*"|[^ \t]+/g) || []).map(entry =>{
+        let n = entry.length;
+        if ((entry[0] === "'" && entry[n - 1] === "'") ||
+            (entry[0] === '"' && entry[n - 1] === '"')) {
+          return entry.substring(1, n - 1);
+        }
+        return entry;
+      });
+    })
+  ]);
+
+  // Load files and parse YAML files
+  let cfgs = [];
+  for (let file of options.files) {
+    let data;
+
+    // Load data from file
+    try {
+      data = fs.readFileSync(file, {encoding: 'utf-8'});
+    } catch (err) {
+      // Don't print error, if the file is just missing
+      if (err.code !== 'ENOENT') {
+        debug("Failed to load: %s, err: %s", file, err, err.stack);
+      } else {
+        debug("Config file missing: %s", file);
+      }
+      continue;
+    }
+
+    // Load YAML from data
+    try {
+      data = yaml.safeLoad(data, {
+        filename: file,
+        schema: YAML_SCHEMA
+      });
+    } catch (err) {
+      debug("Failed to parse YAML from %s, err: %s",
+            file, err.toString(), err.stack);
+      throw new Error("Can't parse YAML from: " + file + " " + err.toString());
+    }
+    // Add defaults to list of configurations if present
+    if (data.Defaults) {
+      assert(typeof(data.Defaults) === 'object',
+             "'Defaults' must be an object");
+      cfgs.unshift(data.Defaults);
+    }
+
+    // Add profile to list of configurations, if it is given
+    if (data.Profiles && options.profile && data.Profiles[options.profile]) {
+      let profile = data.Profiles[options.profile];
+      assert(typeof(profile) === 'object', "profile must be an object");
+      cfgs.unshift(profile);
+    }
   }
 
-  // Load configuration from file
-  if (options.filename) {
-    // Config from current working folder if present
-    cfg.file('local', {
-      dir:    process.cwd(),
-      file:   options.filename + '.conf.json',
-      search: true
-    });
-
-    // User configuration
-    cfg.file('user', {
-      dir:    process.env.HOME,
-      file:   '.' + options.filename + '.conf.json',
-      search: true
-    });
-
-    // Global configuration
-    cfg.file('global', {
-      dir:    '/etc',
-      file:   options.filename + '.conf.json',
-      search: true
-    });
-  }
-
-  // Load default values from profile
-  cfg.overrides(options.profile);
-
-  // Load defaults
-  cfg.defaults(options.defaults);
-
-  // Return configuration provider
-  return cfg;
+  // Combine all the configuration keys
+  return _.defaultsDeep.apply(_, cfgs);
 };
 
 // Export config
