ab583717cc04e73c57c3b648fb457420f6d5f2db
Morgan Phillips <winter2718@gmail.com>
2015-10-13 11:59:21 -0700

    Merge pull request #2 from taskcluster/minor-fixes
    Minor fixes, expiration, and random stuff...

--- services/secrets/lib/data.js
+++ services/secrets/lib/data.js
@@ -1,9 +1,8 @@
 import base from 'taskcluster-base';
 import _ from 'lodash';
+import assert from 'assert';
 
-var data = {} = module.exports;
-
-data.SecretEntity = base.Entity.configure({
+let SecretEntity = base.Entity.configure({
     version:          1,
     signEntities:     true,
     partitionKey:     base.Entity.keys.ConstantKey('secrets'),
@@ -15,10 +14,35 @@ data.SecretEntity = base.Entity.configure({
     }
 });
 
+// Export SecretEntity
+exports.SecretEntity = SecretEntity;
+
 /** Return JSON representation of the secret */
-data.SecretEntity.prototype.json = function() {
+SecretEntity.prototype.json = function() {
   return {
     secret: _.cloneDeep(this.secret),
     expires: this.expires.toJSON()
   };
+};
+
+/** Check if the resource is stale */
+SecretEntity.prototype.isExpired = function() {
+  return (new Date()).getTime() > this.expires.getTime();
+};
+
+/**
+ * Expire secrets that are past their expiration.
+ *
+ * Returns a promise that all expired secrets have been deleted.
+ */
+SecretEntity.expire = async function(now) {
+  assert(now instanceof Date, "now must be given as option");
+  var count = 0;
+  await base.Entity.scan.call(this, {
+    expires:          base.Entity.op.lessThan(now)
+  }, {
+    limit:            250, // max number of concurrent delete operations
+    handler:          (secret) => { count++; return secret.remove(true); }
+  });
+  return count;
 };
