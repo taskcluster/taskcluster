14d82e334b1789eab9f02e3303c8aa51e9d2a356
Dustin J. Mitchell <dustin@mozilla.com>
2015-10-16 13:36:59 -0400

    comments

--- services/hooks/hooks/data.js
+++ services/hooks/hooks/data.js
@@ -14,13 +14,21 @@ var Hook = base.Entity.configure({
     groupId:            base.Entity.types.String,
     hookId:             base.Entity.types.String,
     metadata:           base.Entity.types.JSON,
+    // task template
     task:               base.Entity.types.JSON,
+    // pulse bindings
     bindings:           base.Entity.types.JSON,
+    // timings for the task (in fromNow format, e.g., "1 day")
     deadline:           base.Entity.types.String,
     expires:            base.Entity.types.String,
+    // schedule for this task
     schedule:           base.Entity.types.JSON,
+    // access token used to trigger this task via webhook
     accessToken:        base.Entity.types.SlugId,
+    // the taskId that will be used next time this hook is scheduled;
+    // this allows scheduling to be idempotent
     nextTaskId:         base.Entity.types.SlugId,
+    // next date at which this task is scheduled to run
     nextScheduledDate:  base.Entity.types.Date,
   }
 });
