20b80564e26f7785e785b8799b432a5133de497a
Jonas Finnemann Jensen <jopsen@gmail.com>
2015-10-20 10:02:49 -0700

    Merge pull request #58 from djmitche/bug1193607
    Bug 1193607: update schema for limite scope charset

--- services/queue/schemas/constants.js
+++ services/queue/schemas/constants.js
@@ -84,4 +84,7 @@ module.exports = {
 
   // Slugid pattern, for when-ever that is useful
   "slugid-pattern":  "^[A-Za-z0-9_-]{8}[Q-T][A-Za-z0-9_-][CGKOSWaeimquy26-][A-Za-z0-9_-]{10}[AQgw]$",
+
+  // Pattern for scope names, for when-ever that is useful
+  "scope-pattern":   "^[\x20-\x7e]*",
 };
