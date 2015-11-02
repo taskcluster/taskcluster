dd4d73822d1ba8796787585c4e0562f743ed678b
Jonas Finnemann Jensen <jopsen@gmail.com>
2015-11-02 14:01:13 -0800

    Merge pull request #61 from taskcluster/issue-temp-creds
    Issue temp creds

--- services/queue/schemas/constants.js
+++ services/queue/schemas/constants.js
@@ -76,6 +76,53 @@ module.exports = {
     "format":       "date-time"
   },
 
+  // Task credentials issued with claim and reclaim, covering task.scopes
+  'task-credentials': {
+    type: 'object',
+    description: [
+      "Temporary credentials granting `task.scopes` and the scope:",
+      "`queue:claim-task:<taskId>/<runId>` which allows the worker to reclaim",
+      "the task, upload artifacts and report task resolution.",
+      "",
+      "The temporary credentials are set to expire after `takenUntil`. They",
+      "won't expire exactly at `takenUntil` but shortly after, hence, requests",
+      "coming close `takenUntil` won't have problems even if there is a little",
+      "clock drift.",
+      "",
+      "Workers should use these credentials when making requests on behalf of",
+      "a task. This includes requests to create artifacts, reclaiming the task",
+      "reporting the task `completed`, `failed` or `exception`.",
+      "",
+      "Note, a new set of temporary credentials is issued when the worker",
+      "reclaims the task."
+    ].join('\n'),
+    properties: {
+      clientId: {
+        type: 'string',
+        minLength: 1,
+        description: [
+          "The `clientId` for the temporary credentials."
+        ].join('\n')
+      },
+      accessToken: {
+        type: 'string',
+        minLength: 1,
+        description: [
+          "The `accessToken` for the temporary credentials."
+        ].join('\n')
+      },
+      certificate: {
+        type: 'string',
+        minLength: 1,
+        description: [
+          "The `certificate` for the temporary credentials, these are required",
+          "for the temporary credentials to work."
+        ].join('\n')
+      },
+    },
+    required: ['clientId', 'accessToken', 'certificate']
+  },
+
   // Message version numbers
   "message-version": {
     "description":  "Message version",
