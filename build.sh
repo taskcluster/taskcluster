ebcff077d3902530146c96d8f237a9801d9a9a1e
Pete Moore <pmoore@mozilla.com>
2015-11-05 12:30:20 +0100

    Bug 1221239 - added check that no PANIC lines appear for future-proofing

--- build.sh
+++ build.sh
@@ -36,6 +36,10 @@ go get github.com/pierrre/gotestcover
 "${GOPATH}/bin/gotestcover" -v -coverprofile=coverage.report ./...
 go tool cover -func=coverage.report
 
+# Make sure no PANIC lines appear in model-data.txt
+# See https://bugzilla.mozilla.org/show_bug.cgi?id=1221239
+grep -q PANIC codegenerator/model/model-data.txt && exit 68
+
 # finally check that generated files have been committed, and that
 # formatting code resulted in no changes...
 git status
