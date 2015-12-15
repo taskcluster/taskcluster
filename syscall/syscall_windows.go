30e2030aa0172e96f5652cda5a4fdf4f12762b25
Pete Moore <pmoore@mozilla.com>
2015-12-15 15:41:29 +0100

    fixes

--- syscall/syscall_windows.go
+++ syscall/syscall_windows.go
@@ -1,3 +1,3 @@
-package main
+package syscall
 
 //sys   CreateProcessWithLogonW(username *uint16, domain *uint16, password *uint16, logonFlags uint32, appName *uint16, commandLine *uint16, creationFlags uint32, env *uint16, currentDir *uint16, startupInfo *StartupInfo, outProcInfo *ProcessInformation) (err error)
