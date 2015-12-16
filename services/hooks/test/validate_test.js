a495c2e331589b04e9b33be7f32ca989c44cba37
Dustin J. Mitchell <dustin@mozilla.com>
2015-12-16 10:43:27 -0500

    missed schema fixes

--- services/hooks/test/validate_test.js
+++ services/hooks/test/validate_test.js
@@ -7,47 +7,45 @@ suite('validate', function() {
       folder: path.join(__dirname, '..', 'schemas'),
       constants: require('../schemas/constants'),
       schemaPrefix: 'hooks/v1/',
-      preload: [
-        'http://schemas.taskcluster.net/queue/v1/create-task-request.json'
-      ]},
-      basePath: path.join(__dirname, 'validate_test'),
-      schemaPrefix: 'http://schemas.taskcluster.net/',
-      cases: [
-        {
-          schema: 'hooks/v1/create-hook-request.json',
-          path: 'create-hook-request.json',
-          success: true
-        },
-        {
-          schema: 'hooks/v1/schedule.json',
-          path: 'schedule-none.json',
-          success: true
-        },
-        {
-          schema: 'hooks/v1/schedule.json',
-          path: 'schedule-daily.json',
-          success: true
-        },
-        {
-          schema: 'hooks/v1/schedule.json',
-          path: 'schedule-weekly.json',
-          success: true
-        },
-        {
-          schema: 'hooks/v1/schedule.json',
-          path: 'schedule-weekday.json',
-          success: true
-        },
-        {
-          schema: 'hooks/v1/schedule.json',
-          path: 'schedule-monthly.json',
-          success: true
-        },
-        {
-          schema: 'hooks/v1/schedule.json',
-          path: 'schedule-biweekly.json',
-          success: true
-        }
-      ]
+    },
+    basePath: path.join(__dirname, 'validate_test'),
+    schemaPrefix: 'http://schemas.taskcluster.net/',
+    cases: [
+      {
+        schema: 'hooks/v1/create-hook-request.json',
+        path: 'create-hook-request.json',
+        success: true
+      },
+      {
+        schema: 'hooks/v1/schedule.json',
+        path: 'schedule-none.json',
+        success: true
+      },
+      {
+        schema: 'hooks/v1/schedule.json',
+        path: 'schedule-daily.json',
+        success: true
+      },
+      {
+        schema: 'hooks/v1/schedule.json',
+        path: 'schedule-weekly.json',
+        success: true
+      },
+      {
+        schema: 'hooks/v1/schedule.json',
+        path: 'schedule-weekday.json',
+        success: true
+      },
+      {
+        schema: 'hooks/v1/schedule.json',
+        path: 'schedule-monthly.json',
+        success: true
+      },
+      {
+        schema: 'hooks/v1/schedule.json',
+        path: 'schedule-biweekly.json',
+        success: true
+      }
+    ]
   });
 });
