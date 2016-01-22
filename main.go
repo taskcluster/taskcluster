1b0f4ac12420cd5a4790b00a5bc8bc3895a9470e
Pete Moore <pmoore@mozilla.com>
2016-01-22 16:18:00 +0100

    Bumped version number to 3.0.6

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 	"github.com/taskcluster/taskcluster-client-go/tcclient"
 )
 
-var version = "Taskcluster proxy 3.0.5"
+var version = "Taskcluster proxy 3.0.6"
 var usage = `
 Taskcluster authentication proxy. By default this pulls all scopes from a
 particular task but additional scopes may be added by specifying them after the
