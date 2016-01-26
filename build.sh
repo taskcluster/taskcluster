4bedc515e3436ab0273284923cd371a3fd093f6e
Pete Moore <pmoore@mozilla.com>
2016-01-26 10:30:02 +0100

    Fixed build.sh to use new location of model-data.txt file

--- build.sh
+++ build.sh
@@ -56,7 +56,7 @@ go tool cover -func=coverage.report
 
 # Make sure no PANIC lines appear in model-data.txt
 # See https://bugzilla.mozilla.org/show_bug.cgi?id=1221239
-grep -q PANIC codegenerator/model/model-data.txt && exit 68
+grep -q PANIC codegenerator/model-data.txt && exit 68
 
 # finally check that generated files have been committed, and that formatting
 # code resulted in no changes...
