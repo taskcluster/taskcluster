f9ec21bdc5cf59b127e13c4d90d4ec2a130b6198
Jonas Finnemann Jensen <jopsen@gmail.com>
2016-01-26 08:10:02 -0800

    Merge pull request #74 from taskcluster/remove-pending-tasks-scope
    Remove pending tasks scope

--- services/queue/test/api/querytasks_test.js
+++ services/queue/test/api/querytasks_test.js
@@ -10,7 +10,7 @@ suite('Query tasks', function() {
   var helper      = require('./helper');
 
   test("pendingTasks >= 1", async () => {
-    var taskDef = {
+    let taskDef = {
       provisionerId:    'no-provisioner',
       workerType:       'query-test-worker',
       schedulerId:      'my-scheduler',
@@ -32,27 +32,63 @@ suite('Query tasks', function() {
       }
     };
 
-    var taskId1 = slugid.v4();
-    var taskId2 = slugid.v4();
+    let taskId1 = slugid.v4();
+    let taskId2 = slugid.v4();
 
     debug("### Create tasks");
     await Promise.all([
       helper.queue.createTask(taskId1, taskDef),
       helper.queue.createTask(taskId2, taskDef)
-    ])
+    ]);
 
-    var r1 = await helper.queue.pendingTasks(
+    let r1 = await helper.queue.pendingTasks(
       'no-provisioner',
       'query-test-worker'
     );
     assume(r1.pendingTasks).is.greaterThan(1);
+
+    // Result is cached for 20 seconds, so adding one more and checking should
+    // give the same result, as we're not waiting for the timeout
+    await helper.queue.createTask(taskId1, taskDef);
+
+    // Note: There is some timing here, but since the queue.pendingTasks result
+    // is cached it ought to be really fast and take less than 20 seconds to
+    // do: queue.createTask + queue.pendingTasks, if not that's also sort of a
+    // bug we should investigate
+    let r2 = await helper.queue.pendingTasks(
+      'no-provisioner',
+      'query-test-worker'
+    );
+    assume(r2.pendingTasks).is.equals(r1.pendingTasks);
+
+    // WARNING: The test below this point is not fast and certainly not robust
+    // enough to run all the time. But it can be easily activated if messing
+    // with queueservice.js and you want to ensure that it still works.
+    // Just comment out the return statement below.
+    return; // STOP TEST HERE
+    console.log("WARNING: Unstable test running, should be disabled on master");
+    await base.testing.poll(async () => {
+      // At some point in the future we have to got fetch a new result saying
+      // more tasks are now in the queue...
+      let r3 = await helper.queue.pendingTasks(
+        'no-provisioner',
+        'query-test-worker'
+      );
+      assume(r3.pendingTasks).is.greaterThan(r1.pendingTasks);
+    }, 30, 1000);
   });
 
   test("pendingTasks == 0", async () => {
-    var r1 = await helper.queue.pendingTasks(
+    let r1 = await helper.queue.pendingTasks(
       'no-provisioner',
       'empty-test-worker'
     );
     assume(r1.pendingTasks).equals(0);
+
+    let r2 = await helper.queue.pendingTasks(
+      'no-provisioner',
+      'empty-test-worker'
+    );
+    assume(r2.pendingTasks).equals(0);
   });
 });
