9569a5dad831532bf7ff7ea92fa93902fe3f819a
Dustin J. Mitchell <dustin@mozilla.com>
2016-01-26 00:10:33 +0000

    Merge taskcluster/taskcluster-auth:bug-1234929 (PR #33)

--- services/auth/auth/scoperesolver.js
+++ services/auth/auth/scoperesolver.js
@@ -260,6 +260,7 @@ class ScopeResolver extends events.EventEmitter {
   createSignatureValidator(options = {}) {
     let validator = base.API.createSignatureValidator({
       nonceManager: options.clientLoader,
+      expandScopes: (scopes) => this.resolve(scopes),
       clientLoader: async (clientId) => {
         let client = this._clientCache[clientId];
         if (!client) {
@@ -275,18 +276,7 @@ class ScopeResolver extends events.EventEmitter {
         return client;
       }
     });
-    return (req) => {
-      return validator(req).then(result => {
-        if (result.status === 'auth-success') {
-          // This is only necessary if authorizedScopes or temporary credentials
-          // was used, otherwise it should already be the fixed-point.
-          // We should refactor base.API.createSignatureValidator to facilitate
-          // this... But for now this is okay...
-          result.scopes = this.resolve(result.scopes);
-        }
-        return result;
-      });
-    };
+    return validator;
   }
 
   /**
