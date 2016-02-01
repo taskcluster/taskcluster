d30d42e8dd40ba0d798ccebc80f208da280c27fa
Dustin J. Mitchell <dustin@mozilla.com>
2016-02-01 14:48:07 +0000

    Merge djmitche/taskcluster-auth:util-methods (PR #38)

--- services/auth/test/client_test.js
+++ services/auth/test/client_test.js
@@ -77,6 +77,38 @@ suite('api (client)', function() {
     assume(client2.expandedScopes).contains('assume:client-id:' + CLIENT_ID);
 
     await helper.events.waitFor('e1');
+    await helper.auth.deleteClient(CLIENT_ID);
+  });
+
+  test("auth.listClients", async() => {
+    let suffixes = ['/aa', '/bb', '/bb/1', '/bb/2'];
+    await Promise.all(suffixes.map(suffix => 
+      helper.auth.createClient(CLIENT_ID + suffix, {
+        expires: taskcluster.fromNow('1 hour'),
+        description: 'test client',
+      })
+    ));
+
+    let gotSuffixes = (result) =>
+      _.map(_.filter(result,
+                     c => c.clientId.startsWith(CLIENT_ID)),
+            c => c.clientId.substr(CLIENT_ID.length)).sort();
+
+    // get all clients
+    assume(gotSuffixes(await helper.auth.listClients())).to.deeply.equal(suffixes);
+
+    // prefix filtering
+    assume(gotSuffixes(await helper.auth.listClients({prefix: CLIENT_ID + '/bb'})))
+    .to.deeply.equal(['/bb', '/bb/1', '/bb/2']);
+    assume(gotSuffixes(await helper.auth.listClients({prefix: CLIENT_ID + '/bb/'})))
+    .to.deeply.equal(['/bb/1', '/bb/2']);
+    assume(gotSuffixes(await helper.auth.listClients({prefix: CLIENT_ID + '/c'})))
+    .to.deeply.equal([]);
+
+    // clean up
+    await Promise.all(suffixes.map(suffix =>
+      helper.auth.deleteClient(CLIENT_ID + suffix)
+    ));
   });
 
   test("auth.createClient (with scopes)", async () => {
@@ -283,4 +315,108 @@ suite('api (client)', function() {
     });
   });
 
+  let assumeScopesetsEqual = (ss1, ss2) => {
+    ss1.scopes.sort();
+    ss2.scopes.sort();
+    assume(ss1).deeply.equal(ss2);
+  }
+
+  test("auth.expandScopes with empty scopeset", async () => {
+    assumeScopesetsEqual(await helper.auth.expandScopes({scopes: []}), {scopes: []});
+  });
+
+  test("auth.expandScopes with non-expanding scopes", async () => {
+    let scopes = ['myapi:a', 'myapi:b'];
+    assume(await helper.auth.expandScopes({scopes: scopes}))
+    .to.deeply.equal({scopes: scopes});
+    assumeScopesetsEqual(await helper.auth.expandScopes({scopes}), {scopes})
+  });
+
+  test("auth.expandScopes with expanding scopes", async () => {
+    await helper.auth.deleteRole('myrole:a');
+    await helper.auth.deleteRole('myrole:b');
+
+    await helper.events.listenFor('role-a', helper.authEvents.roleCreated({
+      roleId:  'myrole:a'
+    }));
+    await helper.events.listenFor('role-b', helper.authEvents.roleCreated({
+      roleId:  'myrole:b'
+    }));
+
+    await helper.auth.createRole('myrole:a', {
+      description: 'test role',
+      scopes: ['myapi:a:a', 'myapi:a:b']
+    });
+    await helper.auth.createRole('myrole:b', {
+      description: 'test role',
+      scopes: ['assume:myrole:a', 'myapi:b:a']
+    });
+    await helper.events.waitFor('role-a');
+    await helper.events.waitFor('role-b');
+
+    assumeScopesetsEqual(await helper.auth.expandScopes({scopes: [
+      'assume:myrole:b',
+      'myapi:c',
+    ]}), {scopes: [
+      'assume:myrole:a',
+      'assume:myrole:b',
+      'myapi:a:a',
+      'myapi:a:b',
+      'myapi:b:a',
+      'myapi:c'
+    ]});
+
+    await helper.auth.deleteRole('myrole:a');
+    await helper.auth.deleteRole('myrole:b');
+  });
+
+  test("auth.currentScopes with root credentials", async () => {
+    assumeScopesetsEqual(await helper.auth.currentScopes(), {scopes: ['*']});
+  });
+
+  test("auth.currentScopes with root credentials and authorizedScopes", async () => {
+    let auth = new helper.Auth({
+      baseUrl:          helper.baseUrl,
+      credentials: {
+        clientId:       'root',
+        accessToken:    helper.cfg.app.rootAccessToken,
+      },
+      authorizedScopes: ['myapi:a', 'myapi:b'],
+    });
+    assumeScopesetsEqual(await auth.currentScopes(),
+      {scopes: ['myapi:a', 'myapi:b']});
+  });
+
+  test("auth.currentScopes with temp credentials", async () => {
+    let auth = new helper.Auth({
+      baseUrl:          helper.baseUrl,
+      credentials: taskcluster.createTemporaryCredentials({
+        expiry:       taskcluster.fromNow('10 min'),
+        scopes:       ['myapi:x', 'myapi:y'],
+        credentials:  {
+          clientId:       'root',
+          accessToken:    helper.cfg.app.rootAccessToken,
+        },
+      }),
+    });
+    assumeScopesetsEqual(await auth.currentScopes(),
+      {scopes: ['myapi:x', 'myapi:y']});
+  });
+
+  test("auth.currentScopes with temp credentials and authorizedScopes", async () => {
+    let auth = new helper.Auth({
+      baseUrl:          helper.baseUrl,
+      credentials: taskcluster.createTemporaryCredentials({
+        expiry:       taskcluster.fromNow('10 min'),
+        scopes:       ['myapi:x', 'myapi:y'],
+        credentials:  {
+          clientId:       'root',
+          accessToken:    helper.cfg.app.rootAccessToken,
+        },
+      }),
+      authorizedScopes: ['myapi:x'],
+    });
+    assumeScopesetsEqual(await auth.currentScopes(),
+      {scopes: ['myapi:x']});
+  });
 });
