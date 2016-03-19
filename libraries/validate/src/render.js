a1493a9e29adc0c4be5aecbff22aef6de2b8ac36
Brian Stack <bstack@mozilla.com>
2016-03-18 23:09:03 -0700

    Merge branch 'fixes'

--- libraries/validate/src/render.js
+++ libraries/validate/src/render.js
@@ -4,22 +4,22 @@ let _ = require('lodash');
 /** Render {$const: <key>} into JSON schema */
 function render (schema, constants) {
   // Replace val with constant, if it is an {$const: <key>} schema
-  var substitute = (val) => {
+  let substitute = (val) => {
     // Primitives and arrays shouldn't event be considered
     if (!(val instanceof Object) || val instanceof Array) {
       return undefined;
     }
 
     // Check if there is a key and only one key
-    var key = val['$const'];
+    let key = val['$const'];
     if (key === undefined || typeof key != 'string' || _.keys(val).length != 1) {
       return undefined;
     }
 
     // Check that there's a constant for the key
-    var constant = constants[key];
+    let constant = constants[key];
     if (constant === undefined) {
-      return undefined;
+      throw new Error('Warning! Undefined constant: ' + key);
     }
 
     // Clone constant
---
+++ libraries/validate/test/schemas/README.md
@@ -0,0 +1,4 @@
+README THING
+============
+
+This file exists to make sure that this library handles non schema files correctly.
