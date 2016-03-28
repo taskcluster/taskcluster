9b7ad4a87e45029403902c46428a4a43dd2c7a62
Jonas Finnemann Jensen <jopsen@gmail.com>
2016-03-28 15:34:41 -0700

    Merge pull request #51 from taskcluster/sentry-access
    Added sentry manager

--- services/auth/bin/server.js
+++ services/auth/bin/server.js
@@ -1,20 +1,21 @@
 #!/usr/bin/env node
-var base               = require('taskcluster-base');
-var data               = require('../auth/data');
-var v1                 = require('../auth/v1');
-var path               = require('path');
-var debug              = require('debug')('server');
-var Promise            = require('promise');
-var AWS                = require('aws-sdk-promise');
-var raven              = require('raven');
-var exchanges          = require('../auth/exchanges');
-var ScopeResolver      = require('../auth/scoperesolver');
-var signaturevalidator = require('../auth/signaturevalidator');
-var taskcluster        = require('taskcluster-client');
-var url                = require('url');
-var validate           = require('taskcluster-lib-validate');
-var loader             = require('taskcluster-lib-loader');
-var app                = require('taskcluster-lib-app');
+let base               = require('taskcluster-base');
+let data               = require('../auth/data');
+let v1                 = require('../auth/v1');
+let path               = require('path');
+let debug              = require('debug')('server');
+let Promise            = require('promise');
+let AWS                = require('aws-sdk-promise');
+let raven              = require('raven');
+let exchanges          = require('../auth/exchanges');
+let ScopeResolver      = require('../auth/scoperesolver');
+let signaturevalidator = require('../auth/signaturevalidator');
+let taskcluster        = require('taskcluster-client');
+let url                = require('url');
+let validate           = require('taskcluster-lib-validate');
+let loader             = require('taskcluster-lib-loader');
+let app                = require('taskcluster-lib-app');
+let SentryManager      = require('../auth/sentrymanager');
 
 // Create component loader
 let load = loader({
@@ -52,6 +53,11 @@ let load = loader({
     }
   },
 
+  sentryManager: {
+    requires: ['cfg'],
+    setup: ({cfg}) => new SentryManager(cfg.app.sentry),
+  },
+
   resolver: {
     requires: ['cfg'],
     setup: ({cfg}) => new ScopeResolver({
@@ -60,8 +66,8 @@ let load = loader({
   },
 
   Client: {
-    requires: ['cfg', 'drain', 'resolver'],
-    setup: ({cfg, drain, resolver}) =>
+    requires: ['cfg', 'drain', 'resolver', 'process'],
+    setup: ({cfg, drain, resolver, process}) =>
       data.Client.setup({
         table:        cfg.app.clientTableName,
         credentials:  cfg.azure || {},
@@ -69,21 +75,21 @@ let load = loader({
         cryptoKey:    cfg.app.tableCryptoKey,
         drain:        drain,
         component:    cfg.app.statsComponent,
-        process:      'server',
+        process,
         context:      {resolver}
       })
   },
 
   Role: {
-    requires: ['cfg', 'drain', 'resolver'],
-    setup: ({cfg, drain, resolver}) =>
+    requires: ['cfg', 'drain', 'resolver', 'process'],
+    setup: ({cfg, drain, resolver, process}) =>
       data.Role.setup({
         table:        cfg.app.rolesTableName,
         credentials:  cfg.azure || {},
         signingKey:   cfg.app.tableSigningKey,
         drain:        drain,
         component:    cfg.app.statsComponent,
-        process:      'server',
+        process,
         context:      {resolver}
       })
   },
@@ -97,7 +103,7 @@ let load = loader({
   },
 
   publisher: {
-    requires: ['cfg', 'validator', 'drain'],
+    requires: ['cfg', 'validator', 'drain', 'process'],
     setup: ({cfg, validator, drain, process}) =>
       exchanges.setup({
         credentials:      cfg.pulse,
@@ -108,13 +114,19 @@ let load = loader({
         aws:              cfg.aws,
         drain:            drain,
         component:        cfg.app.statsComponent,
-        process:          'server'
+        process,
       })
   },
 
   api: {
-    requires: ['cfg', 'Client', 'Role', 'validator', 'publisher', 'resolver', 'drain', 'raven'],
-    setup: async ({cfg, Client, Role, validator, publisher, resolver, drain, raven}) => {
+    requires: [
+      'cfg', 'Client', 'Role', 'validator', 'publisher', 'resolver',
+      'drain', 'raven', 'sentryManager'
+    ],
+    setup: async ({
+      cfg, Client, Role, validator, publisher, resolver, drain, raven,
+      sentryManager
+    }) => {
       // Set up the Azure tables
       await Role.ensureTable();
       await Client.ensureTable();
@@ -147,6 +159,7 @@ let load = loader({
           sts:                new AWS.STS(cfg.aws),
           azureAccounts:      cfg.app.azureAccounts,
           signatureValidator,
+          sentryManager,
         },
         validator,
         signatureValidator,
@@ -184,11 +197,24 @@ let load = loader({
       return serverApp.createServer();
     }
   },
-}, ['profile']);
 
-// If server.js is executed start the server
+  'expire-sentry': {
+    requires: ['cfg', 'sentryManager'],
+    setup: async ({cfg, sentryManager}) => {
+      let now = taskcluster.fromNow(cfg.app.sentryExpirationDelay);
+      if (isNaN(now)) {
+        console.log("FATAL: sentryExpirationDelay is not valid!");
+        process.exit(1);
+      }
+      await sentryManager.purgeExpiredKeys(now);
+    }
+  }
+}, ['profile', 'process']);
+
+// If this file is executed launch component from first argument
 if (!module.parent) {
-  load('server', {
+  load(process.argv[2], {
+    process: process.argv[2],
     profile: process.env.NODE_ENV,
   }).catch(err => {
     console.log(err.stack);
