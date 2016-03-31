b0786db002921d3ec7d40053ac2e6799679ab290
Pete Moore <pmoore@mozilla.com>
2016-03-31 18:43:53 +0200

    Sleep before updating worker type

--- worker_types/create.sh
+++ worker_types/create.sh
@@ -101,6 +101,11 @@ echo "$(date): To monitor the AMI creation process, see:"
 echo
 echo "             https://us-west-2.console.aws.amazon.com/ec2/v2/home?region=us-west-2#Images:visibility=owned-by-me;search=${IMAGE_ID};sort=desc:platform"
 
+echo "$(date): I've triggered the snapshot of instance ${INSTANCE_ID} as ${IMAGE_ID} - but now we will need to wait 45 minutes("'!'") for it to be created..."
+
+# sleep 45 mins, the AMI snapshot takes forever
+sleep 2700
+
 "${GOPATH}/bin/update-worker-type" "${IMAGE_ID}" "${WORKER_TYPE}"
 
 echo
