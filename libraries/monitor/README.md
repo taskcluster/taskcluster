5c68e7ba14e620821d16531672d2f1a1325cc4f8
Brian Stack <bstack@mozilla.com>
2016-04-07 12:36:41 -0700

    Proper testing

--- libraries/monitor/README.md
+++ libraries/monitor/README.md
@@ -18,14 +18,7 @@ Options and Defaults
 Testing
 -------
 
-To run the integration tests for this library, you'll need a taskcluster client with the `auth:sentry:tc-lib-monitor` and `auth:statsum:tc-lib-monitor` scopes available.
-You can create this client with the [Taskcluster tools](https://tools.taskcluster.net/auth/clients). Once it's been created, you can copy
-the `user-config-example.yml` file into `user-config.yml` and replace the proper fields with your new creds.
-
-Once this is set up, you can `npm install` and `npm test`. You can set `DEBUG=taskcluster-lib-monitor,test` if you want to see what's going on.
-
-The Sentry tests will create errors on [the tc-lib-monitor project](https://app.getsentry.com/taskcluster/tc-lib-monitor/), but notifications are
-turned off for it.
+`npm install` and `npm test`. You can set `DEBUG=taskcluster-lib-monitor,test` if you want to see what's going on. There are no keys required to test this library.
 
 License
 -------
--- libraries/monitor/test/statsum_test.js
+++ libraries/monitor/test/statsum_test.js
@@ -2,20 +2,39 @@ suite('Statsum', () => {
   let assert = require('assert');
   let monitoring = require('../');
   let debug = require('debug')('test');
-  let config = require('typed-env-config');
+  let nock = require('nock');
+  let authmock = require('./authmock');
 
+  let statsumScope = null;
   let monitor = null;
-  let cfg = config();
 
   suiteSetup(async () => {
+    authmock.setup();
+
+    statsumScope = nock('https://statsum.taskcluster.net')
+      .filteringRequestBody(/.*/, '*')
+      .post(/v1\/project\/tc-lib-monitor/, '*')
+      .reply(200, 'OK');
+
     monitor = await monitoring({
       project: 'tc-lib-monitor',
-      credentials: cfg.taskcluster.credentials,
+      credentials: {clientId: 'test-client', accessToken: 'test'},
       patchGlobal: false,
+      reportStatsumErrors: false,
     });
   });
 
-  test('should blah blah', function () {
+  suiteTeardown(async () => {
+    authmock.teardown();
+  });
+
+  test('should have written to something', async function (done) {
+    monitor.count('testing', 10);
+    await monitor.flush();
+    if (!statsumScope.isDone()) {
+      done(new Error('Error! Did not call' + statsumScope.pendingMocks()));
+    }
+    done();
   });
 
 });
