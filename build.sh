c21ceaa3d09062dca176aed293d5e251e858d4d5
Pete Moore <pmoore@mozilla.com>
2016-04-07 12:08:38 +0200

    Updated build script to work only with go versions go1.x for x >=5, and also removed steps to download go commands (vet, cover) which are included in these versions of the go language

--- build.sh
+++ build.sh
@@ -1,10 +1,25 @@
-#!/bin/bash -xveu
+#!/bin/bash -eu
 
 # options:
 #   -n  skip code generation
 #   -d  update timestamp included in generated docs
 
 cd "$(dirname "${0}")"
+
+GO_VERSION="$(go version 2>/dev/null | cut -f3 -d' ')"
+GO_MAJ="$(echo "${GO_VERSION}" | cut -f1 -d'.')"
+GO_MIN="$(echo "${GO_VERSION}" | cut -f2 -d'.')"
+if [ -z "${GO_VERSION}" ]; then
+  echo "Have you installed go? I get no result from \`go version\` command." >&2
+  exit 64
+elif [ "${GO_MAJ}" != "go1" ] || [ "${GO_MIN}" -lt 5 ]; then
+  echo "Go version go1.x needed, where x >= 5, but the version I found is: '${GO_VERSION}'" >&2
+  echo "I found it here:" >&2
+  which go >&2
+  echo "The complete output of \`go version\` command is:" >&2
+  go version >&2
+  exit 65
+fi
 
 # in case build.sh was run with -d option since last build
 git checkout -f codegenerator/model-data.txt
@@ -25,12 +40,15 @@ while getopts ":nd" opt; do
 done
 
 export UNIX_TIMESTAMP
+echo "GO_VERSION = '${GO_VERSION}'"
 echo "UNIX_TIMESTAMP = '${UNIX_TIMESTAMP}'"
 # having GOOS for anything than local system will break running the tests
 unset GOOS
 
+# remove any binaries built from any fork
 rm -rf "${GOPATH}"/pkg/*/github.com/*/taskcluster-client-go
-go clean -i -x ./...
+# remove any binaries from this fork
+go clean -i ./...
 
 # generate code
 go get github.com/docopt/docopt-go
@@ -40,14 +58,12 @@ go get github.com/taskcluster/jsonschema2go
 "${GENERATE}" && go generate ./...
 
 # fetch deps/build/install taskcluster-client-go
-go get -t -v -x ./...
+go get -t -v ./...
 go fmt ./...
 
-go get golang.org/x/tools/cmd/vet
-go vet -x ./...
+go vet ./...
 
 go get github.com/axw/gocov/gocov
-go get golang.org/x/tools/cmd/cover
 go get github.com/pierrre/gotestcover
 # since gotestcover can have 0 exit code even with failures, also run tests
 # with go test
