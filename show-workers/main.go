192106f2ab2a2879fdb8b37b5dc3ba71681157c5
Pete Moore <pmoore@mozilla.com>
2016-05-24 10:38:20 +0200

    Filter out terminated instances from show-workers

--- show-workers/main.go
+++ show-workers/main.go
@@ -166,6 +166,17 @@ func main() {
 														aws.String("running"),
 													},
 												},
+												// filter out terminated instances
+												&ec2.Filter{
+													Name: aws.String("instance-state-name"),
+													Values: []*string{
+														aws.String("pending"),
+														aws.String("running"),
+														aws.String("shutting-down"),
+														aws.String("stopping"),
+														aws.String("stopped"),
+													},
+												},
 											},
 										},
 									)
