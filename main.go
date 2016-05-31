280119ac8370e795a5fe292fca8631804bb538c7
Pete Moore <pmoore@mozilla.com>
2016-05-31 22:12:53 +0200

    Version bump to 2.0.0alpha43

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ var (
 	config             *Config
 	configFile         string
 
-	version = "generic-worker 2.0.0alpha42"
+	version = "generic-worker 2.0.0alpha43"
 	usage   = `
 generic-worker
 generic-worker is a taskcluster worker that can run on any platform that supports go (golang).
