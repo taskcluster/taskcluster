3e71bbac417531f36943f1523b51d70410943fea
Brian Stack <bstack@mozilla.com>
2016-06-01 10:08:03 -0700

    Merge pull request #62 from taskcluster/more-info
    Add more info on errors

--- services/github/src/github.js
+++ services/github/src/github.js
@@ -36,6 +36,7 @@ github.updateStatus = async function (api, user, repo, sha, params) {
   } catch (e) {
     if (e.status === 404) {
       debug(`Could not post status to ${user}/${repo}@${sha}`);
+      debug(e.message);
       return;
     }
     throw e;
@@ -52,6 +53,7 @@ github.addCommitComment = async function (api, user, repo, sha, body) {
   } catch (e) {
     if (e.status === 404) {
       debug(`Could add comment to to ${user}/${repo}@${sha}`);
+      debug(e.message);
       return;
     }
     throw e;
