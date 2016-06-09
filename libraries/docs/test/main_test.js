2577ccf6c0b9bcc4dccf1c1d22f337b87a8310b7
kristelteng <kristelteng@gmail.com>
2016-06-08 20:30:57 -0400

    Merge pull request #3 from taskcluster/more-tdd
    Add a schema test case

--- libraries/docs/test/main_test.js
+++ libraries/docs/test/main_test.js
@@ -3,6 +3,15 @@ suite('End to End', () => {
   let path = require('path');
   let documenter = require('../');
   let debug = require('debug')('test');
+  let validator = require('taskcluster-lib-validate');
+
+  let validate = null;
+
+  setup(async () => {
+    validate = await validator({
+      folder: path.join(__dirname, 'schemas'),
+    });
+  });
 
   test('simplest case with nothing to do', function() {
     return documenter({
@@ -10,6 +19,16 @@ suite('End to End', () => {
       bucket: 'taskcluster-raw-docs-test',
       project: 'taskcluster-lib-docs',
       version: '0.0.1',
+    });
+  });
+
+  test('only schemas', function() {
+    return documenter({
+      folder: path.join(__dirname, 'docs'),
+      bucket: 'taskcluster-raw-docs-test',
+      project: 'taskcluster-lib-docs',
+      version: '0.0.1',
+      schemas: validate.schemas,
     });
   });
 
