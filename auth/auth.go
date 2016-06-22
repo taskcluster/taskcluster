63cf82d751572577f13fdad30819e9568c7fb897
Pete Moore <pmoore@mozilla.com>
2016-06-22 22:37:16 +0200

    Regenerated library to pick up latest schema changes

--- auth/auth.go
+++ auth/auth.go
@@ -63,7 +63,7 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/auth/v1/api.json together with the input and output schemas it references, downloaded on
-// Mon, 6 Jun 2016 at 14:28:00 UTC. The code was generated
+// Wed, 22 Jun 2016 at 20:36:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package auth
 
