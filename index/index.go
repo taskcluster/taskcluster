3cdbd05b3b40cdab36c7690751dcef966eff6e7d
Pete Moore <pmoore@mozilla.com>
2016-06-26 21:29:51 +0200

    Regenerated library to pick up latest schema changes

--- index/index.go
+++ index/index.go
@@ -118,7 +118,7 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/index/v1/api.json together with the input and output schemas it references, downloaded on
-// Fri, 24 Jun 2016 at 13:28:00 UTC. The code was generated
+// Sun, 26 Jun 2016 at 19:28:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package index
 
