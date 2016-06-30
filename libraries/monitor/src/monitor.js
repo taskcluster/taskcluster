08d78332c0093dd4acc5fb15a2f9b7500a112625
Jonas Finnemann Jensen <jopsen@gmail.com>
2016-06-30 18:02:52 +0100

    Merge pull request #29 from taskcluster/fix-recursive-errors
    An error sending an error can cause another error

--- libraries/monitor/src/monitor.js
+++ libraries/monitor/src/monitor.js
@@ -31,9 +31,17 @@ class Monitor {
         };
       }
       return sentry;
-    }).catch(err => {});
+    }).catch(err => {
+      console.log('Failed to get access to sentry, err: %s, jsoN: %j', err.stack, err);
+      return {client: null, expires: new Date(0)};
+    });
 
     return this._sentry.then(sentry => {
+      if (!sentry.client) {
+        console.log('Can\'t report to sentry, error not reported: ', err.stack);
+        return Promise.resolve();
+      }
+      
       sentry.client.captureException(err, {
         tags: _.defaults({
           prefix: this._opts.project + (this._opts.prefix || '.root'),
