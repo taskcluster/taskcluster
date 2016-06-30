5b72ed3134abb5b6be703a4de0858bd01f9e5418
Pete Moore <pmoore@mozilla.com>
2016-06-30 21:56:55 +0200

    Hopefully fixed travis test failure

--- plat_all-unix-style.go
+++ plat_all-unix-style.go
@@ -34,16 +34,17 @@ func immediateShutdown() {
 
 func startup() error {
 	log.Printf("Detected %s platform", runtime.GOOS)
-	return nil
+	TaskUser = OSUser{
+		HomeDir:  os.Getwd(),
+		Name:     "",
+		Password: "",
+	}
+	return os.MkdirAll(filepath.Join(TaskUser.HomeDir, "public", "logs"), 0700)
 }
 
 func (task *TaskRun) generateCommand(index int, writer io.Writer) error {
 	cmd := exec.Command(task.Payload.Command[index][0], task.Payload.Command[index][1:]...)
 	commandName := fmt.Sprintf("command_%06d", index)
-	err := os.MkdirAll(filepath.Join(TaskUser.HomeDir, "public", "logs"), 0700)
-	if err != nil {
-		return err
-	}
 	log, err := os.Create(filepath.Join(TaskUser.HomeDir, "public", "logs", commandName+".log"))
 	if err != nil {
 		return err
