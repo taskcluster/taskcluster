b5592c3e098c9874c1928bd2e7bf7f3eada7b8fb
Brian Stack <bstack@mozilla.com>
2016-07-05 11:28:05 -0700

    Merge pull request #5 from taskcluster/schema_tarball
    Schema tarball

--- libraries/docs/src/docs.js
+++ libraries/docs/src/docs.js
@@ -1,4 +1,35 @@
-function documenter() {
+let tar = require('tar-stream');
+let _ = require('lodash');
+let assert = require('assert');
+let fs = require('fs');
+let path = require('path');
+
+async function documenter(options) {
+
+  options = _.defaults({}, options, {
+    schemas: [],
+  });
+  assert(options.schemas, 'options.schemas must be given');
+  assert(options.schemas instanceof Array, 'options.schemas must be an array');
+
+  let schemas = options.schemas;
+  let tarball = tar.pack();
+
+  //add schemas to tarball
+  schemas.forEach(schema => {
+    let data = JSON.stringify(schema, null, 2);
+    tarball.entry({name: 'schema/' + schema.id}, data);
+  });
+
+  tarball.finalize();
+  // pipe the pack stream somewhere
+  tarball.pipe(process.stdout);
+
+  let output = {
+    tarball,
+  };
+
+  return output;
 }
 
 module.exports = documenter;
