5bd043d6e02eaeebce99c6bb8fd1cec6ab5f7348
John Ford <john@johnford.org>
2016-07-11 16:06:47 +0200

    more fixes and tests

--- libraries/iterate/test/iterate_test.js
+++ libraries/iterate/test/iterate_test.js
@@ -20,6 +20,9 @@ describe('iteration functionality', () => {
 });
 
 
+// I split out tests that verify that all the looping stuff works as expected.
+// I'd rather use fake timers, but there are lots of timeouts involved and I
+// couldn't get it to play nice
 describe('timing tests', () => {
   it('should be able to start and stop', done => {
     let iterations = 0;
@@ -49,12 +52,72 @@ describe('timing tests', () => {
     });
 
     setTimeout(() => {
-      assume(iterations).equals(5);  
+      assume(iterations).equals(5);
+      assume(i.currentIteration).equals(5);
       assume(i.keepGoing).is.ok();
       i.stop();
       assume(i.keepGoing).is.not.ok();
       done();
     }, 5000);
+  });
+
+  it('should error when iteration watchdog expires', done => {
+    let iterations = 0;
+
+    let i = new subject({
+      maxIterationTime: 5,
+      watchDog: 1,
+      waitTime: 1,
+      handler: async (watchdog, state) => {
+        // In order to get the looping stuff to work, I had to stop the
+        // watchdog timer.  This will be tested in the tests for the
+        // Iterate.iterate method
+        watchdog.on('expired', () => {
+          assume(i.currentIteration).equals(0);
+          debug('correctly getting expired watchdog timer');
+          i.stop();
+          done();
+        });
+        return new Promise((res, rej) => {
+          setTimeout(res, 2000);
+        });
+
+      }
+    });
+
+    i.start();
+
+    i.overallWatchDog.on('expired', () => {
+      done(new Error('overall watchdog expiration'));
+    });
+
+  });
+
+  it('should error when overall watchdog expires', done => {
+    let iterations = 0;
+
+    let i = new subject({
+      maxIterationTime: 1,
+      watchDog: 1,
+      waitTime: 1,
+      handler: async (watchdog, state) => {
+        watchdog.stop();
+        return new Promise((res, rej) => {
+          setTimeout(res, 5000);
+        });
+      }
+    });
+
+    i.start();
+
+    i.overallWatchDog.on('expired', () => {
+      debug('correctly getting expired watchdog timer');
+      i.stop();
+      done();
+    });
+
+
+
   });
 
 
