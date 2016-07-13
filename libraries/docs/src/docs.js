d7039ea53666b347bb18caac631d72ea74b32632
Brian Stack <bstack@mozilla.com>
2016-07-13 15:50:51 -0700

    Merge pull request #6 from taskcluster/docs_tarball
    Docs tarball

--- libraries/docs/src/docs.js
+++ libraries/docs/src/docs.js
@@ -1,13 +1,15 @@
 let tar = require('tar-stream');
 let _ = require('lodash');
 let assert = require('assert');
-let fs = require('fs');
+let fs = require('mz/fs');
 let path = require('path');
+let recursiveReadSync = require('recursive-readdir-sync');
 
 async function documenter(options) {
 
   options = _.defaults({}, options, {
     schemas: [],
+    docsFolder: null,
   });
   assert(options.schemas, 'options.schemas must be given');
   assert(options.schemas instanceof Array, 'options.schemas must be an array');
@@ -15,20 +17,31 @@ async function documenter(options) {
   let schemas = options.schemas;
   let tarball = tar.pack();
 
-  //add schemas to tarball
+  // add schemas to tarball
   schemas.forEach(schema => {
     let data = JSON.stringify(schema, null, 2);
     tarball.entry({name: 'schema/' + schema.id}, data);
   });
 
+  if (options.docsFolder !== null) {
+    // add docs to tarball
+    let docs = options.docsFolder;
+    let files = recursiveReadSync(options.docsFolder);
+
+    await Promise.all(files.map(async (file) => {
+      //remove the path
+      let relativePath = path.basename(file);
+      let data = await fs.readFile(file, {encoding: 'utf8'});
+      tarball.entry({name: 'docs/' + relativePath}, data);
+    }));
+  }
+
   tarball.finalize();
-  // pipe the pack stream somewhere
-  tarball.pipe(process.stdout);
+  // tarball.pipe(process.stdout);
 
   let output = {
     tarball,
   };
-
   return output;
 }
 
