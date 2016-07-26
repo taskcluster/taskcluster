43bd3788b06e2638447cae928a8fd6009cd2ec99
Brian Stack <bstack@mozilla.com>
2016-07-25 17:53:03 -0700

    Merge pull request #8 from taskcluster/gzip_tarball
    Gzip tarball

--- libraries/docs/src/docs.js
+++ libraries/docs/src/docs.js
@@ -4,44 +4,70 @@ let assert = require('assert');
 let fs = require('mz/fs');
 let path = require('path');
 let recursiveReadSync = require('recursive-readdir-sync');
+let zlib = require('zlib');
+let rootdir = require('app-root-dir');
 
 async function documenter(options) {
-
   options = _.defaults({}, options, {
-    schemas: [],
-    docsFolder: null,
+    tier: null,
+    schemas: {},
+    menuIndex: 10,
+    docsFolder: rootdir.get() + '/docs',
+    references: [],
   });
+
   assert(options.schemas, 'options.schemas must be given');
-  assert(options.schemas instanceof Array, 'options.schemas must be an array');
+
+  assert(options.tier, 'options.tier must be given');
+  assert(['core', 'platform'].indexOf(options.tier) !== -1, 'options.tier is either core or platform');
+
+  let tarball = tar.pack();
+  let metadata = {version: 1, tier: options.tier, menuIndex: options.menuIndex};
+  let data = JSON.stringify(metadata, null, 2);
+  tarball.entry({name: 'metadata.json'}, data);
 
   let schemas = options.schemas;
-  let tarball = tar.pack();
-
-  // add schemas to tarball
-  schemas.forEach(schema => {
+  _.forEach(schemas, (name, schema) => {
     let data = JSON.stringify(schema, null, 2);
-    tarball.entry({name: 'schema/' + schema.id}, data);
+    tarball.entry({name: 'schema/' + name}, data);
   });
 
-  if (options.docsFolder !== null) {
-    // add docs to tarball
-    let docs = options.docsFolder;
-    let files = recursiveReadSync(options.docsFolder);
+  let references = options.references;
+  references.forEach(reference => {
+    let data = JSON.stringify(reference, null, 2);
+    tarball.entry({name: 'references/' + reference.name + '.json'}, data);
+  });
 
-    await Promise.all(files.map(async (file) => {
-      //remove the path
-      let relativePath = path.basename(file);
-      let data = await fs.readFile(file, {encoding: 'utf8'});
-      tarball.entry({name: 'docs/' + relativePath}, data);
-    }));
+  if (options.docsFolder) {
+    try {
+      let docs = options.docsFolder;
+      let files = recursiveReadSync(options.docsFolder);
+      await Promise.all(files.map(async (file) => {
+        let relativePath = path.basename(file);
+        let data = await fs.readFile(file, {encoding: 'utf8'});
+        tarball.entry({name: 'docs/' + relativePath}, data);
+      }));
+    } catch (err) {
+      if (err.code == 'ENOENT') {
+        console.log('Docs folder does not exist');
+      } else {
+        throw err;
+      }
+    }
   }
 
+  // the stream was added
+  // no more entries
   tarball.finalize();
   // tarball.pipe(process.stdout);
 
+  let gzip = zlib.createGzip();
+  let tgz = tarball.pipe(gzip);
+
   let output = {
-    tarball,
+    tgz,
   };
+
   return output;
 }
 
