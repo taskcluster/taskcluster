26fc6a878577b68cb4f241ea1469923669ac2258
Dustin J. Mitchell <dustin@v.igoro.us>
2016-08-26 08:43:44 -0400

    Merge pull request #36 from djmitche/bug1274321
    Bug 1274321: reflect the expiration time of credentials in req.expires

--- libraries/api/test/auth_test.js
+++ libraries/api/test/auth_test.js
@@ -47,7 +47,8 @@ suite("api/auth", function() {
   }, async function(req, res) {
     res.status(200).json({
       scopes: await req.scopes(),
-      clientId: await req.clientId()
+      clientId: await req.clientId(),
+      expires: await req.expires(),
     });
   });
 
@@ -397,6 +398,7 @@ suite("api/auth", function() {
         assert(res.body.scopes.length === 1, "wrong number of scopes");
         assert(res.body.scopes[0] === 'service:magic', "failed scopes");
         assert(res.body.clientId == 'test-client', "bad clientId");
+        assert(/\d{4}-\d{2}-\d{2}.*/.test(res.body.expires), "bad expires");
       });
   });
 
