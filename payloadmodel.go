c2982c4c266715bbd3b68863dab9536397eb58d3
Pete Moore <pmoore@mozilla.com>
2016-09-09 11:59:03 +0200

    Regenerated types based on schema changes to task payload

--- payloadmodel.go
+++ payloadmodel.go
@@ -7,31 +7,50 @@ package main
 import tcclient "github.com/taskcluster/taskcluster-client-go"
 
 type (
-	// This schema defines the structure of the `payload` property referred to
-	// in a Task Cluster Task definition.
+	// This schema defines the structure of the `payload` property referred to in a
+	// TaskCluster Task definition.
 	GenericWorkerPayload struct {
-		// Artifacts to be published. For example: `{ "type": "file", "path":
-		// "builds\\firefox.exe", "expires": "2015-08-19T17:30:00.000Z" }`
+
+		// Artifacts to be published. For example:
+		// `{ "type": "file", "path": "builds\\firefox.exe", "expires": "2015-08-19T17:30:00.000Z" }`
 		Artifacts []struct {
+
 			// Date when artifact should expire must be in the future
 			Expires tcclient.Time `json:"expires"`
+
 			// Filesystem path of artifact
 			Path string `json:"path"`
-			// Artifacts can be either an individual `file` or a `directory`
-			// containing potentially multiple files with recursively included
-			// subdirectories
+
+			// Artifacts can be either an individual `file` or a `directory` containing
+			// potentially multiple files with recursively included subdirectories.
+			//
+			// Possible values:
+			//   * "file"
+			//   * "directory"
 			Type string `json:"type"`
-		} `json:"artifacts"`
-		// One entry per command (consider each entry to be interpreted as a
-		// full line of a Windows™ .bat file). For example: `["set", "echo
-		// hello world > hello_world.txt", "set GOPATH=C:\\Go"]`.
+		} `json:"artifacts,omitempty"`
+
+		// One array per command (each command is an array of arguments). Several arrays
+		// for several commands.
 		Command [][]string `json:"command"`
-		// Example: ```{ "PATH": "C:\\Windows\\system32;C:\\Windows", "GOOS":
-		// "darwin" }```
-		Env map[string]string `json:"env"`
+
+		// Example: ```{ "PATH": "C:\\Windows\\system32;C:\\Windows", "GOOS": "darwin" }```
+		Env map[string]string `json:"env,omitempty"`
+
 		// Feature flags enable additional functionality.
-		Features EnabledFeatures `json:"features,omitempty"`
+		Features struct {
+
+			// An artifact named chainOfTrust.json.asc should be generated
+			// which will include information for downstream tasks to build
+			// a level of trust for the artifacts produced by the task and
+			// the environment it ran in.
+			ChainOfTrust bool `json:"chainOfTrust,omitempty"`
+		} `json:"features,omitempty"`
+
 		// Maximum time the task container can run in seconds
+		//
+		// Mininum:    1
+		// Maximum:    86400
 		MaxRunTime int `json:"maxRunTime"`
 	}
 )
