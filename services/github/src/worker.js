4e937378c8bb1f7a5eee050f94612c46f1043580
Brian Stack <bstack@mozilla.com>
2016-09-20 14:36:18 -0700

    Switch to sha instead of ref for finding from sha

--- services/github/src/worker.js
+++ services/github/src/worker.js
@@ -31,7 +31,7 @@ worker.webHookHandler = async function(message, context) {
     taskclusterConfig = await context.github.repos(
       message.payload.organization, message.payload.repository
     ).contents('.taskcluster.yml').read({
-      ref: message.payload.details['event.head.sha'],
+      sha: message.payload.details['event.head.sha'],
     });
   } catch (e) {
     if (e.status === 404) {
