910eb0f072ef497c1d1bee2867dabce2c6f96332
Pete Moore <pmoore@mozilla.com>
2016-09-22 15:33:24 +0200

    Another release.sh fix

--- release.sh
+++ release.sh
@@ -98,4 +98,4 @@ find . -name userdata | while read file; do
 done
 git commit -m "Version bump from ${OLD_VERSION} to ${NEW_VERSION}"
 git tag "v${NEW_VERSION}"
-git push "${OFFICIAL_GIT_REPO}" "+refs/tags/${NEW_VERSION}:refs/tags/${NEW_VERSION}" "+refs/tags/${NEW_VERSION}:refs/heads/master"
+git push "${OFFICIAL_GIT_REPO}" "+refs/tags/v${NEW_VERSION}:refs/tags/v${NEW_VERSION}" "+refs/tags/v${NEW_VERSION}:refs/heads/master"
