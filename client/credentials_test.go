f596284f712b2eea7281314dc4d1353e4087fcb6
joannah nanjekye <nanjekyejoannah@gmail.com>
2016-09-27 19:31:40 +0300

    Added a test case for the credentials

--- client/credentials_test.go
+++ client/credentials_test.go
@@ -1,54 +1,61 @@
 package client
 
 import (
-	"testing"
+	"crypto/sha1"
+	"hash"
+	"net/http"
 	"strconv"
-	"github.com/stretchr/testify/assert"	
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestCredentials(t *testing.T){
+func TestCredentials(t *testing.T) {
 
-	 assert := assert.New(t)
+	assert := assert.New(t)
 
 	credentials := &Credentials{
-		ClientID          : "tester" ,
-		AccessToken       : "no-secret",
+		ClientID:    "tester",
+		AccessToken: "no-secret",
 	}
 
 	var credentialsTests = []struct {
 		method string
-		url  string
-		host string
-		port int
-		hdr  string
-		now  int64
-		perr error
-		verr error
-		key  string
-		hash func() hash.Hash
-		reply bool
-	}
-	{
+		url    string
+		host   string
+		port   int
+		hdr    string
+		now    int64
+		perr   error
+		verr   error
+		key    string
+		hash   hash.Hash
+		reply  bool
+	}{
+
 		{
-			hdr:  `Hawk id="1", ts="1353788437", nonce="k3j4h2", mac="zy79QQ5/EYFmQqutVnYb73gAc/U=", ext="hello"`,
-			hash: sha1.New,
-			method : "GET",
-			url : "/resource/4?filter=a",
-			host : "example.com",
-			port : 8080,
-			now: 1353788437,
-			key : "werxhqb98rpaxn39848xrunpaw3489ruxnpa98w4rxn",
+			hdr:    `Hawk id="1", ts="1353788437", nonce="k3j4h2", mac="zy79QQ5/EYFmQqutVnYb73gAc/U=", ext="hello"`,
+			hash:   sha1.New(),
+			method: "POST",
+			url:    "https://auth.taskcluster.net/v1/test-authenticate",
+			host:   "example.com",
+			port:   8080,
+			now:    1353788437,
+			key:    "werxhqb98rpaxn39848xrunpaw3489ruxnpa98w4rxn",
 		},
 	}
 
 	request := &http.Request{
-				Method:     credentialsTests.method,
-				RequestURI: credentialsTests.url,
-				Host:       credentialsTests.host + ":" + strconv.Itoa(credentialsTests.port),
-				Header:     http.Header{"Authorization": {credentialsTests.hdr}},
-			}
-
-	credentials.SignRequest(request,credentialsTests.hash)
-	auth, errors := credentials.newAuth(credentialsTests.method ,credentialsTests.url,credentialsTests.hash)
-	assert.Nil(errors)
+		Method:     credentialsTests[0].method,
+		RequestURI: credentialsTests[0].url,
+		Host:       credentialsTests[0].host + ":" + strconv.Itoa(credentialsTests[0].port),
+		Header:     http.Header{"Authorization": {credentialsTests[0].hdr}},
+	}
+	if credentialsTests[0].hash != nil {
+		panic(credentialsTests[0].hash)
+	}
+	credentials.SignRequest(request, credentialsTests[0].hash)
+	auth, errors := credentials.newAuth(credentialsTests[0].method, credentialsTests[0].url, credentialsTests[0].hash)
+	assert.Equal(nil, errors)
+	assert.Equal(auth.RequestHeader(), `Hawk id="1", ts="1353788437", nonce="k3j4h2", mac="zy79QQ5/EYFmQqutVnYb73gAc/U=", ext="hello"`)
 }
