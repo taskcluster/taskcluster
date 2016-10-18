4e2fe2ca137fb126051bf05b3e0e22bd4b2e6cc2
Pete Moore <pmoore@mozilla.com>
2016-10-18 21:25:44 +0200

    Regenerated library to pick up latest schema changes

--- github/types.go
+++ github/types.go
@@ -2,4 +2,79 @@
 
 package github
 
-type ()
+import (
+	tcclient "github.com/taskcluster/taskcluster-client-go"
+)
+
+type (
+	// A paginated list of builds
+	//
+	// See http://schemas.taskcluster.net/github/v1/build-list.json#
+	Builds1 struct {
+
+		// A simple list of builds.
+		//
+		// See http://schemas.taskcluster.net/github/v1/build-list.json#/properties/builds
+		Builds []struct {
+
+			// The initial creation time of the build. This is when it became pending.
+			//
+			// See http://schemas.taskcluster.net/github/v1/build-list.json#/properties/builds/items/properties/created
+			Created tcclient.Time `json:"created"`
+
+			// Github organization associated with the build.
+			//
+			// Syntax:     ^([a-zA-Z0-9-_%]*)$
+			// Min length: 1
+			// Max length: 100
+			//
+			// See http://schemas.taskcluster.net/github/v1/build-list.json#/properties/builds/items/properties/organization
+			Organization string `json:"organization"`
+
+			// Github repository associated with the build.
+			//
+			// Syntax:     ^([a-zA-Z0-9-_%]*)$
+			// Min length: 1
+			// Max length: 100
+			//
+			// See http://schemas.taskcluster.net/github/v1/build-list.json#/properties/builds/items/properties/repository
+			Repository string `json:"repository"`
+
+			// Github revision associated with the build.
+			//
+			// Min length: 40
+			// Max length: 40
+			//
+			// See http://schemas.taskcluster.net/github/v1/build-list.json#/properties/builds/items/properties/sha
+			Sha string `json:"sha"`
+
+			// Github status associated with the build.
+			//
+			// Possible values:
+			//   * "pending"
+			//   * "success"
+			//   * "error"
+			//   * "failure"
+			//
+			// See http://schemas.taskcluster.net/github/v1/build-list.json#/properties/builds/items/properties/state
+			State string `json:"state"`
+
+			// Taskcluster task-group associated with the build.
+			//
+			// Syntax:     ^[A-Za-z0-9_-]{8}[Q-T][A-Za-z0-9_-][CGKOSWaeimquy26-][A-Za-z0-9_-]{10}[AQgw]$
+			//
+			// See http://schemas.taskcluster.net/github/v1/build-list.json#/properties/builds/items/properties/taskGroupId
+			TaskGroupID string `json:"taskGroupId"`
+
+			// The last updated of the build. If it is done, this is when it finished.
+			//
+			// See http://schemas.taskcluster.net/github/v1/build-list.json#/properties/builds/items/properties/updated
+			Updated tcclient.Time `json:"updated"`
+		} `json:"builds"`
+
+		// Passed back from Azure to allow us to page through long result sets.
+		//
+		// See http://schemas.taskcluster.net/github/v1/build-list.json#/properties/continuationToken
+		ContinuationToken string `json:"continuationToken,omitempty"`
+	}
+)
