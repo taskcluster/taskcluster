4819ad1f1d4b53fc83c90a56039ff1b502ea0209
Pete Moore <pmoore@mozilla.com>
2016-11-02 19:28:59 +0100

    Regenerated library to pick up latest schema changes

--- notify/notify.go
+++ notify/notify.go
@@ -32,7 +32,7 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/notify/v1/api.json together with the input and output schemas it references, downloaded on
-// Mon, 31 Oct 2016 at 23:23:00 UTC. The code was generated
+// Wed, 2 Nov 2016 at 18:27:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package notify
 
@@ -124,11 +124,8 @@ func (myNotify *Notify) Irc(payload *PostIRCMessageRequest) error {
 	return err
 }
 
-// Stability: *** EXPERIMENTAL ***
-//
-// Documented later...
-//
-// **Warning** this api end-point is **not stable**.
+// Respond without doing anything.
+// This endpoint is used to check that the service is up.
 //
 // See https://docs.taskcluster.net/reference/core/notify/api-docs#ping
 func (myNotify *Notify) Ping() error {
