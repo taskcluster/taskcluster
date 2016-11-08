263aae476ae5090649fb6eef7f582e784b708671
Pete Moore <pmoore@mozilla.com>
2016-11-08 22:25:18 +0100

    Regenerated library to pick up latest schema changes

--- queue/queue.go
+++ queue/queue.go
@@ -38,7 +38,7 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/queue/v1/api.json together with the input and output schemas it references, downloaded on
-// Wed, 2 Nov 2016 at 18:27:00 UTC. The code was generated
+// Tue, 8 Nov 2016 at 21:24:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package queue
 
@@ -636,11 +636,8 @@ func (myQueue *Queue) PendingTasks(provisionerId, workerType string) (*CountPend
 	return responseObject.(*CountPendingTasksResponse), err
 }
 
-// Stability: *** EXPERIMENTAL ***
-//
-// Documented later...
-//
-// **Warning** this api end-point is **not stable**.
+// Respond without doing anything.
+// This endpoint is used to check that the service is up.
 //
 // See https://docs.taskcluster.net/reference/platform/queue/api-docs#ping
 func (myQueue *Queue) Ping() error {
