10ee84b9c3a41fcbbab35000e9f7fc968ae8d415
Pete Moore <pmoore@mozilla.com>
2016-11-26 19:43:09 +0100

    Regenerated library to pick up latest schema changes

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -39,7 +39,7 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/scheduler/v1/api.json together with the input and output schemas it references, downloaded on
-// Thu, 24 Nov 2016 at 21:24:00 UTC. The code was generated
+// Sat, 26 Nov 2016 at 18:42:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package scheduler
 
