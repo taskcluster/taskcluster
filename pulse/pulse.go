e74342993d61837047a46d08b58db6e33a9d74a6
Pete Moore <pmoore@mozilla.com>
2016-12-15 17:25:13 +0100

    Regenerated library to pick up latest schema changes

--- pulse/pulse.go
+++ pulse/pulse.go
@@ -35,12 +35,13 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/pulse/v1/api.json together with the input and output schemas it references, downloaded on
-// Mon, 28 Nov 2016 at 21:24:00 UTC. The code was generated
+// Thu, 15 Dec 2016 at 16:24:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package pulse
 
 import (
 	"net/url"
+	"time"
 
 	tcclient "github.com/taskcluster/taskcluster-client-go"
 )
@@ -79,8 +80,6 @@ func New(credentials *tcclient.Credentials) *Pulse {
 //
 // An overview of the Rabbit cluster
 //
-// **Warning** this api end-point is **not stable**.
-//
 // See https://docs.do.not.exist.yet.service.not.in.production#overview
 func (myPulse *Pulse) Overview() (*RabbitOverviewResponse, error) {
 	cd := tcclient.ConnectionData(*myPulse)
@@ -91,8 +90,6 @@ func (myPulse *Pulse) Overview() (*RabbitOverviewResponse, error) {
 // Stability: *** EXPERIMENTAL ***
 //
 // A list of exchanges in the rabbit cluster
-//
-// **Warning** this api end-point is **not stable**.
 //
 // See https://docs.do.not.exist.yet.service.not.in.production#exchanges
 func (myPulse *Pulse) Exchanges() (*RabbitMQExchanges, error) {
@@ -105,16 +102,39 @@ func (myPulse *Pulse) Exchanges() (*RabbitMQExchanges, error) {
 //
 // Creates a namespace, given the taskcluster credentials with scopes.
 //
-// **Warning** this api end-point is **not stable**.
+// Required scopes:
+//   * pulse:namespace:<namespace>
+//
+// See https://docs.do.not.exist.yet.service.not.in.production#createNamespace
+func (myPulse *Pulse) CreateNamespace(namespace string, payload *NamespaceCreationRequest) (*NamespaceCreationResponse, error) {
+	cd := tcclient.ConnectionData(*myPulse)
+	responseObject, _, err := (&cd).APICall(payload, "POST", "/namespace/"+url.QueryEscape(namespace), new(NamespaceCreationResponse), nil)
+	return responseObject.(*NamespaceCreationResponse), err
+}
+
+// Stability: *** EXPERIMENTAL ***
+//
+// Gets a namespace, given the taskcluster credentials with scopes.
 //
 // Required scopes:
 //   * pulse:namespace:<namespace>
 //
 // See https://docs.do.not.exist.yet.service.not.in.production#namespace
-func (myPulse *Pulse) Namespace(namespace string, payload *NamespaceCreationRequest) (*NamespaceCreationResponse, error) {
+func (myPulse *Pulse) Namespace(namespace string) error {
 	cd := tcclient.ConnectionData(*myPulse)
-	responseObject, _, err := (&cd).APICall(payload, "POST", "/namespace/"+url.QueryEscape(namespace), new(NamespaceCreationResponse), nil)
-	return responseObject.(*NamespaceCreationResponse), err
+	_, _, err := (&cd).APICall(nil, "GET", "/namespace/"+url.QueryEscape(namespace), nil, nil)
+	return err
+}
+
+// Returns a signed URL for Namespace, valid for the specified duration.
+//
+// Required scopes:
+//   * pulse:namespace:<namespace>
+//
+// See Namespace for more details.
+func (myPulse *Pulse) Namespace_SignedURL(namespace string, duration time.Duration) (*url.URL, error) {
+	cd := tcclient.ConnectionData(*myPulse)
+	return (&cd).SignedURL("/namespace/"+url.QueryEscape(namespace), nil, duration)
 }
 
 // Respond without doing anything.
