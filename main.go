c757afeed12166a81dac628c0c0dc34e53c1c9e2
Pete Moore <pmoore@mozilla.com>
2016-12-19 17:53:39 +0100

    Version bump from 7.2.1 to 7.2.2

--- main.go
+++ main.go
@@ -61,7 +61,7 @@ var (
 		&MountsFeature{},
 	}
 
-	version = "7.2.1"
+	version = "7.2.2"
 	usage   = `
 generic-worker
 generic-worker is a taskcluster worker that can run on any platform that supports go (golang).
