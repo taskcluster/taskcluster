e809d2bfe7a98c4d5343c460bdbb8d4742adf98e
Pete Moore <pmoore@mozilla.com>
2016-12-30 10:24:17 +0100

    Regenerated library to pick up latest schema changes

--- login/login.go
+++ login/login.go
@@ -24,7 +24,7 @@
 //
 // and then call one or more of myLogin's methods, e.g.:
 //
-//  err := myLogin.Ping(.....)
+//  data, err := myLogin.CredentialsFromPersonaAssertion(.....)
 // handling any errors...
 //  if err != nil {
 //  	// handle error...
@@ -34,7 +34,7 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/login/v1/api.json together with the input and output schemas it references, downloaded on
-// Wed, 21 Dec 2016 at 16:23:00 UTC. The code was generated
+// Fri, 30 Dec 2016 at 09:23:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package login
 
@@ -54,10 +54,10 @@ type Login tcclient.ConnectionData
 //  	AccessToken: os.Getenv("TASKCLUSTER_ACCESS_TOKEN"),
 //  	Certificate: os.Getenv("TASKCLUSTER_CERTIFICATE"),
 //  }
-//  myLogin := login.New(creds)                              // set credentials
-//  myLogin.Authenticate = false                             // disable authentication (creds above are now ignored)
-//  myLogin.BaseURL = "http://localhost:1234/api/Login/v1"   // alternative API endpoint (production by default)
-//  err := myLogin.Ping(.....)                               // for example, call the Ping(.....) API endpoint (described further down)...
+//  myLogin := login.New(creds)                                   // set credentials
+//  myLogin.Authenticate = false                                  // disable authentication (creds above are now ignored)
+//  myLogin.BaseURL = "http://localhost:1234/api/Login/v1"        // alternative API endpoint (production by default)
+//  data, err := myLogin.CredentialsFromPersonaAssertion(.....)   // for example, call the CredentialsFromPersonaAssertion(.....) API endpoint (described further down)...
 //  if err != nil {
 //  	// handle errors...
 //  }
@@ -72,9 +72,21 @@ func New(credentials *tcclient.Credentials) *Login {
 
 // Stability: *** EXPERIMENTAL ***
 //
-// Documented later...
+// Given an [assertion](https://developer.mozilla.org/en-US/Persona/Quick_setup), return an appropriate set of temporary credentials.
 //
-// **Warning** this api end-point is **not stable**.
+// The supplied audience must be on a whitelist of TaskCluster-related
+// sites configured in the login service.  This is not a general-purpose
+// assertion-verification service!
+//
+// See https://docs.taskcluster.net/reference/core/login/api-docs#credentialsFromPersonaAssertion
+func (myLogin *Login) CredentialsFromPersonaAssertion(payload *PersonaAssertionRequest) (*CredentialsResponse, error) {
+	cd := tcclient.ConnectionData(*myLogin)
+	responseObject, _, err := (&cd).APICall(payload, "POST", "/persona", new(CredentialsResponse), nil)
+	return responseObject.(*CredentialsResponse), err
+}
+
+// Respond without doing anything.
+// This endpoint is used to check that the service is up.
 //
 // See https://docs.taskcluster.net/reference/core/login/api-docs#ping
 func (myLogin *Login) Ping() error {
--- login/types.go
+++ login/types.go
@@ -2,4 +2,39 @@
 
 package login
 
-type ()
+type (
+	// A response containing temporary credentials.
+	//
+	// See http://schemas.taskcluster.net/login/v1/credentials-response.json#
+	CredentialsResponse struct {
+
+		// Syntax:     ^[a-zA-Z0-9_-]{22,66}$
+		//
+		// See http://schemas.taskcluster.net/login/v1/credentials-response.json#/properties/accessToken
+		AccessToken string `json:"accessToken"`
+
+		// See http://schemas.taskcluster.net/login/v1/credentials-response.json#/properties/certificate
+		Certificate string `json:"certificate"`
+
+		// Syntax:     ^[A-Za-z0-9@/:._-]+$
+		//
+		// See http://schemas.taskcluster.net/login/v1/credentials-response.json#/properties/clientId
+		ClientID string `json:"clientId"`
+	}
+
+	// See http://schemas.taskcluster.net/login/v1/persona-request.json#
+	PersonaAssertionRequest struct {
+
+		// The Persona assertion from `navigator.id.get`
+		//
+		// See http://schemas.taskcluster.net/login/v1/persona-request.json#/properties/assertion
+		Assertion string `json:"assertion"`
+
+		// The audience against which to verify the assertion, in the format
+		// `https://site.com:443`.  This must be from a whitelist of sites
+		// configured in the login service.
+		//
+		// See http://schemas.taskcluster.net/login/v1/persona-request.json#/properties/audience
+		Audience string `json:"audience"`
+	}
+)
