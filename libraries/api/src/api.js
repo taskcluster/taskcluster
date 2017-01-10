7b8e9c8f4af938ef61cab98ba7521503137678a8
Dustin J. Mitchell <dustin@v.igoro.us>
2017-01-10 10:03:47 -0500

    Merge pull request #49 from taskcluster/bug1316915
    Bug 1316915: improve error handling in tc-lib-api

--- libraries/api/src/api.js
+++ libraries/api/src/api.js
@@ -133,10 +133,7 @@ var schema = function(validate, options) {
         return res.reportError(
           'InputValidationError',
           error,
-        {
-          schema: options.input,
-          payload: req.body
-        });
+        {schema: options.input});
       }
     }
     // Add a reply method sending JSON replies, this will always reply with HTTP
@@ -648,7 +645,9 @@ var STABILITY_LEVELS = _.values(stability);
  *   title:     "My API Method",
  *   description: [
  *     "Description of method in markdown, enjoy"
- *   ].join('\n')
+ *   ].join('\n'),
+ *   cleanPayload: payload => payload,           // function to 'clean' the payload for
+ *                                               // error messages (e.g., remove secrets)
  * }
  *
  * The handler parameter is a normal connect/express request handler, it should
@@ -798,7 +797,8 @@ API.prototype.router = function(options) {
     // Add authentication, schema validation and handler
     middleware.push(
       errors.BuildReportErrorMethod(
-        entry.name, this._options.errorCodes, (monitor || options.raven)
+        entry.name, this._options.errorCodes, (monitor || options.raven),
+        entry.cleanPayload
       ),
       bodyParser.text({
         limit:          options.inputLimit,
