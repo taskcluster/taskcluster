0ca3247ea26a8e71d2d86a1d6afb3b19c6b3fe6b
Pete Moore <pmoore@mozilla.com>
2017-01-24 16:33:04 +0100

    Created build script for windows

---
+++ build.cmd
@@ -0,0 +1,24 @@
+:: main script to build and test generic worker on windows
+
+@echo on
+
+:: cd to dir containing this script
+pushd %~dp0
+
+go get github.com/taskcluster/livelog github.com/gordonklaus/ineffassign || exit /b %ERRORLEVEL%
+cd gw-codegen
+go get -v || exit /b %ERRORLEVEL%
+cd ..
+go generate || exit /b %ERRORLEVEL%
+go get -v -t ./... || exit /b %ERRORLEVEL%
+
+:: this counts the number of lines returned by git status
+:: dump temp file a directory higher, otherwise git status reports the tmp1.txt file!
+git status --porcelain | C:\Windows\System32\find.exe /v /c "" > ..\tmp1.txt
+set /P lines=<..\tmp1.txt
+:: this checks that if more than 0 lines are returned, we fail
+if %lines% gtr 0 exit /b 64
+
+set GORACE=history_size=7
+go test -v ./... || exit /b %ERRORLEVEL%
+ineffassign . || exit /b %ERRORLEVEL%
