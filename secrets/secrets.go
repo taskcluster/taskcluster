f970aa863bc273e91b646e5a689c161a16d281a4
Pete Moore <pmoore@mozilla.com>
2017-01-25 02:23:53 +0100

    Regenerated library to pick up latest schema changes

--- secrets/secrets.go
+++ secrets/secrets.go
@@ -36,7 +36,7 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/secrets/v1/api.json together with the input and output schemas it references, downloaded on
-// Fri, 20 Jan 2017 at 20:24:00 UTC. The code was generated
+// Wed, 25 Jan 2017 at 01:22:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package secrets
 
@@ -146,6 +146,16 @@ func (mySecrets *Secrets) List() (*SecretsList, error) {
 //
 // See https://docs.taskcluster.net/reference/core/secrets/api-docs#ping
 func (mySecrets *Secrets) Ping() error {
+	cd := tcclient.ConnectionData(*mySecrets)
+	_, _, err := (&cd).APICall(nil, "GET", "/ping", nil, nil)
+	return err
+}
+
+// Respond without doing anything.
+// This endpoint is used to check that the service is up.
+//
+// See https://docs.taskcluster.net/reference/core/secrets/api-docs#ping
+func (mySecrets *Secrets) Ping1() error {
 	cd := tcclient.ConnectionData(*mySecrets)
 	_, _, err := (&cd).APICall(nil, "GET", "/ping", nil, nil)
 	return err
