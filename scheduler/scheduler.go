654d64ef2198fd4d904ad558595d9d524004f28d
Pete Moore <pmoore@mozilla.com>
2017-01-25 17:25:24 +0100

    Regenerated library to pick up latest schema changes

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -39,7 +39,7 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/scheduler/v1/api.json together with the input and output schemas it references, downloaded on
-// Wed, 25 Jan 2017 at 01:22:00 UTC. The code was generated
+// Wed, 25 Jan 2017 at 16:24:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package scheduler
 
--- secrets/secrets.go
+++ secrets/secrets.go
@@ -36,7 +36,7 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/secrets/v1/api.json together with the input and output schemas it references, downloaded on
-// Wed, 25 Jan 2017 at 01:22:00 UTC. The code was generated
+// Wed, 25 Jan 2017 at 16:24:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package secrets
 
@@ -139,23 +139,11 @@ func (mySecrets *Secrets) List() (*SecretsList, error) {
 	return responseObject.(*SecretsList), err
 }
 
-// Stability: *** EXPERIMENTAL ***
-//
-// Respond without doing anything.  This endpoint is used to check that
-// the service is up.
-//
-// See https://docs.taskcluster.net/reference/core/secrets/api-docs#ping
-func (mySecrets *Secrets) Ping() error {
-	cd := tcclient.ConnectionData(*mySecrets)
-	_, _, err := (&cd).APICall(nil, "GET", "/ping", nil, nil)
-	return err
-}
-
 // Respond without doing anything.
 // This endpoint is used to check that the service is up.
 //
 // See https://docs.taskcluster.net/reference/core/secrets/api-docs#ping
-func (mySecrets *Secrets) Ping1() error {
+func (mySecrets *Secrets) Ping() error {
 	cd := tcclient.ConnectionData(*mySecrets)
 	_, _, err := (&cd).APICall(nil, "GET", "/ping", nil, nil)
 	return err
