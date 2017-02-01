0fa896d5fcac6a47825612a3bfccb4a05c6bdcf5
Dustin J. Mitchell <dustin@v.igoro.us>
2017-02-01 14:22:18 -0500

    Merge pull request #89 from taskcluster/optional-autoexpire
    Add support for optional client autoexpire

---
+++ services/auth/test/purgeExpiredClients_test.js
@@ -0,0 +1,53 @@
+suite('Client.purgeExpired', function() {
+  var helper      = require('./helper');
+  var assume      = require('assume');
+  var taskcluster = require('taskcluster-client');
+
+  const CLIENT_ID = 'nobody/sds:ad_asd/df-sAdSfchsdfsdfs';
+
+  setup(async () => {
+    await helper.auth.deleteClient(CLIENT_ID);
+  });
+
+  const testClient = async ({expires, deleteOnExpiration}) => {
+    await helper.auth.createClient(CLIENT_ID, {
+      expires: taskcluster.fromNow(expires),
+      description: 'test',
+      deleteOnExpiration,
+    });
+  };
+
+  const assertClientPresent = async () => {
+    let client = await helper.auth.client(CLIENT_ID);
+    assume(client.clientId).to.equal(CLIENT_ID);
+  };
+
+  const assertClientAbsent = async () => {
+    try {
+      await helper.auth.client(CLIENT_ID);
+    } catch (err) {
+      assume(err.statusCode).to.equal(404);
+      return;
+    }
+    throw new Error('client should be absent');
+  };
+
+  test('does not delete unexpired clients', async() => {
+    await testClient({expires: '1 hour', deleteOnExpiration: true});
+    await helper.Client.purgeExpired(new Date());
+    await assertClientPresent();
+  });
+
+  test('does not delete expired clients with !deleteOnExpiration', async() => {
+    await testClient({expires: '1 hour', deleteOnExpiration: false});
+    await helper.Client.purgeExpired(taskcluster.fromNow('2 hours'));
+    await assertClientPresent();
+  });
+
+  test('deletes expired clients with deleteOnExpiration', async() => {
+    await testClient({expires: '1 hour', deleteOnExpiration: true});
+    await helper.Client.purgeExpired(taskcluster.fromNow('2 hours'));
+    await assertClientAbsent();
+  });
+
+});
