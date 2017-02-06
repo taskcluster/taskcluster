e0f59d8a6ef610c7e71ec533e5b7c2ea6489838e
Dustin J. Mitchell <dustin@v.igoro.us>
2017-02-06 15:48:36 -0500

    Merge pull request #112 from djmitche/test-fixes2
    Delete all build entries, and don't assume they're returned in order

--- services/github/test/api_test.js
+++ services/github/test/api_test.js
@@ -6,8 +6,13 @@
 suite('api', () => {
   let helper = require('./helper');
   let assert = require('assert');
+  let _ = require('lodash');
 
   suiteSetup(async () => {
+    await helper.Builds.scan({}, {
+      handler: build => build.remove(),
+    });
+
     await helper.Builds.create({
       organization: 'abc123',
       repository: 'def456',
@@ -53,19 +58,24 @@ suite('api', () => {
   test('all builds', async function() {
     let builds = await helper.github.builds();
     assert.equal(builds.builds.length, 4);
+    builds.builds = _.orderBy(builds.builds, ['organization', 'repository']);
     assert.equal(builds.builds[0].organization, 'abc123');
-    assert.equal(builds.builds[1].organization, 'ghi789');
+    assert.equal(builds.builds[1].organization, 'abc123');
+    assert.equal(builds.builds[2].organization, 'abc123');
+    assert.equal(builds.builds[3].organization, 'ghi789');
   });
 
   test('org builds', async function() {
     let builds = await helper.github.builds({organization: 'abc123'});
     assert.equal(builds.builds.length, 3);
+    builds.builds = _.orderBy(builds.builds, ['organization', 'repository']);
     assert.equal(builds.builds[0].organization, 'abc123');
   });
 
   test('repo builds', async function() {
     let builds = await helper.github.builds({organization: 'abc123', repository: 'xyz'});
     assert.equal(builds.builds.length, 2);
+    builds.builds = _.orderBy(builds.builds, ['organization', 'repository']);
     assert.equal(builds.builds[0].organization, 'abc123');
     assert.equal(builds.builds[0].repository, 'xyz');
   });
@@ -77,6 +87,7 @@ suite('api', () => {
       sha: 'y650871208002a13ba35cf232c0e30d2c3d64783',
     });
     assert.equal(builds.builds.length, 1);
+    builds.builds = _.orderBy(builds.builds, ['organization', 'repository']);
     assert.equal(builds.builds[0].organization, 'abc123');
     assert.equal(builds.builds[0].repository, 'xyz');
     assert.equal(builds.builds[0].sha, 'y650871208002a13ba35cf232c0e30d2c3d64783');
