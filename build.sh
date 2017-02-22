77ab61ce6b525d6b1c44cf694d65069adf577a1e
Brian Stack <bstack@mozilla.com>
2017-02-22 12:53:40 -0800

    Update build.sh version mismatch message

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ if [ -z "${GO_VERSION}" ]; then
   echo "Have you installed go? I get no result from \`go version\` command." >&2
   exit 64
 elif [ "${GO_MAJ}" != "go1" ] || [ "${GO_MIN}" -lt 7 ]; then
-  echo "Go version go1.x needed, where x >= 5, but the version I found is: '${GO_VERSION}'" >&2
+  echo "Go version go1.x needed, where x >= 7, but the version I found is: '${GO_VERSION}'" >&2
   echo "I found it here:" >&2
   which go >&2
   echo "The complete output of \`go version\` command is:" >&2
