50f964fd4df95a1eb5f3968fbaeb99b751827870
Pete Moore <pmoore@mozilla.com>
2017-04-14 00:24:12 +0200

    Regenerated library to pick up latest schema changes

--- pulse/types.go
+++ pulse/types.go
@@ -3,9 +3,6 @@
 package pulse
 
 import (
-	"encoding/json"
-	"errors"
-
 	tcclient "github.com/taskcluster/taskcluster-client-go"
 )
 
@@ -27,10 +24,12 @@ type (
 		// See http://schemas.taskcluster.net/pulse/v1/list-namespaces-response.json#/properties/namespaces
 		Namespaces []struct {
 
-			// The contact information which will be handed off to the notification service
+			// E-mail address that will reach people who can address problems with runaway queue length.
+			// The service will send warning notifications to this address before forcibly deleting the
+			// queue.
 			//
 			// See http://schemas.taskcluster.net/pulse/v1/list-namespaces-response.json#/properties/namespaces/items/properties/contact
-			Contact json.RawMessage `json:"contact"`
+			Contact string `json:"contact"`
 
 			// Date-time at which this namespace was first claimed.
 			//
@@ -55,10 +54,12 @@ type (
 	// See http://schemas.taskcluster.net/pulse/v1/namespace.json#
 	Namespace1 struct {
 
-		// The contact information which will be handed off to the notification service
+		// E-mail address that will reach people who can address problems with runaway queue length.
+		// The service will send warning notifications to this address before forcibly deleting the
+		// queue.
 		//
 		// See http://schemas.taskcluster.net/pulse/v1/namespace.json#/properties/contact
-		Contact json.RawMessage `json:"contact"`
+		Contact string `json:"contact"`
 
 		// Date-time at which this namespace was first claimed.
 		//
@@ -82,10 +83,12 @@ type (
 	// See http://schemas.taskcluster.net/pulse/v1/namespace-request.json#
 	NamespaceCreationRequest struct {
 
-		// The contact information which will be handed off to the notification service
+		// E-mail address that will reach people who can address problems with runaway queue length.
+		// The service will send warning notifications to this address before forcibly deleting the
+		// queue.
 		//
 		// See http://schemas.taskcluster.net/pulse/v1/namespace-request.json#/properties/contact
-		Contact json.RawMessage `json:"contact"`
+		Contact string `json:"contact"`
 
 		// Date-time after which the username, and all associated queues and
 		// exchanges, should be deleted. This can be updated on every claim, with no
@@ -100,10 +103,12 @@ type (
 	// See http://schemas.taskcluster.net/pulse/v1/namespace-response.json#
 	NamespaceCreationResponse struct {
 
-		// The contact information which will be handed off to the notification service
+		// E-mail address that will reach people who can address problems with runaway queue length.
+		// The service will send warning notifications to this address before forcibly deleting the
+		// queue.
 		//
 		// See http://schemas.taskcluster.net/pulse/v1/namespace-response.json#/properties/contact
-		Contact json.RawMessage `json:"contact"`
+		Contact string `json:"contact"`
 
 		// Date-time at which this namespace was first claimed.
 		//
@@ -136,44 +141,6 @@ type (
 		//
 		// See http://schemas.taskcluster.net/pulse/v1/namespace-response.json#/properties/username
 		Username string `json:"username"`
-	}
-
-	// Request to post a message on IRC.
-	//
-	// See http://schemas.taskcluster.net/pulse/v1/irc-request.json#
-	PostIRCMessageRequest struct {
-
-		// The contact method (eg. irc)
-		//
-		// Possible values:
-		//   * "irc"
-		//
-		// See http://schemas.taskcluster.net/pulse/v1/irc-request.json#/properties/method
-		Method string `json:"method"`
-
-		// Details for the contact method (eg. irc channel)
-		//
-		// See http://schemas.taskcluster.net/pulse/v1/irc-request.json#/properties/payload
-		Payload struct {
-
-			// Channel to post the message in. Please note that you **must** supply
-			// either `user` or `channel`, you cannot supply both.
-			//
-			// Syntax:     ^[#&][^ ,\u0007]{1,199}$
-			//
-			// See http://schemas.taskcluster.net/pulse/v1/irc-request.json#/properties/payload/properties/channel
-			Channel string `json:"channel,omitempty"`
-
-			// User to post the message to. Please note that you **must** supply
-			// either `user` or `channel`, you cannot supply both.
-			//
-			// Syntax:     ^[A-Za-z\[\]\\~_\^{|}][A-Za-z0-9\-\[\]\\~_\^{|}]{0,254}$
-			// Min length: 1
-			// Max length: 255
-			//
-			// See http://schemas.taskcluster.net/pulse/v1/irc-request.json#/properties/payload/properties/user
-			User string `json:"user,omitempty"`
-		} `json:"payload"`
 	}
 
 	// An array of RabbitMQ exchanges containing the details of RabbitMQ exchanges
@@ -237,96 +204,4 @@ type (
 		// See http://schemas.taskcluster.net/pulse/v1/rabbit-overview.json#/properties/rabbitmq_version
 		Rabbitmq_Version string `json:"rabbitmq_version"`
 	}
-
-	// Request to send an email
-	//
-	// See http://schemas.taskcluster.net/pulse/v1/email-request.json#
-	SendEmailRequest struct {
-
-		// The contact method (eg. email)
-		//
-		// Possible values:
-		//   * "email"
-		//
-		// See http://schemas.taskcluster.net/pulse/v1/email-request.json#/properties/method
-		Method string `json:"method"`
-
-		// Details for the contact method (eg. email address)
-		//
-		// See http://schemas.taskcluster.net/pulse/v1/email-request.json#/properties/payload
-		Payload struct {
-
-			// E-mail address to which the message should be sent
-			//
-			// See http://schemas.taskcluster.net/pulse/v1/email-request.json#/properties/payload/properties/address
-			Address string `json:"address"`
-
-			// Reply-to e-mail (this property is optional)
-			//
-			// See http://schemas.taskcluster.net/pulse/v1/email-request.json#/properties/payload/properties/replyTo
-			ReplyTo string `json:"replyTo,omitempty"`
-		} `json:"payload"`
-	}
-
-	// See http://schemas.taskcluster.net/pulse/v1/irc-request.json#/properties/payload/oneOf[0]
-	Var json.RawMessage
-
-	// See http://schemas.taskcluster.net/pulse/v1/irc-request.json#/properties/payload/oneOf[1]
-	Var1 json.RawMessage
-
-	// See http://schemas.taskcluster.net/pulse/v1/list-namespaces-response.json#/properties/namespaces/items/properties/contact/oneOf[0]
-	Var2 PostIRCMessageRequest
-
-	// See http://schemas.taskcluster.net/pulse/v1/list-namespaces-response.json#/properties/namespaces/items/properties/contact/oneOf[1]
-	Var3 SendEmailRequest
-
-	// See http://schemas.taskcluster.net/pulse/v1/namespace.json#/properties/contact/oneOf[0]
-	Var4 PostIRCMessageRequest
-
-	// See http://schemas.taskcluster.net/pulse/v1/namespace.json#/properties/contact/oneOf[1]
-	Var5 SendEmailRequest
-
-	// See http://schemas.taskcluster.net/pulse/v1/namespace-request.json#/properties/contact/oneOf[0]
-	Var6 PostIRCMessageRequest
-
-	// See http://schemas.taskcluster.net/pulse/v1/namespace-request.json#/properties/contact/oneOf[1]
-	Var7 SendEmailRequest
-
-	// See http://schemas.taskcluster.net/pulse/v1/namespace-response.json#/properties/contact/oneOf[0]
-	Var8 PostIRCMessageRequest
-
-	// See http://schemas.taskcluster.net/pulse/v1/namespace-response.json#/properties/contact/oneOf[1]
-	Var9 SendEmailRequest
 )
-
-// MarshalJSON calls json.RawMessage method of the same name. Required since
-// Var is of type json.RawMessage...
-func (this *Var) MarshalJSON() ([]byte, error) {
-	x := json.RawMessage(*this)
-	return (&x).MarshalJSON()
-}
-
-// UnmarshalJSON is a copy of the json.RawMessage implementation.
-func (this *Var) UnmarshalJSON(data []byte) error {
-	if this == nil {
-		return errors.New("Var: UnmarshalJSON on nil pointer")
-	}
-	*this = append((*this)[0:0], data...)
-	return nil
-}
-
-// MarshalJSON calls json.RawMessage method of the same name. Required since
-// Var1 is of type json.RawMessage...
-func (this *Var1) MarshalJSON() ([]byte, error) {
-	x := json.RawMessage(*this)
-	return (&x).MarshalJSON()
-}
-
-// UnmarshalJSON is a copy of the json.RawMessage implementation.
-func (this *Var1) UnmarshalJSON(data []byte) error {
-	if this == nil {
-		return errors.New("Var1: UnmarshalJSON on nil pointer")
-	}
-	*this = append((*this)[0:0], data...)
-	return nil
-}
