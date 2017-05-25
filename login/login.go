7e4502788e2a0d26965deddea7baef8d81f35cde
Pete Moore <pmoore@mozilla.com>
2017-05-25 21:25:42 +0200

    Regenerated library to pick up latest schema changes

--- login/login.go
+++ login/login.go
@@ -24,7 +24,7 @@
 //
 // and then call one or more of myLogin's methods, e.g.:
 //
-//  data, err := myLogin.CredentialsFromPersonaAssertion(.....)
+//  err := myLogin.Ping(.....)
 // handling any errors...
 //  if err != nil {
 //  	// handle error...
@@ -34,7 +34,7 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/login/v1/api.json together with the input and output schemas it references, downloaded on
-// Tue, 16 May 2017 at 17:06:00 UTC. The code was generated
+// Thu, 25 May 2017 at 19:24:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package login
 
@@ -56,10 +56,10 @@ type Login tcclient.Client
 //  	AccessToken: os.Getenv("TASKCLUSTER_ACCESS_TOKEN"),
 //  	Certificate: os.Getenv("TASKCLUSTER_CERTIFICATE"),
 //  }
-//  myLogin := login.New(creds)                                   // set credentials
-//  myLogin.Authenticate = false                                  // disable authentication (creds above are now ignored)
-//  myLogin.BaseURL = "http://localhost:1234/api/Login/v1"        // alternative API endpoint (production by default)
-//  data, err := myLogin.CredentialsFromPersonaAssertion(.....)   // for example, call the CredentialsFromPersonaAssertion(.....) API endpoint (described further down)...
+//  myLogin := login.New(creds)                              // set credentials
+//  myLogin.Authenticate = false                             // disable authentication (creds above are now ignored)
+//  myLogin.BaseURL = "http://localhost:1234/api/Login/v1"   // alternative API endpoint (production by default)
+//  err := myLogin.Ping(.....)                               // for example, call the Ping(.....) API endpoint (described further down)...
 //  if err != nil {
 //  	// handle errors...
 //  }
@@ -70,21 +70,6 @@ func New(credentials *tcclient.Credentials) *Login {
 		Authenticate: true,
 	})
 	return &myLogin
-}
-
-// Stability: *** EXPERIMENTAL ***
-//
-// Given an [assertion](https://developer.mozilla.org/en-US/Persona/Quick_setup), return an appropriate set of temporary credentials.
-//
-// The supplied audience must be on a whitelist of TaskCluster-related
-// sites configured in the login service.  This is not a general-purpose
-// assertion-verification service!
-//
-// See https://docs.taskcluster.net/reference/core/login/api-docs#credentialsFromPersonaAssertion
-func (myLogin *Login) CredentialsFromPersonaAssertion(payload *PersonaAssertionRequest) (*CredentialsResponse, error) {
-	cd := tcclient.Client(*myLogin)
-	responseObject, _, err := (&cd).APICall(payload, "POST", "/persona", new(CredentialsResponse), nil)
-	return responseObject.(*CredentialsResponse), err
 }
 
 // Respond without doing anything.
