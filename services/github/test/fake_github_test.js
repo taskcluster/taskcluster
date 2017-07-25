8f22a730c58995933173a7739200c626704e2b89
Brian Stack <bstack@mozilla.com>
2017-07-25 08:29:53 -0700

    Merge pull request #208 from taskcluster/upgrade-github-lib
    Upgrade GitHub lib

---
+++ services/github/test/fake_github_test.js
@@ -0,0 +1,23 @@
+let assert = require('assert');
+let _ = require('lodash');
+let github = require('github');
+let fakeGithubAuth = require('./github-auth');
+
+suite('fake github', function() {
+
+  function checkKeys(obj, platonic) {
+    let ours = _.filter(Object.keys(obj), k => !k.startsWith('_'));
+    let theirs = Object.keys(platonic);
+    assert.deepEqual(_.difference(ours, theirs), []);
+    _.forEach(ours, k => {
+      if (_.isObject(obj[k]) && obj[k].isSinonProxy) {
+        checkKeys(obj[k], platonic[k]);
+      }
+    });
+  }
+
+  test('matches real lib', async function() {
+    let inst = await fakeGithubAuth().getInstallationGithub();
+    checkKeys(inst, github());
+  });
+});
