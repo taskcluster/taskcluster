c5b62c21b48fdd440e9cac57f53eb434c9601912
Brian Stack <bstack@mozilla.com>
2017-08-04 14:37:57 -0700

    Merge pull request #11 from taskcluster/remove-superagent-promise
    Remove superagent-promise

--- libraries/app/test/app_test.js
+++ libraries/app/test/app_test.js
@@ -2,10 +2,10 @@ suite('app', function() {
   var assert  = require('assert');
   var path    = require('path');
   var subject = require('../');
-  var request = require('superagent-promise')(require('superagent'), require('promise'));
+  var request = require('superagent');
 
   // Test app creation
-  test('app({port: 1459})', function() {
+  test('app({port: 1459})', async function() {
     // Create a simple app
     var app = subject({
       port:       1459,
@@ -22,16 +22,12 @@ suite('app', function() {
     });
 
     // Create server
-    var server = null;
-    return app.createServer().then(function(server_) {
-      server = server_;
-      return request.get('http://localhost:1459/test').end();
-    }).then(function(res) {
-      assert(res.ok, 'Got response');
-      assert(res.text == 'Okay this works', 'Got the right text');
-      assert(res.headers['strict-transport-security'] === 'max-age=7776000');
-      return server.terminate();
-    });
+    var server = await app.createServer();
+    var res = await request.get('http://localhost:1459/test');
+    assert(res.ok, 'Got response');
+    assert(res.text == 'Okay this works', 'Got the right text');
+    assert(res.headers['strict-transport-security'] === 'max-age=7776000');
+    return server.terminate();
   });
 });
 
