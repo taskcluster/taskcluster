010dfa2d45dee43474ad77a001368855f33dca5e
Chinmay Kousik <chinmaykousik1@gmail.com>
2017-08-26 20:41:13 +0530

    removed duplicate header error

--- whproxy/wsproxy.go
+++ whproxy/wsproxy.go
@@ -40,7 +40,8 @@ func (p *proxy) websocketProxy(w http.ResponseWriter, r *http.Request, session *
 			k == "Connection" ||
 			k == "Sec-Websocket-Key" ||
 			k == "Sec-Websocket-Version" ||
-			k == "Sec-Websocket-Extensions" {
+			k == "Sec-Websocket-Extensions" ||
+			k == "Sec-Websocket-Protocol" {
 			continue
 		}
 		reqHeader[k] = v
