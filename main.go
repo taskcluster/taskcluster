b6c12daf4d84dcbfec4b2aec4c86807213ea2327
Pete Moore <pmoore@mozilla.com>
2017-08-30 12:18:50 +0200

    Bumped version number to 4.0.0 as this is not backwardly compatible

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 	"github.com/taskcluster/taskcluster-client-go/queue"
 )
 
-var version = "Taskcluster proxy 3.1.1"
+var version = "Taskcluster proxy 4.0.0"
 var usage = `
 Taskcluster authentication proxy. By default this pulls all scopes from a
 particular task but additional scopes may be added by specifying them after the
