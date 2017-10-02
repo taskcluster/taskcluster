6acdff817ead782a455d11bb4f4e1331404f4386
Simon Fraser <sfraser@mozilla.com>
2017-10-02 22:12:21 +0100

    Add long help description

--- cmds/completions/completions.go
+++ cmds/completions/completions.go
@@ -14,8 +14,15 @@ func init() {
 	use := "completions <filename (default:" + defaultFilename + ")>"
 	completionsCommand := &cobra.Command{
 		Short: "Provides bash completion script.",
-		RunE:  genCompletion,
-		Use:   use,
+		Long: `Writes a bash completion script to the path specified, or the default filename if not given.
+
+To use, do one of the following:
+'source bash_completion.sh' to add to your current shell,
+Add 'source bash_completion.sh' to your bash login scripts
+On Linux you can also copy it to /etc/bash_completion.d/ so that future bash shells have it active.
+        `,
+		RunE: genCompletion,
+		Use:  use,
 	}
 	root.Command.AddCommand(completionsCommand)
 }
