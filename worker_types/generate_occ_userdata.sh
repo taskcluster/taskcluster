3a58cd1d0a56709bd171663739f0b82e369ec31d
Pete Moore <pmoore@mozilla.com>
2017-10-19 20:17:16 +0200

    Regenerated userdata for occ worker types

--- worker_types/generate_occ_userdata.sh
+++ worker_types/generate_occ_userdata.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
+#!/bin/bash -e
 cd "$(dirname "${0}")"
 
+echo "Removing..."
+rm -v gecko-*/userdata
+
+echo "Generating..."
 curl -L 'https://github.com/mozilla-releng/OpenCloudConfig/tree/master/userdata/Manifest' 2>/dev/null | sed -n 's/.*\(gecko[^.]*\)\.json.*/\1/p' | sort -u | while read m
 do
   echo "${m}"
   mkdir -p "${m}"
-  go run transform-occ/main.go "${m}" > "${m}/userdata"
+  go run transform-occ/main.go "${m}" > "${m}/userdata" || rm "${m}/userdata"
 done
