510590ba2f7dcb3142956866f77f16c92481462e
Hassan Ali <helfi92@gmail.com>
2017-10-31 14:45:04 -0400

    Merge pull request #12 from helfi92/intersection-scopes
    Bug 1410507 - Add an intersection function to taskcluster-lib-scopes

--- libraries/scopes/test/scopes_test.js
+++ libraries/scopes/test/scopes_test.js
@@ -1,5 +1,5 @@
-var assert  = require('assert');
-var utils   = require('../lib/scopes.js');
+import assert from 'assert';
+import utils from '../lib/scopes.js';
 
 suite('validScope', function() {
   test('Normal-looking scopes are OK', function() {
@@ -99,3 +99,106 @@ suite('scopeMatch', function() {
     mktest(['foo:bar'], [], false));
 });
 
+suite('scopeIntersection', () => {
+  const testScopeIntersection = (scope1, scope2, expected, message) => {
+    assert.deepEqual(utils.scopeIntersection(scope1, scope2).sort(), expected.sort(), message);
+    assert.deepEqual(utils.scopeIntersection(scope2, scope1).sort(), expected.sort(), message);
+  };
+
+  test('single exact match, [string]', () => {
+    const scope = ['foo:bar'];
+
+    testScopeIntersection(scope, scope, scope, `expected ${scope}`);
+  });
+
+  test('empty [string] in scopesets', () => {
+    const scope1 = ['foo:bar'];
+    const scope2 = [''];
+
+    testScopeIntersection(scope1, scope2, [], 'expected an empty set');
+  });
+
+  test('prefix', () => {
+    const scope1 = ['foo:bar'];
+    const scope2 = ['foo:*'];
+
+    testScopeIntersection(scope1, scope2, scope1, `expected ${scope1}`);
+  });
+
+  test('star not at end', () => {
+    const scope1 = ['foo:bar:bing'];
+    const scope2 = ['foo:*:bing'];
+
+    testScopeIntersection(scope1, scope2, [], 'expected an empty set');
+  });
+
+  test('star at beginning', () => {
+    const scope1 = ['foo:bar'];
+    const scope2 = ['*:bar'];
+
+    testScopeIntersection(scope1, scope2, [], 'expected an empty set');
+  });
+
+  test('prefix with no star', () => {
+    const scope1 = ['foo:bar'];
+    const scope2 = ['foo:'];
+
+    testScopeIntersection(scope1, scope2, [], 'expected empty set');
+  });
+
+  test('star but not prefix', () => {
+    const scope1 = ['foo:bar:*'];
+    const scope2 = ['bar:bing'];
+
+    testScopeIntersection(scope1, scope2, [], 'expected empty set');
+  });
+
+  test('star but not prefix', () => {
+    const scope1 = ['bar:*'];
+    const scope2 = ['foo:bar:bing'];
+
+    testScopeIntersection(scope1, scope2, [], 'expected empty set');
+  });
+
+  test('disjunction', () => {
+    const scope1 = ['bar:*'];
+    const scope2 = ['foo:x', 'bar:x'];
+
+    testScopeIntersection(scope1, scope2, ['bar:x'], 'expected [\'bar:x\']');
+  });
+
+  test('conjuction', () => {
+    const scope1 = ['bar:y', 'foo:x'];
+    const scope2 = ['bar:*', 'foo:x'];
+
+    testScopeIntersection(scope1, scope2, scope1, `expected ${scope1}`);
+  });
+
+  test('empty pattern', () => {
+    const scope1 = [''];
+    const scope2 = ['foo:bar'];
+
+    testScopeIntersection(scope1, scope2, [], 'expected empty set');
+  });
+
+  test('empty patterns', () => {
+    const scope1 = [];
+    const scope2 = ['foo:bar'];
+
+    testScopeIntersection(scope1, scope2, [], 'expected empty set');
+  });
+
+  test('bare star', () => {
+    const scope1 = ['foo:bar', 'bar:bing'];
+    const scope2 = ['*'];
+
+    testScopeIntersection(scope1, scope2, scope1, `expected ${scope1}`);
+  });
+
+  test('empty conjunction in scopesets', () => {
+    const scope1 = ['foo:bar'];
+    const scope2 = [];
+
+    testScopeIntersection(scope1, scope2, [], 'expected empty set');
+  });
+});
