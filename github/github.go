1dfafefc7f88d6c6165b10316061d16aa3c3adfa
Pete Moore <pmoore@mozilla.com>
2017-10-31 20:24:31 +0100

    Regenerated library to pick up latest schema changes

--- github/github.go
+++ github/github.go
@@ -38,7 +38,7 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/github/v1/api.json together with the input and output schemas it references, downloaded on
-// Tue, 31 Oct 2017 at 15:23:00 UTC. The code was generated
+// Tue, 31 Oct 2017 at 19:23:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package github
 
--- queue/queue.go
+++ queue/queue.go
@@ -38,7 +38,7 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/queue/v1/api.json together with the input and output schemas it references, downloaded on
-// Tue, 31 Oct 2017 at 15:23:00 UTC. The code was generated
+// Tue, 31 Oct 2017 at 19:23:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package queue
 
@@ -847,7 +847,7 @@ func (myQueue *Queue) GetWorker(provisionerId, workerType, workerGroup, workerId
 // possessed.
 //
 // Required scopes:
-//   * queue:declare-worker:<provisionerId>/<workerType>/<workerGroup><workerId>#<property>
+//   * queue:declare-worker:<provisionerId>/<workerType>/<workerGroup>/<workerId>#<property>
 //
 // See https://docs.taskcluster.net/reference/platform/queue/api-docs#declareWorker
 func (myQueue *Queue) DeclareWorker(provisionerId, workerType, workerGroup, workerId string, payload *WorkerRequest) (*WorkerResponse, error) {
