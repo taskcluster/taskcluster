a70fce3d967b70327dc4643b6bfb4f0c61e96442
Brian Stack <bstack@mozilla.com>
2017-11-02 11:06:35 -0400

    Merge pull request #33 from taskcluster/spring-clean
    Spring cleaning

--- libraries/validate/test/invalid_test.js
+++ libraries/validate/test/invalid_test.js
@@ -3,17 +3,15 @@ suite('Invalid Schema Tests', () => {
   let validator = require('../');
   let debug = require('debug')('test');
 
-  test('invalid schema throws error', async (done) => {
+  test('invalid schema throws error', async () => {
     try {
       let validate = await validator({
         folder: 'test/invalid-schemas',
         baseUrl: 'http://localhost:1203/',
       });
-      assert(false, 'Bad schema should\'ve thrown an exception!');
+      return assert(false, 'Bad schema should\'ve thrown an exception!');
     } catch (e) {
       debug('Bad schema has thrown an exception correctly.');
-    } finally {
-      done();
     }
   });
 
