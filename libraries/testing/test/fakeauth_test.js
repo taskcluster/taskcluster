7f4e2e08a472f5fb9047a525981df5a320f75a99
Dustin J. Mitchell <dustin@v.igoro.us>
2017-11-03 22:28:29 -0400

    Merge pull request #23 from djmitche/spring-cleaning-fall-2017
    Spring cleaning, fall 2017

--- libraries/testing/test/fakeauth_test.js
+++ libraries/testing/test/fakeauth_test.js
@@ -1,7 +1,7 @@
 var _            = require('lodash');
 var nock         = require('nock');
 var hawk         = require('hawk');
-var request      = require('superagent-promise');
+var request      = require('superagent');
 var validator    = require('taskcluster-lib-validate');
 var API          = require('taskcluster-lib-api');
 var App          = require('taskcluster-lib-app');
@@ -11,8 +11,8 @@ var Promise      = require('promise');
 var path         = require('path');
 
 var testApi = new API({
-  title:        "Test Server",
-  description:  "for testing"
+  title:        'Test Server',
+  description:  'for testing',
 });
 
 testApi.declare({
@@ -21,23 +21,22 @@ testApi.declare({
   name:         'test',
   scopes:       [[]],
   deferAuth:    true,
-  title:        "Test function",
-  description:  "for testing"
+  title:        'Test function',
+  description:  'for testing',
 }, function(req, res) {
   return res.status(200).json({
-    hasTestScope: req.satisfies([['test.scope']], true)
+    hasTestScope: req.satisfies([['test.scope']], true),
   });
 });
 
-
 suite('fakeauth', function() {
-  var fakeauth = require('../../lib/fakeauth');
+  var fakeauth = require('../lib/fakeauth');
   var server;
 
   suiteSetup(function() {
     return validator({
       folder:  path.join(__dirname, 'schemas'),
-      baseUrl: "http://localhost:1234",
+      baseUrl: 'http://localhost:1234',
     }).then(function(validator) {
       // Create application
       var app = App({
@@ -82,10 +81,10 @@ suite('fakeauth', function() {
         id:         clientId,
         key:        'unused',
         algorithm:  'sha256',
-      }
+      },
     };
     if (extContent) {
-      content['ext'] = new Buffer(JSON.stringify(extContent)).toString('base64')
+      content['ext'] = new Buffer(JSON.stringify(extContent)).toString('base64');
     }
 
     var header = hawk.client.header(reqUrl, 'GET', content);
@@ -96,56 +95,55 @@ suite('fakeauth', function() {
       request
         .get(reqUrl)
         .set('Authorization', header.field)
-        .end().then(function(res) {
+        .then(function(res) {
           console.log(res.body);
           return res;
         }),
       request
         .get(bewitUrl)
-        .end().then(function(res) {
+        .then(function(res) {
           console.log(res.body);
           return res;
-        })
+        }),
     ]);
   };
 
   test('using a rawClientId', function() {
-    fakeauth.start({'client1': ['test.scope']});
+    fakeauth.start({client1: ['test.scope']});
     return callApi('client1').then(function(responses) {
       for (var res of responses) {
-        assert(res.ok && res.body.hasTestScope, "Request failed");
+        assert(res.ok && res.body.hasTestScope, 'Request failed');
       }
     });
   });
 
   test('using authorizedScopes', function() {
-    fakeauth.start({'client1': ['some.other.scope']});
+    fakeauth.start({client1: ['some.other.scope']});
     return callApi('client1', {
-        authorizedScopes: ['test.scope'],
-      }).then(function(responses) {
+      authorizedScopes: ['test.scope'],
+    }).then(function(responses) {
       for (var res of responses) {
-        assert(res.ok && res.body.hasTestScope, "Request failed");
+        assert(res.ok && res.body.hasTestScope, 'Request failed');
       }
     });
   });
 
   test('using temp creds', function() {
-    fakeauth.start({'client1': ['some.other.scope']});
+    fakeauth.start({client1: ['some.other.scope']});
     var tempCreds = taskcluster.createTemporaryCredentials({
       scopes: ['test.scope'],
-      expiry: taskcluster.fromNow("1d"),
+      expiry: taskcluster.fromNow('1d'),
       credentials: {
         clientId: 'client1',
-        accessToken: 'unused'
-      }
+        accessToken: 'unused',
+      },
     });
     return callApi('client1', {
-        certificate: JSON.parse(tempCreds.certificate)
-      }).then(function(responses) {
+      certificate: JSON.parse(tempCreds.certificate),
+    }).then(function(responses) {
       for (var res of responses) {
-        assert(res.ok && res.body.hasTestScope, "Request failed");
+        assert(res.ok && res.body.hasTestScope, 'Request failed');
       }
     });
   });
 });
-
