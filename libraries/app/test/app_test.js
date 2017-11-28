f542968f75ebeb5ab3ad0e2878f79ea635feaafa
Dustin J. Mitchell <dustin@v.igoro.us>
2017-11-28 11:59:25 -0500

    Merge pull request #17 from djmitche/bug1412005
    Bug 1412005: add robots.txt and sitemap.xml

--- libraries/app/test/app_test.js
+++ libraries/app/test/app_test.js
@@ -5,29 +5,50 @@ suite('app', function() {
   var request = require('superagent');
 
   // Test app creation
-  test('app({port: 1459})', async function() {
-    // Create a simple app
-    var app = subject({
-      port:       1459,
-      env:        'development',
-      forceSSL:   false,
-      forceHSTS:  true,
-      trustProxy: false,
-    });
-    assert(app, 'Should have an app');
+  suite('app({port: 1459})', function() {
+    var server;
 
-    // Add an end-point
-    app.get('/test', function(req, res) {
-      res.status(200).send('Okay this works');
+    suiteSetup(async function() {
+      // Create a simple app
+      var app = subject({
+        port:       1459,
+        env:        'development',
+        forceSSL:   false,
+        forceHSTS:  true,
+        trustProxy: false,
+      });
+      assert(app, 'Should have an app');
+
+      // Add an end-point
+      app.get('/test', function(req, res) {
+        res.status(200).send('Okay this works');
+      });
+
+      // Create server
+      server = await app.createServer();
     });
 
-    // Create server
-    var server = await app.createServer();
-    var res = await request.get('http://localhost:1459/test');
-    assert(res.ok, 'Got response');
-    assert.equal(res.text, 'Okay this works', 'Got the right text');
-    assert.equal(res.headers['strict-transport-security'], 'max-age=7776000000; includeSubDomains');
-    return server.terminate();
+    test('get /test', async function() {
+      var res = await request.get('http://localhost:1459/test');
+      assert(res.ok, 'Got response');
+      assert.equal(res.text, 'Okay this works', 'Got the right text');
+    });
+
+    test('hsts header', async function() {
+      var res = await request.get('http://localhost:1459/test');
+      assert.equal(res.headers['strict-transport-security'], 'max-age=7776000000; includeSubDomains');
+    });
+
+    test('/robots.txt', async function() {
+      var res = await request.get('http://localhost:1459/robots.txt');
+      assert(res.ok, 'Got response');
+      assert.equal(res.text, 'User-Agent: *\nDisallow: /\n', 'Got the right text');
+      assert.equal(res.headers['content-type'], 'text/plain; charset=utf-8');
+    });
+
+    suiteTeardown(function() {
+      return server.terminate();
+    });
   });
 });
 
