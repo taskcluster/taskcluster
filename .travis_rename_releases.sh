b1ed2c37ec1178f8ef716673b6688e6a06e34216
Pete Moore <pmoore@mozilla.com>
2017-11-30 20:42:58 +0100

    fix to release script

--- .travis_rename_releases.sh
+++ .travis_rename_releases.sh
@@ -5,7 +5,7 @@
 # set an environment variable for its location that we can use in .travis.yml for
 # publishing back to github.
 
-# all cross-compiled binaries are in subdirectories: ${GOPATH}/bin/${GOOS}_${GOARCH}/
+# all cross-compiled binaries are in subdirectories: ${GOPATH}/bin/${GIMME_OS}_${GIMME_ARCH}/
 # linux 64 bit, not cross-compiled, breaks this rule and is in ${GOPATH}/bin
 # therefore move it to match the convention of the others, to simplify subsequent steps
 # note: we don't know what we built, so only move it if we happen to be linux amd64 travis job
@@ -16,10 +16,10 @@ fi
 
 # linux, darwin:
 FILE_EXT=""
-[ "${GOOS}" == "windows" ] && FILE_EXT=".exe"
+[ "${GIMME_OS}" == "windows" ] && FILE_EXT=".exe"
 
 # let's rename the release file because it has a 1:1 mapping with what it is called on
 # github releases, and therefore the name for each platform needs to be unique so that
 # they don't overwrite each other. Set a variable that can be used in .travis.yml
-export RELEASE_FILE="${TRAVIS_BUILD_DIR}/generic-worker-${GOOS}-${GOARCH}${FILE_EXT}"
-mv "${GOPATH}/bin/${GOOS}_${GOARCH}/generic-worker${FILE_EXT}" "${RELEASE_FILE}"
+export RELEASE_FILE="${TRAVIS_BUILD_DIR}/generic-worker-${GIMME_OS}-${GIMME_ARCH}${FILE_EXT}"
+mv "${GOPATH}/bin/${GIMME_OS}_${GIMME_ARCH}/generic-worker${FILE_EXT}" "${RELEASE_FILE}"
