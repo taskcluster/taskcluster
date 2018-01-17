d011b28fed492e320e1251001611a76bceffe49f
Gregory Szorc <gps@mozilla.com>
2018-01-16 18:27:27 -0800

    Initialize instance storage in cloud-init
    Before, instance storage was initialized as part of the
    /etc/default/docker script, which is sourced as part of
    /etc/init/docker, which is run as part of starting the docker
    service. This was a really wonky place to put storage
    initialization!
    
    We create a new per-boot cloud-init script containing the logic
    for initializing instance storage. The old docker init
    script simply does a sanity check for the presence of an expected
    directory and aborts if cloud-init didn't do its job.
    
    There is a potential regression here.
    
    Because the old instance storage code ran as part of the docker
    init routine and the docker service would retry if it failed,
    we could run the instance storage code multiple times. Before,
    an initially failing run could recover in a subsequent run.
    Now, we will never recover because instance storage code only
    runs once. This could lead to "zombie workers."
    
    On one hand, I understand the concern. On the other, it looks like
    the previous code was more paranoid than necessary with regards
    to error handling. I could only see there being an issue if there
    is a race condition preventing the sequence of vgcreate + lvcreate
    + mkfs.ext4 from working. I looked through Ansible's code for
    lvm and filesystem management and I don't see any extra checks
    after running these commands to ensure they actually worked.
    I think the exit code should be all that we need to care about.
    We execute the script with `set -e`, so we should be covered. One
    thing we could consider doing is shutting down the instance
    on failure so it doesn't become a zombie. Let's see if this
    actually fails first before adding more code.

---
+++ workers/docker-worker/deploy/template/var/lib/cloud/scripts/per-boot/instance-storage.sh
@@ -0,0 +1,84 @@
+#!/bin/bash -ve
+
+# The goal of this script is to initialize instance storage.
+# We create an LVM logical volume from all available storage devices,
+# format it, mount it, and ensure basic structure is in place.
+
+# Create logical volume
+# Do not attempt to create if volume already exists (upstart respawn).
+if ! lvdisplay | grep instance_storage; then
+    echo "Creating logical volume 'instance_storage'"
+    # Find instance storage devices
+    # c5 and newer has nvme* devices. The nvmeN devices can't be used
+    # with vgcreate. But nvmeNnN can.
+    if [ -e /dev/nvme0 ]; then
+        devices=$(ls /dev/nvme*n* | grep -v '/dev/nvme0')
+    else
+        devices=$(ls /dev/xvd* | grep -v '/dev/xvda')
+    fi
+
+    if [ -z "${devices}" ]; then
+        echo "could not find devices to use for instance storage"
+        exit 1
+    fi
+
+    echo "Found devices: $devices"
+
+    # Unmount block-device if already mounted, the first block-device always is
+    for d in $devices; do umount $d || true; done
+
+    # Create volume group containing all instance storage devices
+    echo $devices | xargs vgcreate instance_storage
+
+    # Create logical volume with all storage
+    lvcreate -l 100%VG -n all instance_storage
+else
+    echo "Logical volume 'instance_storage' already exists"
+fi
+
+# Check to see if instance_storage-all is mounted already
+if ! df -T /dev/mapper/instance_storage-all | grep 'ext4'; then
+    # Format logical volume with ext4
+    echo "Logical volume does not appear mounted."
+    echo "Formating 'instance_storage' as ext4"
+
+    if ! mkfs.ext4 /dev/instance_storage/all; then
+        echo "Could not format 'instance_storage' as ext4."
+        exit 1
+    else
+        echo "Succesfully formated 'instance_storage' as ext4."
+        echo "Mounting logical volume"
+
+        # Our assumption is that workers are ephemeral. If errors are encountered, the
+        # worker should be thrown away. Workers are never rebooted. So filesystem
+        # durability isn't too important to us.
+
+        # Default mount options: rw,relatime,errors=remount-ro,data=ordered
+        #
+        # We make the following changes:
+        #
+        # errors=panic -- The worker is unusable if the mount isn't writable. So
+        # panic if we encounter this.
+        #
+        # data=writeback -- Don't require write ordering between journal and main
+        # filesystem. Since we don't have a separate journal device, this probably
+        # does little. But in theory it relaxes durability so it shouldn't hurt.
+        #
+        # nobarrier -- Loosen restrictions around writes to journal.
+        #
+        # commit=60 -- By default, ext4 tries to sync every 5s to ensure
+        # minimal data loss in case of system failure. We increase that to 60s to
+        # avoid excessive filesystem sync. The filesystem will still write out
+        # changes in the background. And a `sync()` issued by an application can
+        # still force a full flush sooner. But ext4 itself won't be flushing all
+        # changes as often.
+        mount -o 'rw,relatime,errors=panic,data=writeback,nobarrier,commit=60' /dev/instance_storage/all /mnt
+    fi
+else
+    echo "Logical volume 'instance_storage' is already mounted."
+fi
+
+echo "Creating docker specific directories"
+mkdir -p /mnt/var/lib/docker
+mkdir -p /mnt/docker-tmp
+mkdir -p /mnt/var/cache/docker-worker
