7f642cd73ae428bd50539092290d169ca411fd3a
Dustin J. Mitchell <dustin@v.igoro.us>
2018-01-18 09:48:30 -0500

    Merge pull request #240 from taskcluster/spring-cleaning-fall-2017
    Spring cleaning fall 2017

--- services/queue/test/minimumtask_test.js
+++ services/queue/test/minimumtask_test.js
@@ -3,7 +3,6 @@ suite('Create task (w. defaults)', () => {
   var assert      = require('assert');
   var slugid      = require('slugid');
   var _           = require('lodash');
-  var Promise     = require('promise');
   var taskcluster = require('taskcluster-client');
   var assume      = require('assume');
   var helper      = require('./helper');
