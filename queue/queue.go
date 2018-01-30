69bfa5415def8593d677b3b20fe8f94b814d7697
Pete Moore <pmoore@mozilla.com>
2018-01-30 12:22:49 +0100

    Regenerated library to pick up latest schema changes

--- queue/queue.go
+++ queue/queue.go
@@ -38,7 +38,7 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/queue/v1/api.json together with the input and output schemas it references, downloaded on
-// Tue, 30 Jan 2018 at 02:22:00 UTC. The code was generated
+// Tue, 30 Jan 2018 at 11:21:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package queue
 
