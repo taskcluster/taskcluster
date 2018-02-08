f119faf3a6a199df3c6240e833eb4c3739b90429
Brian Stack <bstack@mozilla.com>
2018-02-08 09:46:08 -0800

    Merge pull request #4 from taskcluster/update-rules
    Update rules to get object spread

--- libraries/eslint-config/index.js
+++ libraries/eslint-config/index.js
@@ -7,6 +7,9 @@ module.exports = {
   "parserOptions": {
     "ecmaVersion": 8,
     "sourceType": "module",
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+    }
   },
 
   "rules": {
