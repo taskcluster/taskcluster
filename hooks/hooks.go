d90c9fc35dc673bd39b4574be3df4e0701fb953e
Pete Moore <pmoore@mozilla.com>
2018-02-13 17:23:42 +0100

    Regenerated library to pick up latest schema changes

--- hooks/hooks.go
+++ hooks/hooks.go
@@ -48,7 +48,7 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/hooks/v1/api.json together with the input and output schemas it references, downloaded on
-// Mon, 12 Feb 2018 at 16:22:00 UTC. The code was generated
+// Tue, 13 Feb 2018 at 16:22:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package hooks
 
