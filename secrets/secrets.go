36db7fbe679b05108392edae271408f5653eff43
Pete Moore <pmoore@mozilla.com>
2018-02-17 23:22:44 +0100

    Regenerated library to pick up latest schema changes

--- secrets/secrets.go
+++ secrets/secrets.go
@@ -36,7 +36,7 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/secrets/v1/api.json together with the input and output schemas it references, downloaded on
-// Sat, 17 Feb 2018 at 21:22:00 UTC. The code was generated
+// Sat, 17 Feb 2018 at 22:21:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package secrets
 
