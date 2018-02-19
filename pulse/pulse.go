aa9f43f926ff5c20156bfb330af4c9a0e985cd68
Pete Moore <pmoore@mozilla.com>
2018-02-20 00:22:48 +0100

    Regenerated library to pick up latest schema changes

--- pulse/pulse.go
+++ pulse/pulse.go
@@ -35,7 +35,7 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/pulse/v1/api.json together with the input and output schemas it references, downloaded on
-// Sat, 17 Feb 2018 at 22:21:00 UTC. The code was generated
+// Mon, 19 Feb 2018 at 23:22:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package pulse
 
