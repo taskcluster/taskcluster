59d0124b20e389b9afce2574a5b1a6a6c6a4e5c4
Pete Moore <pmoore@mozilla.com>
2018-02-24 02:22:34 +0100

    Regenerated library to pick up latest schema changes

--- github/github.go
+++ github/github.go
@@ -38,7 +38,7 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/github/v1/api.json together with the input and output schemas it references, downloaded on
-// Fri, 23 Feb 2018 at 02:22:00 UTC. The code was generated
+// Sat, 24 Feb 2018 at 01:22:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package github
 
