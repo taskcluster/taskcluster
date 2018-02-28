42e47a3554d7045aa7185f8d04c127c2b18882c8
Eli Perelman <eli@eliperelman.com>
2018-02-28 17:30:57 -0600

    Clients mutations

--- services/web-server/src/resolvers/Client.js
+++ services/web-server/src/resolvers/Client.js
@@ -7,4 +7,26 @@ export default {
       return loaders.client.load(clientId);
     },
   },
+  Mutation: {
+    resetAccessToken(parent, { clientId }, { clients }) {
+      return clients.auth.resetAccessToken(clientId);
+    },
+    createClient(parent, { clientId, client }, { clients }) {
+      return clients.auth.createClient(clientId, client);
+    },
+    updateClient(parent, { clientId, client }, { clients }) {
+      return clients.auth.updateClient(clientId, client);
+    },
+    enableClient(parent, { clientId }, { clients }) {
+      return clients.auth.enableClient(clientId);
+    },
+    disableClient(parent, { clientId }, { clients }) {
+      return clients.auth.disableClient(clientId);
+    },
+    async deleteClient(parent, { clientId }, { clients }) {
+      await clients.auth.deleteClient(clientId);
+
+      return clientId;
+    },
+  },
 };
