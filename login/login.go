2903691b972cbf55165136022f2e7d32030dd8f7
Pete Moore <pmoore@mozilla.com>
2018-03-02 00:22:52 +0100

    Regenerated library to pick up latest schema changes

--- login/login.go
+++ login/login.go
@@ -31,7 +31,7 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/login/v1/api.json together with the input and output schemas it references, downloaded on
-// Thu, 1 Mar 2018 at 15:21:00 UTC. The code was generated
+// Thu, 1 Mar 2018 at 23:22:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package login
 
