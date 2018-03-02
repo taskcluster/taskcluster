789641d0fea652a967af41de76cdaed37f55f108
Eli Perelman <eli@eliperelman.com>
2018-03-02 14:20:05 -0600

    Secrets mutations

--- services/web-server/src/resolvers/Secrets.js
+++ services/web-server/src/resolvers/Secrets.js
@@ -7,4 +7,21 @@ export default {
       return loaders.secret.load(name);
     },
   },
+  Mutation: {
+    async createSecret(parent, { name, secret }, { clients }) {
+      await clients.secrets.set(name, secret);
+
+      return secret;
+    },
+    async updateSecret(parent, { name, secret }, { clients }) {
+      await clients.secrets.set(name, secret);
+
+      return secret;
+    },
+    async deleteSecret(parent, { name }, { clients }) {
+      await clients.secrets.remove(name);
+
+      return name;
+    },
+  },
 };
