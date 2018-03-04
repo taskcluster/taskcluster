fa73dac40e34ffbe6ff2198396424c01dfddd1d8
Pete Moore <pmoore@mozilla.com>
2018-03-05 00:22:30 +0100

    Regenerated library to pick up latest schema changes

--- awsprovisioner/awsprovisioner.go
+++ awsprovisioner/awsprovisioner.go
@@ -56,7 +56,7 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/aws-provisioner/v1/api.json together with the input and output schemas it references, downloaded on
-// Sun, 4 Mar 2018 at 17:22:00 UTC. The code was generated
+// Sun, 4 Mar 2018 at 23:21:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package awsprovisioner
 
