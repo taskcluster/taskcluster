e44a3f2e3db2671fe9b48177a12ba2791da2b9ed
Pete Moore <pmoore@mozilla.com>
2018-03-06 15:48:43 +0100

    Regenerated library to pick up latest schema changes

--- purgecache/purgecache.go
+++ purgecache/purgecache.go
@@ -35,7 +35,7 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/purge-cache/v1/api.json together with the input and output schemas it references, downloaded on
-// Mon, 5 Mar 2018 at 23:22:00 UTC. The code was generated
+// Tue, 6 Mar 2018 at 14:48:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package purgecache
 
