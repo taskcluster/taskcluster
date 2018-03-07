9836d949b1c823fd4f5659c5eb25648195ab0a4e
Dustin J. Mitchell <dustin@v.igoro.us>
2018-03-07 14:47:22 -0500

    Merge pull request #86 from fiennyangeln/upgrade-api
    Upgrade the taskcluster-lib-api

--- services/hooks/src/v1.js
+++ services/hooks/src/v1.js
@@ -30,6 +30,7 @@ var api = new API({
     'https://docs.taskcluster.net/reference/core/taskcluster-hooks/docs/firing-hooks',
     'for more information.',
   ].join('\n'),
+  name: 'hooks',
   schemaPrefix:  'http://schemas.taskcluster.net/hooks/v1/',
 });
 
