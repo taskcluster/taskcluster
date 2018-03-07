df0225d6742e25abf54d060ad413f963e72f5a58
Dustin J. Mitchell <dustin@mozilla.com>
2018-03-07 21:30:50 +0000

    run schedule_hooks test all the time
    This does not actually require credentials anymore.  At least, it works
    for me :)

--- services/hooks/test/schedule_hooks_test.js
+++ services/hooks/test/schedule_hooks_test.js
@@ -3,11 +3,6 @@ suite('bin/schedule-hooks.js', function() {
   var Scheduler         = require('../src/scheduler');
   var helper            = require('./helper');
 
-  // these tests require Azure credentials (for the Hooks table)
-  if (!helper.haveRealCredentials) {
-    this.pending = true;
-  }
-
   test('schedule_hooks launches a scheduler', async () => {
     var scheduler = await helper.load('schedulerNoStart', helper.loadOptions);
     assert(scheduler instanceof Scheduler);
