16b1bb14b7b3c2fadb64d6ec5193004aa0e05974
Brian Stack <bstack@mozilla.com>
2018-03-12 14:43:22 -0700

    Merge pull request #87 from taskcluster/bug-1428417
    Bug 1428417

--- libraries/client/src/client.js
+++ libraries/client/src/client.js
@@ -57,6 +57,9 @@ var _defaultOptions = {
   randomizationFactor: 0.25,
   // Maximum retry delay (defaults to 30 seconds)
   maxDelay:       30 * 1000,
+
+  // The prefix of any api calls. e.g. https://taskcluster.net/api/
+  rootUrl: process.env.TASKCLUSTER_ROOT,
 };
 
 /** Make a request for a Client instance */
@@ -125,13 +128,16 @@ var makeRequest = function(client, method, url, payload, query) {
  *   // Limit the set of scopes requests with this client may make.
  *   // Note, that your clientId must have a superset of the these scopes.
  *   authorizedScopes:  ['scope1', 'scope2', ...]
- *   baseUrl:         'http://.../v1'   // baseUrl for API requests
- *   exchangePrefix:  'queue/v1/'       // exchangePrefix prefix
- *   retries:         5,                // Maximum number of retries
- *   monitor:         await Monitor()   // From taskcluster-lib-monitor
+ *   baseUrl:         'http://.../v1'                // baseUrl for API requests
+ *   exchangePrefix:  'queue/v1/'                    // exchangePrefix prefix
+ *   retries:         5,                             // Maximum number of retries
+ *   monitor:         await Monitor()                // From taskcluster-lib-monitor
+ *   rootUrl:         'https://taskcluster.net/api/' // prefix for all api calls
  * }
  *
  * `baseUrl` and `exchangePrefix` defaults to values from reference.
+ *
+ * `rootUrl` and `baseUrl` are mutually exclusive.
  */
 exports.createClient = function(reference, name) {
   if (!name || typeof name !== 'string') {
@@ -140,6 +146,9 @@ exports.createClient = function(reference, name) {
 
   // Client class constructor
   var Client = function(options) {
+    if (options && options.baseUrl && options.rootUrl) {
+      throw new Error('baseUrl and rootUrl are mutually exlcusive. Prefer rootUrl.');
+    }
     this._options = _.defaults({}, options || {}, {
       baseUrl:          reference.baseUrl        || '',
       exchangePrefix:   reference.exchangePrefix || '',
@@ -147,6 +156,12 @@ exports.createClient = function(reference, name) {
 
     // Remove possible trailing slash from baseUrl
     this._options.baseUrl = this._options.baseUrl.replace(/\/$/, '');
+
+    if (this._options.rootUrl) {
+      // We can remove the second half of this assignment once all api definitions are upgraded to have `name`
+      const urlPrefix = reference.name || reference.baseUrl.split('//')[1].split('.')[0];
+      this._options.baseUrl = `${this._options.rootUrl}/${urlPrefix}/v${reference.version + 1}`;
+    }
 
     if (this._options.stats) {
       throw new Error('options.stats is now deprecated! Use options.monitor instead.');
