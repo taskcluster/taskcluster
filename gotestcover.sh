99ce317385d046576ed269ce01120e01893740e1
Pete Moore <pmoore@mozilla.com>
2018-03-15 14:19:47 +0100

    gotestcover.sh fix

--- gotestcover.sh
+++ gotestcover.sh
@@ -14,6 +14,7 @@ HEAD_REV="$(git rev-parse HEAD)"
 PACKAGE_LIST="$(mktemp -t package-list.tmp.XXXXXXXXXX)"
 go list ./... > "${PACKAGE_LIST}"
 
+while read package
 do
   CGO_ENABLED=1 go test -ldflags "-X github.com/taskcluster/generic-worker.revision=${HEAD_REV}" -race -timeout 1h -covermode=atomic "-coverprofile=${TEMP_SINGLE_REPORT}" "${package}"
   if [ -f "${TEMP_SINGLE_REPORT}" ]; then
