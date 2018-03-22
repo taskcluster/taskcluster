febe84969f4b84c2d4b28a35b3db6cb675fc28bb
Pete Moore <pmoore@mozilla.com>
2018-03-22 23:36:49 +0100

    Version bump from 10.7.5 to 10.7.6

--- main.go
+++ main.go
@@ -53,7 +53,7 @@ var (
 	logName = "public/logs/live_backing.log"
 	logPath = filepath.Join("generic-worker", "live_backing.log")
 
-	version  = "10.7.5"
+	version  = "10.7.6"
 	revision = "" // this is set during build with `-ldflags "-X main.revision=$(git rev-parse HEAD)"`
 	usage    = `
 generic-worker
