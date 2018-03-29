88e982d9cd0fe485abb024c4bda02eb299dc9ce9
fiennyangeln <fifi.angelina@yahoo.com>
2018-03-29 23:58:39 +0800

    Fail if same route and method is declared twice
    Done by modifying the declare method in API, add test, and fix
    test that uses same method and route declaration
    
    Bug 1449697

--- libraries/api/test/route_test.js
+++ libraries/api/test/route_test.js
@@ -207,4 +207,25 @@ suite('api/route', function() {
       }
     });
   });
+
+  test('no duplicate route and method', function() {
+    api.declare({
+      method:       'get',
+      route:        '/test',
+      name:         'test',
+      title:        'Test',
+      description:  'Test',
+    }, function(req, res) {});
+
+    assert.throws(function() {
+      api.declare({
+        method:       'get',
+        route:        '/test',
+        name:         'testDuplicate',
+        title:        'Test',
+        description:  'Test',
+      }, function(req, res) {});
+    }, /Identical route and method/);
+  });
+
 });
