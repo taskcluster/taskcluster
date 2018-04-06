87d3123d285df3a414b2497d8d30592a7466700d
Brian Stack <bstack@mozilla.com>
2018-04-05 21:22:55 -0700

    Add lib-docs to requirements

--- infrastructure/builder/src/build/other.js
+++ infrastructure/builder/src/build/other.js
@@ -8,7 +8,6 @@ const git = require('simple-git/promise');
 const doT = require('dot');
 const {quote} = require('shell-quote');
 const yaml = require('js-yaml');
-const libDocs = require('taskcluster-lib-docs');
 const {gitClone} = require('./utils');
 
 const generateOtherTasks = ({tasks, baseDir, spec, cfg, name, cmdOptions}) => {
