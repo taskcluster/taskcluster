b4d6480581e8350594952cc65463b1c150490dc9
Dustin J. Mitchell <dustin@v.igoro.us>
2018-04-13 16:12:29 -0400

    Merge pull request #68 from taskcluster/bug1452000
    Bug 1452000 - add an 'enable' option

--- libraries/monitor/test/sentry_test.js
+++ libraries/monitor/test/sentry_test.js
@@ -5,55 +5,89 @@ suite('Sentry', () => {
   let debug = require('debug')('test');
   let nock = require('nock');
   let authmock = require('./authmock');
+  let capcon = require('capture-console');
 
   let monitor = null;
 
-  suiteSetup(async () => {
-    authmock.setup();
+  suite('enabled', function() {
+    suiteSetup(async () => {
+      authmock.setup();
 
-    monitor = await monitoring({
-      project: 'tc-lib-monitor',
-      credentials: {clientId: 'test-client', accessToken: 'test'},
-      patchGlobal: false,
+      monitor = await monitoring({
+        project: 'tc-lib-monitor',
+        credentials: {clientId: 'test-client', accessToken: 'test'},
+        patchGlobal: false,
+      });
+    });
+
+    suiteTeardown(() => {
+      authmock.teardown();
+    });
+
+    test('should log to sentry', function(done) {
+
+      let sentryScope = nock('https://app.getsentry.com')
+        .filteringRequestBody(/.*/, '*')
+        .post('/api/12345/store/', '*')
+        .twice()
+        .reply(200, () => {
+          debug('called Sentry.');
+        })
+        .post('/api/12345/store/', '*')
+        .reply(200, () => {
+          debug('called Sentry the correct amount of times.');
+          done();
+        });
+
+      Promise.all([
+        monitor.reportError('create sentry error test'),
+        monitor.reportError('another time'),
+        monitor.captureError('this is the same as reportError'),
+      ]).then(results => assert.deepEqual(results, [true, true, true]));
+    });
+
+    test('should handle sentry error', async function() {
+      let stdout = '';
+      capcon.startIntercept(process.stdout, data => { stdout += data; });
+
+      let sentryScope = nock('https://app.getsentry.com')
+        .filteringRequestBody(/.*/, '*')
+        .post('/api/12345/store/', '*')
+        .reply(500, () => {
+          debug('called Sentry, returned 500');
+        });
+      try {
+        assert.equal(false, await monitor.reportError('stranger things'));
+      } finally {
+        capcon.stopIntercept(process.stdout);
+      }
+
+      assert(/failed to send exception to sentry/.test(stdout));
+      assert(/Failed to log error to Sentry:/.test(stdout));
     });
   });
 
-  suiteTeardown(() => {
-    authmock.teardown();
-  });
-
-  test('should log to sentry', function(done) {
-
-    let sentryScope = nock('https://app.getsentry.com')
-      .filteringRequestBody(/.*/, '*')
-      .post('/api/12345/store/', '*')
-      .twice()
-      .reply(200, () => {
-        debug('called Sentry.');
-      })
-      .post('/api/12345/store/', '*')
-      .reply(200, () => {
-        debug('called Sentry the correct amount of times.');
-        done();
+  suite('not enabled', function() {
+    suiteSetup(async () => {
+      monitor = await monitoring({
+        project: 'tc-lib-monitor',
+        enable: false,
       });
+    });
 
-    Promise.all([
-      monitor.reportError('create sentry error test'),
-      monitor.reportError('another time'),
-      monitor.captureError('this is the same as reportError'),
-    ]).then(results => assert.deepEqual(results, [true, true, true]));
+    test('should log to stdout', async function() {
+      let stdout = '';
+      capcon.startIntercept(process.stdout, data => { stdout += data; });
+
+      try {
+        await monitor.reportError(new Error('uhoh'));
+      } finally {
+        capcon.stopIntercept(process.stdout);
+      }
+
+      assert(/reportError/.test(stdout));
+      assert(/Error: uhoh/m.test(stdout));
+      assert(/sentry_test\.js/.test(stdout)); // check for stack trace
+    });
   });
-
-  test('should handle sentry error', async function() {
-
-    let sentryScope = nock('https://app.getsentry.com')
-      .filteringRequestBody(/.*/, '*')
-      .post('/api/12345/store/', '*')
-      .reply(500, () => {
-        debug('called Sentry, returned 500');
-      });
-
-    assert.equal(false, await monitor.reportError('stranger things'));
-  });
-
 });
