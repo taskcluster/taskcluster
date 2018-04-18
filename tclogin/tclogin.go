1ef93af307045f85faae721e5ccba0a06db1b3e8
Pete Moore <pmoore@mozilla.com>
2018-04-18 17:22:43 +0200

    Regenerated library to pick up latest schema changes

--- tclogin/tclogin.go
+++ tclogin/tclogin.go
@@ -33,7 +33,7 @@
 //
 // The source code of this go package was auto-generated from the API definition at
 // http://references.taskcluster.net/login/v1/api.json together with the input and output schemas it references, downloaded on
-// Wed, 18 Apr 2018 at 15:18:00 UTC. The code was generated
+// Wed, 18 Apr 2018 at 15:21:00 UTC. The code was generated
 // by https://github.com/taskcluster/taskcluster-client-go/blob/master/build.sh.
 package tclogin
 
