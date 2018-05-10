5fe52078cb32ab411eb9f80501ba88aaef26f839
John Ford <john@johnford.org>
2018-05-10 19:17:37 +0200

    Fix pip api breakage

--- devDep.py
+++ devDep.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python
 import setup
 import pip
-pip.main(['install', '--upgrade'] + setup.tests_require + setup.install_requires)
+
+def x():
+    print("No Pip Main found")
+    exit(1)
+pipMain = x
+
+try:
+    pipMain = pip.main
+except:
+    pass
+
+try:
+    pipMajor = int(pip.__version__.split('.')[0])
+    if pipMajor >= 10:
+        import pip._internal
+        pipMain = pip._internal.main
+except:
+    pass
+
+pipMain(['install', '--upgrade'] + setup.tests_require + setup.install_requires)
