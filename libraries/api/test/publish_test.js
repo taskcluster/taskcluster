5326d18240fc7e0cc917c3c667a158284fd1f46f
Dustin J. Mitchell <dustin@v.igoro.us>
2018-05-18 09:46:30 -0400

    Redeployability Great Library Update - 10.0 (#98)
    Updates to support redeployability
    
    * Bug 1460026 - require a `version` be specified in API constructor,
      and also remove some rather outdated and confusing documentation
      comments.
    * Bug 1460026 - add note that API.name = serviceName
    * Bug 1460026 - require rootUrl as api.setup(..) arg
    * Bug 1460026 - remove babel
    * Bug 1460026 - update tc-client
    * Bug 1460026 - use rootUrl to configure auth, prohibit authBaseUrl
    * Bug 1460026 - require node > 8
    * Bug 1460026 - tc-lib-urls .schema(..) no longer takes a version
    * Bug 1460026 - separate an "api" from a "service"
    
       APIs are declared, and the setup function returns a service object that
       is specialized to the given rootUrl.
    
       This is a big refactor with some JS modernization along the way.  It
       also upgrades lib-api to be compatible with other concurrent library
       changes such as lib-validate requiring a rootUrl and serviceName.
    
    * Bug 1460026 - refactor into APIBuilder -> API
    * Bug 1460026 - documentation updates
    * Bug 1460026 - change api terminology to builder in api.js
    * handle the ping endpoint once
    * fix up require() calls
    * Bug 1460026 - fix tests to work with newest verisons of other libraries
    * upgrade taskcluster-client

--- libraries/api/test/publish_test.js
+++ libraries/api/test/publish_test.js
@@ -1,10 +1,10 @@
-suite('api/publish', function() {
-  var subject         = require('../');
-  var config          = require('typed-env-config');
-  var aws             = require('aws-sdk');
-  var assert          = require('assert');
-  var Promise         = require('promise');
+const APIBuilder      = require('../');
+const config          = require('typed-env-config');
+const aws             = require('aws-sdk');
+const assert          = require('assert');
+const libUrls         = require('taskcluster-lib-urls');
 
+suite('api/publish', function() {
   var cfg = config({});
 
   if (!cfg.aws || !cfg.referenceTestBucket) {
@@ -14,112 +14,113 @@ suite('api/publish', function() {
   }
 
   // Test simple method
-  test('publish minimal reference', function() {
+  test('publish minimal reference', async function() {
     // Create test api
-    var api = new subject({
+    var builder = new APIBuilder({
       title:        'Test Api',
       description:  'Another test api',
       name:         'test',
+      version:      'v1',
     });
 
     // Declare a simple method
-    api.declare({
+    builder.declare({
       method:       'get',
       route:        '/test0',
       name:         'test0',
       title:        'Test End-Point',
       description:  'Place we can call to test something',
-      stability:    subject.stability.stable,
+      stability:    APIBuilder.stability.stable,
     }, function(req, res) {
       res.send(200, 'Hello World');
     });
 
     // Declare some methods with some fun scopes
-    api.declare({
+    builder.declare({
       method:       'get',
       route:        '/test1',
       name:         'test1',
       title:        'Test End-Point',
       description:  'Place we can call to test something',
-      stability:    subject.stability.stable,
+      stability:    APIBuilder.stability.stable,
       scopes:       {AllOf: ['foo:bar']},
     }, function(req, res) {
       res.send(200, 'Hello World');
     });
-    api.declare({
+    builder.declare({
       method:       'get',
       route:        '/test2',
       name:         'test2',
       title:        'Test End-Point',
       description:  'Place we can call to test something',
-      stability:    subject.stability.stable,
+      stability:    APIBuilder.stability.stable,
       scopes:       {AnyOf: ['foo:bar']},
     }, function(req, res) {
       res.send(200, 'Hello World');
     });
-    api.declare({
+    builder.declare({
       method:       'get',
       route:        '/test3',
       name:         'test3',
       title:        'Test End-Point',
       description:  'Place we can call to test something',
-      stability:    subject.stability.stable,
+      stability:    APIBuilder.stability.stable,
       scopes:       {if: 'not_public', then: {AllOf: ['foo:bar']}},
     }, function(req, res) {
       res.send(200, 'Hello World');
     });
-    api.declare({
+    builder.declare({
       method:       'get',
       route:        '/test4',
       name:         'test4',
       title:        'Test End-Point',
       description:  'Place we can call to test something',
-      stability:    subject.stability.stable,
+      stability:    APIBuilder.stability.stable,
       scopes:       {AllOf: [{for: 'foo', in: 'whatever', each: 'bar:<foo>'}]},
     }, function(req, res) {
       res.send(200, 'Hello World');
     });
-    api.declare({
+    builder.declare({
       method:       'get',
       route:        '/test5',
       name:         'test5',
       title:        'Test End-Point',
       description:  'Place we can call to test something',
-      stability:    subject.stability.stable,
+      stability:    APIBuilder.stability.stable,
       scopes:       {AllOf: [{for: 'foo', in: 'whatever', each: 'bar:<foo>'}]},
     }, function(req, res) {
       res.send(200, 'Hello World');
     });
-    api.declare({
+    builder.declare({
       method:       'get',
       route:        '/test6',
       name:         'test6',
       title:        'Test End-Point',
       description:  'Place we can call to test something',
-      stability:    subject.stability.stable,
+      stability:    APIBuilder.stability.stable,
       scopes:       {if: 'not_public', then: {AllOf: ['abc', {AnyOf: ['e']}, {for: 'a', in: 'b', each: 'c'}]}},
     }, function(req, res) {
       res.send(200, 'Hello World');
     });
 
-    return api.publish({
-      baseUrl:              'http://localhost:23243/v1',
-      referencePrefix:      'base/test/simple-api.json',
+    const api = await builder.build({
+      rootUrl:              libUrls.testRootUrl(),
       referenceBucket:      cfg.referenceTestBucket,
       aws:                  cfg.aws,
-    }).then(function() {
-      // Get the file... we don't bother checking the contents this is good
-      // enough
-      var s3 = new aws.S3(cfg.aws);
-      return s3.getObject({
-        Bucket:     cfg.referenceTestBucket,
-        Key:        'base/test/simple-api.json',
-      }).promise();
-    }).then(function(res) {
-      var reference = JSON.parse(res.Body);
-      assert(reference.entries, 'Missing entries');
-      assert.equal(reference.entries.length, 8);
-      assert(reference.title, 'Missing title');
     });
+
+    await api.publish();
+
+    // Get the file... we don't bother checking the contents this is good
+    // enough
+    const s3 = new aws.S3(cfg.aws);
+    const res = await s3.getObject({
+      Bucket:     cfg.referenceTestBucket,
+      Key:        'test/v1/api.json',
+    });
+    const reference = JSON.parse(res.Body);
+    assert(reference.entries, 'Missing entries');
+    assert.equal(reference.entries.length, 8);
+    assert(reference.title, 'Missing title');
   });
 });
