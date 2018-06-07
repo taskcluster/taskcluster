e6f9bdf2f989a7dcf54f45bc91f8d9740205fc81
Pete Moore <pmoore@mozilla.com>
2018-06-07 21:30:52 +0200

    Added new test TestReclaimCancelledTask

--- taskstatus_test.go
+++ taskstatus_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"path/filepath"
 	"testing"
+	"time"
 )
 
 // Makes sure that if a running task gets cancelled externally, the worker does not shut down
@@ -9,4 +11,42 @@ func TestResolveResolvedTask(t *testing.T) {
 	defer setup(t)()
 	td, payload := cancelTask(t)
 	_ = submitAndAssert(t, td, payload, "exception", "canceled")
+}
+
+func TestReclaimCancelledTask(t *testing.T) {
+	defer setup(t)()
+	mounts := []MountEntry{
+		// requires scope "generic-worker:cache:banana-cache"
+		&WritableDirectoryCache{
+			CacheName: "banana-cache",
+			Directory: filepath.Join("my-task-caches", "bananas"),
+		},
+	}
+
+	td, payload := cancelTask(t)
+	td.Scopes = []string{"generic-worker:cache:banana-cache"}
+	payload.Command = append(payload.Command, sleep(30)...)
+	payload.Mounts = toMountArray(t, &mounts)
+	reclaimEvery5Seconds = true
+	start := time.Now()
+	taskID := submitAndAssert(t, td, payload, "exception", "canceled")
+	end := time.Now()
+	reclaimEvery5Seconds = false
+
+	expectedArtifacts := ExpectedArtifacts{
+		"public/logs/live_backing.log": {
+			Extracts: []string{
+				"Preserving cache: Moving",
+			},
+			ContentType:     "text/plain; charset=utf-8",
+			ContentEncoding: "gzip",
+			Expires:         td.Expires,
+		},
+	}
+
+	expectedArtifacts.Validate(t, taskID, 0)
+
+	if duration := end.Sub(start); duration.Seconds() > 20 {
+		t.Fatalf("Task should have expired in around five seconds, but took %v", duration)
+	}
 }
