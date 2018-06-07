380ce380fe2ab3635f0fdf01c18f3890f1197146
Rok Garbas <rok@garbas.si>
2018-06-07 16:39:25 +0200

    relengapi-proxy: 2.0.1 -> 2.2.0

--- workers/docker-worker/deploy/packer/app/scripts/deploy.sh
+++ workers/docker-worker/deploy/packer/app/scripts/deploy.sh
@@ -44,7 +44,7 @@ sudo depmod
 docker pull taskcluster/taskcluster-proxy:4.0.1
 docker pull taskcluster/livelog:v4
 docker pull taskcluster/dind-service:v4.0
-docker pull taskcluster/relengapi-proxy:2.0.1
+docker pull taskcluster/relengapi-proxy:2.2.0
 
 # Reboot the machine on OOM
 # Ref: http://www.oracle.com/technetwork/articles/servers-storage-dev/oom-killer-1911807.html
@@ -52,7 +52,7 @@ sudo sh -c 'echo "vm.panic_on_oom=1" >> /etc/sysctl.conf'
 sudo sh -c 'echo "kernel.panic=1" >> /etc/sysctl.conf'
 
 # Export the images as a tarball to load when insances are initialized
-docker save taskcluster/taskcluster-proxy:4.0.1 taskcluster/livelog:v4 taskcluster/dind-service:v4.0 taskcluster/relengapi-proxy:2.0.1 > /home/ubuntu/docker_worker/docker_worker_images.tar
+docker save taskcluster/taskcluster-proxy:4.0.1 taskcluster/livelog:v4 taskcluster/dind-service:v4.0 taskcluster/relengapi-proxy:2.2.0 > /home/ubuntu/docker_worker/docker_worker_images.tar
 
 # Blow away local docker state because it is never used. On actual workers
 # per-instance storage is initialized and Docker state goes there.
