c55e989fec6eea143da3011f0bc45c10176ca81c
Dustin J. Mitchell <dustin@v.igoro.us>
2018-06-12 19:45:27 -0400

    Merge pull request #2 from djmitche/bug1467210
    Bug 1467210 - Serve a `manifest.json` file (in a new format)

--- libraries/references/src/store.js
+++ libraries/references/src/store.js
@@ -1,11 +1,37 @@
+const _ = require('lodash');
 const url = require('url');
 const path = require('path');
 const util = require('util');
 const rimraf = util.promisify(require('rimraf'));
 const mkdirp = util.promisify(require('mkdirp'));
 const fs = require('fs');
+const libUrls = require('taskcluster-lib-urls');
 
 const writeFile = util.promisify(fs.writeFile);
+
+const storeManifest = async (output, references) => {
+  const manifest = {services: []};
+  references.forEach(({serviceName, version, ...reference}) => {
+    let service = _.find(manifest.services, {serviceName});
+    if (!service) {
+      service = {serviceName};
+      manifest.services.push(service);
+    }
+
+    // distinguish references for exchanges and APIs by presence of exchangePrefix
+    if (reference.exchangePrefix) {
+      _.defaults(service, {pulse: []});
+      service.pulse.push({version, reference: libUrls.exchangeReference('', serviceName, version)});
+    } else {
+      _.defaults(service, {apis: []});
+      service.apis.push({version, reference: libUrls.apiReference('', serviceName, version)});
+    }
+  });
+
+  const dirname = path.join(output, 'references');
+  await mkdirp(dirname);
+  await writeFile(path.join(dirname, 'manifest.json'), JSON.stringify(manifest, null, 2));
+};
 
 const storeReferences = async (output, references) => {
   await Promise.all(references.map(async reference => {
@@ -35,6 +61,7 @@ const store = async ({references, schemas, output}) => {
 
   await Promise.all([
     storeReferences(output, references),
+    storeManifest(output, references),
     storeSchemas(output, schemas),
   ]);
 };
