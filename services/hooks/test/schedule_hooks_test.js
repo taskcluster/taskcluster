562a9dbcf187e8d7b0abc920127b6475474b1071
Dustin J. Mitchell <dustin@v.igoro.us>
2018-06-25 18:24:21 -0400

    Merge pull request #105 from djmitche/bug1462764
    Bug 1462764 - upgrade all libraries to rootUrl-savvy versions

--- services/hooks/test/schedule_hooks_test.js
+++ services/hooks/test/schedule_hooks_test.js
@@ -1,15 +1,20 @@
 const assert = require('assert');
 const Scheduler = require('../src/scheduler');
 const helper = require('./helper');
+const libUrls = require('taskcluster-lib-urls');
 
-suite('bin/schedule-hooks.js', function() {
-  setup(function() {
+suite('schedule_hooks_test.js', function() {
+  suiteSetup(async function() {
+    await helper.secrets.setup();
     helper.load.save();
+    helper.load.cfg('taskcluster.rootUrl', libUrls.testRootUrl());
   });
 
-  teardown(function() {
+  suiteTeardown(function() {
     helper.load.restore();
   });
+
+  helper.withHook(true, () => false); // always mock, since this is a simple setup
 
   test('schedule_hooks launches a scheduler', async () => {
     const scheduler = await helper.load('schedulerNoStart');
