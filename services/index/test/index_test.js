2ce36d47423d3e6c1b6d8bea176121a0c9ae8f80
Dustin J. Mitchell <dustin@v.igoro.us>
2018-06-25 12:53:30 -0400

    Merge pull request #52 from ydidwania/lib-upgrades
    Redeployability lib upgrades - Code Sprint

--- services/index/test/index_test.js
+++ services/index/test/index_test.js
@@ -1,30 +1,36 @@
-suite('Indexing', () => {
-  var assert      = require('assert');
-  var debug       = require('debug')('index:test:index_test');
-  var helper      = require('./helper');
-  var slugid      = require('slugid');
-  var _           = require('lodash');
-  var testing     = require('taskcluster-lib-testing');
-  var taskcluster = require('taskcluster-client');
-  var data = require('../src/data');
+const assert = require('assert');
+const debug = require('debug')('index:test:index_test');
+const helper = require('./helper');
+const slugid = require('slugid');
+const _ = require('lodash');
+const testing = require('taskcluster-lib-testing');
+const taskcluster = require('taskcluster-client');
+const data = require('../src/data');
 
-  var makeTask = function() {
+helper.secrets.mockSuite('index_test.js', ['taskcluster'], function(mock, skipping) {
+  helper.withEntities(mock, skipping);
+  helper.withFakeQueue(mock, skipping);
+  helper.withPulse(mock, skipping);
+  helper.withServer(mock, skipping);
+
+  const makeTask = function() {
     return {
       provisionerId: 'dummy-test-provisioner',
       workerType: 'dummy-test-worker-type',
       scopes: [],
       routes: [
-        helper.routePrefix + '.',
-        helper.routePrefix + '.my-ns',
-        helper.routePrefix + '.my-ns.my-indexed-thing',
-        helper.routePrefix + '.my-ns.my-indexed-thing-again',
-        helper.routePrefix + '.my-ns.one-ns.my-indexed-thing',
-        helper.routePrefix + '.my-ns.another-ns.my-indexed-thing-again',
-        helper.routePrefix + '.my-ns.slash/things-are-ignored',
+        'index',
+        'index.my-ns',
+        'index.my-ns.my-indexed-thing',
+        'index.my-ns.my-indexed-thing-again',
+        'index.my-ns.one-ns.my-indexed-thing',
+        'index.my-ns.another-ns.my-indexed-thing-again',
+        'index.my-ns.slash/things-are-ignored',
       ],
       retries: 3,
       created: (new Date()).toJSON(),
       deadline: (new Date()).toJSON(),
+      expires: taskcluster.fromNow('1 day'),
       payload: {},
       metadata: {
         name: 'Print `"Hello World"` Once',
@@ -39,8 +45,8 @@ suite('Indexing', () => {
   };
 
   test('Run task and test indexing', async function() {
-    var taskId = slugid.nice();
-    var task = makeTask();
+    const taskId = slugid.nice();
+    const task = makeTask();
     helper.queue.addTask(taskId, task);
 
     debug('### Send fake message, taskid ' + taskId);
@@ -52,12 +58,10 @@ suite('Indexing', () => {
         status: {taskId},
       },
     };
-    helper.handlers.listener.fakeMessage(message);
+    helper.listener.fakeMessage(message);
 
     debug('### Find task in index');
-    let result = await testing.poll(function() {
-      return helper.index.findTask('my-ns.my-indexed-thing');
-    });
+    let result = await testing.poll(() => helper.index.findTask('my-ns.my-indexed-thing'));
     assert.equal(result.taskId, taskId, 'Wrong taskId');
 
     debug('### Find task in index (again)');
@@ -96,8 +100,8 @@ suite('Indexing', () => {
   });
 
   test('Run task with a .extra and test indexing', async function() {
-    var taskId = slugid.nice();
-    var task = makeTask();
+    const taskId = slugid.nice();
+    const task = makeTask();
     task.extra = {
       index: {
         rank:       42,
@@ -118,7 +122,7 @@ suite('Indexing', () => {
         status: {taskId},
       },
     };
-    helper.handlers.listener.fakeMessage(message);
+    helper.listener.fakeMessage(message);
 
     debug('### Find task in index');
     let result = await testing.poll(function() {
@@ -135,13 +139,13 @@ suite('Indexing', () => {
 
   test('Expiring Indexed Tasks', async function() {
     // Create expiration
-    var expiry = new Date();
+    const expiry = new Date();
 
     expiry.setDate(expiry.getDate() - 1);
     
-    var myns     = slugid.v4();
-    var taskId   = slugid.v4();
-    var taskId2  = slugid.v4();
+    const myns     = slugid.v4();
+    const taskId   = slugid.v4();
+    const taskId2  = slugid.v4();
     await helper.index.insertTask(myns + '.my-task', {
       taskId:     taskId,
       rank:       41,
@@ -166,9 +170,9 @@ suite('Indexing', () => {
     assert(result.taskId === taskId2, 'Wrong taskId');
     
     // Set now to one day in the past 
-    var now = taskcluster.fromNow('- 1 day');
+    const now = taskcluster.fromNow('- 1 day');
     debug('Expiring indexed tasks at: %s, from before %s', new Date(), now);
-    await helper.handlers.IndexedTask.expireTasks(now);
+    await helper.IndexedTask.expireTasks(now);
     
     try {
       await helper.index.findTask(myns + '.my-task');
@@ -182,11 +186,11 @@ suite('Indexing', () => {
 
   test('Expiring Namespace', async function() {
     // Create expiration
-    var expiry = new Date();
+    const expiry = new Date();
     expiry.setDate(expiry.getDate() - 1);
-    var myns     = slugid.v4();
-    var taskId   = slugid.v4();
-    var taskId2  = slugid.v4();
+    const myns     = slugid.v4();
+    const taskId   = slugid.v4();
+    const taskId2  = slugid.v4();
 
     await helper.index.insertTask(myns+'.one-ns.my-task', {
       taskId:     taskId,
@@ -210,11 +214,11 @@ suite('Indexing', () => {
     result = await helper.index.findTask(myns + '.another-ns.my-task');
     assert(result.taskId === taskId2, 'Wrong taskId');
     // Set now to one day in the past 
-    var now = new Date();
+    const now = new Date();
     now.setDate(now.getDate() - 1);
     
     debug('Expiring namespace at: %s, from before %s', new Date(), now);
-    await helper.handlers.Namespace.expireEntries(now);
+    await helper.Namespace.expireEntries(now);
     
     result = await helper.index.listNamespaces(myns, {});
     assert.equal(result.namespaces.length, 1, 'Expected 1 namespace');
@@ -224,19 +228,19 @@ suite('Indexing', () => {
     
   });
 
-  var insert10Tasks = async function(myns) {
-    var expiry = new Date();
+  const insert10Tasks = async function(myns) {
+    const expiry = new Date();
     expiry.setDate(expiry.getDate() + 10);
-    var res = [];
-    for (var i = 1; i <= 10; i++) {
-      let taskId = slugid.nice();
+    const res = [];
+    for (let i = 1; i <= 10; i++) {
+      const taskId = slugid.nice();
       await helper.index.insertTask(myns + '.my-task' + _.toString(i) + '.new' + _.toString(i), {
         taskId:     taskId,
         rank:       i,
         data:       {hello: 'world ' + _.toString(i)},
         expires:    expiry.toJSON(),
       });
-      let result = await helper.index.findTask(myns + '.my-task' + _.toString(i) + '.new' + _.toString(i));
+      const result = await helper.index.findTask(myns + '.my-task' + _.toString(i) + '.new' + _.toString(i));
       res.push(result);
       assert(result.taskId === taskId, 'Wrong taskId');
     }
@@ -244,21 +248,21 @@ suite('Indexing', () => {
   };
 
   test('list top-level namespaces test limit and continuationToken params', async function() {
-    var myns = slugid.v4();
-    let tasks = await insert10Tasks(myns);
+    const myns = slugid.v4();
+    const tasks = await insert10Tasks(myns);
     debug('listNamespaces returns continuationToken after limit = 10');
 
     let i = 1;
     let continuationToken = undefined;
     do {
-      let query = {limit: 1};
+      const query = {limit: 1};
       if (!_.isUndefined(continuationToken)) {
         _.extend(query, {continuationToken});
       }
-      let result = await helper.index.listNamespaces(myns, query);
+      const result = await helper.index.listNamespaces(myns, query);
       debug('listNamespaces returns 1 entry');
       assert.equal(result.namespaces.length, 1, 'Expected 1 namespace');
-      let obj = result.namespaces[0];
+      const obj = result.namespaces[0];
       debug('listNamespaces entries match the namespace regex');
       assert.equal(
         new RegExp(myns + '.my-task').test(obj.namespace) &&
