a31734fa2842f25c71ed9308ca026f3e84aa4464
Dustin J. Mitchell <dustin@v.igoro.us>
2018-06-25 12:53:30 -0400

    Merge pull request #52 from ydidwania/lib-upgrades
    Redeployability lib upgrades - Code Sprint

--- services/index/test/api_test.js
+++ services/index/test/api_test.js
@@ -1,29 +1,28 @@
-suite('API', () => {
-  var assert      = require('assert');
-  var debug       = require('debug')('index:test:api_test');
-  var helper      = require('./helper');
-  var slugid      = require('slugid');
-  var _           = require('lodash');
-  var taskcluster = require('taskcluster-client');
-  var request     = require('superagent');
+const assert = require('assert');
+const debug = require('debug')('index:test:api_test');
+const helper = require('./helper');
+const slugid = require('slugid');
+const _ = require('lodash');
+const taskcluster = require('taskcluster-client');
+const request = require('superagent');
+const assume = require('assume');
+const libUrls = require('taskcluster-lib-urls');
 
-  // Artifact names that we have assigned scopes to testing credentials for.
-  var publicArtifactName = 'public/dummy-test-provisioner.log';
-  var privateArtifactName = 'private/dummy-test-provisioner.log';
-
-  // Create expiration
-  var expiry = new Date();
-  expiry.setMinutes(expiry.getMinutes() + 25);
+helper.secrets.mockSuite('api_test.js', ['taskcluster'], function(mock, skipping) {
+  helper.withEntities(mock, skipping);
+  helper.withFakeQueue(mock, skipping);
+  helper.withPulse(mock, skipping);
+  helper.withServer(mock, skipping);
 
   test('insert (and rank)', async function() {
-    var myns    = slugid.v4();
-    var taskId  = slugid.v4();
-    var taskId2  = slugid.v4();
+    const myns    = slugid.v4();
+    const taskId  = slugid.v4();
+    const taskId2  = slugid.v4();
     await helper.index.insertTask(myns + '.my-task', {
       taskId:     taskId,
       rank:       41,
       data:       {hello: 'world'},
-      expires:    expiry.toJSON(),
+      expires:    taskcluster.fromNow('25 minutes'),
     });
     let result = await helper.index.findTask(myns + '.my-task');
     assert(result.taskId === taskId, 'Wrong taskId');
@@ -32,14 +31,14 @@ suite('API', () => {
       taskId:     taskId2,
       rank:       42,
       data:       {hello: 'world - again'},
-      expires:    expiry.toJSON(),
+      expires:    taskcluster.fromNow('25 minutes'),
     });
     result = await helper.index.findTask(myns + '.my-task');
     assert(result.taskId === taskId2, 'Wrong taskId');
   });
 
   test('find (non-existing)', async function() {
-    var ns = slugid.v4() + '.' + slugid.v4();
+    const ns = slugid.v4() + '.' + slugid.v4();
     try {
       await helper.index.findTask(ns);
     } catch (err) {
@@ -50,8 +49,17 @@ suite('API', () => {
   });
 
   suite('listing things', function() {
-    var assume = require('assume');
     suiteSetup(async function() {
+      if (skipping()) {
+        this.skip();
+      }
+    });
+
+    setup('insert tasks to list', async function() {
+      if (skipping()) {
+        return;
+      }
+
       const paths = [
         'abc', 'abc.def', 'abc.def2',
         'bbc',
@@ -66,23 +74,19 @@ suite('API', () => {
         'ppt', 'ppt.stu',    
       ];
 
-      const expires = expiry.toJSON();
-      var expired = new Date();
-      expired.setDate(expired.getDate() - 1);
-      const new_expires = expired.toJSON();
       const taskId = slugid.v4();
 
       for (let path of paths) {
-        await helper.index.insertTask(path, {taskId, rank: 13, data: {}, expires});
+        await helper.index.insertTask(path, {taskId, rank: 13, data: {}, expires: taskcluster.fromNow('1 day')});
       }
 
       for (let path of expired_paths) {
-        await helper.index.insertTask(path, {taskId, rank: 13, data: {}, expires: new_expires});
+        await helper.index.insertTask(path, {taskId, rank: 13, data: {}, expires: taskcluster.fromNow('-1 day')});
       }
     });
 
-    var testValidNamespaces = function(list, VALID_PREFIXES=['abc', 'bbc', 'cbc']) {
-      let namespaces = [];
+    const testValidNamespaces = function(list, VALID_PREFIXES=['abc', 'bbc', 'cbc']) {
+      const namespaces = [];
       const INVALID_PREFIXES = ['pqr', 'ppt'];
       list.forEach(function(ns) {
         namespaces.push(ns.namespace);
@@ -99,43 +103,43 @@ suite('API', () => {
     };
 
     test('list top-level namespaces', async function() {
-      let result = await helper.index.listNamespaces('', {});
+      const result = await helper.index.listNamespaces('', {});
       testValidNamespaces(result.namespaces, ['abc', 'bbc', 'cbc', 'dbc']);
     });
 
     test('list top-level namespaces with continuation', async function() {
-      let opts = {limit: 1};
+      const opts = {limit: 1};
       let results = [];
       
       while (1) {
-        let result = await helper.index.listNamespaces('', opts);
+        const result = await helper.index.listNamespaces('', opts);
         results = results.concat(result.namespaces);
         if (!result.continuationToken) {
           break;
         }
         opts.continuationToken = result.continuationToken;
       }
-      assert.equal(results.length, 6);
+      assert.equal(results.length, 4);
       testValidNamespaces(results, ['abc', 'bbc', 'cbc', 'dbc']);
     });
 
     test('list top-level namespaces (without auth)', async function() {
-      var index = new helper.Index();
-      let result = await index.listNamespaces('', {});
+      helper.scopes('none');
+      const result = await helper.index.listNamespaces('', {});
       testValidNamespaces(result.namespaces, ['abc', 'bbc', 'cbc', 'dbc']);
     });
 
     test('list top-level tasks', async function() {
-      let result = await helper.index.listTasks('', {});
+      const result = await helper.index.listTasks('', {});
       testValidNamespaces(result.tasks);
     });
 
     test('list top-level tasks with continuation', async function() {
-      let opts = {limit: 1};
+      const opts = {limit: 1};
       let results = [];
 
       while (1) {
-        let result = await helper.index.listTasks('', opts);
+        const result = await helper.index.listTasks('', opts);
         results = results.concat(result.tasks);
         if (!result.continuationToken) {
           break;
@@ -148,20 +152,20 @@ suite('API', () => {
     });
 
     test('list top-level tasks (without auth)', async function() {
-      var index = new helper.Index();
-      let result = await index.listTasks('', {});
+      helper.scopes('none');
+      const result = await helper.index.listTasks('', {});
       testValidNamespaces(result.tasks);
     });
 
     test('list top-level tasks', async function() {
-      let result = await helper.index.listTasks('', {});
+      const result = await helper.index.listTasks('', {});
       testValidNamespaces(result.tasks);
     });
 
     test('findTask throws 404 for expired tasks', async function() {
-      var myns    = slugid.v4();
-      var taskId  = slugid.v4();
-      var expired = new Date();
+      const myns    = slugid.v4();
+      const taskId  = slugid.v4();
+      const expired = new Date();
       expired.setDate(expired.getDate() - 1);
       const new_expires = expired.toJSON();
 
@@ -182,7 +186,7 @@ suite('API', () => {
   });
 
   test('access public artifact', async function() {
-    let taskId = slugid.nice();
+    const taskId = slugid.nice();
     debug('### Insert task into index');
     await  helper.index.insertTask('my.name.space', {
       taskId:     taskId,
@@ -192,20 +196,21 @@ suite('API', () => {
     });
 
     debug('### Download public artifact using index');
-    var url = helper.index.buildUrl(
+    const url = helper.index.buildUrl(
       helper.index.findArtifactFromTask,
       'my.name.space',
       'public/abc.zip'
     );
-    var res = await request.get(url).redirects(0).catch(function(err) {
+    const res = await request.get(url).redirects(0).catch(function(err) {
       return err.response;
     });
     assert.equal(res.statusCode, 303, 'Expected 303 redirect');
-    assert.equal(res.headers.location, `https://queue.taskcluster.net/v1/task/${taskId}/artifacts/public%2Fabc.zip`);
+    assert.equal(res.headers.location,
+      libUrls.api(helper.rootUrl, 'queue', 'v1', `/task/${taskId}/artifacts/public%2Fabc.zip`));
   });
 
   test('access private artifact (with * scope)', async function() {
-    let taskId = slugid.nice();
+    const taskId = slugid.nice();
     debug('### Insert task into index');
     await  helper.index.insertTask('my.name.space', {
       taskId:     taskId,
@@ -215,43 +220,38 @@ suite('API', () => {
     });
 
     debug('### Download private artifact using index');
-    var url = helper.index.buildSignedUrl(
+    const url = helper.index.buildSignedUrl(
       helper.index.findArtifactFromTask,
       'my.name.space',
       'not-public/abc.zip'
     );
-    var res = await request.get(url).redirects(0).catch(function(err) {
+    const res = await request.get(url).redirects(0).catch(function(err) {
       return err.response;
     });
     assert.equal(res.statusCode, 303, 'Expected 303 redirect');
-    let location = res.headers.location.replace(/bewit=.*/, 'bewit=xyz');
-    assert.equal(location, `https://queue.taskcluster.net/v1/task/${taskId}/artifacts/not-public%2Fabc.zip?bewit=xyz`);
+    const location = res.headers.location.replace(/bewit=.*/, 'bewit=xyz');
+    assert.equal(location,
+      libUrls.api(helper.rootUrl, 'queue', 'v1', `/task/${taskId}/artifacts/not-public%2Fabc.zip?bewit=xyz`));
   });
 
   test('access private artifact (with no scopes)', async function() {
-    let taskId = slugid.nice();
+    const taskId = slugid.nice();
     debug('### Insert task into index');
-    await  helper.index.insertTask('my.name.space', {
+    await helper.index.insertTask('my.name.space', {
       taskId:     taskId,
       rank:       41,
       data:       {hello: 'world'},
       expires:    taskcluster.fromNowJSON('24 hours'),
     });
 
-    debug('### Download private artifact using index with no scopes');
-    var index = new taskcluster.Index({
-      baseUrl: helper.baseUrl,
-      credentials: {
-        clientId: 'public-only-client',
-        accessToken: 'none',
-      },
-    });
-    var url = index.buildSignedUrl(
+    debug('### Download private artifact using index without queue:get-artifact:..');
+    helper.scopes('some-scope');
+    const url = helper.index.buildSignedUrl(
       helper.index.findArtifactFromTask,
       'my.name.space',
       'not-public/abc.zip'
     );
-    var res = await request.get(url).redirects(0).catch(function(err) {
+    const res = await request.get(url).redirects(0).catch(function(err) {
       return err.response;
     });
     assert.equal(res.statusCode, 403, 'Expected 403 Forbidden');
