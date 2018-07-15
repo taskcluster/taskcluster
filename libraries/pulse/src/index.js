3f495ae75ee4ef9d796f92b356b7c4f06600d69a
Dustin J. Mitchell <dustin@v.igoro.us>
2018-07-15 19:46:00 -0400

    Implement PulseConsumer (#3)
    Implements a new "high-level" consumer to handle the common case -- an application that wishes to listen persistently on a queue.
    
    Many thanks to @Biboswan for writing a substantial part of this squashed path; see https://github.com/taskcluster/taskcluster-lib-pulse/pull/3 for details.

--- libraries/pulse/src/index.js
+++ libraries/pulse/src/index.js
@@ -1,5 +1,7 @@
 const {Client} = require('./client');
+const {consume} = require('./consumer');
 
 module.exports = {
   Client,
+  consume,
 };
--- libraries/pulse/test/client_test.js
+++ libraries/pulse/test/client_test.js
@@ -4,6 +4,8 @@ const amqplib = require('amqplib');
 const assert = require('assert');
 const assume = require('assume');
 const debugModule = require('debug');
+const libMonitor = require('taskcluster-lib-monitor');
+const slugid = require('slugid');
 
 const PULSE_CONNECTION_STRING = process.env.PULSE_CONNECTION_STRING;
 
@@ -48,8 +50,6 @@ suite('buildConnectionString', function() {
 });
 
 const connectionTests = connectionString => {
-  let client;
-
   // use a unique name for each test run, just to ensure nothing interferes
   const unique = new Date().getTime().toString();
   const exchangeName = `exchanges/test/${unique}`;
@@ -58,11 +58,10 @@ const connectionTests = connectionString => {
   const message = new Buffer('Hello');
   const debug = debugModule('test');
 
-  setup(function() {
-    client = new Client({
-      connectionString,
-      retirementDelay: 50,
-    });
+  let monitor;
+
+  setup(async function() {
+    monitor = await libMonitor({project: 'tests', mock: true});
   });
 
   // publish a message to the exchange using just amqplib, declaring the
@@ -85,36 +84,109 @@ const connectionTests = connectionString => {
 
   test('start and immediately stop', async function() {
     let gotConnection = false;
+    const client = new Client({
+      connectionString,
+      retirementDelay: 50,
+      minReconnectionInterval: 20,
+      monitor,
+    });
     client.on('connected', () => { gotConnection = true; });
-    client.start();
     await client.stop();
     assume(gotConnection).to.equal(false);
   });
 
-  test('reconnect interval', async function() {
-    let client = new Client({
+  test('activeConnection', async function() {
+    const client = new Client({
       connectionString,
-      recycleInterval: 10,
+      retirementDelay: 50,
+      minReconnectionInterval: 20,
+      monitor,
+    });
+    assume(client.activeConnection).to.equal(undefined);
+    await new Promise((resolve, reject) => {
+      client.on('connected', conn => {
+        try {
+          assume(client.activeConnection).to.equal(conn);
+        } catch (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+    await client.stop();
+    assume(client.activeConnection).to.equal(undefined);
+  });
+
+  test('recycle interval', async function() {
+    let recycles = 0;
+    const oldMethod = Client.prototype.recycle;
+    Client.prototype.recycle = () => { recycles++; };
+    try {
+      const client = new Client({
+        connectionString,
+        recycleInterval: 10,
+        retirementDelay: 50,
+        monitor,
+      });
+
+      await new Promise(resolve => setTimeout(resolve, 100));
+      await client.stop();
+      assume(recycles).is.gt(5);
+    } finally {
+      Client.prototype.recycle = oldMethod;
+    }
+  });
+
+  test('minReconnectionInterval', async function() {
+    let connections = 0;
+    const oldConnect = amqplib.connect;
+    amqplib.connect = async () => {
+      connections++;
+      throw new Error('uhoh');
+    };
+
+    const client = new Client({
+      connectionString,
+      retirementDelay: 50,
+      minReconnectionInterval: 10,
+      monitor,
     });
 
-    let recycles = 0;
-    client.recycle = () => { recycles++; };
-    client.start();
-    await new Promise(resolve => setTimeout(resolve, 100));
-    await client.stop();
-    assume(recycles).is.gt(5);
+    try {
+      // Run the client for 100ms.  At 10ms per connection, wes should get about 10
+      // connections; if the minReconnectionInterval doesn't work, we'll get a lot
+      // more than that!
+      await new Promise(resolve => setTimeout(resolve, 100));
+      await client.stop();
+    } finally {
+      amqplib.connect = oldConnect;
+    }
+    assume(connections).is.between(5, 15);
   });
 
   test('start and stop after connection is established', async function() {
+    const client = new Client({
+      connectionString,
+      retirementDelay: 50,
+      minReconnectionInterval: 20,
+      monitor,
+    });
+
     await new Promise((resolve, reject) => {
-      client.on('connected', () => {
+      client.onConnected(() => {
         client.stop().then(resolve, reject);
       });
-      client.start();
     });
   });
 
   test('start, fail, and then stop', async function() {
+    const client = new Client({
+      connectionString,
+      retirementDelay: 50,
+      minReconnectionInterval: 20,
+      monitor,
+    });
+
     await new Promise((resolve, reject) => {
       client.once('connected', connection => {
         connection.failed();
@@ -122,11 +194,79 @@ const connectionTests = connectionString => {
           client.stop().then(resolve, reject);
         });
       });
-      client.start();
     });
   });
 
+  test('withConnection', async function() {
+    const client = new Client({
+      connectionString,
+      retirementDelay: 50,
+      minReconnectionInterval: 20,
+      monitor,
+    });
+
+    let gotConnection = false;
+    let finishedWithConnection = false;
+    client.withConnection(conn => { gotConnection = true; })
+      .then(() => { finishedWithConnection = true; });
+
+    assume(finishedWithConnection).to.equal(false);
+    await new Promise((resolve, reject) => {
+      client.once('connected', () => {
+        client.stop().then(resolve, reject);
+      });
+    });
+
+    // check that the withConnection function was called, and that its
+    // promise is resolved.
+    assume([gotConnection, finishedWithConnection]).to.eqls([true, true]);
+  });
+
+  test('withChannel', async function() {
+    const client = new Client({
+      connectionString,
+      retirementDelay: 50,
+      minReconnectionInterval: 20,
+      monitor,
+    });
+
+    const queueName = client.fullObjectName('queue', slugid.v4());
+
+    let gotException;
+    try {
+      await client.withChannel(async chan => {
+        await chan.assertQueue(queueName);
+        // throw an error to exercise error-handling code
+        throw new Error('uhoh');
+      });
+    } catch (err) {
+      if (/uhoh/.test(err)) {
+        // note that we can't tell if the channel was closed properly
+        gotException = true;
+      } else {
+        throw err;
+      }
+    }
+    assume(gotException).to.equal(true);
+
+    let queueInfo;
+    await client.withChannel(async chan => {
+      queueInfo = await chan.checkQueue(queueName);
+      await chan.deleteQueue(queueName);
+    });
+
+    await client.stop();
+    assume(queueInfo.queue).to.equal(queueName);
+  });
+
   test('consumer (with failures)', async function() {
+    const client = new Client({
+      connectionString,
+      retirementDelay: 50,
+      minReconnectionInterval: 20,
+      monitor,
+    });
+
     let failureCount = 0;
     let messageReceived = 0;
 
@@ -172,7 +312,6 @@ const connectionTests = connectionString => {
 
         // start publishing the message
         publishMessage().catch(reject);
-        client.start();
       });
 
       assume(messageReceived).to.equal(1);
@@ -183,13 +322,14 @@ const connectionTests = connectionString => {
 };
 
 suite('Client', function() {
-  suite('constructor', function() {
+  suite('constructor', async function() {
+    const monitor = await libMonitor({project: 'tests', mock: true});
     test('rejects connectionString *and* username', function() {
-      assume(() => new Client({username: 'me', connectionString: 'amqps://..'}))
+      assume(() => new Client({username: 'me', connectionString: 'amqps://..', monitor}))
         .throws(/along with/);
     });
     test('requires either connectionString *or* username', function() {
-      assume(() => new Client({}))
+      assume(() => new Client({monitor}))
         .throws(/is required/);
     });
   });
