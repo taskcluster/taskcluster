2ca1b590451513963f385f933a7574db530ced23
Hassan Ali <helfi92@gmail.com>
2018-08-16 14:40:41 -0400

    Add jest (#96)

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -70,6 +70,12 @@ module.exports = {
         neutrino.config.devtool('cheap-module-source-map');
       }
 
+      // There's a bug in @neutrinojs/jest where globals are injected in `src`.
+      // To that end, we should only inject this middleware when NODE_ENV is 'test'.
+      if (process.env.NODE_ENV === 'test') {
+        neutrino.use('@neutrinojs/jest');
+      }
+
       neutrino.config.output.publicPath('/');
       neutrino.config.module
         .rule('graphql')
