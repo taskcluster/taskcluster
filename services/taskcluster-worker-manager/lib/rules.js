5080bf767c4464b073a6a36e1b0d3653e57eae04
John Ford <john@johnford.org>
2018-09-03 12:53:59 +0200

    Bug 1487651 - [worker-manager] Implement Ruleset

---
+++ services/taskcluster-worker-manager/lib/rules.js
@@ -0,0 +1,280 @@
+"use strict";
+
+const ErrorCodes = Object.freeze({
+  // InvalidConditions represents an error while converting the object literal
+  // representation of a Conditions object into a Conditions object
+  InvalidConditions: 'InvalidConditions',
+
+  // InvalidSatisfiers represents an error caused by passing an invalid object
+  // for the satisfiers object needed
+  InvalidSatisfiers: 'InvalidSatisfiers',
+
+  // InvalidRule represents an error caused by a malformed Rule
+  InvalidRule: 'InvalidRule',
+
+  // InvalidValues represents an error caused by passing in an invalid
+  // values object for assignment
+  InvalidValues: 'InvalidValues',
+});
+
+/**
+ * Conditions encapsulate how a set of conditions are evaluated.  Conditions
+ * are objects which map condition name (e.g. workerType) to either an
+ * acceptable value (e.g. 'worker1') or to a list of acceptable values (e.g.
+ * ['worker1', 'worker2']).  Conditions are evaluated against a set of
+ * satisfiers, which is an object which maps condition name to value.  In the
+ * case of a list of acceptable values, if any acceptable value matches, the
+ * condition is considered satisfied
+ */
+class Conditions {
+
+  /**
+   * Construct a Conditions class given an object representation of a
+   * conditions class and the rule to which the conditions belong
+   */
+  constructor(conditions, rule) {
+    // We want a reference to our rule, but only for error reporting so it's
+    // not mandatory.
+    if (rule) {
+      this._rule = rule;
+    }
+
+    if (conditions !== null && typeof conditions !== 'object') {
+        this._throw(ErrorCodes.InvalidConditions, 'conditions must be null or an object');
+    }
+
+    // Validate that every condition is either a string or a list of strings
+    if (conditions) {
+      for (let condition of Object.keys(conditions)) {
+        if (typeof conditions[condition] !== 'string') {
+          if (Array.isArray(conditions[condition])) {
+            for (let aCondition of conditions[condition]) {
+              if (typeof aCondition !== 'string') {
+                this._throw(ErrorCodes.InvalidConditions, 'each condition must be string');
+              }
+            }
+          } else {
+            this._throw(ErrorCodes.InvalidConditions, 'condition must be a string or list of strings');
+          }
+        }
+      }
+    }
+
+    this.conditions = conditions;
+  }
+
+  /**
+   * Throw errors from these conditions so they can be traced back
+   * to the rule they came from
+   */
+  _throw(code, msg) {
+    let err = new Error(msg);
+    err.code = code;
+    throw err;
+  }
+
+  /**
+   * Compare the specified value to the required value.  This is its own
+   * function to make it easier to replace the comparison system, e.g.
+   * switching from globbing to regular expressions
+   *
+   * Return true if the value satisfies the specification
+   */
+  _compare(condition, satisfier) {
+    return condition === satisfier;
+  }
+
+  // Evaluate a specific condition of the conditions against the value
+  // provided
+  _evaluateCondition(condition, satisfier) {
+    if (typeof condition === 'string') {
+      return this._compare(condition, satisfier);
+    }
+
+    // condition can be a single item or a list which makes naming icky. Think
+    // as if this were "let condition of conditions"
+    for (let aCondition of condition) {
+      if (this._compare(aCondition, satisfier)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * Evaluate all provided conditions.  Return true if the conditions are
+   * satisfied or false if any condition is not satisified
+   */
+  evaluate(satisfiers) {
+    if (this.conditions === null) {
+      return true;
+    }
+
+    if (typeof satisfiers !== 'object') {
+      this._throw(ErrorCodes.InvalidSatisfiers, 'satisfiers must be an object');
+    }
+
+    for (let condition of Object.keys(this.conditions)) {
+      if (!satisfiers[condition]) {
+        this._throw(ErrorCodes.InvalidSatisfiers, `required satisfier for condition ${condition} not specified`);
+      }
+      if (!this._evaluateCondition(this.conditions[condition], satisfiers[condition])) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  /**
+   * Return a list of satisfier keys which are required to evaluate this
+   * ruleset
+   */
+  requiredSatisfiers() {
+    return Object.keys(this.conditions);
+  }
+}
+
+/**
+ * Rule encapsulates how a rule is to be evaluated.
+ */
+class Rule {
+  constructor({ruleId, conditions, values, description}) {
+    if (typeof ruleId !== 'string') {
+      this._throw(ErrorCodes.InvalidRule, 'ruleId must be string');
+    }
+    this.ruleId = ruleId
+
+    if (typeof conditions !== 'object') {
+      this._throw(ErrorCodes.InvalidConditions, 'conditions must be an object');
+    }
+    this.conditions = new Conditions(conditions, this);
+
+    if (typeof values !== 'object') {
+      this._throw(ErrorCodes.InvalidValues, 'values must be an object');
+    }
+    this.values = values;
+
+    if (typeof description !== 'string') {
+      this._throw(ErrorCodes.InvalidRule, 'description must be a string');
+    }
+  }
+
+  _throw(code, msg) {
+    let err = new Error(msg);
+    err.code = code;
+    err.ruleId = this.ruleId || '<unknown>';
+    err.description = this.description || '<unknown>';
+    throw err;
+  }
+
+  /**
+   * Evaluate a rule's conditions against provided condition satisfiers and if
+   * the conditions are satisfied, set the rule's values to the provided target
+   * object
+   */
+  evaluate(satisfiers, target) {
+    if (this.conditions.evaluate(satisfiers)) {
+      assign(target, this.values);
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Return a list of satisfier keys which are required to evaluate this
+   * ruleset
+   */
+  requiredSatisfiers() {
+    return this.conditions.requiredSatisfiers();
+  }
+}
+
+/**
+ * Assign into the target object, the properties from the values object.
+ * Assignments are nested, so if values has a property 'a.b.c: 1', the target
+ * will have a, a.b and a.b.c created as objects.  If property already exists
+ * on the target object, it is overwritten with the new property.  If a
+ * property was an object, a non-object can overwrite it and vice-versa.  If a
+ * values object property is the value `null`, that property on the target
+ * object is deleted
+ */
+function assign (target, values) {
+  function assignProperty(target, targetProperty, value) {
+    if (value === null) {
+      delete target[targetProperty];
+    } else if (['string', 'number', 'boolean'].includes(typeof value)) {
+      target[targetProperty] = value;
+    } else if (typeof value === 'object') {
+      if (typeof target[targetProperty] !== 'object') {
+        target[targetProperty] = {};
+      }
+      for (let valueProperty of Object.keys(value)) {
+        assignProperty(target[targetProperty], valueProperty, value[valueProperty]);
+      }
+    } else {
+      throw new Error('assigned values must be null, string, number, boolean or objects');
+    }
+  }
+
+  for (let property of Object.keys(values)) {
+    assignProperty(target, property, values[property]);
+  }
+}
+
+/**
+ * Ruleset represents all rules as well as an interface to evaluate them
+ * against a set of condition satisfiers.
+ */
+class Ruleset {
+  constructor({rules}) {
+    if (!Array.isArray(rules)) {
+      throw new Error('rules must be an array');
+    }
+
+    this.rules = rules.map(rule => new Rule(rule));
+  }
+
+  /**
+   * Given a set of condition satisfiers, return the values set according to
+   * the rules.  A condition satisfier is an object which maps condition name
+   * to a string value to compare against.  For information about how
+   * Conditions work, see the Conditions class.
+   *
+   * Returns an object which is built from all the values in each rule.
+   */
+  evaluate(satisfiers) {
+    let target = {};
+    for (let rule of this.rules) {
+      rule.evaluate(satisfiers, target);
+    }
+    return target;
+  }
+
+  /**
+   * Return a list of satisfier keys which are required to evaluate this
+   * ruleset
+   */
+  requiredSatisfiers() {
+    let requiredSatisfiers = [];
+
+    for (let rule of this.rules) {
+      for (let requiredSatisfier of rule.requiredSatisfiers()) {
+        if (!requiredSatisfiers.includes(requiredSatisfier)) {
+          requiredSatisfiers.push(requiredSatisfiers);
+        }
+      }
+    }
+    return requiredSatisfiers.sort();
+  }
+
+}
+
+module.exports = {
+  ErrorCodes,
+  Conditions,
+  Rule,
+  Ruleset,
+  assign,
+}
---
+++ services/taskcluster-worker-manager/test/rules_test.js
@@ -0,0 +1,351 @@
+
+const sinon = require('sinon');
+const assume = require('assume');
+
+const {Ruleset, Rule, Conditions, assign} = require('../lib/rules');
+
+suite('assign()', () => {
+  test('should be able to assign an empty object', () => {
+    let expected = {a: 1, b: true, c: 'john'};
+    let actual = {};
+
+    assign(actual, expected);
+    assume(actual).deeply.equals(expected);
+  });
+
+  test('should be able to overwrite non-nested properties in an object', () => {
+    let expected = {a: 1, b: true, c: 'john'};
+    let actual = {a: 2, b: false, c: 'notjohn'};
+
+    assign(actual, expected);
+    assume(actual).deeply.equals(expected);
+  });
+
+  test('should be able to overwrite nested properties in an object', () => {
+    let expected = {a: 1, b: {c: {d: 2}}};
+    let actual = {a: 1, b: {c: {d: 3}}};
+
+    assign(actual, expected);
+    assume(actual).deeply.equals(expected);
+  });
+
+  test('should be able to overwrite non-object property with object', () => {
+    let expected = {a: 1, b: {c: {d: 2}}};
+    let actual = {a: 1, b: {c: 3}};
+
+    assign(actual, expected);
+    assume(actual).deeply.equals(expected);
+  });
+
+  test('should be able to create non-existing object', () => {
+    let expected = {a: 1, b: {c: {d: 2}}};
+    let actual = {};
+
+    assign(actual, expected);
+    assume(actual).deeply.equals(expected);
+  });
+
+  test('should be able to delete properties in an object', () => {
+    let expected = {a:1};
+    let actual = {a:1, b:2};
+
+    assign(actual, {b: null});
+    assume(actual).deeply.equals(expected);
+  });
+
+  test('should throw for a function property', () => {
+    assume(() => {
+      assign({}, {a: () => {}});
+    }).throws(/^assigned values must be/);
+  });
+
+});
+
+suite('Conditions', () => {
+  let nullCondition = new Conditions(null);
+  suite('_compare()', () => {
+    test('should return true', () => {
+      assume(nullCondition._compare('string', 'string')).is.ok();
+    });
+    test('should return false', () => {
+      assume(nullCondition._compare('string', 'string')).is.ok();
+    });
+  });
+
+  suite('_evaluateCondition()', () => {
+    test('should return true when a single string satisifies requirement', () => {
+      let conditions = new Conditions({
+        string: 'test',
+      });
+      assume(conditions.evaluate({string: 'test'})).is.ok();
+    });
+    
+    test('should return true when one of many string satisifies requirement', () => {
+      let conditions = new Conditions({
+        string: ['abc', 'test', 'def'],
+      });
+      assume(conditions.evaluate({string: 'test'})).is.ok();
+    });
+
+    test('should return false when a single string does not satisfy requirement', () => {
+      let conditions = new Conditions({
+        string: 'nottest',
+      });
+      assume(conditions.evaluate({string: 'test'})).is.not.ok();
+    });
+    
+    test('should return false when none of many string satisifies requirement', () => {
+      let conditions = new Conditions({
+        string: ['abc', 'def'],
+      });
+      assume(conditions.evaluate({string: 'test'})).is.not.ok();
+    });
+  });
+
+  suite('evaluate()', () => {
+    test('should return true when condition is null', () => {
+      let conditions = new Conditions(null);
+      assume(conditions.evaluate()).is.ok();
+    });
+
+    test('should throw when satisfiers is not object', () => {
+      let conditions = new Conditions({string: 'test'});
+      assume(() => {
+        conditions.evaluate(() => {});
+      }).throws(/^satisfiers must be an object/);
+    });
+
+    test('should throw when a condition has no satisfier', () => {
+      let conditions = new Conditions({string: 'test'});
+      assume(() => {
+        conditions.evaluate({other: 'test'});
+      }).throws(/^required satisfier for condition/);
+    });
+
+    test('should return true when all requirements are met', () => {
+      let conditions = new Conditions({string: 'test'});
+      assume(conditions.evaluate({string: 'test'})).is.ok();
+    });
+
+    test('should return false when not all requirements are met', () => {
+      let conditions = new Conditions({string: 'test'});
+      assume(conditions.evaluate({string: 'not-test'})).is.not.ok();
+    });
+  });
+
+  suite('invalid input', () => {
+    test('should not allow a non-string condition', () => {
+      assume(() => {
+        new Conditions({
+          property: () => {},
+        });
+      }).throws(/^condition must be a string or list/);
+    });
+
+    test('should not allow a non-string condition in a list', () => {
+      assume(() => {
+        new Conditions({
+          property: [() => {}],
+        });
+      }).throws(/^each condition must be string$/);
+    });
+  });
+});
+
+suite('Rule', () => {
+  suite('evaluate()', () => {
+    let rule;
+    let target;
+
+    setup(() => {
+      target = {
+        rule1val: 'not-set',
+      };
+      rule = new Rule({
+        ruleId: 'rule-1',
+        conditions: {
+          string: 'test',
+        },
+        values: {
+          rule1val: 'set',
+        },
+        description: 'test rule',
+      });    
+    });
+
+    test('should return true and set values with satisfied requirements', () => {
+      let outcome = rule.evaluate({string: 'test'}, target);
+      assume(outcome).is.ok();
+      assume(target).deeply.equals({rule1val: 'set'});
+    });
+    
+    test('should return false and not set values with unsatisfied requirements', () => {
+      let outcome = rule.evaluate({string: 'nottest'}, target);
+      assume(outcome).is.not.ok();
+      assume(target).deeply.equals({rule1val: 'not-set'});
+    });
+  });
+
+  suite('invalid input', () => {
+    test('should throw with invalid ruleId', () => {
+      assume(() => {
+        new Rule({ruleId: 123, conditions: {}, values: {}, description: ''});
+      }).throws(/^ruleId must be string/);
+    });
+
+    test('should throw with invalid conditions', () => {
+      assume(() => {
+        new Rule({ruleId: 'id', conditions: 123, values: {}, description: ''});
+      }).throws(/^conditions must be an object/);
+    });
+
+    test('should throw with invalid values', () => {
+      assume(() => {
+        new Rule({ruleId: 'id', conditions: {}, values: 123, description: ''});
+      }).throws(/^values must be an object/);
+    });
+
+    test('should throw with invalid description', () => {
+      assume(() => {
+        new Rule({ruleId: 'id', conditions: {}, values: {}, description: 123});
+      }).throws(/^description must be a string/);
+    });
+  });
+});
+
+suite('Ruleset', () => {
+  
+  let rules;
+
+  setup(() => {
+    rules = new Ruleset({
+      rules: [{
+        ruleId: 'rule1',
+        conditions: {
+          workerType: 'worker1',
+          provider: ['ec2', 'gcp'],
+        },
+        values: {
+          rule1: 'matched',
+        },
+        description: 'worker1 in ec2 or gcp',
+      }, {
+        ruleId: 'rule2',
+        conditions: {
+          workerType: 'worker1',
+          provider: 'ec2',
+        },
+        values: {
+          rule2: 'matched', 
+        },
+        description: 'worker1 in ec2 only',
+      }, {
+        ruleId: 'rule3',
+        conditions: {
+          workerType: 'worker2',
+          provider: ['ec2', 'gcp'],
+        },
+        values: {
+          rule3: 'matched',
+        },
+        description: 'worker2 in ec2 or gcp',
+      }, {
+        ruleId: 'rule4',
+        conditions: {
+          workerType: 'worker2',
+          provider: 'gcp',
+        },
+        values: {
+          rule4: 'matched',
+        },
+        description: 'worker2 in gcp only',
+      }],
+    });
+  });
+
+  test('should match worker1/ec2 correctly', () => {
+    assume(rules.evaluate({workerType: 'worker1', provider: 'ec2'})).deeply.equals({
+      rule1: 'matched',
+      rule2: 'matched',
+    });
+  });
+
+  test('should match worker1/gcp correctly', () => {
+    assume(rules.evaluate({workerType: 'worker1', provider: 'ec2'})).deeply.equals({
+      rule1: 'matched',
+      rule2: 'matched',
+    });
+  });
+
+  test('should match worker2/ec2 correctly', () => {
+    assume(rules.evaluate({workerType: 'worker2', provider: 'ec2'})).deeply.equals({
+      rule3: 'matched',
+    });
+  });
+  test('should match worker2/gcp correctly', () => {
+    assume(rules.evaluate({workerType: 'worker2', provider: 'gcp'})).deeply.equals({
+      rule3: 'matched',
+      rule4: 'matched',
+    });
+  });
+});
+
+suite('Required Satisfiers', () => {
+  test('should be able to list required satisfiers on Conditions', () => {
+    let cond = new Conditions({string1: 'abc', string2: 'def'});
+    assume(cond.requiredSatisfiers()).is.array(['string1', 'string2']);
+  });
+  
+  test('should be able to list required satisfiers on Rules', () => {
+    let rule = new Rule({
+      ruleId: 'rule-1',
+      conditions: {string1: 'abc', string2: 'def'},
+      values: {},
+      description: '',
+    });
+    assume(rule.requiredSatisfiers()).is.array(['string1', 'string2']);
+  });
+  
+  test('should be able to list required satisfiers on Conditions', () => {
+    let rules = new Ruleset({
+      rules: [{
+        ruleId: 'rule1',
+        conditions: {
+          string1: 'worker1',
+          string2: ['ec2', 'gcp'],
+        },
+        values: {},
+        description: '',
+      }, {
+        ruleId: 'rule2',
+        conditions: {
+          string2: 'worker1',
+        },
+        values: {},
+        description: '',
+      }, {
+        ruleId: 'rule3',
+        conditions: {
+          string3: 'worker2',
+          string4: ['ec2', 'gcp'],
+        },
+        values: {},
+        description: '',
+      }, {
+        ruleId: 'rule4',
+        conditions: {
+          string5: 'worker2',
+        },
+        values: {},
+        description: '',
+      }],
+    });
+    assume(rules.requiredSatisfiers()).is.array([
+      'string1',
+      'string2', 
+      'string3',
+      'string4',
+      'string5',
+    ]);
+  });
+});
