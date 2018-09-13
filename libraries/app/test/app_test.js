8dcbb4e99e31664d34485d07703ac9ebf496a402
John Ford <jhford@users.noreply.github.com>
2018-09-13 18:07:51 +0200

    Merge pull request #24 from taskcluster/request-id
    Bug 1488387 - Attach Request ID to all requests objects and http response headers

--- libraries/app/test/app_test.js
+++ libraries/app/test/app_test.js
@@ -1,9 +1,11 @@
+var assert = require('assert');
+var path = require('path');
+var App = require('../');
+var request = require('superagent');
+var express = require('express');
+var isUUID = require('is-uuid');
+
 suite('app', function() {
-  var assert = require('assert');
-  var path = require('path');
-  var App = require('../');
-  var request = require('superagent');
-  var express = require('express');
 
   // Test app creation
   suite('app({port: 1459})', function() {
@@ -16,6 +18,12 @@ suite('app', function() {
           const router = express.Router();
           router.get('/test', function(req, res) {
             res.status(200).send('Okay this works');
+          });
+          router.get('/req-id', function(req, res) {
+            res.status(200).send(JSON.stringify({
+              header: req.headers['x-request-id'],
+              valueSet: req.requestId,
+            }));
           });
           app.use('/api/test/v1', router);
         },
@@ -41,6 +49,27 @@ suite('app', function() {
     test('hsts header', async function() {
       var res = await request.get('http://localhost:1459/api/test/v1/test');
       assert.equal(res.headers['strict-transport-security'], 'max-age=7776000000; includeSubDomains');
+    });
+
+    test('request ids', async function() {
+      var res = await request
+        .get('http://localhost:1459/api/test/v1/req-id')
+        .buffer();
+      var body = JSON.parse(res.text);
+      assert(isUUID.v4(res.headers['x-for-request-id']));
+      assert(!isUUID.v4(body.header));
+      assert(isUUID.v4(body.valueSet));
+    });
+
+    test('request ids (heroku)', async function() {
+      var res = await request
+        .get('http://localhost:1459/api/test/v1/req-id')
+        .set('X-Request-Id', 'TestingRequestId')
+        .buffer();
+      var body = JSON.parse(res.text);
+      assert.equal(res.headers['x-for-request-id'], 'TestingRequestId');
+      assert.equal(body.header, 'TestingRequestId');
+      assert.equal(body.valueSet, 'TestingRequestId');
     });
 
     test('/robots.txt', async function() {
