7e64823bd398319bb7e6c2a1804c52265f8388da
Dustin J. Mitchell <dustin@mozilla.com>
2018-10-03 16:27:43 +0000

    fix incorrect testing conditional

--- libraries/pulse/src/publisher.js
+++ libraries/pulse/src/publisher.js
@@ -39,7 +39,7 @@ class Exchanges {
 
   async publisher({rootUrl, schemaset, client, sendDeadline, publish, aws}) {
     let publisher;
-    if (process.env.NODE_ENV !== 'development') {
+    if (process.env.NODE_ENV !== 'production') {
       this.exchangePrefix = `exchange/${client.namespace}/${this.version}/`;
     }
 
