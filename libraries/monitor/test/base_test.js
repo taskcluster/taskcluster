7b48a974ee5acd2047e2c83dabea2e0c9fbdc33c
Dustin J. Mitchell <dustin@mozilla.com>
2018-10-22 21:26:53 +0000

    Bug 1501005 - add oneShot(..)

--- libraries/monitor/test/base_test.js
+++ libraries/monitor/test/base_test.js
@@ -5,21 +5,30 @@ class TestMonitor extends BaseMonitor {
   constructor() {
     super();
     this.measures = [];
+    this.counts = [];
+    this.errors = [];
   }
 
   measure(prefix, ms) {
     this.measures.push({prefix, ms});
   }
+
+  count(prefix, val) {
+    this.counts.push({prefix, val});
+  }
+
+  reportError(err) {
+    this.errors.push(err);
+  }
 }
 
 suite('BaseMonitor', function() {
+  let monitor;
+  setup(function() {
+    monitor = new TestMonitor();
+  });
+
   suite('timer', function() {
-    let monitor;
-
-    setup(function() {
-      monitor = new TestMonitor();
-    });
-
     const takes100ms = () => new Promise(resolve => setTimeout(() => resolve(13), 100));
     const checkMonitor = (len) => {
       // check this after a short delay, as otherwise the Promise.resolve
@@ -74,6 +83,39 @@ suite('BaseMonitor', function() {
       }
       assert(err && /uhoh/.test(err.message));
       await checkMonitor(1);
+    });
+  });
+
+  suite('oneShot', function() {
+    const oldExit = process.exit;
+    let exitStatus = null;
+
+    suiteSetup('mock exit', function() {
+      process.exit = (s) => { exitStatus = s; };
+    });
+
+    suiteTeardown('unmock exit', function() {
+      process.exit = oldExit;
+    });
+
+    setup('clear exitStatus', function() {
+      exitStatus = null;
+    });
+
+    test('successful async function', async function() {
+      await monitor.oneShot('expire', async () => {});
+      assert.equal(exitStatus, 0);
+      assert.equal(monitor.measures[0].prefix, 'expire.duration');
+      assert.equal(monitor.counts[0].prefix, 'expire.done');
+      assert.equal(monitor.errors.length, 0);
+    });
+
+    test('unsuccessful async function', async function() {
+      await monitor.oneShot('expire', async () => { throw new Error('uhoh'); });
+      assert.equal(exitStatus, 1);
+      assert.equal(monitor.measures[0].prefix, 'expire.duration');
+      assert.equal(monitor.counts.length, 0);
+      assert(monitor.errors[0].toString().match(/uhoh/), monitor.errors[0]);
     });
   });
 });
