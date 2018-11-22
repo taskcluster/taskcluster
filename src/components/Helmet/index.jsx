eeb355e1194c8e50edc3305ed54ea6a1bed354f9
Jason Yang <jasonjyang1@gmail.com>
2018-11-22 13:31:14 -0500

    Fix favicon logo not loading #270 (#293)

--- src/components/Helmet/index.jsx
+++ src/components/Helmet/index.jsx
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import { string } from 'prop-types';
 import { Helmet as ReactHelmet } from 'react-helmet';
-import { lowerCase } from 'change-case';
 import { taskGroupState } from '../../utils/prop-types';
+import { TASK_STATE } from '../../utils/constants';
+import Logo from '../../images/logo.png';
+import LogoCompleted from '../../images/logoCompleted.png';
+import LogoFailed from '../../images/logoFailed.png';
+import LogoRunning from '../../images/logoRunning.png';
 
 export default class Helmet extends Component {
   static propTypes = {
@@ -20,19 +24,26 @@ export default class Helmet extends Component {
     title: null,
   };
 
+  getFavicon() {
+    switch (this.props.state) {
+      case TASK_STATE.COMPLETED:
+        return LogoCompleted;
+      case TASK_STATE.FAILED:
+        return LogoFailed;
+      case TASK_STATE.RUNNING:
+        return LogoRunning;
+      default:
+        return Logo;
+    }
+  }
+
   render() {
-    const { state, title } = this.props;
+    const { title } = this.props;
+    const Favicon = this.getFavicon();
 
     return (
       <ReactHelmet>
-        {state ? (
-          <link
-            href={`/src/images/logo${lowerCase(state)}.png`}
-            rel="shortcut icon"
-          />
-        ) : (
-          <link href="/src/images/logo.png" rel="shortcut icon" />
-        )}
+        <link href={Favicon} rel="shortcut icon" />
         {title && <title>{title}</title>}
       </ReactHelmet>
     );
