538f87033b5f6ff0b9287128a57631481949fdcd
Brian Stack <bstack@mozilla.com>
2018-12-19 18:12:23 -0800

    Merge pull request #41 from taskcluster/remove-downloader
    Remove unused download functions

--- libraries/docs/src/index.js
+++ libraries/docs/src/index.js
@@ -1,6 +1,4 @@
 const documenter = require('./documenter');
-const downloader = require('./downloader');
 
 documenter.documenter = documenter; // for compoatibility
-documenter.downloader = downloader;
 module.exports = documenter;
