9bb2082b1098b3e9c0e45910e6ceeef96016e10a
Dustin J. Mitchell <dustin@mozilla.com>
2018-12-26 13:51:12 -0500

    Version bump from 4.1.1 to 5.0.0

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 )
 
 var (
-	version  = "4.1.1"
+	version  = "5.0.0"
 	revision = "" // this is set during build with `-ldflags "-X main.revision=$(git rev-parse HEAD)"`
 	usage    = `
 Taskcluster authentication proxy. By default this pulls all scopes from a
