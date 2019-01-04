0c66f2bd19d438747f93ee00df35120dd7cf1ebb
Dustin J. Mitchell <dustin@mozilla.com>
2019-01-04 22:52:57 +0000

    Bug 1517662 - use per-test azure queues to avoid conflicts

--- services/queue/test/helper.js
+++ services/queue/test/helper.js
@@ -178,16 +178,19 @@ exports.withQueueService = (mock, skipping) => {
     if (mock) {
       helper.load.cfg('azure.fake', true);
       helper.queueService = await helper.load('queueService');
-    }
-  });
-
-  suiteSetup(async function() {
-    if (skipping()) {
-      return;
-    }
-
-    if (mock) {
       helper.queueService.client._reset();
+    } else {
+      // ensure we are using unique queue names from run to run of this service
+      // so that tests do not interfere with one another.  This prefix can only
+      // be 6 characters long..
+      const pfx = 'q' + new Date().getTime().toString().slice(-5);
+      const cfg = await helper.load('cfg');
+      helper.load.cfg('app.queuePrefix', pfx);
+      helper.load.cfg('app.claimQueue', `${pfx}-claim`);
+      helper.load.cfg('app.deadlineQueue', `${pfx}-deadline`);
+      helper.load.cfg('app.resolvedQueue', `${pfx}-resolved`);
+
+      helper.queueService = await helper.load('queueService');
     }
   });
 };
