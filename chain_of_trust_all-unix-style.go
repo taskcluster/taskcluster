53002ec899a2035fda9fb9a1c376c37a4eaf2de8
Aki Sasaki <aki@escapewindow.com>
2019-01-16 14:19:17 -0800

    fix chain_of_trust_all-unix-style.go

--- chain_of_trust_all-unix-style.go
+++ chain_of_trust_all-unix-style.go
@@ -17,7 +17,7 @@ func (cot *ChainOfTrustTaskFeature) ensureTaskUserCantReadPrivateCotKey() error 
 		config.Ed25519SigningKeyLocation,
 	}
 	for _, path := range signingKeyPaths {
-		c, err := process.NewCommand([]string{"/bin/cat", path, cwd, cot.task.EnvVars())
+		c, err := process.NewCommand([]string{"/bin/cat", path}, cwd, cot.task.EnvVars())
 		if err != nil {
 			panic(fmt.Errorf("SERIOUS BUG: Could not create command (not even trying to execute it yet) to cat private chain of trust key - %v", err))
 		}
