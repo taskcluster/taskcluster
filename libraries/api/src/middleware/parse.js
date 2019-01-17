d5bbcc9776c5e8e9653afb7a8657989dbf967db5
Dustin J. Mitchell <dustin@mozilla.com>
2019-01-17 16:39:10 +0000

    Pass errors in body parsing to Express

--- libraries/api/src/middleware/parse.js
+++ libraries/api/src/middleware/parse.js
@@ -11,26 +11,34 @@ const parseBody = ({inputLimit}) => {
     type: 'application/json',
   });
   return (req, res, next) => {
-    wrapped(req, res, () => {
-      // Use JSON middleware, and add hack to store text as req.text
-      if (typeof req.body === 'string' && req.body !== '') {
-        req.text = req.body;
-        try {
-          req.body = JSON.parse(req.text);
-          if (!(req.body instanceof Object)) {
-            throw new Error('Must be an object or array');
-          }
-        } catch (err) {
-          return res.reportError(
-            'MalformedPayload', 'Failed to parse JSON: {{errMsg}}', {
-              errMsg: err.message,
-            });
-        }
-      } else {
-        req.text = '';
-        req.body = {};
+    wrapped(req, res, (err) => {
+      if (err) {
+        return next(err);
       }
-      next();
+
+      try {
+        // Use JSON middleware, and add hack to store text as req.text
+        if (typeof req.body === 'string' && req.body !== '') {
+          req.text = req.body;
+          try {
+            req.body = JSON.parse(req.text);
+            if (!(req.body instanceof Object)) {
+              throw new Error('Must be an object or array');
+            }
+          } catch (err) {
+            return res.reportError(
+              'MalformedPayload', 'Failed to parse JSON: {{errMsg}}', {
+                errMsg: err.message,
+              });
+          }
+        } else {
+          req.text = '';
+          req.body = {};
+        }
+        next();
+      } catch(err) {
+        next(err);
+      }
     });
   };
 };
