f24189d8a74552d4f6213432f869d488c424ce4d
ojaswin <ojaswin@hotmail.com>
2019-01-23 21:55:36 +0530

    Create azure table for blacklist

---
+++ services/notify/src/blacklisted.js
@@ -0,0 +1,13 @@
+const Entity = require('azure-entities');
+
+const BlacklistedNotification = Entity.configure({
+  version: 1,
+  partitionKey: Entity.keys.StringKey('notificationType'),
+  rowKey: Entity.keys.StringKey('notificationAddress'),
+  properties: {
+    // the type could be email, pulse, irc-user or irc-channel
+    notificationType: Entity.keys.String,
+    // the address of the blacklisted destination
+    notificationAddress: Entity.keys.JSON,
+  }
+});
