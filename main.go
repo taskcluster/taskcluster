839d7f77330461045bf8d2b381490974ff541469
Pete Moore <pmoore@mozilla.com>
2019-02-06 17:02:28 +0100

    Version bump from 13.0.0 to 13.0.1

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ var (
 	logName = "public/logs/live_backing.log"
 	logPath = filepath.Join("generic-worker", "live_backing.log")
 
-	version  = "13.0.0"
+	version  = "13.0.1"
 	revision = "" // this is set during build with `-ldflags "-X main.revision=$(git rev-parse HEAD)"`
 )
 
