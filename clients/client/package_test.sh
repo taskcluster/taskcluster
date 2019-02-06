fe47c0be5d8c3727e3393381e0737b4822a45d5d
Dustin J. Mitchell <dustin@mozilla.com>
2019-02-06 22:12:24 +0000

    Bug 1523807 - check that published taskcluster-client can be used

---
+++ clients/client/package_test.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+
+set -e -x
+
+# test that the package can be installed without other dependencies in the monorepo,
+# by installing it in a temporary directory and checking that it can be require()d
+# and call an API method
+
+tmpdir=$(mktemp -d)
+trap "cd /; rm -rf $tmpdir" EXIT
+
+yarn pack --filename $tmpdir/client.tgz
+cd $tmpdir
+yarn add ./client.tgz
+node <<'EOF'
+const taskcluster = require('taskcluster-client');
+
+const main = async () => {
+    if (process.env.TASKCLUSTER_ROOT_URL) {
+        const auth = new taskcluster.Auth(taskcluster.fromEnvVars());
+        console.log(await auth.ping());
+    } else {
+        console.log('No TASKCLUSTER_ROOT_URL; not trying to call an API method');
+    }
+};
+main().then(console.log('Test OK!'), err => console.error(err));
+EOF
