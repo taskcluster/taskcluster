5c3165c88492e4541b10d0eb43612f1159bbaac8
owlishDeveloper <bugzeeeeee@gmail.com>
2019-02-17 18:58:05 -0800

    Declare exchanges

--- services/github/src/exchanges.js
+++ services/github/src/exchanges.js
@@ -3,8 +3,7 @@ const _ = require('lodash');
 const assert = require('assert');
 
 /** Build common routing key construct for `exchanges.declare` */
-const commonRoutingKey = function(options) {
-  options = options || {};
+const commonRoutingKey = function(options = {}) {
   let routingKey = [
     {
       name: 'routingKeyKind',
@@ -139,6 +138,40 @@ exchanges.declare({
   messageBuilder: commonMessageBuilder,
   routingKeyBuilder: msg => _.pick(msg, 'organization', 'repository'),
   CCBuilder: commonCCBuilder,
+});
+
+/** check run exchange */
+exchanges.declare({
+  exchange: 'check-run',
+  name: 'checkRun',
+  title: 'GitHub Check Run Event',
+  description: [
+    'When a GitHub check run event is posted it will be broadcast on this',
+    'exchange with the designated `organization` and `repository`',
+    'in the routing-key along with event specific metadata in the payload.',
+  ].join('\n'),
+  routingKey: commonRoutingKey({hasActions: true}),
+  schema: 'github-check-run-message.yml',
+  messageBuilder: commonMessageBuilder,
+  routingKeyBuilder: msg => _.pick(msg, 'organization', 'repository', 'action'),
+  CCBuilder: () => [],
+});
+
+/** check suite exchange */
+exchanges.declare({
+  exchange: 'check-suite',
+  name: 'checkSuite',
+  title: 'GitHub Check Suite Event',
+  description: [
+    'When a GitHub check suite event is posted it will be broadcast on this',
+    'exchange with the designated `organization` and `repository`',
+    'in the routing-key along with event specific metadata in the payload.',
+  ].join('\n'),
+  routingKey: commonRoutingKey({hasActions: true}),
+  schema: 'github-check-suite-message.syml',
+  messageBuilder: commonMessageBuilder,
+  routingKeyBuilder: msg => _.pick(msg, 'organization', 'repository', 'action'),
+  CCBuilder: () => [],
 });
 
 // Export exchanges
