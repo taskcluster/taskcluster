acf20cff6fc7fcee14912ba554a707001d425e42
Hassan Ali <helfi92@gmail.com>
2019-02-25 08:55:09 -0500

    Add required changes for migrating neutrino to 9.0.0-rc.0

--- ui/.neutrinorc.js
+++ ui/.neutrinorc.js
@@ -1,5 +1,4 @@
 const merge = require('deepmerge');
-const babelMerge = require('babel-merge');
 const { join, resolve } = require('path');
 
 require('@babel/register')({
@@ -8,9 +7,6 @@ require('@babel/register')({
 });
 
 const theme = require('./src/theme').default;
-
-const env = process.env.NODE_ENV;
-const isEnvProduction = env === 'production';
 
 module.exports = {
   options: {
@@ -112,10 +108,10 @@ module.exports = {
               ...options,
               plugins: options.plugins
                 // @babel/plugin-proposal-decorators needs to come before @babel/plugin-proposal-class-properties
-                .filter(plugin => !plugin[0].includes('plugin-proposal-class-properties')).concat([
+                .filter(plugin => !plugin[0].includes('plugin-proposal-class-properties'))
+                .concat([
                   [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
                   [require.resolve('@babel/plugin-proposal-class-properties'), { loose: true }],
-                  isEnvProduction ? require.resolve('babel-plugin-transform-react-remove-prop-types') : null
                 ]).filter(Boolean)
             }));
 
