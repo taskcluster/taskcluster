3694687a3cd0171439caffbbf146ffe1436e04dd
Varun Dey <varun_dey@symantec.com>
2019-02-26 00:48:28 +0530

    Removing redundant sign-in chooser

--- ui/src/components/SignInDialog/index.jsx
+++ ui/src/components/SignInDialog/index.jsx
@@ -1,16 +1,6 @@
 import React, { Component } from 'react';
 import { withApollo } from 'react-apollo';
 import { bool, func } from 'prop-types';
-import Avatar from '@material-ui/core/Avatar';
-import Dialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemText from '@material-ui/core/ListItemText';
-import GithubCircleIcon from 'mdi-react/GithubCircleIcon';
-import LoginVariantIcon from 'mdi-react/LoginVariantIcon';
 import { withAuth } from '../../utils/Auth';
 import CredentialsDialog from './CredentialsDialog';
 
@@ -20,10 +10,6 @@ export default class SignInDialog extends Component {
   static propTypes = {
     open: bool.isRequired,
     onClose: func.isRequired,
-  };
-
-  state = {
-    credentialsDialogOpen: false,
   };
 
   componentDidMount() {
@@ -43,14 +29,6 @@ export default class SignInDialog extends Component {
       false
     );
   }
-
-  handleCredentialsDialogClose = () => {
-    this.setState({ credentialsDialogOpen: false });
-  };
-
-  handleCredentialsDialogOpen = () => {
-    this.setState({ credentialsDialogOpen: true });
-  };
 
   handleCredentialsSignIn = async credentials => {
     const inOneWeek = new Date();
@@ -73,46 +51,13 @@ export default class SignInDialog extends Component {
 
   render() {
     const { onClose, open } = this.props;
-    const { credentialsDialogOpen } = this.state;
 
     return (
-      <Dialog
+      <CredentialsDialog
+        onSignIn={this.handleCredentialsSignIn}
         open={open}
         onClose={onClose}
-        aria-labelledby="sign-in-dialog-title">
-        <DialogTitle id="sign-in-dialog-title">Sign In</DialogTitle>
-        <DialogContent>
-          <List>
-            {process.env.LOGIN_STRATEGIES.includes('github') && (
-              <ListItem
-                button
-                component="a"
-                href="/login/github"
-                target="_blank">
-                <ListItemAvatar>
-                  <Avatar>
-                    <GithubCircleIcon />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText primary="Sign in with GitHub" />
-              </ListItem>
-            )}
-            <ListItem button onClick={this.handleCredentialsDialogOpen}>
-              <ListItemAvatar>
-                <Avatar>
-                  <LoginVariantIcon />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary="Sign in with credentials" />
-            </ListItem>
-          </List>
-          <CredentialsDialog
-            onSignIn={this.handleCredentialsSignIn}
-            open={credentialsDialogOpen}
-            onClose={this.handleCredentialsDialogClose}
-          />
-        </DialogContent>
-      </Dialog>
+      />
     );
   }
 }
