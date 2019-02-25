6ff75e02ab27e8271c95f8beb11f91a182719313
Dustin J. Mitchell <dustin@mozilla.com>
2019-02-25 21:33:01 +0000

    use non-deprecated github auth

--- services/github/src/github-auth.js
+++ services/github/src/github-auth.js
@@ -22,50 +22,31 @@ const retryPlugin = (octokit, options) => {
     }
   });
 };
-const Github = require('@octokit/rest').plugin([retryPlugin]);
+const Octokit = require('@octokit/rest').plugin([retryPlugin]);
 
 module.exports = async ({cfg}) => {
-  let github = new Github({
-    promise: Promise,
-  });
-
-  let setupToken = _ => {
-    let inteToken = jwt.sign(
+  const getIntegrationGithub = async () => {
+    const inteToken = jwt.sign(
       {iss: cfg.github.credentials.integrationId},
       cfg.github.credentials.privatePEM,
       {algorithm: 'RS256', expiresIn: '1m'},
     );
-    try {
-      github.authenticate({type: 'app', token: inteToken});
-    } catch (e) {
-      debug('Authentication as app failed!');
-      throw e;
-    }
-    return github;
+
+    return new Octokit({auth: `bearer ${inteToken}`});
+  };
+
+  const getInstallationGithub = async (inst_id) => {
+    const inteGithub = await getIntegrationGithub();
+    // Authenticating as installation
+    const instaToken = (await inteGithub.apps.createInstallationToken({
+      installation_id: inst_id,
+    })).data;
+    const instaGithub = new Octokit({auth: `token ${instaToken.token}`});
+    debug(`Authenticated as installation: ${inst_id}`);
+    return instaGithub;
   };
 
   // This object insures that the authentication is delayed until we need it.
   // Also, the authentication happens not just once in the beginning, but for each request.
-  return {
-    getIntegrationGithub: async _ => {
-      setupToken();
-      return github;
-    },
-    getInstallationGithub: async (inst_id) => {
-      setupToken();
-      // Authenticating as installation
-      var instaToken = (await github.apps.createInstallationToken({
-        installation_id: inst_id,
-      })).data;
-      let gh = new Github({promise: Promise});
-      try {
-        gh.authenticate({type: 'token', token: instaToken.token});
-        debug(`Authenticated as installation: ${inst_id}`);
-      } catch (e) {
-        debug('Authentication as app failed!');
-        throw e;
-      }
-      return gh;
-    },
-  };
+  return {getIntegrationGithub, getInstallationGithub};
 };
