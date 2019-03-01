22f24317cdd9ce1a85e0626d84af3914b4214d05
Hassan Ali <helfi92@gmail.com>
2019-03-01 11:25:37 -0500

    Use Markdown component from @mozilla-frontend-infra/components

--- ui/src/components/WorkerTypesTable/index.jsx
+++ ui/src/components/WorkerTypesTable/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import Markdown from '@mozilla-frontend-infra/components/Markdown';
 import { withStyles } from '@material-ui/core/styles';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
@@ -20,7 +21,6 @@ import StatusLabel from '../StatusLabel';
 import DateDistance from '../DateDistance';
 import TableCellListItem from '../TableCellListItem';
 import ConnectionDataTable from '../ConnectionDataTable';
-import Markdown from '../Markdown';
 import { VIEW_WORKER_TYPES_PAGE_SIZE } from '../../utils/constants';
 import sort from '../../utils/sort';
 import Link from '../../utils/Link';
