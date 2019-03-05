84a879a81f157061fe26b0ce084c3e480ae9c2b4
Andrew Erickson <aerickson@mozilla.com>
2019-03-05 12:17:44 -0800

    wip

--- services/queue/test/querytasks_test.js
+++ services/queue/test/querytasks_test.js
@@ -98,4 +98,80 @@ helper.secrets.mockSuite(__filename, ['taskcluster', 'aws', 'azure'], function(m
     );
     assume(r2.pendingTasks).equals(0);
   });
+
+  // aje
+  //
+  // process:
+  //   - add task
+  //   - check that initial value is empty as expected
+  //   - claim task
+  //   - verify that last_consumed value is expected
+  test('last_consumed works', async () => {
+    const taskDef = {
+      provisionerId: 'no-provisioner-extended-extended',
+      workerType: 'query-test-worker-extended-extended',
+      schedulerId: 'my-scheduler',
+      taskGroupId: 'dSlITZ4yQgmvxxAi4A8fHQ',
+      routes: [],
+      retries: 5,
+      created: taskcluster.fromNowJSON(),
+      deadline: taskcluster.fromNowJSON('2 minutes'),
+      scopes: [],
+      payload: {},
+      metadata: {
+        name: 'Unit testing task',
+        description: 'Task created during unit tests',
+        owner: 'jonsafj@mozilla.com',
+        source: 'https://github.com/taskcluster/taskcluster-queue',
+      },
+      tags: {
+        purpose: 'taskcluster-testing',
+      },
+    };
+
+    const taskId1 = slugid.v4();
+    const taskId2 = slugid.v4();
+
+    debug('### Create tasks');
+    await Promise.all([
+      helper.queue.createTask(taskId1, taskDef),
+      helper.queue.createTask(taskId2, taskDef),
+    ]);
+
+    // const r1 = await helper.queue.pendingTasks(
+    //   'no-provisioner-extended-extended',
+    //   'query-test-worker-extended-extended',
+    // );
+    // assume(r1.pendingTasks).is.greaterThan(1);
+
+    // // Result is cached for 20 seconds, so adding one more and checking should
+    // // give the same result, as we're not waiting for the timeout
+    // await helper.queue.createTask(taskId1, taskDef);
+
+    // // Note: There is some timing here, but since the queue.pendingTasks result
+    // // is cached it ought to be really fast and take less than 20 seconds to
+    // // do: queue.createTask + queue.pendingTasks, if not that's also sort of a
+    // // bug we should investigate
+    // const r2 = await helper.queue.pendingTasks(
+    //   'no-provisioner-extended-extended',
+    //   'query-test-worker-extended-extended',
+    // );
+    // assume(r2.pendingTasks).is.equals(r1.pendingTasks);
+
+    // // WARNING: The test below this point is not fast and certainly not robust
+    // // enough to run all the time. But it can be easily activated if messing
+    // // with queueservice.js and you want to ensure that it still works.
+    // // Just comment out the return statement below.
+    // return; // STOP TEST HERE
+    // console.log('WARNING: Unstable test running, should be disabled on master');
+    // await testing.poll(async () => {
+    //   // At some point in the future we have to got fetch a new result saying
+    //   // more tasks are now in the queue...
+    //   const r3 = await helper.queue.pendingTasks(
+    //     'no-provisioner-extended-extended',
+    //     'query-test-worker-extended-extended',
+    //   );
+    //   assume(r3.pendingTasks).is.greaterThan(r1.pendingTasks);
+    // }, 30, 1000);
+  });
 });
