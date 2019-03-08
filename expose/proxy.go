taskcluster/generic-worker

Bug 1436195 - implement exposing services publicly
This introduces a new package designed to expose services available on
the worker's loopback interface to the public Internet.  The services
can include both HTTP services, which are proxied directly, and TCP
services, which are made available via binary messages on a websocket.

expose/util/proxy.go
@@ -1,0 +1,73 @@
+package util
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"time"
+)
+
+type ExposeProxy interface {
+	// stop the proxy
+	Close() error
+}
+
+type httpProxy struct {
+	server   *http.Server
+}
+
+// Start a proxy running on all interfaces on this host, at an arbitrary port,
+// forwarding traffic to the given port on localhost.
+func ProxyHTTP(listener net.Listener, targetPort uint16) (ExposeProxy, error) {
+	targetURL, _ := url.Parse(fmt.Sprintf("http://127.0.0.1:%d", targetPort))
+	proxy := httputil.NewSingleHostReverseProxy(targetURL)
+
+	// We want to support streaming responses that are not detected as such (for
+	// example, a streaming logfile).  So, arrange to flush any buffered data after
+	// 100ms.
+	proxy.FlushInterval = 100 * time.Millisecond
+
+	server := &http.Server{
+		Handler: websockCompatibleHandlerFunc(proxy, targetPort),
+	}
+
+	go func() {
+		server.Serve(listener)
+	}()
+
+	return &httpProxy{server}, nil
+}
+
+func (p *httpProxy) Close() error {
+	err := p.server.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type tcpPortProxy struct {
+	server *http.Server
+}
+
+func ProxyTCPPort(listener net.Listener, targetPort uint16) (ExposeProxy, error) {
+	server := &http.Server{
+		Handler: websocketToTCPHandlerFunc(targetPort),
+	}
+
+	go func() {
+		server.Serve(listener)
+	}()
+
+	return &tcpPortProxy{server}, nil
+}
+
+func (p *tcpPortProxy) Close() error {
+	err := p.server.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}

expose/util/ws2tcp.go
@@ -1,0 +1,139 @@
+package util
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+var keepAliveInterval time.Duration
+
+// Accept websocket connections at path / and forward them to the local port.
+func websocketToTCPHandlerFunc(targetPort uint16) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+
+		if !websocket.IsWebSocketUpgrade(r) {
+			http.NotFound(w, r)
+			return
+		}
+
+		upgrader := websocket.Upgrader{
+			Subprotocols: websocket.Subprotocols(r),
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+		}
+
+		wsconn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Could not upgrade: %s", err), 500)
+			return
+		}
+		defer wsconn.Close()
+
+		// only one thing is allowed to write to the wsconn at a time
+		var writeLock sync.Mutex
+
+		tcpconn, err := net.Dial("tcp", fmt.Sprintf("127.0.0.1:%d", targetPort))
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Could not connect to TCP port: %s", err), 502)
+			return
+		}
+		defer tcpconn.Close()
+
+		// when it's time to tear down the connection, tear down both sides
+		kill := func() {
+			tcpconn.Close()
+			wsconn.Close()
+		}
+
+		// Handle control messages
+		wsconn.SetPingHandler(func(appData string) error {
+			writeLock.Lock()
+			defer writeLock.Unlock()
+			return wsconn.WriteControl(websocket.PongMessage, []byte(appData), time.Now().Add(5*time.Second))
+		})
+
+		// send keepalives every 10s
+		ticker := time.NewTicker(10 * time.Second)
+		go func() {
+			for {
+				<-ticker.C
+
+				writeLock.Lock()
+				err := wsconn.WriteControl(websocket.PingMessage, nil, time.Now().Add(5*time.Second))
+				writeLock.Unlock()
+				if err != nil {
+					kill()
+					return
+				}
+			}
+		}()
+		defer ticker.Stop()
+
+		wsconn.SetCloseHandler(func(code int, text string) error {
+			kill()
+			return nil
+		})
+
+		// bridge the two connections together, breaking the connection entirely
+		// when any piece fails and completing this function when both directions
+		// are closed
+		var wg sync.WaitGroup
+		wg.Add(2)
+
+		go func() {
+			defer kill()
+			for {
+				messageType, payload, err := wsconn.ReadMessage()
+				if err != nil {
+					// typically this means the connection was closed, but
+					// regardless of the error we want to close the TCP connection.
+					return
+				}
+				if messageType != websocket.BinaryMessage {
+					// this is a protocol error; we don't support Text
+					return
+				}
+
+				_, err = tcpconn.Write(payload)
+				if err != nil {
+					// An error occurred before the full payload could
+					// be written; close the connection
+					return
+				}
+			}
+		}()
+
+		go func() {
+			defer kill()
+			var buf []byte = make([]byte, 1024*512)
+			for {
+				n, err := tcpconn.Read(buf)
+				if err != nil {
+					return
+				}
+				var data = buf[:n]
+
+				writeLock.Lock()
+				err = wsconn.WriteMessage(websocket.BinaryMessage, data)
+				writeLock.Unlock()
+				if err != nil {
+					return
+				}
+			}
+		}()
+
+		wg.Wait()
+
+		return
+	}
+}

expose/util/wsproxy.go
@@ -1,0 +1,206 @@
+package util
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httputil"
+	"sync/atomic"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+// httputil.ReverseProxy in Go1.10 doesn't support websockets, so we intercept
+// that particular situation and do so manually.  This can all be removed once
+// generic-worker is upgraded to Go1.12 or higher.
+
+// What follows is based on websocktunnel's wsmux/wsmux.go
+type proxy struct {
+	targetPort uint16
+}
+
+var debug = false
+
+func (p *proxy) logf(format string, v ...interface{}) {
+	if debug {
+		fmt.Printf(format+"\n", v...)
+	}
+}
+
+func (p *proxy) logerrorf(format string, v ...interface{}) {
+	if debug {
+		fmt.Errorf(format+"\n", v...)
+	}
+}
+
+func (p *proxy) websocketProxy(w http.ResponseWriter, r *http.Request) error {
+	// at this point, we are sure that r is a http websocket upgrade request
+	p.logf("creating WS bridge: path=%s", r.URL.RequestURI())
+	dialer := &websocket.Dialer{
+		Subprotocols: websocket.Subprotocols(r),
+	}
+
+	// create new header
+	// copy request headers to new header. Avoid websocket headers
+	// as these will be added by Dial
+	reqHeader := make(http.Header)
+	for k, v := range r.Header {
+		if k == "Upgrade" ||
+			k == "Connection" ||
+			k == "Sec-Websocket-Key" ||
+			k == "Sec-Websocket-Version" ||
+			k == "Sec-Websocket-Extensions" ||
+			k == "Sec-Websocket-Protocol" {
+			continue
+		}
+		reqHeader[k] = v
+	}
+
+	uri := fmt.Sprintf("ws://127.0.0.1:%d", p.targetPort)
+	p.logf("dialing ws at: %s", uri)
+	tunnelConn, _, err := dialer.Dial(uri, reqHeader)
+	if err != nil {
+		p.logerrorf("could not dial tunnel: path=%s, error: %v", r.URL.RequestURI(), err)
+		return err
+	}
+
+	upgrader := websocket.Upgrader{
+		Subprotocols: websocket.Subprotocols(r),
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	viewerConn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		p.logerrorf("could not upgrade client connection: path=%s, error: %v", r.URL.RequestURI(), err)
+		// close tunnel connection
+		_ = tunnelConn.Close()
+		return err
+	}
+	p.logf("initiating connection bridge")
+
+	// bridge both websocket connections
+	bridgeErr := p.bridgeConn(tunnelConn, viewerConn)
+	if bridgeErr != nil {
+		p.logerrorf("bridge closed with err: %v", bridgeErr)
+	}
+
+	if bridgeErr != nil {
+		return bridgeErr
+	}
+	return err
+}
+
+func (p *proxy) bridgeConn(conn1 *websocket.Conn, conn2 *websocket.Conn) error {
+	// set ping and pong handlers
+	conn1.SetPingHandler(forwardControl(websocket.PingMessage, conn2))
+	conn2.SetPingHandler(forwardControl(websocket.PingMessage, conn1))
+
+	conn1.SetPongHandler(forwardControl(websocket.PongMessage, conn2))
+	conn2.SetPongHandler(forwardControl(websocket.PongMessage, conn1))
+
+	// set close handlers
+	conn1.SetCloseHandler(func(code int, text string) error {
+		return nil
+	})
+	conn2.SetCloseHandler(func(code int, text string) error {
+		return nil
+	})
+
+	// ensure connections are closed after bridge exits
+	defer func() {
+		_ = conn1.Close()
+		_ = conn2.Close()
+	}()
+
+	kill := make(chan bool, 1)
+	var eSrc, eDest atomic.Value
+	// Wait until errors are written
+
+	go func() {
+		err := copyWsData(conn1, conn2, kill)
+		if err != nil {
+			eSrc.Store(err)
+		}
+	}()
+	go func() {
+		err := copyWsData(conn2, conn1, kill)
+		if err != nil {
+			eDest.Store(err)
+		}
+	}()
+
+	<-kill
+	if err, ok := eSrc.Load().(error); ok && err != nil {
+		return err
+	}
+	if err, ok := eDest.Load().(error); ok && err != nil {
+		return err
+	}
+	return nil
+}
+
+func copyWsData(dest *websocket.Conn, src *websocket.Conn, kill chan bool) error {
+	defer asyncClose(kill)
+	for {
+		mtype, reader, err := src.NextReader()
+		if err != nil {
+			// Abnormal closure occurs if the websocket is over a wsmux stream
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseNormalClosure,
+				websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				return err
+			}
+			return nil
+		}
+		writer, err := dest.NextWriter(mtype)
+		if err != nil {
+			// Abnormal closure occurs if the websocket is over a wsmux stream
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseNormalClosure,
+				websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				return err
+			}
+			return nil
+		}
+		_, err = io.Copy(writer, reader)
+		_ = writer.Close()
+		if err != nil {
+			return err
+		}
+
+		select {
+		case <-kill:
+			return nil
+		default:
+		}
+	}
+}
+
+func forwardControl(messageType int, dest *websocket.Conn) func(string) error {
+	return func(appData string) error {
+		return dest.WriteControl(messageType, []byte(appData), time.Now().Add(20*time.Second))
+	}
+}
+
+func asyncClose(kill chan bool) {
+	select {
+	case <-kill:
+	default:
+		close(kill)
+	}
+}
+
+// the main entry point: generate an http.HandlerFunc that will mostly just use
+// the given ReverseProxy, but will handle websockets, too.
+func websockCompatibleHandlerFunc(reverseproxy *httputil.ReverseProxy, targetPort uint16) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if websocket.IsWebSocketUpgrade(r) {
+			p := proxy{targetPort: targetPort}
+			_ = p.websocketProxy(w, r)
+		}
+
+		// not a websocket thing; defer to ReverseProxy
+		reverseproxy.ServeHTTP(w, r)
+		return
+	}
+}

taskcluster/taskcluster

expose/proxy.go
@@ -1,0 +1,73 @@
+package expose
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"time"
+)
+
+type exposeProxy interface {
+	// stop the proxy
+	Close() error
+}
+
+type httpProxy struct {
+	server *http.Server
+}
+
+// Start a proxy running on all interfaces on this host, at an arbitrary port,
+// forwarding traffic to the given port on localhost.
+func proxyHTTP(listener net.Listener, targetPort uint16) (exposeProxy, error) {
+	targetURL, _ := url.Parse(fmt.Sprintf("http://127.0.0.1:%d", targetPort))
+	proxy := httputil.NewSingleHostReverseProxy(targetURL)
+
+	// We want to support streaming responses that are not detected as such (for
+	// example, a streaming logfile).  So, arrange to flush any buffered data after
+	// 100ms.
+	proxy.FlushInterval = 100 * time.Millisecond
+
+	server := &http.Server{
+		Handler: websockCompatibleHandlerFunc(proxy, targetPort),
+	}
+
+	go func() {
+		server.Serve(listener)
+	}()
+
+	return &httpProxy{server}, nil
+}
+
+func (p *httpProxy) Close() error {
+	err := p.server.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type tcpPortProxy struct {
+	server *http.Server
+}
+
+func proxyTCPPort(listener net.Listener, targetPort uint16) (exposeProxy, error) {
+	server := &http.Server{
+		Handler: websocketToTCPHandlerFunc(targetPort),
+	}
+
+	go func() {
+		server.Serve(listener)
+	}()
+
+	return &tcpPortProxy{server}, nil
+}
+
+func (p *tcpPortProxy) Close() error {
+	err := p.server.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}

expose/ws2tcp.go
@@ -1,0 +1,139 @@
+package expose
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+var keepAliveInterval time.Duration
+
+// Accept websocket connections at path / and forward them to the local port.
+func websocketToTCPHandlerFunc(targetPort uint16) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+
+		if !websocket.IsWebSocketUpgrade(r) {
+			http.NotFound(w, r)
+			return
+		}
+
+		upgrader := websocket.Upgrader{
+			Subprotocols: websocket.Subprotocols(r),
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+		}
+
+		wsconn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Could not upgrade: %s", err), 500)
+			return
+		}
+		defer wsconn.Close()
+
+		// only one thing is allowed to write to the wsconn at a time
+		var writeLock sync.Mutex
+
+		tcpconn, err := net.Dial("tcp", fmt.Sprintf("127.0.0.1:%d", targetPort))
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Could not connect to TCP port: %s", err), 502)
+			return
+		}
+		defer tcpconn.Close()
+
+		// when it's time to tear down the connection, tear down both sides
+		kill := func() {
+			tcpconn.Close()
+			wsconn.Close()
+		}
+
+		// Handle control messages
+		wsconn.SetPingHandler(func(appData string) error {
+			writeLock.Lock()
+			defer writeLock.Unlock()
+			return wsconn.WriteControl(websocket.PongMessage, []byte(appData), time.Now().Add(5*time.Second))
+		})
+
+		// send keepalives every 10s
+		ticker := time.NewTicker(10 * time.Second)
+		go func() {
+			for {
+				<-ticker.C
+
+				writeLock.Lock()
+				err := wsconn.WriteControl(websocket.PingMessage, nil, time.Now().Add(5*time.Second))
+				writeLock.Unlock()
+				if err != nil {
+					kill()
+					return
+				}
+			}
+		}()
+		defer ticker.Stop()
+
+		wsconn.SetCloseHandler(func(code int, text string) error {
+			kill()
+			return nil
+		})
+
+		// bridge the two connections together, breaking the connection entirely
+		// when any piece fails and completing this function when both directions
+		// are closed
+		var wg sync.WaitGroup
+		wg.Add(2)
+
+		go func() {
+			defer kill()
+			for {
+				messageType, payload, err := wsconn.ReadMessage()
+				if err != nil {
+					// typically this means the connection was closed, but
+					// regardless of the error we want to close the TCP connection.
+					return
+				}
+				if messageType != websocket.BinaryMessage {
+					// this is a protocol error; we don't support Text
+					return
+				}
+
+				_, err = tcpconn.Write(payload)
+				if err != nil {
+					// An error occurred before the full payload could
+					// be written; close the connection
+					return
+				}
+			}
+		}()
+
+		go func() {
+			defer kill()
+			var buf []byte = make([]byte, 1024*512)
+			for {
+				n, err := tcpconn.Read(buf)
+				if err != nil {
+					return
+				}
+				var data = buf[:n]
+
+				writeLock.Lock()
+				err = wsconn.WriteMessage(websocket.BinaryMessage, data)
+				writeLock.Unlock()
+				if err != nil {
+					return
+				}
+			}
+		}()
+
+		wg.Wait()
+
+		return
+	}
+}

expose/wsproxy.go
@@ -1,0 +1,200 @@
+package expose
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httputil"
+	"sync/atomic"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+// httputil.ReverseProxy in Go1.10 doesn't support websockets, so we intercept
+// that particular situation and do so manually.  This can all be removed once
+// generic-worker is upgraded to Go1.12 or higher.
+
+// What follows is based on websocktunnel's wsmux/wsmux.go
+type proxy struct {
+	targetPort uint16
+}
+
+var debug = false
+
+func (p *proxy) logf(format string, v ...interface{}) {
+	if debug {
+		fmt.Printf(format+"\n", v...)
+	}
+}
+
+func (p *proxy) websocketProxy(w http.ResponseWriter, r *http.Request) error {
+	// at this point, we are sure that r is a http websocket upgrade request
+	p.logf("creating WS bridge: path=%s", r.URL.RequestURI())
+	dialer := &websocket.Dialer{
+		Subprotocols: websocket.Subprotocols(r),
+	}
+
+	// create new header
+	// copy request headers to new header. Avoid websocket headers
+	// as these will be added by Dial
+	reqHeader := make(http.Header)
+	for k, v := range r.Header {
+		if k == "Upgrade" ||
+			k == "Connection" ||
+			k == "Sec-Websocket-Key" ||
+			k == "Sec-Websocket-Version" ||
+			k == "Sec-Websocket-Extensions" ||
+			k == "Sec-Websocket-Protocol" {
+			continue
+		}
+		reqHeader[k] = v
+	}
+
+	uri := fmt.Sprintf("ws://127.0.0.1:%d", p.targetPort)
+	p.logf("dialing ws at: %s", uri)
+	tunnelConn, _, err := dialer.Dial(uri, reqHeader)
+	if err != nil {
+		p.logf("could not dial tunnel: path=%s, error: %v", r.URL.RequestURI(), err)
+		return err
+	}
+
+	upgrader := websocket.Upgrader{
+		Subprotocols: websocket.Subprotocols(r),
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	viewerConn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		p.logf("could not upgrade client connection: path=%s, error: %v", r.URL.RequestURI(), err)
+		// close tunnel connection
+		_ = tunnelConn.Close()
+		return err
+	}
+	p.logf("initiating connection bridge")
+
+	// bridge both websocket connections
+	bridgeErr := p.bridgeConn(tunnelConn, viewerConn)
+	if bridgeErr != nil {
+		p.logf("bridge closed with err: %v", bridgeErr)
+	}
+
+	if bridgeErr != nil {
+		return bridgeErr
+	}
+	return err
+}
+
+func (p *proxy) bridgeConn(conn1 *websocket.Conn, conn2 *websocket.Conn) error {
+	// set ping and pong handlers
+	conn1.SetPingHandler(forwardControl(websocket.PingMessage, conn2))
+	conn2.SetPingHandler(forwardControl(websocket.PingMessage, conn1))
+
+	conn1.SetPongHandler(forwardControl(websocket.PongMessage, conn2))
+	conn2.SetPongHandler(forwardControl(websocket.PongMessage, conn1))
+
+	// set close handlers
+	conn1.SetCloseHandler(func(code int, text string) error {
+		return nil
+	})
+	conn2.SetCloseHandler(func(code int, text string) error {
+		return nil
+	})
+
+	// ensure connections are closed after bridge exits
+	defer func() {
+		_ = conn1.Close()
+		_ = conn2.Close()
+	}()
+
+	kill := make(chan bool, 1)
+	var eSrc, eDest atomic.Value
+	// Wait until errors are written
+
+	go func() {
+		err := copyWsData(conn1, conn2, kill)
+		if err != nil {
+			eSrc.Store(err)
+		}
+	}()
+	go func() {
+		err := copyWsData(conn2, conn1, kill)
+		if err != nil {
+			eDest.Store(err)
+		}
+	}()
+
+	<-kill
+	if err, ok := eSrc.Load().(error); ok && err != nil {
+		return err
+	}
+	if err, ok := eDest.Load().(error); ok && err != nil {
+		return err
+	}
+	return nil
+}
+
+func copyWsData(dest *websocket.Conn, src *websocket.Conn, kill chan bool) error {
+	defer asyncClose(kill)
+	for {
+		mtype, reader, err := src.NextReader()
+		if err != nil {
+			// Abnormal closure occurs if the websocket is over a wsmux stream
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseNormalClosure,
+				websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				return err
+			}
+			return nil
+		}
+		writer, err := dest.NextWriter(mtype)
+		if err != nil {
+			// Abnormal closure occurs if the websocket is over a wsmux stream
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseNormalClosure,
+				websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				return err
+			}
+			return nil
+		}
+		_, err = io.Copy(writer, reader)
+		_ = writer.Close()
+		if err != nil {
+			return err
+		}
+
+		select {
+		case <-kill:
+			return nil
+		default:
+		}
+	}
+}
+
+func forwardControl(messageType int, dest *websocket.Conn) func(string) error {
+	return func(appData string) error {
+		return dest.WriteControl(messageType, []byte(appData), time.Now().Add(20*time.Second))
+	}
+}
+
+func asyncClose(kill chan bool) {
+	select {
+	case <-kill:
+	default:
+		close(kill)
+	}
+}
+
+// the main entry point: generate an http.HandlerFunc that will mostly just use
+// the given ReverseProxy, but will handle websockets, too.
+func websockCompatibleHandlerFunc(reverseproxy *httputil.ReverseProxy, targetPort uint16) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if websocket.IsWebSocketUpgrade(r) {
+			p := proxy{targetPort: targetPort}
+			_ = p.websocketProxy(w, r)
+		}
+
+		// not a websocket thing; defer to ReverseProxy
+		reverseproxy.ServeHTTP(w, r)
+		return
+	}
+}