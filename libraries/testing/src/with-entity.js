9eefb843c6e60a7227f90aea13f9db71fdd1d5cc
Dustin J. Mitchell <dustin@mozilla.com>
2019-03-11 15:40:58 +0000

    Bug 1533937 - use a datestamp in the table suffix

--- libraries/testing/src/with-entity.js
+++ libraries/testing/src/with-entity.js
@@ -2,8 +2,14 @@ const {sasCredentials} = require('taskcluster-lib-azure');
 const slugid = require('slugid');
 
 // a suffix used to generate unique table names so that parallel test runs do not
-// interfere with one another.  We remove these at the end of the test run.
-const TABLE_SUFFIX = slugid.nice().replace(/[_-]/g, '');
+// interfere with one another.  The table name includes the date so that old tables
+// can be removed by an out-of-band process (as CI for services does not have
+// permission to remove tables).
+const TABLE_SUFFIX = (() => {
+  const date = new Date().toJSON().split('T')[0].replace(/-/g, '');
+  const rand = slugid.nice().replace(/[_-]/g, '').slice(0, 8);
+  return `T${date}T${rand}`;
+})();
 
 // withEntity: monkey-patch an entity class to either use inmemory data
 // or to use a unique table name, and set up to ensure the table exists and
