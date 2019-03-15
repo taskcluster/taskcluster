ae45db83fbcf5947d271b7674718dc8fd5932469
Hassan Ali <helfi92@gmail.com>
2019-03-15 11:41:21 -0400

    Add esm output to client-web

--- clients/client-web/.neutrinorc.js
+++ clients/client-web/.neutrinorc.js
@@ -1,45 +1,69 @@
-const { merge } = require('@neutrinojs/compile-loader');
+const name = 'taskcluster';
+const envs = ['@neutrinojs/env', ['NODE_ENV', 'TASKCLUSTER_ROOT_URL']];
+const lint = ['neutrino-preset-mozilla-frontend-infra/node-lint', {
+  rules: {
+    'no-underscore-dangle': 'off'
+  }
+}];
+
+const middleware = (neutrino) => {
+  const { merge } = require('@neutrinojs/compile-loader');
+
+  if (process.env.NODE_ENV === 'test') {
+    neutrino.config.devtool('inline-source-map');
+  } else {
+    neutrino.config.devtool('source-map');
+    neutrino.config.externals({
+      hawk: 'hawk',
+      'crypto-js': 'crypto-js',
+      'query-string': {
+        commonjs: 'query-string',
+        commonjs2: 'query-string',
+        amd: 'query-string',
+        root: 'queryString'
+      }
+    });
+  }
+
+  neutrino.config.resolve.alias.set('hawk', 'hawk/dist/browser.js');
+  neutrino.config.module
+    .rule('compile')
+    .use('babel')
+    .tap(options => merge(options, {
+      plugins: [
+        require.resolve('babel-plugin-transform-object-rest-spread'),
+        [require.resolve('babel-plugin-transform-class-properties'), { spec: true }],
+      ]
+    }));
+
+  if (process.env.NODE_ENV === 'test') {
+    neutrino.use('@neutrinojs/karma');
+  }
+};
 
 module.exports = {
   use: [
-    ['neutrino-preset-mozilla-frontend-infra/node-lint', {
-      rules: {
-        'no-underscore-dangle': 'off'
+    ['@neutrinojs/fork', {
+      configs: {
+        umd: {
+          use: [
+            lint,
+            ['@neutrinojs/library', { name }],
+            envs,
+            middleware,
+          ]
+        },
+        esm: {
+          options: {
+            output: 'build/esm',
+          },
+          use: [
+            ['@neutrinojs/library', { name, libraryTarget: 'var' }],
+            envs,
+            middleware,
+          ]
+        },
       }
     }],
-    ['@neutrinojs/library', { name: 'taskcluster' }],
-    ['@neutrinojs/env', ['NODE_ENV', 'TASKCLUSTER_ROOT_URL']],
-    (neutrino) => {
-      if (process.env.NODE_ENV === 'test') {
-        neutrino.config.devtool('inline-source-map');
-      } else {
-        neutrino.config.devtool('source-map');
-        neutrino.config.externals({
-          hawk: 'hawk',
-          'crypto-js': 'crypto-js',
-          'query-string': {
-            commonjs: 'query-string',
-            commonjs2: 'query-string',
-            amd: 'query-string',
-            root: 'queryString'
-          }
-        });
-      }
-
-      neutrino.config.resolve.alias.set('hawk', 'hawk/dist/browser.js');
-      neutrino.config.module
-        .rule('compile')
-        .use('babel')
-        .tap(options => merge(options, {
-          plugins: [
-            require.resolve('babel-plugin-transform-object-rest-spread'),
-            [require.resolve('babel-plugin-transform-class-properties'), { spec: true }],
-          ]
-        }));
-
-      if (process.env.NODE_ENV === 'test') {
-        neutrino.use('@neutrinojs/karma');
-      }
-    },
-  ],
+  ]
 };
