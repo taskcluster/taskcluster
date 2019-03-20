e9fa80ede11f3f33062e6f376d5abe04fae2209a
owlishDeveloper <bugzeeeeee@gmail.com>
2019-03-19 22:18:24 -0700

    Add more owlish understanding of provider

---
+++ services/worker-manager/src/providers/gcpprovider.js
@@ -0,0 +1,148 @@
+'use strict';
+
+const {Bid} = require('../bid');
+const {Provider} = require('../provider');
+const {Worker} = require('../worker');
+
+class GCPProvider extends Provider {
+
+  /**
+   * Construct a GCP Provider
+   */
+  constructor({id}) {
+    super({id});
+  }
+
+  /**
+   * this is called in provisioner
+   *
+   * @param states Array<String>
+   * @param workerTypes Array<String>
+   * @returns {Promise<Array<Worker>>}
+   */
+  async listWorkers({states, workerTypes}) {
+    console.log(`Got states: ${states}, and worker types: ${workerTypes}`);
+
+    const workersInAllStates = workerTypes.flatMap(wt => {
+      let gcpInfo = gcp.getWrokersOfType(wt); // pseudocode start
+      // this whole part may actually be wrong....maybe we don't map over gcpInfo.....capacity is just .length of smth
+      // also, I see the potential to use this.queryWorkerState and this.workerInfo here
+      return gcpInfo.map(i => new Worker({
+        id: i.id,
+        group: i.group,
+        workerType: i.workerType,
+        state: i.state,
+        capacity: i.capacity,
+        workerConfigurationId: i.workerConfigurationId, // ???
+        providerData: i.fluff, // pseudocode end
+      }));
+    });
+
+    const workersInNeededStates = workersInAllStates.filter(
+      wt => states.includes(wt.state)
+    );
+
+    return workersInNeededStates;
+  }
+
+  async queryWorkerState({workerId}) {
+    console.log(`Got worker id: ${workerId}`);
+    return 'This should be a string';
+  }
+
+  /**
+   * I'm not sure why this isn't async
+   * @param worker
+   */
+  workerInfo({worker}) {
+    console.log(`Got worker: ${worker}`);
+    return {};
+  }
+
+  async initiate() {
+    console.log('😳');
+  }
+
+  async terminate() {
+    console.log('😷');
+  }
+
+  /**
+   * This is called in provisioner
+   *
+   * @param workerType String
+   * @param workerConfiguration Proxy<WorkerConfiguration>
+   * @param demand Number
+   * @returns {Promise<Array<Bid>>}
+   */
+  async proposeBids({workerType, workerConfiguration, demand}) {
+    console.log(`Got worker type: ${workerType}, worker config: ${workerConfiguration}, demand: ${demand}`);
+    return [
+      new Bid({
+        providerId: this.id,
+        workerType,
+        workerConfigurationId: workerConfiguration.id,
+        expires, // ???
+        price, // ???
+        capacity, // ???
+        utilityFactor, // ???
+        firm, // ???
+        reliability, // ???
+        estimatedDelay, // ???
+        providerData, // ???
+      }),
+    ];
+  }
+
+  /**
+   * This is called in the provisioner
+   *
+   * @param Array<Bid> (accepted bids for this provider)
+   * @returns void
+   */
+  async submitBids({bids}) {
+    console.log(`Got bids: ${bids}. Submitting...`);
+  }
+
+  /**
+   * This is called in provisioner. Actually seems to be optional to implement
+   *
+   * @param bids Array<Bid> (all the bids are for this provider)
+   * @returns void
+   */
+  async rejectBids({bids}) {
+    console.log(`Got bids: ${bids}. Rejecting...`);
+  }
+
+  /**
+   * This method must not return until the request to terminate all workers is completed.
+   * @returns {Promise<void>}
+   */
+  async terminateAllWorkers() {
+    console.log(`Terminating workers... Please hold`);
+    return;
+  }
+
+  /**
+   *
+   * @param workerType
+   * @returns {Promise<void>}
+   */
+  async terminateWorkerType({workerType}) {
+    console.log(`Got worker type: ${workerType}. Terminating`);
+  }
+
+  /**
+   *
+   * @param workers
+   * @returns {Promise<void>}
+   */
+  async terminateWorkers({workers}) {
+    console.log(`Got workers: ${workers}. Terminating...`);
+  }
+
+}
+
+module.exports = {
+  GCPProvider,
+};
