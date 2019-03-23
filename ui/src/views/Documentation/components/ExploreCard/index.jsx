bce0d8df52fcc4c6b273a8ba644dcca3185a689b
Hassan Ali <helfi92@gmail.com>
2019-03-23 13:30:03 -0400

    Make card clickable instead of "See more" button

--- ui/src/views/Documentation/components/ExploreCard/index.jsx
+++ ui/src/views/Documentation/components/ExploreCard/index.jsx
@@ -4,14 +4,16 @@ import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Button from '../../../../components/Button';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import Anchor from '../Anchor';
 
 const useStyles = withStyles(theme => ({
   root: {
     position: 'relative',
     height: theme.spacing.unit * 25,
+  },
+  cardActionArea: {
+    height: '100%',
   },
   titleCardContent: {
     display: 'flex',
@@ -20,31 +22,25 @@ const useStyles = withStyles(theme => ({
   title: {
     marginLeft: theme.spacing.unit,
   },
-  cardActions: {
-    borderTop: `1px solid ${theme.palette.divider}`,
-    position: 'absolute',
-    bottom: 0,
-    width: '100%',
-  },
 }));
 
 function ExploreCard({ classes, to, title, description, icon, ...props }) {
   return (
     <Card classes={{ root: classes.root }} {...props}>
-      <CardContent className={classes.titleCardContent}>
-        {icon}
-        <Typography variant="title" className={classes.title}>
-          {title}
-        </Typography>
-      </CardContent>
-      <CardContent>
-        <Typography>{description}</Typography>
-      </CardContent>
-      <CardActions className={classes.cardActions}>
-        <Button component={Anchor} href={to} size="small">
-          See More
-        </Button>
-      </CardActions>
+      <CardActionArea
+        className={classes.cardActionArea}
+        component={Anchor}
+        href={to}>
+        <CardContent className={classes.titleCardContent}>
+          {icon}
+          <Typography variant="title" className={classes.title}>
+            {title}
+          </Typography>
+        </CardContent>
+        <CardContent>
+          <Typography>{description}</Typography>
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 }
