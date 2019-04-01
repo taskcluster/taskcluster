2d4e8e904c7fe7630452b949a191e29cc4614aa1
Brian Stack <bstack@mozilla.com>
2019-04-01 14:14:07 -0700

    Start writing out helm config

--- infrastructure/builder/src/generate/generators/procfiles.js
+++ infrastructure/builder/src/generate/generators/procfiles.js
@@ -1,7 +1,10 @@
+const assert = require('assert');
 const path = require('path');
-const {services, readYAML, writeFile} = require('../util');
+const {services, readYAML, writeJSON, writeFile} = require('../util');
 
 const SERVICES = services();
+
+const PROC_TYPES = ['web', 'heroku-only', 'cron', 'build', 'background'];
 
 exports.tasks = [];
 
@@ -26,8 +29,39 @@ SERVICES.forEach(name => {
       await writeFile(path.join('services', name, 'Procfile'), res.join('\n'));
 
       return {
-        [`procs-${name}`]: procs,
+        [`procs-${name}`]: {procs, name},
       };
     },
   });
+});
+
+exports.tasks.push({
+  title: `Generate Helm values.yaml`,
+  requires: SERVICES.map(name => `procs-${name}`),
+  provides: [
+    'helm-values',
+  ],
+  run: async (requirements, utils) => {
+    const values = {
+      web: [],
+      background: [],
+      cron: [],
+      secrets: [],
+    };
+    Object.values(requirements).forEach(({name, procs}) => {
+      Object.entries(procs).forEach(([proc, meta]) => {
+        assert(PROC_TYPES.includes(meta.type), `Process type ${meta.type} not recognized.`);
+        if (['heroku-only', 'build'].includes(meta.type)) {
+          return;
+        }
+        const {type, ...res} = meta;
+        values[meta.type].push({...res, service: name, proc});
+      });
+    });
+
+    await writeJSON(path.join('infrastructure', 'helm', 'values.yaml'), values);
+    return {
+      'helm-values': values,
+    };
+  },
 });
