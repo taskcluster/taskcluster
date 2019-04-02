e46d3c87bcfaf50e9d983e6c39725ce9c31e916b
Ojaswin <ojaswin@hotmail.com>
2019-04-02 18:37:24 +0530

    [Bug 1520918] [UI] Add UI for the denylist (#311)

---
+++ ui/src/views/Denylist/routes.js
@@ -0,0 +1,26 @@
+import lazy from '../../utils/lazy';
+
+const ViewDenylistAddress = lazy(() =>
+  import(/* webpackChunkName: 'Denylist.ViewDenylistAddress' */ './ViewDenylistAddress')
+);
+const ViewDenylistAddresses = lazy(() =>
+  import(/* webpackChunkName: 'Denylist.ViewDenylistAddresses' */ './ViewDenylistAddresses')
+);
+
+export default path => [
+  {
+    component: ViewDenylistAddress,
+    path: `${path}/add`,
+    isNewAddress: true,
+  },
+  {
+    component: ViewDenylistAddress,
+    path: `${path}/:notificationAddress`,
+  },
+  {
+    component: ViewDenylistAddresses,
+    path,
+    description:
+      'Manage the notifications denylist. This page allows you to view or delete the denylisted notification addresses.',
+  },
+];
