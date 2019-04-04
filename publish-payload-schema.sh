6beb5a9a41ea5a4e2bc80c8fabca23be719bb4c8
Pete Moore <pmoore@mozilla.com>
2019-04-04 13:19:59 +0200

    Fix publish-payload-schema.sh

--- publish-payload-schema.sh
+++ publish-payload-schema.sh
@@ -1,15 +1,22 @@
 #!/bin/bash -e
 
+function pub {
+  dest="${1}"
+  src="${2}"
+  payload_file="$(mktemp -t payload.json.XXXXXX)"
+  cat "${src}" | "$(go env GOPATH)/bin/yamltojson" > "${payload_file}"
+  aws s3 cp "${payload_file}" "s3://schemas.taskcluster.net/generic-worker/v1/${dest}"
+  rm "${payload_file}"
+  curl "https://schemas.taskcluster.net/generic-worker/v1/${dest}"
+}
+
 cd "$(dirname "${0}")"
 go get github.com/taskcluster/generic-worker/yamltojson
 
-for build in docker_linux native_linux native_windows native_darwin; do
-  payload_file="$(mktemp -t payload.json.XXXXXX)"
-  cat "${build}.yml" | "$(go env GOPATH)/bin/yamltojson" > "${payload_file}"
-  aws s3 cp "${payload_file}" "s3://schemas.taskcluster.net/generic-worker/v1/${build}.json"
-  rm "${payload_file}"
-  curl "https://schemas.taskcluster.net/generic-worker/v1/${build}.json"
-done
+pub docker_linux.json   docker_linux.yml
+pub native_linux.json   native_all-unix-style.yml
+pub native_windows.json native_windows.yml
+pub native_darwin.json  native_all-unix-style.yml
 
 echo
 echo "Schema updated - check out https://docs.taskcluster.net/reference/workers/generic-worker/payload"
