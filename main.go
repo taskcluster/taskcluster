f264397879ab9cb3b67e1f83d332859d4692a7ad
Pete Moore <pmoore@mozilla.com>
2019-04-08 18:39:53 +0200

    Version bump from 14.0.0 to 14.0.1

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ var (
 	logName = "public/logs/live_backing.log"
 	logPath = filepath.Join("generic-worker", "live_backing.log")
 
-	version  = "14.0.0"
+	version  = "14.0.1"
 	revision = "" // this is set during build with `-ldflags "-X main.revision=$(git rev-parse HEAD)"`
 )
 
