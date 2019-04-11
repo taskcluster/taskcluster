0268da0a3eeb8da11965a2b9e28664a09e74f7d3
owlishDeveloper <bugzeeeeee@gmail.com>
2019-04-11 15:34:30 -0700

    Define Worker Types table view

--- ui/src/views/WorkerManager/WMViewWorkerTypes/index.jsx
+++ ui/src/views/WorkerManager/WMViewWorkerTypes/index.jsx
@@ -1,10 +1,51 @@
 import { hot } from 'react-hot-loader';
 import React, { Component } from 'react';
 import Dashboard from '../../../components/Dashboard';
+import workerTypesQuery from './WMWorkerTypes.graphql';
+import {graphql} from 'react-apollo';
+import Spinner from '@mozilla-frontend-infra/components/Spinner';
+import ErrorPanel from '../../../components/ErrorPanel';
+import WMWorkerTypesTable from '../../../components/WMWorkerTypesTable';
+import Search from '../../../components/Search';
 
 @hot(module)
-export default class WorkerManager extends Component {
+@graphql(workerTypesQuery)
+export default class WMWorkerTypesView extends Component {
+  state = {
+    workerTypeSearch: '',
+  };
+
+  handleWorkerTypeSearchSubmit = workerTypeSearch => {
+    this.setState({ workerTypeSearch });
+  };
+
   render() {
-    return <Dashboard title="Hello" />;
+    const {data: {loading, error, WMWorkerTypeSummaries}} = this.props;
+    const { workerTypeSearch } = this.state;
+
+    return (
+      <Dashboard
+        title="Worker Types"
+        search={
+          <Search
+            disabled={loading}
+            onSubmit={this.handleWorkerTypeSearchSubmit}
+            placeholder="Worker type contains"
+          />
+        }
+      >
+        <>
+          {!WMWorkerTypeSummaries && loading && <Spinner loading />}
+          <ErrorPanel error={error} />
+          {
+            WMWorkerTypeSummaries &&
+            <WMWorkerTypesTable
+              searchTerm={workerTypeSearch}
+              workerTypes={WMWorkerTypeSummaries}
+            />
+          }
+        </>
+      </Dashboard>
+    );
   }
 }
