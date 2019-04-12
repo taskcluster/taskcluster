6c3bc97a2c60c3541fbebab7b9a52d514581adb5
Pete Moore <pmoore@mozilla.com>
2019-04-12 12:34:55 +0200

    Update update-ssl-creds script to update ssl creds in taskcluster secrets rather than in worker type definitions

--- update-ssl-creds/main.go
+++ update-ssl-creds/main.go
@@ -5,7 +5,9 @@ import (
 	"log"
 
 	"github.com/taskcluster/slugid-go/slugid"
+	tcclient "github.com/taskcluster/taskcluster-client-go"
 	"github.com/taskcluster/taskcluster-client-go/tcawsprovisioner"
+	"github.com/taskcluster/taskcluster-client-go/tcsecrets"
 )
 
 const (
@@ -17,18 +19,26 @@ const (
 
 func main() {
 	prov := tcawsprovisioner.NewFromEnv()
+	ss := tcsecrets.NewFromEnv()
 	workerTypes, err := prov.ListWorkerTypes()
 	if err != nil {
 		panic(err)
 	}
 	for _, wt := range *workerTypes {
 		changed := false
-		log.Printf("Querying worker type %v...", wt)
-		def, err := prov.WorkerType(wt)
+		secretName := "worker-type:aws-provisioner-v1/" + wt
+		log.Printf("Querying secret %v...", secretName)
+		sec, err := ss.Get(secretName)
 		if err != nil {
+			if apiException, isAPIException := err.(*tcclient.APICallException); isAPIException {
+				if apiException.CallSummary.HTTPResponse.StatusCode == 404 {
+					log.Printf("WARNING: Secret not found: %v - skipping...", secretName)
+					continue
+				}
+			}
 			panic(err)
 		}
-		secrets := def.Secrets
+		secrets := sec.Secret
 		var s map[string]interface{}
 		err = json.Unmarshal(secrets, &s)
 		if err != nil {
@@ -50,29 +60,57 @@ func main() {
 			}
 		}
 		if changed {
-			gw := s["generic-worker"].(map[string]interface{})
+			log.Printf("Updating secret %v...", secretName)
+			secretBytes, err := json.Marshal(s)
+			if err != nil {
+				panic(err)
+			}
+			var secretRM json.RawMessage
+			err = json.Unmarshal(secretBytes, &secretRM)
+			if err != nil {
+				panic(err)
+			}
+			sec.Secret = secretRM
+			err = ss.Set(secretName, sec)
+			if err != nil {
+				panic(err)
+			}
+			log.Printf("Querying worker type %v...", wt)
+			def, err := prov.WorkerType(wt)
+			if err != nil {
+				panic(err)
+			}
+			var userdata map[string]interface{}
+			err = json.Unmarshal(def.UserData, &userdata)
+			if err != nil {
+				panic(err)
+			}
+			if userdata["genericWorker"] == nil {
+				log.Printf("WARNING: No `userdata.genericWorker` property in worker type definition %v - skipping...", wt)
+				continue
+			}
+			gw := userdata["genericWorker"].(map[string]interface{})
 			conf := gw["config"].(map[string]interface{})
 			oldDeploymentID := conf["deploymentId"].(string)
 			newDeploymentID := slugid.Nice()
 			log.Printf("New deploymentId for %v: %v => %v", wt, oldDeploymentID, newDeploymentID)
 			conf["deploymentId"] = newDeploymentID
-			newBase64EncodedBytes, err := json.Marshal(s)
+			userdataBytes, err := json.Marshal(userdata)
 			if err != nil {
 				panic(err)
 			}
 			var rm json.RawMessage
-			err = json.Unmarshal(newBase64EncodedBytes, &rm)
+			err = json.Unmarshal(userdataBytes, &rm)
 			if err != nil {
 				panic(err)
 			}
-			def.Secrets = rm
-			allBytes, err := json.Marshal(def)
+			def.UserData = rm
+			workerTypeDefinitionBytes, err := json.Marshal(def)
 			if err != nil {
 				panic(err)
 			}
-
 			var req tcawsprovisioner.CreateWorkerTypeRequest
-			err = json.Unmarshal(allBytes, &req)
+			err = json.Unmarshal(workerTypeDefinitionBytes, &req)
 			if err != nil {
 				panic(err)
 			}
