bd4eb70c1d384bce4a1251b2ea30e9a5fff4c21c
owlishDeveloper <bugzeeeeee@gmail.com>
2019-04-12 11:46:12 -0700

    Define loader / resolver for worker types table. Add some mock data to display table

--- services/web-server/src/loaders/WorkerManager.js
+++ services/web-server/src/loaders/WorkerManager.js
@@ -1,3 +1,53 @@
+import DataLoader from 'dataloader';
+import sift from 'sift';
+
 export default ({ workerManager }) => {
-  const workerTypes;
+  // const WMWorkerTypeSummaries = new DataLoader(queries =>
+  //   Promise.all(
+  //     queries.map(async (filter) => {
+  //       const summaries = await workerManager.listWorkerTypes();
+  //
+  //       return filter ? sift(filter, summaries) : summaries;
+  //     })
+  //   )
+  // );
+
+  const WMWorkerTypeSummaries = new DataLoader(queries => {
+    return Promise.all(
+      queries.map( ({ filter }) => {
+        const summaries = [
+          {
+            name: 'banana',
+            pendingCapacity: 0,
+            runningCapacity: 1,
+            pendingTasks: 0,
+            lastActive: new Date(),
+            lastResolved: new Date(),
+            failedNumber: 0,
+            exceptionNumber: 1,
+            unscheduledNumber: 2,
+            provider: 'aws',
+          },
+          {
+            name: 'pineapple',
+            pendingCapacity: 1,
+            runningCapacity: 3,
+            pendingTasks: 1,
+            lastActive: new Date('December 17, 1995 03:24:00'),
+            lastResolved: new Date('December 17, 1995 03:24:00'),
+            failedNumber: 1,
+            exceptionNumber: 2,
+            unscheduledNumber: 3,
+            provider: 'gcp',
+          },
+        ];
+
+        return filter ? Promise.resolve(sift(filter, summaries)) : Promise.resolve(summaries);
+      })
+    );
+  });
+
+  return {
+    WMWorkerTypeSummaries,
+  };
 };
--- services/web-server/src/resolvers/WorkerManager.js
+++ services/web-server/src/resolvers/WorkerManager.js
@@ -1,5 +1,7 @@
 export default {
   Query: {
-    WMWorkerTypeSummaries() {},
+    WMWorkerTypeSummaries(parent, { filter }, { loaders }) {
+      return loaders.WMWorkerTypeSummaries.load({ filter });
+    },
   },
 };
