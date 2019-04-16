7bc3fef6551716587a40efb88315544e97c6e303
Dustin J. Mitchell <dustin@mozilla.com>
2019-04-16 15:10:56 +0000

    Bug 1528384 - fix polling to show all errors when it fails

--- libraries/testing/src/poll.js
+++ libraries/testing/src/poll.js
@@ -11,19 +11,31 @@ const {sleep} = require('./time');
  * Return a promise that a promise form the `poll` function resolved without
  * error. This will return the first successful poll, and stop polling.
  */
-const poll = function(doPoll, iterations, delay) {
+const poll = async (doPoll, iterations, delay) => {
   delay = delay || 250;
   iterations = iterations === undefined ? 20 : iterations;
-  return doPoll().catch(function(err) {
-    // Re-throw
-    if (iterations !== undefined && iterations <= 0) {
-      throw err;
+  const errors = [];
+  const start = +new Date();
+
+  while (true) {
+    try {
+      return await doPoll();
+    } catch (err) {
+      errors.push({err, when: new Date() - start});
+
+      // Re-throw unless we're out of iterations
+      if (iterations !== undefined && iterations <= 0) {
+        const err = new Error(
+          'Polling iterations exceeded.  Errors ignored during polling:\n' +
+          errors.map(({err, when}) => `${when}ms from start: ${err}`.trim()).join('\n---\n'));
+        throw err;
+      }
+      iterations--;
+
+      debug(`ignoring error while polling: ${err}`);
+      await sleep(delay);
     }
-    debug(`ignoring error while polling: ${err}`);
-    return sleep(delay).then(function() {
-      return poll(doPoll, iterations - 1, delay);
-    });
-  });
+  }
 };
 
 module.exports = poll;
