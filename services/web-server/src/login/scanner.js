4978758090b0f5e974ce1e31070a88d01668014f
Hassan Ali <helfi92@gmail.com>
2019-04-18 10:03:58 -0400

    Add scanner.js to disable clients

---
+++ services/web-server/src/login/scanner.js
@@ -0,0 +1,80 @@
+const scopeUtils = require('taskcluster-lib-scopes');
+const Debug = require('debug');
+const { Auth } = require('taskcluster-client');
+const { CLIENT_ID_PATTERN } = require('../utils/constants');
+
+const debug = Debug('scanner');
+
+module.exports = async (cfg, handlers) => {
+  // NOTE: this function performs once auth operation at a time.  It is better
+  // for scans to take longer than for the auth service to be overloaded.
+  const auth = new Auth({
+    credentials: cfg.taskcluster.credentials,
+    rootUrl: cfg.taskcluster.rootUrl,
+  });
+
+  async function scan(h) {
+    const clients = [];
+    const handler = handlers[h];
+    const query = {prefix: `${handler.identityProviderId}/`};
+
+    // Get all clients
+    while (true) {
+      const clientResponse = await auth.listClients(query);
+
+      query.continuationToken = clientResponse.continuationToken;
+
+      if (clientResponse.clients.length) {
+        clients.push(...(clientResponse.clients));
+      }
+
+      if (!query.continuationToken) {
+        break;
+      }
+    }
+
+    if (!clients.length) {
+      return;
+    }
+
+    // Iterate through the clients, constructing a new User as necessary, comparing
+    // the client's scopes to the User's scopes and disabling where necessary.
+    let user, userScopes;
+
+    for (const client of clients) {
+      debug('examining client', client.clientId);
+
+      const patternMatch = client.clientId.match(CLIENT_ID_PATTERN);
+
+      if (!patternMatch || client.disabled) {
+        continue;
+      }
+
+      const identity = patternMatch && patternMatch[1];
+
+      if (!user || user.identity !== identity) {
+        user = await handler.userFromClientId(client.clientId);
+
+        if (!user) {
+          continue;
+        }
+
+        userScopes = (await auth.expandScopes({ scopes: user.scopes() })).scopes;
+
+        debug('..against user', user.identity);
+      }
+
+      // if this client's expandedScopes are not satisfied by the user's expanded
+      // scopes, disable the client.
+      if (!scopeUtils.scopeMatch(userScopes, [client.expandedScopes])) {
+        await auth.disableClient(client.clientId);
+      }
+    }
+  }
+
+  await Promise.all(
+    Object
+      .keys(cfg.login.handlers)
+      .map(scan)
+  );
+};
