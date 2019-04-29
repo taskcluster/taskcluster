3862b8a7c5da11c876fbfc0362f6b2897ac6b261
Dustin J. Mitchell <dustin@mozilla.com>
2019-04-29 16:49:30 +0000

    Bug 1547589 - add Denier to filter denied addresses
    This also fixes up the test_api.js tests so that they can fail..

---
+++ services/notify/src/denier.js
@@ -0,0 +1,19 @@
+class Denier {
+  constructor({DenylistedNotification, emailBlacklist}) {
+    this.DenylistedNotification = DenylistedNotification;
+    this.emailBlacklist = emailBlacklist;
+  }
+
+  async isDenied(notificationType, notificationAddress) {
+    if (notificationType === 'email') {
+      if (this.emailBlacklist.includes(notificationAddress)) {
+        return true;
+      }
+    }
+
+    const address = {notificationType, notificationAddress};
+    return !!await this.DenylistedNotification.load(address, true);
+  }
+}
+
+module.exports = Denier;
