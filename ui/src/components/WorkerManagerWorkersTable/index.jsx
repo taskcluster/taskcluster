cb315741c364335335025faec5d9f8556d34ecca
owlishDeveloper <bugzeeeeee@gmail.com>
2019-04-30 16:23:55 -0700

    Add table of workers. Define worker view

---
+++ ui/src/components/WorkerManagerWorkersTable/index.jsx
@@ -0,0 +1,238 @@
+import React, { Component } from 'react';
+import DataTable from '../DataTable';
+import TableRow from '@material-ui/core/TableRow/TableRow';
+import TableCell from '@material-ui/core/TableCell/TableCell';
+import TableCellListItem from '../TableCellListItem';
+import Link from '../../utils/Link';
+import ListItemText from '@material-ui/core/ListItemText/ListItemText';
+import Typography from '@material-ui/core/Typography/Typography';
+import LinkIcon from 'mdi-react/LinkIcon';
+import AlertIcon from 'mdi-react/AlertIcon';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import DateDistance from '../DateDistance';
+import ContentCopyIcon from 'mdi-react/ContentCopyIcon';
+import {formatDistanceStrict} from "date-fns";
+import memoize from 'fast-memoize';
+import {camelCase} from 'change-case';
+import {isEmpty, map, pipe, sort as rSort} from 'ramda';
+import sort from '../../utils/sort';
+import {withRouter} from 'react-router-dom';
+import { workerManagerWorkers } from '../../utils/prop-types';
+
+@withRouter
+export default class WorkerManagerWorkersTable extends Component {
+  static propTypes = {
+    workers: workerManagerWorkers.isRequired,
+  };
+
+  sortWorkers = memoize(
+    (workers, sortBy, sortDirection, searchTerm) => {
+      const sortByProperty = camelCase(sortBy);
+      const filteredWorkers = searchTerm
+        ? workers.filter(({ w }) =>
+            w.includes(searchTerm)
+          )
+        : workers;
+
+      return isEmpty(filteredWorkers)
+        ? filteredWorkers
+        : [...filteredWorkers].sort((a, b) => {
+            const firstElement =
+              sortDirection === 'desc' ? b[sortByProperty] : a[sortByProperty];
+            const secondElement =
+              sortDirection === 'desc' ? a[sortByProperty] : b[sortByProperty];
+
+            return sort(firstElement, secondElement);
+           });
+    },
+    {
+      serializer: ([workers, sortBy, sortDirection, searchTerm]) => {
+        const ids = pipe(
+          rSort((a, b) => sort(a.worker, b.worker)),
+          map(({ worker }) => worker)
+        )(workers);
+
+        return `${ids.join('-')}-${sortBy}-${sortDirection}-${searchTerm}`;
+      },
+    }
+  );
+
+  renderTableRow(worker) {
+    const {
+      match: { path },
+    } = this.props;
+    const { workerId, workerGroup, latestTask, firstClaim, quarantineUntil, recentErrors } = worker;
+    const iconSize = 16;
+
+    return (
+          <TableRow key={workerId}>
+
+            <TableCell>{workerGroup}</TableCell>
+
+            <TableCell>
+              <TableCellListItem
+                button
+                component={Link}
+                to={`${path}/worker-types/${workerType}/workers/${workerGroup}/${workerId}`}>
+                <ListItemText
+                  disableTypography
+                  primary={<Typography>{workerId}</Typography>}
+                />
+                <LinkIcon size={iconSize} />
+              </TableCellListItem>
+            </TableCell>
+
+            <CopyToClipboard title={`${firstClaim} (Copy)`} text={firstClaim}>
+              <TableCell>
+                <TableCellListItem button>
+                  <ListItemText
+                    disableTypography
+                    primary={
+                      <Typography>
+                        <DateDistance from={firstClaim} />
+                      </Typography>
+                    }
+                  />
+                  <ContentCopyIcon size={iconSize} />
+                </TableCellListItem>
+              </TableCell>
+            </CopyToClipboard>
+
+            <TableCell>
+              {latestTask ? (
+                <TableCellListItem
+                  button
+                  component={Link}
+                  to={`/tasks/${latestTask.run.taskId}/runs/${
+                    latestTask.run.runId
+                  }`}>
+                  <ListItemText
+                    disableTypography
+                    primary={<Typography>{latestTask.run.taskId}</Typography>}
+                  />
+                  <LinkIcon size={iconSize} />
+                </TableCellListItem>
+              ) : (
+                <Typography>n/a</Typography>
+              )}
+            </TableCell>
+
+            {latestTask ? (
+              <CopyToClipboard
+                title={`${latestTask.run.started} (Copy)`}
+                text={latestTask.run.started}>
+                <TableCell>
+                  <TableCellListItem button>
+                    <ListItemText
+                      disableTypography
+                      primary={
+                        <Typography>
+                          <DateDistance from={latestTask.run.started} />
+                        </Typography>
+                      }
+                    />
+                    <ContentCopyIcon size={iconSize} />
+                  </TableCellListItem>
+                </TableCell>
+              </CopyToClipboard>
+            ) : (
+              <TableCell>
+                <Typography>n/a</Typography>
+              </TableCell>
+            )}
+
+            {latestTask ? (
+              <CopyToClipboard
+                title={`${latestTask.run.resolved} (Copy)`}
+                text={latestTask.run.resolved}>
+                <TableCell>
+                  <TableCellListItem button>
+                    <ListItemText
+                      disableTypography
+                      primary={
+                        <Typography>
+                          <DateDistance from={latestTask.run.resolved} />
+                        </Typography>
+                      }
+                    />
+                    <ContentCopyIcon size={iconSize} />
+                  </TableCellListItem>
+                </TableCell>
+              </CopyToClipboard>
+            ) : (
+              <TableCell>
+                <Typography>n/a</Typography>
+              </TableCell>
+            )}
+
+            <TableCell>
+              <TableCellListItem
+                button
+                component={Link}
+                to={`${path}/worker-types/${workerType}/workers/${workerGroup}/${workerId}/recent-errors`}>
+                <ListItemText
+                  disableTypography
+                  primary={<Typography>{"Click to see errors"}</Typography>}
+                />
+                <AlertIcon size={iconSize} />
+              </TableCellListItem>
+            </TableCell>
+
+            <TableCell>
+              <TableCellListItem
+                button
+                component={Link}
+                to={`${path}/worker-types/${workerType}/workers/${workerGroup}/${workerId}/resources`}>
+                <ListItemText
+                  disableTypography
+                  primary={<Typography>{`${recentErrors}`}</Typography>}
+                />
+                <LinkIcon size={iconSize} />
+              </TableCellListItem>
+            </TableCell>
+
+            <TableCell>
+              {quarantineUntil ? (
+                formatDistanceStrict(new Date(), quarantineUntil, {
+                  unit: 'd',
+                })
+              ) : (
+                <Typography>n/a</Typography>
+              )}
+            </TableCell>
+          </TableRow>
+        )}
+
+  render() {
+    const { workers, searchTerm } = this.props;
+    const { sortBy, sortDirection } = this.state;
+    const sortedWorkers = this.sortWorkers(
+      workers,
+      sortBy,
+      sortDirection,
+      searchTerm
+    );
+
+    return (
+      <DataTable
+        items={sortedWorkers}
+        sortByHeader={sortBy}
+        sortDirection={sortDirection}
+        onHeaderClick={this.handleHeaderClick}
+        renderRow={this.renderTableRow}
+        headers={[
+          'Worker Group',
+          'Worker ID',
+          'First Claim',
+          'Most Recent Task',
+          'Task Started',
+          'Task Resolved',
+          'Recent Provisioning Errors',
+          'Resources',
+          'Quarantined',
+        ]}
+        padding="dense"
+      />
+    );
+  }
+}
--- ui/src/views/WorkerManager/WorkerManagerViewWorkers/index.jsx
+++ ui/src/views/WorkerManager/WorkerManagerViewWorkers/index.jsx
@@ -1,10 +1,41 @@
 import { hot } from 'react-hot-loader';
 import React, { Component } from 'react';
 import Dashboard from '../../../components/Dashboard';
+import ErrorPanel from '../../../components/ErrorPanel';
+import Tabs from '@material-ui/core/Tabs/Tabs';
+import Tab from '@material-ui/core/Tab/Tab';
+import Spinner from '@mozilla-frontend-infra/components/Spinner';
+import WorkerManagerWorkersTable from '../../../components/WorkerManagerWorkersTable';
 
 @hot(module)
 export default class WorkerManagerViewWorkers extends Component {
+  state = {
+    currentTab: 0,
+  };
+
+  handleTabChange = (e, currentTab) => {
+    this.setState({ currentTab });
+  };
+
   render() {
-    return <Dashboard title="Workers View" />;
+    const {currentTab} = this.state;
+
+    return <Dashboard title={`${workerType} - Workers`}>
+      <ErrorPanel error={error} />
+      <ErrorPanel error={this.state.error} />
+
+      <Tabs fullWidth value={currentTab} onChange={this.handleTabChange}>
+        <Tab label="Failures and Exceptions" />
+        <Tab label="Pending" />
+        <Tab label="Running" />
+      </Tabs>
+
+      {loading && <Spinner className={classes.spinner} loading />}
+
+      {!error && !loading && currentTab === 0 && (
+        <WorkerManagerWorkersTable/>
+      )}
+
+    </Dashboard>;
   }
 }
