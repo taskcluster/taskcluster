9accf394340531592c4f7147d77bfea50d9f15fa
Brian Pitts <bpitts@mozilla.com>
2019-05-06 14:52:05 -0400

    Auth service terraform

--- infrastructure/terraform/to_convert_to_helm/auth-service.tf
+++ infrastructure/terraform/to_convert_to_helm/auth-service.tf
@@ -1,37 +1,3 @@
-module "auth_user" {
-  source = "modules/taskcluster-service-iam-user"
-  name   = "taskcluster-auth"
-  prefix = "${var.prefix}"
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "sts:GetFederationToken",
-            "Resource": "arn:aws:sts::${data.aws_caller_identity.current.account_id}:federated-user/TemporaryS3ReadWriteCredentials"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "s3:DeleteObject",
-                "s3:GetObject",
-                "s3:PutObject",
-                "s3:PutObjectAcl",
-                "s3:ListBucket",
-                "s3:GetBucketLocation"
-            ],
-            "Resource": [
-              "${aws_s3_bucket.backups.arn}",
-              "${aws_s3_bucket.backups.arn}/*"
-            ]
-        }
-    ]
-}
-EOF
-}
-
 resource "random_string" "auth_table_signing_key" {
   length = 40
 }
