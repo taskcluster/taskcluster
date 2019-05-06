ade5cefcd7eb4d3cd4aaf18982d3310ad83b24cc
owlishDeveloper <bugzeeeeee@gmail.com>
2019-05-06 11:44:23 -0700

    Add search to the workers view

--- ui/src/views/WorkerManager/WorkerManagerViewWorkers/index.jsx
+++ ui/src/views/WorkerManager/WorkerManagerViewWorkers/index.jsx
@@ -8,25 +8,40 @@ import Spinner from '@mozilla-frontend-infra/components/Spinner';
 import WorkerManagerWorkersTable from '../../../components/WorkerManagerWorkersTable';
 import {graphql} from 'react-apollo';
 import workersQuery from 'WorkerManagerWorkers.graphql';
+import Search from '../../../components/Search';
 
 @hot(module)
 @graphql(workersQuery)
 export default class WorkerManagerViewWorkers extends Component {
   state = {
     currentTab: 0,
+    workerSearch: '',
   };
 
   handleTabChange = (e, currentTab) => {
     this.setState({ currentTab });
   };
 
+  handleWorkerSearchSubmit = workerSearch => {
+    this.setState({ workerSearch });
+  };
+
   render() {
-    const {currentTab} = this.state;
+    const {currentTab, workerSearch} = this.state;
     const {
       data: { loading, error, WorkerManagerWorkerSummaries },
     } = this.props;
 
-    return <Dashboard title={`${workerType} - Workers`}>
+    return <Dashboard
+      title={`${workerType} - Workers`}
+      search={
+        <Search
+          disabled={loading}
+          onSubmit={this.handleWorkerSearchSubmit}
+          placeholder="Worker name contains"
+        />
+      }
+    >
       <ErrorPanel error={error} />
       <ErrorPanel error={this.state.error} />
 
@@ -39,7 +54,10 @@ export default class WorkerManagerViewWorkers extends Component {
       {loading && <Spinner className={classes.spinner} loading />}
 
       {!error && !loading && currentTab === 0 && (
-        <WorkerManagerWorkersTable/>
+        <WorkerManagerWorkersTable
+          searchTerm={workerSearch}
+          workerTypes={WorkerManagerWorkerSummaries}
+        />
       )}
 
     </Dashboard>;
