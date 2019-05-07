a67593e874ae46ce5b9ad5c59c810e4a6715f7aa
Hassan Ali <helfi92@gmail.com>
2019-05-07 15:07:53 -0400

    Reload page when chunk fails to load

--- ui/src/utils/Link.jsx
+++ ui/src/utils/Link.jsx
@@ -10,6 +10,7 @@ import matchRoutes from './matchRoutes';
  * with pre-fetching capabilities.
  */
 export default function Link({ viewName, nav, to, ...props }) {
+  let shouldReload = false;
   const path = typeof to === 'string' ? to : to.pathname;
   const isPathAbsolute = isAbsolute(path);
   const Component = nav ? NavLink : RouterLink;
@@ -22,8 +23,13 @@ export default function Link({ viewName, nav, to, ...props }) {
 
     if (!isPathAbsolute) {
       const matchingRoutes = matchRoutes(path, routes);
+      const components = matchingRoutes.map(({ component }) =>
+        component.preload()
+      );
 
-      matchingRoutes.forEach(({ component }) => component.preload());
+      Promise.all(components).catch(() => {
+        shouldReload = true;
+      });
     }
 
     setPrefetchFlag(true);
@@ -49,6 +55,19 @@ export default function Link({ viewName, nav, to, ...props }) {
     }
   }
 
+  async function handleClick(ev, path) {
+    if (ev.altKey || ev.metaKey || ev.ctrlKey || ev.shiftKey) {
+      return;
+    }
+
+    // The user might try to navigate to a file that
+    // no longer exists (e.g., when a filename chunk is updated when deploying)
+    if (shouldReload) {
+      ev.preventDefault();
+      window.location = path;
+    }
+  }
+
   return isPathAbsolute ? (
     /* eslint-disable jsx-a11y/anchor-has-content */
     <a href={to} {...props} target="_blank" rel="noopener noreferrer" />
@@ -58,6 +77,7 @@ export default function Link({ viewName, nav, to, ...props }) {
       to={to}
       onFocus={handleFocus}
       onMouseOver={handleMouseOver}
+      onClick={ev => handleClick(ev, to)}
     />
   );
 }
