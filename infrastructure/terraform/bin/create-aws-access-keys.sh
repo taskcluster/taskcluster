b1ad744550568ea06e112d9553ae5892ed6410f3
Brian Pitts <bpitts@mozilla.com>
2019-05-08 12:58:54 -0400

    aws creation fixes

--- infrastructure/terraform/bin/create-aws-access-keys.sh
+++ infrastructure/terraform/bin/create-aws-access-keys.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eu
+set -u
 
 # quick way to track this somwhere for now
 # likely port to python and boto later
@@ -13,11 +13,11 @@ taskcluster-auth
 for NAME in $NAMES; do
     FULLNAME="${PREFIX}-${NAME}"
     aws iam list-access-keys --user "$FULLNAME" | grep Active > /dev/null
-    if [[ ! $? ]] ; then
-        echo "creating key in $FULLNAME.json"
-        echo aws iam create-access-key --user-name "$FULLNAME" >> "$FULLNAME.json"
-    else
+    if [[ $? -eq 0 ]] ; then
         echo "skipping $FULLNAME because they already have an active key"
+    else
+        echo "creating key in $FULLNAME.json"
+        aws iam create-access-key --user-name "$FULLNAME" >> "$FULLNAME.json"
     fi
 done
 
