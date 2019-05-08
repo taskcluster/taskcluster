45b2fc09fffffc46e978f1260c4d15f7d43fd30f
Brian Pitts <bpitts@mozilla.com>
2019-05-08 13:45:14 -0400

    Rabbitmq module should not create users
    Since this requires generateing passwords, which we want to keep out of
    terraform state

--- infrastructure/terraform/modules/rabbitmq-user/main.tf
+++ infrastructure/terraform/modules/rabbitmq-user/main.tf
@@ -2,16 +2,6 @@ locals {
   re_name = "${replace(var.project_name, "-", "\\-")}"
 }
 
-resource "random_string" "rabbitmq_pass" {
-  length           = 30
-  override_special = "_-.~"
-}
-
-resource "rabbitmq_user" "user" {
-  name     = "${var.prefix}-${var.project_name}"
-  password = "${random_string.rabbitmq_pass.result}"
-}
-
 resource "rabbitmq_permissions" "permissions" {
   user  = "${rabbitmq_user.user.name}"
   vhost = "${var.rabbitmq_vhost}"
--- infrastructure/terraform/modules/rabbitmq-user/outputs.tf
+++ infrastructure/terraform/modules/rabbitmq-user/outputs.tf
@@ -1,7 +1,3 @@
 output "username" {
   value = "${rabbitmq_user.user.name}"
 }
-
-output "password" {
-  value = "${random_string.rabbitmq_pass.result}"
-}
