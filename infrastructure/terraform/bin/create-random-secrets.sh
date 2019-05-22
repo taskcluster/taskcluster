283e9cea2eb9749d70c3ecd1b1c820eb6b8bbfda
E. Dunham <github@edunham.net>
2019-05-21 17:01:20 -0700

    Secret creation: Access tokens, azure keys

--- infrastructure/terraform/bin/create-random-secrets.sh
+++ infrastructure/terraform/bin/create-random-secrets.sh
@@ -7,3 +7,15 @@ function genpw () {
 }
 
 genpw 65 > purge_cache_access_token
+genpw 65 > built_in_workers_access_token
+genpw 65 > web_server_access_token
+genpw 65 > worker_manager_access_token
+genpw 65 > index_access_token
+genpw 65 > github_access_token
+genpw 65 > hooks_access_token
+genpw 65 > notify_access_token
+genpw 65 > secrets_access_token
+genpw 32 > auth_azure_crypto_key
+genpw 32 > secrets_azure_crypto_key
+genpw 40 > auth_azure_signing_key
+genpw 40 > secrets_azure_signing_key
