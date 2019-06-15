64b96f44ff2738b2811d8eb382c8a6f251bc59d0
Dustin J. Mitchell <dustin@mozilla.com>
2019-06-15 02:50:35 +0000

    Bug 1558530 - improve testing for metadata fetches
    ..and a minor amount of refactoring after those tests are in place.

--- provider/awsprovisioner/metadata_test.go
+++ provider/awsprovisioner/metadata_test.go
@@ -1,6 +1,14 @@
 package awsprovisioner
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/taskcluster/httpbackoff"
+)
 
 type fakeMetadataService struct {
 	UserDataError error
@@ -26,4 +34,57 @@ func (mds *fakeMetadataService) queryMetadata(path string) (string, error) {
 	return res, nil
 }
 
-// TODO: test the real metadata service
+func TestQueryMetadata(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/latest/meta-data/some-data" {
+			w.WriteHeader(200)
+			fmt.Fprintln(w, "42")
+		} else {
+			w.WriteHeader(404)
+			fmt.Fprintln(w, "Not Found")
+		}
+	}))
+	defer ts.Close()
+
+	EC2MetadataBaseURL = ts.URL + "/latest"
+	defer func() {
+		EC2MetadataBaseURL = "http://169.254.169.254/latest"
+	}()
+
+	ms := realMetadataService{}
+
+	rv, err := ms.queryMetadata("/meta-data/some-data")
+	assert.NoError(t, err)
+	assert.Equal(t, "42\n", rv)
+
+	_, err = ms.queryMetadata("/meta-data/NOSUCH")
+	if assert.Error(t, err) {
+		httperr, ok := err.(httpbackoff.BadHttpResponseCode)
+		assert.True(t, ok)
+		assert.Equal(t, 404, httperr.HttpResponseCode)
+	}
+}
+
+func TestQueryUserData(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/latest/user-data" {
+			w.WriteHeader(200)
+			fmt.Fprintln(w, `{"region": "aa-central-2"}`)
+		} else {
+			w.WriteHeader(404)
+			fmt.Fprintf(w, "Not Found: %s", r.URL.Path)
+		}
+	}))
+	defer ts.Close()
+
+	EC2MetadataBaseURL = ts.URL + "/latest"
+	defer func() {
+		EC2MetadataBaseURL = "http://169.254.169.254/latest"
+	}()
+
+	ms := realMetadataService{}
+
+	ud, err := ms.queryUserData()
+	assert.NoError(t, err)
+	assert.Equal(t, "aa-central-2", ud.Region)
+}
