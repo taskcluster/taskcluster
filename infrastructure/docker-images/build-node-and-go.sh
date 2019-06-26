adc7f4207851fe9cbca50babce8f1d2f5df72427
Dustin J. Mitchell <dustin@mozilla.com>
2019-06-26 18:05:40 +0000

    Bug 1560649 - split docker-image build scripts out

--- infrastructure/docker-images/build-node-and-go.sh
+++ infrastructure/docker-images/build-node-and-go.sh
@@ -7,37 +7,12 @@ if [ -z "${node_version}" ]; then
     exit 1
 fi
 
-
 go_version=$(<${repo_root}/.go-version)
 go_version=${go_version#go}
 if [ -z "${go_version}" ]; then
     echo "Could not determine go version from top-level .go-version"
     exit 1
 fi
-
-# First we do the browser-test image
-
-tmpdir=$(mktemp -d)
-trap "cd /; rm -rf ${tmpdir}" EXIT
-cat > ${tmpdir}/Dockerfile <<EOF
-FROM node:${node_version}-stretch
-RUN apt-get update && apt-get install -y firefox-esr xvfb
-EOF
-
-docker build -t "taskcluster/browser-test:${node_version}" ${tmpdir}
-
-# Next we do the rabbit-test image
-
-tmpdir=$(mktemp -d)
-trap "cd /; rm -rf ${tmpdir}" EXIT
-cat > ${tmpdir}/Dockerfile <<EOF
-FROM node:${node_version}-stretch
-RUN apt-get update && apt-get install -y rabbitmq-server
-EOF
-
-docker build -t "taskcluster/rabbit-test:${node_version}" ${tmpdir}
-
-# Then we do the rabbit-test image
 
 tmpdir=$(mktemp -d)
 trap "cd /; rm -rf ${tmpdir}" EXIT
