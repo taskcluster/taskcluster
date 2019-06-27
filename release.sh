9341ea3d581bb8f836916492f652397c3da83b61
Dustin J. Mitchell <dustin@mozilla.com>
2019-06-27 16:38:43 +0000

    update release process

--- release.sh
+++ release.sh
@@ -14,3 +14,12 @@ sed -i -e "s/Version = .*/Version = \"$VERSION\"/" version.go
 git add version.go
 git commit -m "v$VERSION"
 git tag v$VERSION
+
+build() {
+    local output=start-worker-${1}-${2}
+    GOOS="${1}" GOARCH="${2}" CGO_ENABLED=0 go build -o $output
+    echo $output
+}
+echo "Building:"
+build linux amd64
+echo "Attach the above to a new release in GitHub, please"
