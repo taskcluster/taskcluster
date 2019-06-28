28c39154e63ff4f12f65ab5baa9c8aa7d2443e68
Brian Pitts <bpitts@mozilla.com>
2019-06-28 12:22:16 -0400

    Taskcluster services want s3 id not arn (#51)
    See https://github.com/taskcluster/taskcluster/blob/master/infrastructure/terraform/queue-service.tf#L83-L86

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -3,17 +3,17 @@ output "azure_account" {
 }
 
 output "private_artifact_bucket" {
-  value = "${aws_s3_bucket.private_artifacts.arn}"
+  value = "${aws_s3_bucket.private_artifacts.id}"
 }
 
 output "private_blob_artifact_bucket" {
-  value = "${aws_s3_bucket.private_blobs.arn}"
+  value = "${aws_s3_bucket.private_blobs.id}"
 }
 
 output "public_artifact_bucket" {
-  value = "${aws_s3_bucket.public_artifacts.arn}"
+  value = "${aws_s3_bucket.public_artifacts.id}"
 }
 
 output "public_blob_artifact_bucket" {
-  value = "${aws_s3_bucket.public_blobs.arn}"
+  value = "${aws_s3_bucket.public_blobs.id}"
 }
