d31f76480e2d96ba900b663d2c81d526ad1e8cdc
Brian Pitts <bpitts@mozilla.com>
2019-06-28 12:11:16 -0400

    Delete unneeded tf

--- infrastructure/terraform/to_review/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "root_access_token" {
-  sensitive = true
-  value     = "${random_string.auth_root_access_token.result}"
-}
-
-output "websocktunnel_secret" {
-  sensitive = true
-  value     = "${random_string.auth_websocktunnel_secret.result}"
-}
