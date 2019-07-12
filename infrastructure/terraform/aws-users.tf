1dacfd92809a27880a2436ce64d16d251beb0f0e
Brian Stack <bstack@mozilla.com>
2019-07-12 15:33:10 -0700

    Switch to older version of terraform and fmt

--- infrastructure/terraform/aws-users.tf
+++ infrastructure/terraform/aws-users.tf
@@ -40,7 +40,7 @@ resource "aws_sqs_queue" "notify_irc_queue" {
 
 module "notify_user" {
   source = "./modules/taskcluster-service-iam-user"
-  name = "taskcluster-notify"
+  name   = "taskcluster-notify"
   prefix = "${var.prefix}"
 
   policy = <<EOF
