97d9f1666fab6941a74fc11c470a612bfee44962
Hassan Ali <helfi92@gmail.com>
2019-07-19 14:14:04 -0400

    Fix comment in login.js

--- services/web-server/src/utils/login.js
+++ services/web-server/src/utils/login.js
@@ -1,8 +1,4 @@
-/**
- * A function that returns a middleware that renders callback.ejs
- * and saves the session in a table so that the user is not logged out
- * when the server restarts.
- */
+// A function that returns a middleware that renders callback.ejs
 module.exports = (publicUrl) => async (request, response) => {
   response.render('callback', {
     user: request.user,
