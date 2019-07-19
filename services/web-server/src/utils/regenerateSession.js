95286fc6d8f7a3ec8786f9b72c6f056b9799c4f5
Hassan Ali <helfi92@gmail.com>
2019-07-19 14:24:09 -0400

    Make comment in req.session.regenerate clearer

--- services/web-server/src/utils/regenerateSession.js
+++ services/web-server/src/utils/regenerateSession.js
@@ -1,10 +1,12 @@
 // Regenerate a sessionId given a request
-// https://stackoverflow.com/a/26394156/6150432
 module.exports = req => new Promise((resolve) => {
   const temp = req.session.passport;
 
   req.session.regenerate((err) => {
-    // req.session.passport is now undefined
+    // Passport adds a passport key to req.session. When req.session.regenerate runs,
+    // req.session.passport becomes undefined because req.session is recreated.
+    // The callback makes the necessary changes to make passport aware of the current user.
+    // For more information, see https://stackoverflow.com/a/26394156/6150432
     req.session.passport = temp;
     req.session.save((err) => {
       resolve();
