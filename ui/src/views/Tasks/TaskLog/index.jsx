d5a2c9118b8dacff236d22cb08cf73696a33f050
Shubham Chinda <shubhamchinda07@gmail.com>
2019-07-28 00:48:41 +0530

    favicon on logs page

--- ui/src/views/Tasks/TaskLog/index.jsx
+++ ui/src/views/Tasks/TaskLog/index.jsx
@@ -1,11 +1,14 @@
 import { hot } from 'react-hot-loader';
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
 import { withStyles } from '@material-ui/core/styles';
 import ArrowRightIcon from 'mdi-react/ArrowRightIcon';
 import Dashboard from '../../../components/Dashboard';
 import Button from '../../../components/Button';
 import Log from '../../../components/Log';
 import Link from '../../../utils/Link';
+import Helmet from '../../../components/Helmet';
+import taskQuery from './task.graphql';
 
 @hot(module)
 @withStyles(theme => ({
@@ -21,13 +24,24 @@ import Link from '../../../utils/Link';
     right: theme.spacing.unit * 12,
   },
 }))
+@graphql(taskQuery, {
+  options: props => ({
+    fetchPolicy: 'network-only',
+    errorPolicy: 'all',
+    variables: {
+      taskId: props.match.params.taskId,
+    },
+  }),
+})
 export default class TaskLog extends Component {
   render() {
     const { classes, match, stream } = this.props;
+    const { task } = this.props.data;
     const url = decodeURIComponent(match.params.logUrl);
 
     return (
       <Dashboard title="Log" disablePadding>
+        <Helmet state={task && task.status.state} />
         <Log
           url={url}
           stream={stream}
