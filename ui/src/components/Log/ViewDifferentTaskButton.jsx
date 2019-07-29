be49d1459ffd60135b7d3b6ee5416f478918e0e3
ss77995ss <ss77995ss@gmail.com>
2019-07-29 22:40:43 +0800

    Trim <ViewDifferentTaskButton /> component

--- ui/src/components/Log/ViewDifferentTaskButton.jsx
+++ ui/src/components/Log/ViewDifferentTaskButton.jsx
@@ -4,7 +4,6 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import TextField from '@material-ui/core/TextField';
 import FeatureSearchIcon from 'mdi-react/FeatureSearchIcon';
 import SpeedDialAction from '../SpeedDialAction';
@@ -19,10 +18,6 @@ export default class ViewDifferentTaskButton extends Component {
 
   handleChange = e => {
     this.setState({ taskId: e.target.value });
-
-    if (e.target.value) {
-      this.setState({ isValid: true });
-    }
   };
 
   handleClose = () => {
@@ -40,7 +35,7 @@ export default class ViewDifferentTaskButton extends Component {
 
   render() {
     const { open } = this.props;
-    const { dialogOpen, isValid } = this.state;
+    const { dialogOpen } = this.state;
 
     return (
       <Fragment>
@@ -63,25 +58,12 @@ export default class ViewDifferentTaskButton extends Component {
             View Different Task
           </DialogTitle>
           <DialogContent>
-            <DialogContentText>
-              <div>
-                <p>Require to match regular expression:</p>
-                <p>
-                  {
-                    '/^[A-Za-z0-9_-]{8}[Q-T][A-Za-z0-9_-][CGKOSWaeimquy26-][A-Za-z0-9_-]{10}[AQgw]$/'
-                  }
-                </p>
-              </div>
-            </DialogContentText>
             <form onSubmit={this.handleSubmit}>
               <TextField
-                id="text"
                 label="Task ID"
                 autoFocus
                 fullWidth
                 onChange={this.handleChange}
-                type="text"
-                margin="dense"
               />
             </form>
           </DialogContent>
@@ -89,10 +71,9 @@ export default class ViewDifferentTaskButton extends Component {
             <Button onClick={this.handleClose}>Cancel</Button>
             <Button
               onClick={this.handleSubmit}
-              disabled={!isValid}
               variant="contained"
               color="secondary">
-              Go to task
+              View Task
             </Button>
           </DialogActions>
         </Dialog>
