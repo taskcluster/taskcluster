ecb8610c66b042ff94b5fb3d565cfc47d72bc47c
Shubham Chinda <shubhamchinda07@gmail.com>
2019-07-30 19:53:15 +0530

    [UI] Update status favicon while viewing logs (#1090)
    * favicon on logs page
    
    * added getCurrentrun()

--- ui/src/views/Tasks/TaskLog/index.jsx
+++ ui/src/views/Tasks/TaskLog/index.jsx
@@ -1,11 +1,14 @@
 import { hot } from 'react-hot-loader';
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
 import { withStyles } from '@material-ui/core/styles';
 import ArrowRightIcon from 'mdi-react/ArrowRightIcon';
 import Dashboard from '../../../components/Dashboard';
 import Button from '../../../components/Button';
 import Log from '../../../components/Log';
 import Link from '../../../utils/Link';
+import Helmet from '../../../components/Helmet';
+import taskQuery from './task.graphql';
 
 @hot(module)
 @withStyles(theme => ({
@@ -21,13 +24,31 @@ import Link from '../../../utils/Link';
     right: theme.spacing.unit * 12,
   },
 }))
+@graphql(taskQuery, {
+  options: props => ({
+    fetchPolicy: 'network-only',
+    errorPolicy: 'all',
+    variables: {
+      taskId: props.match.params.taskId,
+    },
+  }),
+})
 export default class TaskLog extends Component {
+  getCurrentRun() {
+    return (
+      this.props.data.task &&
+      this.props.data.task.status.runs[this.props.match.params.runId]
+    );
+  }
+
   render() {
     const { classes, match, stream } = this.props;
     const url = decodeURIComponent(match.params.logUrl);
+    const run = this.getCurrentRun();
 
     return (
       <Dashboard title="Log" disablePadding>
+        <Helmet state={run && run.state} />
         <Log
           url={url}
           stream={stream}
