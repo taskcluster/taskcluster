3cf64a8ed329a2ba21d9057220f04f9adeea6e3f
Dustin J. Mitchell <dustin@mozilla.com>
2019-07-31 13:15:02 +0000

    Bug 1570203 - just use 'localhost' for livelogs when stateless DNS secrets are not set

--- workers/docker-worker/src/lib/util/hostname.js
+++ workers/docker-worker/src/lib/util/hostname.js
@@ -3,15 +3,13 @@ const statelessDNSServer = require('stateless-dns-server');
 
 module.exports = function getHostname(config, expires) {
   let statelessConfig = config.statelessHostname || {};
-  if (!statelessConfig || !statelessConfig.enabled) {
+  if (!statelessConfig || !statelessConfig.secret || !statelessConfig.domain) {
     return config.host;
   }
 
-  let secret = statelessConfig.secret || '';
-  let domain = statelessConfig.domain || '';
+  let secret = statelessConfig.secret;
+  let domain = statelessConfig.domain;
   let ip = config.publicIp;
-  assert(secret, 'Must supply a secret for stateless dns server');
-  assert(domain, 'Must supply a domain name used for stateless hostname');
   assert(ip, 'Public IP is not specified in the configuration');
 
   let hostname;
