37d7a6e2dc385a0b3301a72d658cf3f67793674d
Brian Stack <bstack@mozilla.com>
2019-08-12 16:01:57 -0700

    Finish making a nice monitoring suggestion document

--- infrastructure/builder/src/generate/generators/monitoring.js
+++ infrastructure/builder/src/generate/generators/monitoring.js
@@ -18,10 +18,10 @@ SERVICES.forEach(name => {
       const res = [];
       Object.entries(requirements[`procs-${name}`]).forEach(([proc, ext]) => {
         if (ext.type === 'cron') {
-          res.push([name, proc, `taskcluster.${name}`, 'TODO', ext.deadline, cronstrue.toString(ext.schedule)]);
+          res.push([name, proc, `taskcluster.${name}`, ext.deadline, cronstrue.toString(ext.schedule)]);
         }
-        if (ext.type === 'background') {
-          res.push([name, proc, `taskcluster.${name}`, 'TODO', 'continuous', 'continuous']);
+        if (ext.type === 'background' && ext.subType === 'iterate') {
+          res.push([name, proc, `taskcluster.${name}`, 'continuous', 'continuous']);
         }
       });
 
@@ -35,7 +35,7 @@ exports.tasks.push({
   requires: SERVICES.map(name => `monitoring-hints-${name}`),
   provides: [],
   run: async (requirements, utils) => {
-    let res = [['Service', 'Proc', 'Logger', 'Type', 'Deadline (seconds)', 'Schedule']];
+    let res = [['Service', 'Name', 'Logger', 'Deadline (seconds)', 'Schedule']];
     SERVICES.forEach(name => {
       res = res.concat(requirements[`monitoring-hints-${name}`]);
     });
@@ -43,7 +43,23 @@ exports.tasks.push({
     const doc = `
 # Monitoring Suggestions
 
-Taskcluster has several background processes that you should ensure are running.
+Taskcluster has several background processes that you should ensure are running on a schedule. Any of the following will generate messages
+of the form:
+
+\`\`\`json
+{
+  "Type": "monitor.periodic",
+  "Logger": "<Logger>",
+  "Fields": {
+    "name": "<Name>"
+  }
+}
+\`\`\`
+
+They will also have Fields for \`status\`, \`duration\`, and a serialized \`error\` if an error occured.
+
+The processes that have \`continuous\` for their dedaline and schedule run every few minutes and should complete fairly quickly. The rest
+have their schedules and maximum allowed duration defined here.
 
 ${table(res)}
     `.trim();
