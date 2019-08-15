59e0a8b293ed44c09e9e8d2a08244924f0c3af45
Rishabh Budhiraja <rishabh.budhiraja@gmail.com>
2019-08-15 09:31:46 +0530

    Small patch

--- ui/src/views/Provisioners/ViewProvisioners/index.jsx
+++ ui/src/views/Provisioners/ViewProvisioners/index.jsx
@@ -1,25 +1,20 @@
 import { hot } from 'react-hot-loader';
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import Spinner from '@mozilla-frontend-infra/components/Spinner';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 import provisionersQuery from './provisioners.graphql';
 import Dashboard from '../../../components/Dashboard';
 import HelpView from '../../../components/HelpView';
 import ProvisionerDetailsCard from '../../../components/ProvisionerDetailsCard';
 import ErrorPanel from '../../../components/ErrorPanel';
-import Breadcrumbs from '../../../components/Breadcrumbs';
 
 @hot(module)
 @graphql(provisionersQuery)
 @withStyles(theme => ({
   gridItem: {
     marginBottom: theme.spacing.double,
-  },
-  link: {
-    ...theme.mixins.link,
   },
 }))
 export default class ViewProvisioners extends Component {
@@ -37,25 +32,19 @@ export default class ViewProvisioners extends Component {
         {loading && <Spinner loading />}
         <ErrorPanel fixed error={error} />
         {provisioners && (
-          <Fragment>
-            <Breadcrumbs>
-              <Typography color="textSecondary">Provisioners</Typography>
-            </Breadcrumbs>
-            <br />
-            <Grid container spacing={24}>
-              {provisioners.edges.map(({ node: provisioner }) => (
-                <Grid
-                  key={`${provisioner.provisionerId}-item`}
-                  className={classes.gridItem}
-                  item
-                  xs={12}
-                  sm={6}
-                  md={4}>
-                  <ProvisionerDetailsCard dense provisioner={provisioner} />
-                </Grid>
-              ))}
-            </Grid>
-          </Fragment>
+          <Grid container spacing={24}>
+            {provisioners.edges.map(({ node: provisioner }) => (
+              <Grid
+                key={`${provisioner.provisionerId}-item`}
+                className={classes.gridItem}
+                item
+                xs={12}
+                sm={6}
+                md={4}>
+                <ProvisionerDetailsCard dense provisioner={provisioner} />
+              </Grid>
+            ))}
+          </Grid>
         )}
       </Dashboard>
     );
