81e5c792ba955d999b130e20c2f3997ebc910039
Rishabh Budhiraja <rishabh.budhiraja@gmail.com>
2019-08-15 19:43:07 +0530

    Adding breadcrumbs to Provisioners view (#1182)
    * Adding breadcrumbs
    
    * Small patch
    
    * Align breadcrumbs component with dropdown textfield

--- ui/src/views/Provisioners/ViewWorkerTypes/index.jsx
+++ ui/src/views/Provisioners/ViewWorkerTypes/index.jsx
@@ -3,6 +3,7 @@ import React, { Component, Fragment } from 'react';
 import { graphql } from 'react-apollo';
 import dotProp from 'dot-prop-immutable';
 import Spinner from '@mozilla-frontend-infra/components/Spinner';
+import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
@@ -10,17 +11,25 @@ import WorkerTypesTable from '../../../components/WorkerTypesTable';
 import Dashboard from '../../../components/Dashboard';
 import { VIEW_WORKER_TYPES_PAGE_SIZE } from '../../../utils/constants';
 import ErrorPanel from '../../../components/ErrorPanel';
+import Breadcrumbs from '../../../components/Breadcrumbs';
+import Link from '../../../utils/Link';
 import workerTypesQuery from './workerTypes.graphql';
 
 @hot(module)
 @withStyles(theme => ({
-  actionBar: {
+  bar: {
     display: 'flex',
-    flexDirection: 'row-reverse',
+    alignItems: 'center',
+  },
+  breadcrumbsPaper: {
+    marginRight: theme.spacing.unit * 4,
+    flex: 1,
   },
   dropdown: {
     minWidth: 200,
-    marginBottom: theme.spacing.double,
+  },
+  link: {
+    ...theme.mixins.link,
   },
 }))
 @graphql(workerTypesQuery, {
@@ -108,7 +117,19 @@ export default class ViewWorkerTypes extends Component {
           <ErrorPanel fixed error={error} />
           {provisioners && workerTypes && (
             <Fragment>
-              <div className={classes.actionBar}>
+              <div className={classes.bar}>
+                <Breadcrumbs classes={{ paper: classes.breadcrumbsPaper }}>
+                  <Typography
+                    className={classes.link}
+                    component={Link}
+                    to="/provisioners">
+                    Provisioners
+                  </Typography>
+
+                  <Typography color="textSecondary">
+                    {`${provisionerId}`}
+                  </Typography>
+                </Breadcrumbs>
                 <TextField
                   disabled={loading}
                   className={classes.dropdown}
@@ -128,6 +149,7 @@ export default class ViewWorkerTypes extends Component {
                   ))}
                 </TextField>
               </div>
+              <br />
               <WorkerTypesTable
                 workerTypesConnection={workerTypes}
                 provisionerId={provisionerId}
