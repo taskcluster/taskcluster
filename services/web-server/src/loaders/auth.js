d697d7271cd491946b83cad339e13f98b16a56f2
Hassan Ali <helfi92@gmail.com>
2019-08-19 09:11:52 -0400

    Refactor generate credentials into its own utility

--- services/web-server/src/loaders/auth.js
+++ services/web-server/src/loaders/auth.js
@@ -1,9 +1,7 @@
 const DataLoader = require('dataloader');
-const Debug = require('debug');
 const WebServerError = require('../utils/WebServerError');
 const regenerateSession = require('../utils/regenerateSession');
-
-const debug = Debug('loaders.auth');
+const generateCredentials = require('../utils/generateCredentials');
 
 module.exports = (clients, isAuthed, rootUrl, monitor, strategies, req, cfg) => {
   const getCredentials = new DataLoader(queries => {
@@ -11,55 +9,30 @@ module.exports = (clients, isAuthed, rootUrl, monitor, strategies, req, cfg) => 
       queries.map(async () => {
         // Don't report much to the user, to avoid revealing sensitive information, although
         // it is likely in the service logs.
-        const credentialError = new WebServerError('InputError', 'Could not generate credentials for this user');
         const unauthorizedError = new WebServerError('Unauthorized', 'Authentication is required to generate credentials');
 
         if (!req.user) {
           throw unauthorizedError;
         }
 
-        const provider = req.user.identityProviderId;
-        const strategy = strategies[provider];
-
-        if (!strategy) {
-          throw credentialError;
-        }
-
-        const user = await strategy.userFromIdentity(req.user.identity);
-
-        if (!user) {
-          debug(`Could not find user with identity ${req.user.identity}`);
-          throw credentialError;
-        }
-
-        // Create and return temporary credentials, limiting expires to a max of 15 minutes
-        const { credentials: issuer, temporaryCredentials: { startOffset } } = cfg.taskcluster;
-        const { credentials: userCredentials, expires } = user.createCredentials({
-          // issuer
-          credentials: issuer,
-          startOffset,
-          expiry: '15 minutes',
+        const credentials = await generateCredentials({
+          cfg,
+          strategy: strategies[req.user.identityProviderId],
+          identity: req.user.identity,
+          monitor,
         });
 
         await regenerateSession(req);
 
-        // Move expires back by 30 seconds to ensure the user refreshes well in advance of the
-        // actual credential expiration time
-        expires.setSeconds(expires.getSeconds() - 30);
-
-        monitor.log.createCredentials({
-          clientId: userCredentials.clientId,
-          expires,
-          userIdentity: user.identity,
-        });
-
-        return {
-          credentials: userCredentials,
-          expires,
-        };
+        return credentials;
       })
     );
   });
+  const isLoggedIn = new DataLoader(queries =>
+    Promise.all(
+      queries.map(() => Boolean(req.user))
+    )
+  );
 
   const isLoggedIn = new DataLoader(queries =>
     Promise.all(
---
+++ services/web-server/src/utils/generateCredentials.js
@@ -0,0 +1,46 @@
+const debug = require('debug')('utils:generateCredentials');
+const assert = require('assert');
+const WebServerError = require('./WebServerError');
+
+module.exports = async ({ cfg, strategy, identity, monitor }) => {
+  [cfg, strategy, identity, monitor].map(assert.ok);
+
+  // Don't report much to the user, to avoid revealing sensitive information, although
+  // it is likely in the service logs.
+  const credentialError = new WebServerError('InputError', 'Could not generate credentials for this user');
+
+  if (!strategy) {
+    throw credentialError;
+  }
+
+  const user = await strategy.userFromIdentity(identity);
+
+  if (!user) {
+    debug(`Could not find user with identity ${identity}`);
+    throw credentialError;
+  }
+
+  // Create and return temporary credentials, limiting expires to a max of 15 minutes
+  const { credentials: issuer, temporaryCredentials: { startOffset } } = cfg.taskcluster;
+  const { credentials: userCredentials, expires } = user.createCredentials({
+    // issuer
+    credentials: issuer,
+    startOffset,
+    expiry: '15 minutes',
+  });
+
+  // Move expires back by 30 seconds to ensure the user refreshes well in advance of the
+  // actual credential expiration time
+  expires.setSeconds(expires.getSeconds() - 30);
+
+  monitor.log.createCredentials({
+    clientId: userCredentials.clientId,
+    expires,
+    userIdentity: user.identity,
+  });
+
+  return {
+    credentials: userCredentials,
+    expires,
+  };
+};
