25e9a08b1ef308ad5fadba8d2728c6663746615f
Hassan Ali <helfi92@gmail.com>
2019-08-19 20:18:46 -0400

    Lint fixes

--- ui/src/static/env.prod.js
+++ ui/src/static/env.prod.js
@@ -2,7 +2,8 @@ window.env = {
   APPLICATION_NAME: 'Taskcluster',
   TASKCLUSTER_ROOT_URL: 'https://taskcluster.net',
   GRAPHQL_ENDPOINT: 'https://taskcluster-web-server.herokuapp.com/graphql',
-  GRAPHQL_SUBSCRIPTION_ENDPOINT: 'wss://taskcluster-web-server.herokuapp.com/subscription',
+  GRAPHQL_SUBSCRIPTION_ENDPOINT:
+    'wss://taskcluster-web-server.herokuapp.com/subscription',
   DOCS_ONLY: 'false',
   UI_LOGIN_STRATEGY_NAMES: '',
   GA_TRACKING_ID: '',
