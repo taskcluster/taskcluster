a5c3ad57ac4f07f52c0da7535123a32a050d38b4
Hassan Ali <helfi92@gmail.com>
2019-08-21 09:48:47 -0400

    Provide better defaults to graphql endpoints

--- ui/src/static/env.js
+++ ui/src/static/env.js
@@ -1,8 +1,8 @@
 window.env = {
   APPLICATION_NAME: 'Taskcluster',
   TASKCLUSTER_ROOT_URL: 'https://taskcluster.net',
-  GRAPHQL_ENDPOINT: 'http://localhost:5080/graphql',
-  GRAPHQL_SUBSCRIPTION_ENDPOINT: 'ws://localhost:5080/subscription',
+  GRAPHQL_ENDPOINT: '/graphql',
+  GRAPHQL_SUBSCRIPTION_ENDPOINT: '/subscription',
   DOCS_ONLY: 'false',
   UI_LOGIN_STRATEGY_NAMES: 'github mozilla-auth0',
   GA_TRACKING_ID: '',
--- ui/src/static/env.prod.js
+++ ui/src/static/env.prod.js
@@ -1,9 +1,8 @@
 window.env = {
   APPLICATION_NAME: 'Taskcluster',
   TASKCLUSTER_ROOT_URL: 'https://taskcluster.net',
-  GRAPHQL_ENDPOINT: 'https://taskcluster-web-server.herokuapp.com/graphql',
-  GRAPHQL_SUBSCRIPTION_ENDPOINT:
-    'wss://taskcluster-web-server.herokuapp.com/subscription',
+  GRAPHQL_ENDPOINT: '/graphql',
+  GRAPHQL_SUBSCRIPTION_ENDPOINT: '/subscription',
   DOCS_ONLY: 'false',
   UI_LOGIN_STRATEGY_NAMES: '',
   GA_TRACKING_ID: '',
