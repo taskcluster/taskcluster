e622b7165e66b6f252dc738776b3479144f481df
Dustin J. Mitchell <dustin@mozilla.com>
2019-08-25 18:00:24 +0000

    move monitoring docs into deployment section of manual

--- infrastructure/tooling/src/generate/generators/monitoring.js
+++ infrastructure/tooling/src/generate/generators/monitoring.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const cronstrue = require('cronstrue');
 const table = require('markdown-table');
-const {listServices, writeRepoFile} = require('../../utils');
+const {listServices, modifyRepoFile} = require('../../utils');
 
 const SERVICES = listServices();
 
@@ -40,31 +40,8 @@ exports.tasks.push({
       res = res.concat(requirements[`monitoring-hints-${name}`]);
     });
 
-    const doc = `
-<!-- GENERATED DOCUMENTATION DO NOT EDIT -->
-# Monitoring Suggestions
-
-Taskcluster has several background processes that you should ensure are running on a schedule. Any of the following will generate messages
-of the form:
-
-\`\`\`json
-{
-  "Type": "monitor.periodic",
-  "Logger": "<Logger>",
-  "Fields": {
-    "name": "<Name>"
-  }
-}
-\`\`\`
-
-They will also have Fields for \`status\`, \`duration\`, and a serialized \`error\` if an error occured.
-
-The processes that have \`continuous\` for their dedaline and schedule run every few minutes and should complete fairly quickly. The rest
-have their schedules and maximum allowed duration defined here. All times are relative to the timezone of the k8s master.
-
-${table(res)}
-    `.trim();
-
-    await writeRepoFile(path.join('dev-docs', 'monitoring-suggestions.md'), doc);
+    const docFile = path.join('ui', 'docs', 'manual', 'deploying', 'monitoring.md');
+    await modifyRepoFile(docFile,
+      content => content.replace(/(<!-- GENERATED; DO NOT EDIT -->).*/ms, `$1\n${table(res)}`));
   },
 });
