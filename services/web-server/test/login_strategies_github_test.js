7cf33d50c17625e6498ae17b2bcc0905f362c563
Hassan Ali <helfi92@gmail.com>
2019-09-17 10:31:23 -0400

    Adjust tests

--- services/web-server/test/login_strategies_github_test.js
+++ services/web-server/test/login_strategies_github_test.js
@@ -16,35 +16,61 @@ suite(testing.suiteName(), () => {
     ],
     'taskcluster': [
       { login: 'taskcluster' },
+      { login: 'neutrinojs' },
     ],
     'a/c': [],
   };
-  const taskclusterRepos = ['taskcluster-client', 'taskcluster-queue', 'docker-worker'];
-  const neutrinojsRepos = ['neutrino', 'webpack-chain'];
   const repos = {
-    'taskcluster': taskclusterRepos.map(repo => ({ name: repo })),
-    'neutrinojs': neutrinojsRepos.map(repo => ({ name: repo })),
+    'taskcluster': [{ name: 'taskcluster-client' }, { name: 'taskcluster-queue' }],
+    'neutrinojs': [{ name: 'neutrino' }, { name: 'webpack-chain' }],
+  };
+  const userOrgPermissionLevel = {
+    'taskcluster': {
+      'taskcluster-client': {
+        'octocat': { permission: 'admin' },
+        'taskcluster': { permission: 'read' },
+      },
+      'taskcluster-queue': {
+        'octocat': { permission: 'none' },
+        'taskcluster': { permission: 'write' },
+      },
+    },
+    'neutrinojs': {
+      'neutrino': {
+        'octocat': { permission: 'admin' },
+        'taskcluster': { permission: 'admin' },
+      },
+      'webpack-chain': {
+        'octocat': { permission: 'none' },
+        'taskcluster': { permission: 'none' },
+      }
+    },
   };
   class FakeGithubClient {
+    constructor() {
+      this.currentUsername = null;
+    }
+
     async userFromUsername(username) {
+      this.currentUsername = username;
+
       if (username === 'FAIL') {
         throw new Error('uhoh');
       }
+
       const user_id = users[username];
+
       if (!user_id) {
         const err = new Error('No such user');
         err.status = 404;
         throw err;
       }
+
       return {id: user_id};
     }
 
-    async orgsFromUsername(username) {
-      if (username === 'FAIL') {
-        throw new Error('uhoh');
-      }
-
-      const organizations = orgs[username];
+    async listOrgs() {
+      const organizations = orgs[this.currentUsername];
 
       if (!organizations) {
         throw new Error(`orgs for user ${username} not found`);
@@ -65,6 +91,17 @@ suite(testing.suiteName(), () => {
       }
 
       return repositories;
+    }
+
+    async readPermissionLevel(org, repo, username) {
+      const userPermissionLevel = userOrgPermissionLevel[org][repo][username];
+
+      if (!userPermissionLevel) {
+        throw new Error(`permission level for ${username} in ${org}/${repo} not found`);
+      }
+
+      // User has write access if permission is `admin` or `write`
+      return userPermissionLevel;
     }
   }
 
@@ -109,11 +146,18 @@ suite(testing.suiteName(), () => {
 
   test('userFromIdentity roles with known user', async function() {
     const user = await strategy.userFromIdentity('github/10|octocat');
-    assert.deepEqual(user.roles, [...taskclusterRepos.map(repo => `github-group:taskcluster/${repo}`), ...neutrinojsRepos.map(repo => `github-group:neutrinojs/${repo}`)]);
+
+    assert.deepEqual(user.roles, ['github-group:taskcluster/taskcluster-client', 'github-group:neutrinojs/neutrino']);
   });
 
   test('userFromIdentity user with empty roles', async function() {
     const user = await strategy.userFromIdentity('github/30|a!2Fc');
     assert.deepEqual(user.roles, []);
+  });
+
+  test('userFromIdentity only has roles with write access', async function() {
+    const user = await strategy.userFromIdentity('github/20|taskcluster');
+
+    assert.deepEqual(user.roles, ['github-group:taskcluster/taskcluster-queue', 'github-group:neutrinojs/neutrino']);
   });
 });
