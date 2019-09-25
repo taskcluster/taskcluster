ca98510fdef29ffd282ac7aec921b713a726685f
Hassan Ali <helfi92@gmail.com>
2019-09-25 13:55:26 -0400

    Remove unused code

--- services/web-server/src/data/GithubAccessToken.js
+++ services/web-server/src/data/GithubAccessToken.js
@@ -1,7 +1,6 @@
-const assert = require('assert');
 const Entity = require('azure-entities');
 
-const GithubAccessToken = Entity.configure({
+module.exports = Entity.configure({
   version: 1,
   partitionKey: Entity.keys.ConstantKey('githubAccessTokens'),
   rowKey: Entity.keys.StringKey('userId'),
@@ -13,24 +12,3 @@ const GithubAccessToken = Entity.configure({
     accessToken: Entity.types.EncryptedText,
   },
 });
-
-/**
- * Expire GithubAccessToken entries.
- *
- * Returns a promise that all expired GithubAccessToken entries have been deleted
- */
-GithubAccessToken.expire = async function(now) {
-  assert(now instanceof Date, 'now must be given as option');
-  let count = 0;
-
-  await Entity.scan.call(this, {
-    expires: Entity.op.lessThan(now),
-  }, {
-    limit: 250, // max number of concurrent delete operations
-    handler: entry => { count++; return entry.remove(true); },
-  });
-
-  return count;
-};
-
-module.exports = GithubAccessToken;
