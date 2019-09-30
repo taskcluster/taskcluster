b66a970ce31374d17c202a91180a7d52f0c27b48
ededals <edgars.patmalnieks@gmail.com>
2019-09-30 21:33:00 +0300

    fixes test

--- infrastructure/tooling/src/smoketest/checks/succeed.js
+++ infrastructure/tooling/src/smoketest/checks/succeed.js
@@ -7,25 +7,40 @@ exports.tasks.push({
   provides: [
     'built-in/succeed',
   ],
-  run: async () => {
+  run: async (requirements, utils) => {
     let task = {
       provisionerId: 'built-in',
       workerType: 'succeed',
       created: (new Date()).toJSON(),
-      deadline: (new Date()).toJSON(),
-      payload: {},
+      deadline: taskcluster.fromNowJSON('2 minutes'),
       metadata: {
-        name: 'Smoketest built-in/succeed',
-        description: 'A task for built-in/succeed worker type',
-        owner: 'none@taskcluster.com',
-        source: 'https://tools.taskcluster.net/tasks/create',
+        name: "Smoketest built-in/succeed",
+        description: "built-in/succeed task created during smoketest",
+        owner: "smoketest@taskcluster.net",
+        source: "https://taskcluster.net",
       },
+      payload: {},
     };
-    async ()=> {
-      let taskId = taskcluster.slugid();
-      let queue = new taskcluster.Queue();
-      let result = await queue.createTask(taskId, task);
-      return result;
-    };
+    let taskId = taskcluster.slugid();
+    let queue = new taskcluster.Queue({
+      rootUrl: process.env.TASKCLUSTER_ROOT_URL,
+      credentials: {
+        clientId: process.env.TASKCLUSTER_CLIENT_ID,
+        accessToken: process.env.TASKCLUSTER_ACCESS_TOKEN,
+      },
+    });
+    let result = await queue.createTask(taskId, task);
+    while (1){
+      let status = await queue.status(taskId);
+      if (status.state === 'pending'){
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } else {
+        return {
+          ['built-in/succeed']: result.state,
+        };
+
+      }
+    }
+    return result;
   },
 });
