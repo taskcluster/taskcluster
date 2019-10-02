c9ca5b6ef3f357864cb82aa55c2d0f6da28b3a53
Noor Fatima <devnoorfatima@gmail.com>
2019-10-03 00:07:45 +0500

    Added tooltip to the link of source.

--- ui/src/components/TaskDetailsCard/index.jsx
+++ ui/src/components/TaskDetailsCard/index.jsx
@@ -214,7 +214,7 @@ export default class TaskDetailsCard extends Component {
                   component="a"
                   href={task.metadata.source}
                   target="_blank"
-                  rel="noopener noreferrer"
+                  rel="noopener noreferrer" 
                   title ="https://hg.mozilla.org/integration/mozilla-inbound/raw-file/8735151820ebeaf7ee06e9f2ed16868c164d85ab/.taskcluster.yml"
                   >
                   <ListItemText
