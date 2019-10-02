72c0e649611ce8a7fa5c0a6f37c03903653ef150
ededals <edgars.patmalnieks@gmail.com>
2019-10-02 07:06:13 +0300

    Fixing comments

--- infrastructure/tooling/src/smoketest/checks/succeed.js
+++ infrastructure/tooling/src/smoketest/checks/succeed.js
@@ -22,25 +22,35 @@ exports.tasks.push({
       payload: {},
     };
     let taskId = taskcluster.slugid();
-    let queue = new taskcluster.Queue({
-      rootUrl: process.env.TASKCLUSTER_ROOT_URL,
-      credentials: {
-        clientId: process.env.TASKCLUSTER_CLIENT_ID,
-        accessToken: process.env.TASKCLUSTER_ACCESS_TOKEN,
-      },
-    });
-    let result = await queue.createTask(taskId, task);
-    while (1){
+    utils.status({message: 'built-in/succeed taskId: ' + taskId});
+    let queue = new taskcluster.Queue(taskcluster.fromEnvVars());
+    await queue.createTask(taskId, task);
+    let pollForStatus = true;
+    setTimeout(function () {
+      pollForStatus = false;
+    }, 12000);
+    while (pollForStatus){
       let status = await queue.status(taskId);
-      if (status.state === 'pending'){
-        await new Promise(resolve => setTimeout(resolve, 2000));
-      } else {
+      if (status.status.state === 'pending' || status.status.state === 'running'){
+        utils.status({
+          message: 'Polling built-in/succeed task. Current status: ' + status.status.state,
+        });
+        await new Promise(resolve => setTimeout(resolve, 1000));
+      } else if (status.status.state === 'completed') {
         return {
-          ['built-in/succeed']: result.state,
+          ['built-in/succeed']: status.status.state,
         };
-
+      } else {
+        return utils.skip({
+          [`built-in/succeed`]: status.status.state,
+          reason: 'Task finished with status ' + status.status.state,
+        });
       }
     }
-    return result;
+    return utils.skip({
+      [`built-in/succeed`]: 'deadline exceeded',
+      reason: 'Deadline exceeded',
+    });
+
   },
 });
