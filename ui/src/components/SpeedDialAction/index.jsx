fcbfbdf82f34ab1f7f936a5c2ee513ebfe83a05a
rolikasi <bayizbekovedil@gmail.com>
2019-10-02 13:30:24 +0600

    reset speeddialaction component

--- ui/src/components/SpeedDialAction/index.jsx
+++ ui/src/components/SpeedDialAction/index.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
@@ -10,9 +9,6 @@ import { withAuth } from '../../utils/Auth';
 @withStyles(theme => ({
   secondaryIcon: {
     ...theme.mixins.secondaryIcon,
-  },
-  button: {
-    ...theme.mixins.button,
   },
 }))
 /**
@@ -66,69 +62,3 @@ export default class SpeedDialAction extends Component {
     );
   }
 }
-=======
-import React, { Component } from 'react';
-import classNames from 'classnames';
-import { withStyles } from '@material-ui/core/styles';
-import MuiSpeedDialAction from '@material-ui/lab/SpeedDialAction';
-import { string, bool } from 'prop-types';
-import { withAuth } from '../../utils/Auth';
-
-@withAuth
-@withStyles(theme => ({
-  secondaryIcon: {
-    ...theme.mixins.secondaryIcon,
-  }
-}))
-/**
- * A Material UI SpeedDialAction augmented with application specific props.
- */
-export default class SpeedDialAction extends Component {
-  static defaultProps = {
-    className: null,
-    requiresAuth: false,
-  };
-
-  static propTypes = {
-    /** The CSS class name of the wrapper element */
-    className: string,
-    /** If true, the button will be disabled if the user is not authenticated */
-    requiresAuth: bool,
-  };
-
-  render() {
-    const {
-      classes,
-      className,
-      requiresAuth,
-      ButtonProps,
-      user,
-      onAuthorize,
-      onUnauthorize,
-      tooltipTitle,
-      ...props
-    } = this.props;
-    const lackingAuth = requiresAuth && !user;
-    const buttonProps = {
-      ...ButtonProps,
-      ...(lackingAuth ? { disabled: true } : {}),
-    };
-    const title = tooltipTitle
-      ? {
-          tooltipTitle: lackingAuth
-            ? `${tooltipTitle} (Auth Required)`
-            : tooltipTitle,
-        }
-      : null;
-
-    return (
-      <MuiSpeedDialAction
-        className={classNames(classes.secondaryIcon, className)}
-        ButtonProps={buttonProps}
-        {...title}
-        {...props}
-      />
-    );
-  }
-}
->>>>>>> 1b5c0d8c606e507837f0be0d15ef75e57bd8bd64
