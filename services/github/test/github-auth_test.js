5cffdbe16923d49265b52fb58861615f82ff77ab
Dustin J. Mitchell <dustin@mozilla.com>
2019-10-02 22:20:26 +0000

    Allow GitHub PEM to be on a single line

---
+++ services/github/test/github-auth_test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const githubAuth = require('../src/github-auth');
+const testing = require('taskcluster-lib-testing');
+
+suite(testing.suiteName(), function() {
+  suite('getPrivatePEM', function() {
+    const privatePEM = '-----BEGIN RSA PRIVATE KEY-----\nsomekey\n-----END RSA PRIVATE KEY-----';
+
+    test('with actual newlines', function() {
+      const cfg = {github: {credentials: {privatePEM}}};
+      assert.equal(githubAuth.getPrivatePEM(cfg), privatePEM);
+    });
+
+    test('with escaped newlines', function() {
+      const cfg = {github: {credentials: {privatePEM: privatePEM.replace('\n', '\\n')}}};
+      assert.equal(githubAuth.getPrivatePEM(cfg), privatePEM);
+    });
+
+    test('with invalid value', function() {
+      const cfg = {github: {credentials: {privatePEM: 'somekey'}}};
+      assert.throws(() => githubAuth.getPrivatePEM(cfg), /must match/);
+    });
+  });
+});
