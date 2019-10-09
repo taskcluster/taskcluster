1936d3e72abf5a0ffbb24b21ae4fe589d95cc188
rolikasi <bayizbekovedil@gmail.com>
2019-10-09 20:39:13 +0600

    delete duplicate import of markdown component

--- ui/src/components/RoleForm/index.jsx
+++ ui/src/components/RoleForm/index.jsx
@@ -2,7 +2,6 @@ import React, { Component, Fragment } from 'react';
 import { oneOfType, object, string, func, bool } from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import classNames from 'classnames';
-import MarkdownTextArea from '@mozilla-frontend-infra/components/MarkdownTextArea';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
