c4907a5910d78e4223d73d3800a8622f2e8880e5
Pete Moore <pmoore@mozilla.com>
2019-10-16 10:28:28 +0200

    Version bump from 16.3.0 to 16.3.1

--- main.go
+++ main.go
@@ -62,7 +62,7 @@ var (
 	logName = "public/logs/live_backing.log"
 	logPath = filepath.Join("generic-worker", "live_backing.log")
 
-	version  = "16.3.0"
+	version  = "16.3.1"
 	revision = "" // this is set during build with `-ldflags "-X main.revision=$(git rev-parse HEAD)"`
 )
 
