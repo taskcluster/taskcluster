ab8b60b6b053d40fe3e780bd58f8daa39c6cf940
Brian Stack <bstack@mozilla.com>
2019-10-18 20:22:17 -0700

    Replicate roughly the old provisioning logic for now

--- services/worker-manager/src/estimator.js
+++ services/worker-manager/src/estimator.js
@@ -11,7 +11,11 @@ class Estimator {
     const {pendingTasks} = await this.queue.pendingTasks(provisionerId, workerType);
 
     // First we find the amount of capacity we want. This is a very simple approximation
-    const desiredCapacity = Math.max(minCapacity, Math.min(pendingTasks, maxCapacity));
+    // We add runningCapacity here to make the min/max stuff work and then remove it to
+    // decide how much more to request. In other words, we will ask to spawn
+    // enough capacity to cover all pending tasks at any time unless it would
+    // create more than maxCapacity instances
+    const desiredCapacity = Math.max(minCapacity, Math.min(pendingTasks + runningCapacity, maxCapacity));
 
     // Workers turn themselves off so we just return a positive number for
     // how many extra we want if we do want any
