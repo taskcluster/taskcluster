1cbd15a2cfa6a7dfd5c813269fa7393497fdaed1
Hassan Ali <helfi92@gmail.com>
2019-10-21 16:38:26 -0400

    Add CLI login

--- ui/src/components/ClientForm/index.jsx
+++ ui/src/components/ClientForm/index.jsx
@@ -129,47 +129,20 @@ export default class ClientForm extends Component {
     initialThirdPartyClient: {},
   };
 
-  static getDerivedStateFromProps({ isNewClient, client }, state) {
-    if (isNewClient || (state.clientId && state.prevClient === client)) {
-      return null;
-    }
-
-    return {
-      description: client.description,
-      clientId: client.clientId,
-      created: client.created,
-      lastModified: client.lastModified,
-      lastDateUsed: client.lastDateUsed,
-      lastRotated: client.lastRotated,
-      expires: client.expires,
-      deleteOnExpiration: client.deleteOnExpiration,
-      scopeText: client.scopes.join('\n'),
-      expandedScopes: client.expandedScopes,
-      disabled: client.disabled,
-      prevClient: client,
-    };
-  }
-
   query = parse(this.props.location.search.slice(1));
 
   state = {
-    description: this.props.initialThirdPartyClient.description || '',
-    scopeText: this.props.initialThirdPartyClient.scopes
-      ? this.props.initialThirdPartyClient.scopes.join('\n')
-      : '',
-    clientId: this.props.initialThirdPartyClient.clientId || '',
-    created: null,
-    lastModified: null,
-    lastDateUsed: null,
-    lastRotated: null,
-    expires: this.props.initialThirdPartyClient.expires
-      ? new Date(this.props.initialThirdPartyClient.expires)
-      : addYears(new Date(), 1000),
-    deleteOnExpiration: true,
-    expandedScopes: null,
-    disabled: null,
-    // eslint-disable-next-line react/no-unused-state
-    prevClient: null,
+    description: this.props.client.description || '',
+    clientId: this.props.client.clientId || '',
+    created: this.props.client.created || null,
+    lastModified: this.props.client.lastModified || null,
+    lastDateUsed: this.props.client.lastDateUsed || null,
+    lastRotated: this.props.client.lastRotated || null,
+    expires: this.props.client.expires,
+    deleteOnExpiration: this.props.client.deleteOnExpiration,
+    scopeText: (this.props.client.scopes || []).join('\n'),
+    expandedScopes: this.props.client.expandedScopes,
+    disabled: this.props.client.disabled,
   };
 
   handleDeleteClient = () => this.props.onDeleteClient(this.state.clientId);
@@ -279,7 +252,7 @@ export default class ClientForm extends Component {
               />
             </ListItem>
           )}
-          {client && (
+          {!isNewClient && client && (
             <Fragment>
               <ListItem>
                 <ListItemText primary="Client ID" secondary={clientId} />
@@ -346,11 +319,11 @@ export default class ClientForm extends Component {
               fullWidth
               multiline
               spellCheck={false}
-              placeholder={isNewClient ? 'new-scope:for-something:*' : null}
+              placeholder="new-scope:for-something:*"
               value={scopeText}
             />
           </ListItem>
-          {client && expandedScopes.length ? (
+          {!isNewClient && client && expandedScopes.length ? (
             <Fragment>
               <ListItem>
                 <ListItemText
