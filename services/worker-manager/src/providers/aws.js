5c69cb1301bcb3f2f421b771ee1585dd249140b2
owlishDeveloper <bugzeeeeee@gmail.com>
2019-10-27 14:53:06 -0700

    Report ec2.describeInstances errors as workerPool errors
    To figure out which worker pool produces the errors

--- services/worker-manager/src/providers/aws.js
+++ services/worker-manager/src/providers/aws.js
@@ -228,9 +228,23 @@ class AwsProvider extends Provider {
         IncludeAllInstances: true,
       }).promise()).InstanceStatuses;
     } catch (e) {
+
+      const workerPool = this.WorkerPool.load({
+        workerPoolId: worker.workerPoolId,
+      });
+
+      await workerPool.reportError({
+        kind: 'description-error',
+        title: 'Instance Description Error',
+        description: `Error calling AWS API: ${e.message}`,
+        notify: this.notify,
+        WorkerPoolError: this.WorkerPoolError,
+      });
+
       if (e.code === 'InvalidInstanceID.NotFound') { // aws throws this error for instances that had been terminated, too
         return worker.modify(w => {w.state = this.Worker.states.STOPPED;});
       }
+
       throw e;
     }
 
