310da192e7e7b591d843b1a9c4b4985cc1aabdc8
owlishDeveloper <bugzeeeeee@gmail.com>
2019-11-14 15:35:03 -0800

    Put the cache size in env var, and make it optional

--- services/queue/src/main.js
+++ services/queue/src/main.js
@@ -432,7 +432,7 @@ let load = loader({
         blobRegion: ctx.cfg.app.blobArtifactRegion,
         publicBlobBucket: ctx.cfg.app.publicBlobArtifactBucket,
         privateBlobBucket: ctx.cfg.app.privateBlobArtifactBucket,
-        LRUcache: new QuickLRU({maxSize: ctx.cfg.app.taskCacheMaxSize}),
+        LRUcache: new QuickLRU({maxSize: ctx.cfg.app.taskCacheMaxSize || 10}),
       },
       rootUrl: ctx.cfg.taskcluster.rootUrl,
       schemaset: ctx.schemaset,
