06796e45816e0ea7fafd11a736faae3d584054e9
Dustin J. Mitchell <dustin@mozilla.com>
2019-11-18 18:58:31 +0000

    Bug 1596098 - return 400 when a hook is too large

--- services/hooks/src/api.js
+++ services/hooks/src/api.js
@@ -267,6 +267,10 @@ builder.declare({
 
       }));
   } catch (err) {
+    if (err && err.code === 'PropertyTooLarge') {
+      return res.reportError('InputError', err.toString(), {});
+    }
+
     if (!err || err.code !== 'EntityAlreadyExists') {
       throw err;
     }
@@ -363,15 +367,22 @@ builder.declare({
     });
   }
 
-  await hook.modify((hook) => {
-    hook.metadata = hookDef.metadata;
-    hook.bindings = hookDef.bindings;
-    hook.task = hookDef.task;
-    hook.triggerSchema = hookDef.triggerSchema;
-    hook.schedule = schedule;
-    hook.nextTaskId = taskcluster.slugid();
-    hook.nextScheduledDate = nextDate(schedule);
-  });
+  try {
+    await hook.modify((hook) => {
+      hook.metadata = hookDef.metadata;
+      hook.bindings = hookDef.bindings;
+      hook.task = hookDef.task;
+      hook.triggerSchema = hookDef.triggerSchema;
+      hook.schedule = schedule;
+      hook.nextTaskId = taskcluster.slugid();
+      hook.nextScheduledDate = nextDate(schedule);
+    });
+  } catch (err) {
+    if (err && err.code === 'PropertyTooLarge') {
+      return res.reportError('InputError', err.toString(), {});
+    }
+    throw err;
+  }
 
   let definition = await hook.definition();
   this.publisher.hookUpdated({hookGroupId, hookId});
