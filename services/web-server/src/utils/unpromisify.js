5cdaa6d1389e29b806be6352a9b0dbf617089cc5
Dustin J. Mitchell <dustin@mozilla.com>
2019-11-20 18:47:09 +0000

    Add tests

--- services/web-server/src/utils/unpromisify.js
+++ services/web-server/src/utils/unpromisify.js
@@ -1,7 +1,8 @@
 /**
  * Wrap an async `fn` in such a way that it can act as a function taking a callback
  * as its last argument.  That callback either gets cb(err) for errors, or cb(null, res)
- * or (if returnsArray is true) cb(null, ...res)
+ * or (if returnsArray is true) cb(null, ...res).  The resuting function has the
+ * appropriate arity (one more than the input arity, to account for the 'done' argument)
  */
 module.exports = (fn, {returnsArray} = {}) => {
   // eslint-disable-next-line no-unused-vars
