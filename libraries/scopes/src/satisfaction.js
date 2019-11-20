c4ecef740ccf965e48b9b4387ee7b1d7b8ccf3dd
Dustin J. Mitchell <dustin@mozilla.com>
2019-11-20 20:50:59 +0000

    Bug 1588096 - combine lib into a single short file

--- libraries/scopes/src/satisfaction.js
+++
@@ -1,9 +0,0 @@
-exports.patternMatch = (pattern, scope) => {
-  if (scope === pattern) {
-    return true;
-  }
-  if (/\*$/.test(pattern)) {
-    return scope.indexOf(pattern.slice(0, -1)) === 0;
-  }
-  return false;
-};
--- libraries/scopes/src/validate.js
+++
@@ -1,9 +0,0 @@
-const VALID_SCOPE = /^[\x20-\x7e]*$/;
-
-/**
- * Determine whether a scope is valid.  Scopes must be strings of ASCII
- * characters 0x20-0x7e (printable characters, including space but no other
- * whitespace)
- */
-exports.validScope = (scope) =>
-  typeof scope == 'string' && VALID_SCOPE.test(scope);
