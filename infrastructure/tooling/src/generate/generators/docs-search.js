04b44b0bd915d11f8e74563eac06748c53ffa4e5
Hassan Ali <helfi92@gmail.com>
2019-11-28 18:03:43 -0500

    Improve indexing logic
    * Add id field to help in deep-linking
    * Remove comments from fences insted of considering them as headers

--- infrastructure/tooling/src/generate/generators/docs-search.js
+++ infrastructure/tooling/src/generate/generators/docs-search.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const fs = require('fs');
+const GithubSlugger = require('github-slugger');
 const { writeRepoJSON, REPO_ROOT } = require('../../utils');
 
 const DOCS_DIR = path.join(REPO_ROOT, 'ui', 'docs');
 const MD_PARSER = {
   HEADING: /^ {0,3}(#{1,6}) +([^\n]*?)(?: +#+)? *(?:\n+|$)/,
+  FENCES: /^ {0,3}(`{3,}|~{3,})([^`~\n]*)\n(?:|([\s\S]*?)\n)(?: {0,3}\1[~`]* *(?:\n+|$)|$)/,
 };
 
 function walkSync (dir, filelist = []) {
@@ -36,28 +38,33 @@ exports.tasks = [{
     const textFromHash = text => text.replace(/(^#{1,6}\s)/, '');
 
     files.forEach(file => {
+      const slugger = new GithubSlugger();
       const pageHeaders = [];
       const content = fs.readFileSync(file, 'utf8');
 
-      content.split('\n').forEach(line => {
-        const match = MD_PARSER.HEADING.exec(line);
+      content
+        .replace(new RegExp(MD_PARSER.FENCES, 'gm'), '')
+        .split('\n').forEach(line => {
+          const match = line.match(MD_PARSER.HEADING);
 
-        if (match) {
-          const [anchor, hashes] = match;
-          const headerElement = `h${hashes.length}`;
-          const entry = {
-            path: file
-              .replace(DOCS_DIR, '')
-              .replace(path.extname(file), '')
-              .replace('/README', ''),
-            title: hashes.length === 1 ? textFromHash(anchor) : null,
-            subtitle: hashes.length !== 1 ? textFromHash(anchor) : null,
-            element: headerElement,
-          };
+          if (match) {
+            const [anchor, hashes] = match;
+            const headerElement = `h${hashes.length}`;
+            const anchorText = textFromHash(anchor);
+            const entry = {
+              path: file
+                .replace(DOCS_DIR, '')
+                .replace(path.extname(file), '')
+                .replace('/README', ''),
+              title: hashes.length === 1 ? anchorText : null,
+              subtitle: hashes.length !== 1 ? anchorText : null,
+              element: headerElement,
+              id: slugger.slug(anchorText),
+            };
 
-          pageHeaders.push(entry);
-        }
-      });
+            pageHeaders.push(entry);
+          }
+        });
 
       const h1Title = pageHeaders.find(header => header.element === 'h1');
 
