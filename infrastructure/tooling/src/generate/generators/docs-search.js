8ca3155381eead1f009fee68222d2968646dc665
Hassan Ali <helfi92@gmail.com>
2019-11-29 10:34:31 -0500

    Remove resources from the index file

--- infrastructure/tooling/src/generate/generators/docs-search.js
+++ infrastructure/tooling/src/generate/generators/docs-search.js
@@ -37,43 +37,46 @@ exports.tasks = [{
     const files = walkSync(DOCS_DIR).filter(path => path.endsWith('.mdx'));
     const textFromHash = text => text.replace(/(^#{1,6}\s)/, '');
 
-    files.forEach(file => {
-      const slugger = new GithubSlugger();
-      const pageHeaders = [];
-      const content = fs.readFileSync(file, 'utf8');
+    files
+      // resources.mdx links have html in them...
+      .filter(file => file !== path.join(DOCS_DIR, 'resources.mdx'))
+      .forEach(file => {
+        const slugger = new GithubSlugger();
+        const pageHeaders = [];
+        const content = fs.readFileSync(file, 'utf8');
 
-      content
-        .replace(new RegExp(MD_PARSER.FENCES, 'gm'), '')
-        .split('\n').forEach(line => {
-          const match = line.match(MD_PARSER.HEADING);
+        content
+          .replace(new RegExp(MD_PARSER.FENCES, 'gm'), '')
+          .split('\n').forEach(line => {
+            const match = line.match(MD_PARSER.HEADING);
 
-          if (match) {
-            const [anchor, hashes] = match;
-            const headerElement = `h${hashes.length}`;
-            const anchorText = textFromHash(anchor);
-            const entry = {
-              path: file
-                .replace(DOCS_DIR, '')
-                .replace(path.extname(file), '')
-                .replace('/README', ''),
-              title: hashes.length === 1 ? anchorText : null,
-              subtitle: hashes.length !== 1 ? anchorText : null,
-              element: headerElement,
-              id: slugger.slug(anchorText),
-            };
+            if (match) {
+              const [anchor, hashes] = match;
+              const headerElement = `h${hashes.length}`;
+              const anchorText = textFromHash(anchor);
+              const entry = {
+                path: file
+                  .replace(DOCS_DIR, '')
+                  .replace(path.extname(file), '')
+                  .replace('/README', ''),
+                title: hashes.length === 1 ? anchorText : null,
+                subtitle: hashes.length !== 1 ? anchorText : null,
+                element: headerElement,
+                id: slugger.slug(anchorText),
+              };
 
-            pageHeaders.push(entry);
-          }
-        });
+              pageHeaders.push(entry);
+            }
+          });
 
-      const h1Title = pageHeaders.find(header => header.element === 'h1');
+        const h1Title = pageHeaders.find(header => header.element === 'h1');
 
-      if (h1Title) {
-        docsSearch.push(...pageHeaders.map(header => ({ ...header, title: h1Title.title })));
-      } else {
-        docsSearch.push(...pageHeaders);
-      }
-    });
+        if (h1Title) {
+          docsSearch.push(...pageHeaders.map(header => ({ ...header, title: h1Title.title })));
+        } else {
+          docsSearch.push(...pageHeaders);
+        }
+      });
 
     writeRepoJSON('generated/docs-search.json', docsSearch.sort(sort));
   },
