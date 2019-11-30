86c3db55940e6f68bcc009ac6f1cf22f01916cfb
Mustafa Jebara <moosejebara@gmail.com>
2019-11-30 11:00:41 -0500

    Add Drawer Component to display Actions and the Description of a provisioner
    What's left for this Drawer
      - Actions needs to be rendered
      - Actions need to be handled

--- ui/src/components/ProvisionerDetailsTable/index.jsx
+++ ui/src/components/ProvisionerDetailsTable/index.jsx
@@ -6,6 +6,13 @@ import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 import IconButton from '@material-ui/core/IconButton';
 import InformationVariantIcon from 'mdi-react/InformationVariantIcon';
+import {
+  Drawer,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+} from '@material-ui/core';
 import TableCellItem from '../TableCellItem';
 import Link from '../../utils/Link';
 import { withAuth } from '../../utils/Auth';
@@ -13,6 +20,7 @@ import DataTable from '../DataTable';
 import StatusLabel from '../StatusLabel';
 import DateDistance from '../DateDistance';
 import sort from '../../utils/sort';
+import Markdown from '../Markdown';
 
 @withRouter
 @withAuth
@@ -21,12 +29,26 @@ import sort from '../../utils/sort';
     marginLeft: -theme.spacing(2),
     marginRight: theme.spacing(1),
     borderRadius: 4,
-  }
+  },
+  headline: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
+    overflow: 'hidden',
+  },
+  metadataContainer: {
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+    width: 400,
+  },
 }))
 export default class ProvisionerDetailsTable extends Component {
   state = {
     sortBy: null,
     sortDirection: null,
+    drawerOpen: false,
+    drawerProvisioner: null,
   };
 
   sortProvisioners = (provisioners, sortBy, sortDirection) => {
@@ -51,20 +73,38 @@ export default class ProvisionerDetailsTable extends Component {
     this.setState({ sortBy, sortDirection });
   };
 
+  handleDrawerOpen = provisioner => {
+    return () => {
+      this.setState({
+        drawerOpen: true,
+        drawerProvisioner: provisioner,
+      });
+    };
+  };
+
+  handleDrawerClose = () => {
+    this.setState({
+      drawerOpen: false,
+      drawerProvisioner: null,
+    });
+  };
+
   renderTableRow = ({ node: provisioner }) => {
     const iconSize = 16;
     const { classes } = this.props;
 
     return (
-      <TableRow key={`${provisioner.provisionerId}-item`}>
+      <TableRow key={`${provisioner.provisionerId}`}>
         <TableCell>
           <IconButton
             className={classes.infoButton}
             name={provisioner.provisionerId}
-            onClick={this.handleDrawerOpen}>
+            onClick={this.handleDrawerOpen(provisioner)}>
             <InformationVariantIcon size={iconSize} />
           </IconButton>
-          <Link to={`/provisioners/${provisioner.provisionerId}`} title={`Explore worker types for ${provisioner.provisionerId}`}>
+          <Link
+            to={`/provisioners/${provisioner.provisionerId}`}
+            title={`Explore worker types for ${provisioner.provisionerId}`}>
             <TableCellItem button>
               {provisioner.provisionerId}
               <LinkIcon size={iconSize} />
@@ -84,9 +124,48 @@ export default class ProvisionerDetailsTable extends Component {
     );
   };
 
+  renderDrawerContent = () => {
+    const { classes } = this.props;
+    const { drawerProvisioner } = this.state;
+
+    return (
+      <div className={classes.metadataContainer}>
+        <Typography variant="h5" className={classes.headline}>
+          {drawerProvisioner && drawerProvisioner.provisionerId}
+        </Typography>
+        <List>
+          <ListItem>
+            <ListItemText
+              primary="Description"
+              secondary={
+                drawerProvisioner && drawerProvisioner.description ? (
+                  <Markdown>{drawerProvisioner.description}</Markdown>
+                ) : (
+                  'n/a'
+                )
+              }
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary="Actions"
+              secondary={
+                drawerProvisioner && drawerProvisioner.actions.length ? (
+                  <Markdown>Should Render Actions Here</Markdown>
+                ) : (
+                  'n/a'
+                )
+              }
+            />
+          </ListItem>
+        </List>
+      </div>
+    );
+  };
+
   render() {
     const { provisioners } = this.props;
-    const { sortBy, sortDirection } = this.state;
+    const { sortBy, sortDirection, drawerOpen } = this.state;
     const headers = [
       { label: 'Provisioner', id: 'provisionerId', type: 'string' },
       { label: 'Stability', id: 'stability', type: 'string' },
@@ -106,6 +185,12 @@ export default class ProvisionerDetailsTable extends Component {
           renderRow={this.renderTableRow}
           headers={headers}
         />
+        <Drawer
+          anchor="right"
+          open={drawerOpen}
+          onClose={this.handleDrawerClose}>
+          {this.renderDrawerContent()}
+        </Drawer>
       </Fragment>
     );
   }
