bc543cff8641ea2fa7a79cd11f9a9e56bf0006cf
Mustafa Jebara <moosejebara@gmail.com>
2019-11-29 23:29:56 -0500

    Create Provisioners Details Table Component
    To condense the "provsioners" page, a new provisioner component was
    created to replace the old one.
    The new provisioner component uses tables to display the provisioner
    details instead of cards as suggested in issue #2096.
    
    Currently, the component renders a table with all the provisioners and displays
    the id, stability, last available date, and expiration date for each one.
    The component also currently sorts by clicking the header.
    Actions and description will be displayed in a drawer.
    
    What's left for this component:
      - Drawer still needs to be displayed and handled
      - Actions need to be displayed and handled

---
+++ ui/src/components/ProvisionerDetailsTable/index.jsx
@@ -0,0 +1,112 @@
+import React, { Component, Fragment } from 'react';
+import LinkIcon from 'mdi-react/LinkIcon';
+import { withRouter } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import IconButton from '@material-ui/core/IconButton';
+import InformationVariantIcon from 'mdi-react/InformationVariantIcon';
+import TableCellItem from '../TableCellItem';
+import Link from '../../utils/Link';
+import { withAuth } from '../../utils/Auth';
+import DataTable from '../DataTable';
+import StatusLabel from '../StatusLabel';
+import DateDistance from '../DateDistance';
+import sort from '../../utils/sort';
+
+@withRouter
+@withAuth
+@withStyles(theme => ({
+  infoButton: {
+    marginLeft: -theme.spacing(2),
+    marginRight: theme.spacing(1),
+    borderRadius: 4,
+  }
+}))
+export default class ProvisionerDetailsTable extends Component {
+  state = {
+    sortBy: null,
+    sortDirection: null,
+  };
+
+  sortProvisioners = (provisioners, sortBy, sortDirection) => {
+    if (!sortBy) {
+      return provisioners;
+    }
+
+    return provisioners.sort((a, b) => {
+      const firstElement =
+        sortDirection === 'desc' ? b.node[sortBy] : a.node[sortBy];
+      const secondElement =
+        sortDirection === 'desc' ? a.node[sortBy] : b.node[sortBy];
+
+      return sort(firstElement, secondElement);
+    });
+  };
+
+  handleHeaderClick = ({ id: sortBy }) => {
+    const toggled = this.state.sortDirection === 'desc' ? 'asc' : 'desc';
+    const sortDirection = this.state.sortBy === sortBy ? toggled : 'desc';
+
+    this.setState({ sortBy, sortDirection });
+  };
+
+  renderTableRow = ({ node: provisioner }) => {
+    const iconSize = 16;
+    const { classes } = this.props;
+
+    return (
+      <TableRow key={`${provisioner.provisionerId}-item`}>
+        <TableCell>
+          <IconButton
+            className={classes.infoButton}
+            name={provisioner.provisionerId}
+            onClick={this.handleDrawerOpen}>
+            <InformationVariantIcon size={iconSize} />
+          </IconButton>
+          <Link to={`/provisioners/${provisioner.provisionerId}`}>
+            <TableCellItem button>
+              {provisioner.provisionerId}
+              <LinkIcon size={iconSize} />
+            </TableCellItem>
+          </Link>
+        </TableCell>
+        <TableCell>
+          <StatusLabel state={provisioner.stability} />
+        </TableCell>
+        <TableCell>
+          <DateDistance from={provisioner.lastDateActive} />
+        </TableCell>
+        <TableCell>
+          <DateDistance from={provisioner.expires} />
+        </TableCell>
+      </TableRow>
+    );
+  };
+
+  render() {
+    const { provisioners } = this.props;
+    const { sortBy, sortDirection } = this.state;
+    const headers = [
+      { label: 'Provisioner', id: 'provisionerId', type: 'string' },
+      { label: 'Stability', id: 'stability', type: 'string' },
+      { label: 'Last Active', id: 'lastDateActive', type: 'string' },
+      { label: 'Expires', id: 'expires', type: 'string' },
+    ];
+
+    this.sortProvisioners(provisioners, sortBy, sortDirection);
+
+    return (
+      <Fragment>
+        <DataTable
+          items={provisioners}
+          sortByLabel={sortBy}
+          sortDirection={sortDirection}
+          onHeaderClick={this.handleHeaderClick}
+          renderRow={this.renderTableRow}
+          headers={headers}
+        />
+      </Fragment>
+    );
+  }
+}
