95eb5b482c440e438490d120bd9b88a79c89845a
Dustin J. Mitchell <dustin@mozilla.com>
2019-12-04 14:22:40 +0000

    Bug 1601149 - accept non-escaped github PEMs with multiple lines

--- services/github/test/github-auth_test.js
+++ services/github/test/github-auth_test.js
@@ -2,18 +2,19 @@ const assert = require('assert');
 const githubAuth = require('../src/github-auth');
 const testing = require('taskcluster-lib-testing');
 
+const WITH_NEWLINES = '-----BEGIN RSA PRIVATE KEY-----\nsomekey\nline2\n-----END RSA PRIVATE KEY-----';
+const WITH_ESCAPED_NEWLINES = '-----BEGIN RSA PRIVATE KEY-----\\nsomekey\\nline2\\n-----END RSA PRIVATE KEY-----';
+
 suite(testing.suiteName(), function() {
   suite('getPrivatePEM', function() {
-    const privatePEM = '-----BEGIN RSA PRIVATE KEY-----\nsomekey\n-----END RSA PRIVATE KEY-----';
-
     test('with actual newlines', function() {
-      const cfg = {github: {credentials: {privatePEM}}};
-      assert.equal(githubAuth.getPrivatePEM(cfg), privatePEM);
+      const cfg = {github: {credentials: {privatePEM: WITH_NEWLINES}}};
+      assert.equal(githubAuth.getPrivatePEM(cfg), WITH_NEWLINES);
     });
 
     test('with escaped newlines', function() {
-      const cfg = {github: {credentials: {privatePEM: privatePEM.replace(/\n/g, '\\n')}}};
-      assert.equal(githubAuth.getPrivatePEM(cfg), privatePEM);
+      const cfg = {github: {credentials: {privatePEM: WITH_ESCAPED_NEWLINES}}};
+      assert.equal(githubAuth.getPrivatePEM(cfg), WITH_NEWLINES);
     });
 
     test('with invalid value', function() {
