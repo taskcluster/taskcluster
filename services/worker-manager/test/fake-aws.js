4d3ca9c4dac2cb9fcef2799356bcddacfd9e43d3
owlishDeveloper <bugzeeeeee@gmail.com>
2019-12-05 13:43:58 -0800

    Add test cases for user tag specs. Fix bug in fake aws (previously it could assert on defaultLaunchConfig only). Kill tag bug in aws provider

--- services/worker-manager/test/fake-aws.js
+++ services/worker-manager/test/fake-aws.js
@@ -1,46 +1,42 @@
-const assert = require('assert');
-
 module.exports = {
   EC2: {
-    runInstances: ({defaultLaunchConfig, TagSpecifications, UserData}) => launchConfig => {
-      assert.deepStrictEqual(launchConfig,
-        {
-          ...defaultLaunchConfig.launchConfig,
-          MinCount: launchConfig.MinCount, // this is estimator's functionality, no need to test this here
-          MaxCount: launchConfig.MaxCount,
-          TagSpecifications,
-          UserData: Buffer.from(JSON.stringify(UserData)).toString('base64'),
-        },
-      );
+    runInstances: () => {
+      const fake = launchConfig => {
+        fake.calls.push({launchConfig});
 
-      let Instances = [];
-      for (let i = 0; i < launchConfig.MinCount; i++) {
-        Instances.push({
-          InstanceId: `i-${i}`,
-          AmiLaunchIndex: '',
-          ImageId: launchConfig.ImageId,
-          InstanceType: launchConfig.InstanceType || 'm2.micro',
-          Architecture: 'x86',
-          Placement: {
-            AvailabilityZone: 'someregion',
-          },
-          PrivateIpAddress: '1.1.1.1',
-          OwnerId: '123123123',
-          State: {
-            Name: 'running',
-          },
-          StateReason: {
-            Message: 'yOu LaunCHed iT!!!1',
-          },
-        });
-      }
-      return {
-        promise: () => ({
-          Instances,
-          Groups: [],
-          OwnerId: '123123123',
-        }),
+        let Instances = [];
+        for (let i = 0; i < launchConfig.MinCount; i++) {
+          Instances.push({
+            InstanceId: `i-${i}`,
+            AmiLaunchIndex: '',
+            ImageId: launchConfig.ImageId,
+            InstanceType: launchConfig.InstanceType || 'm2.micro',
+            Architecture: 'x86',
+            Placement: {
+              AvailabilityZone: 'someregion',
+            },
+            PrivateIpAddress: '1.1.1.1',
+            OwnerId: '123123123',
+            State: {
+              Name: 'running',
+            },
+            StateReason: {
+              Message: 'yOu LaunCHed iT!!!1',
+            },
+          });
+        }
+        return {
+          promise: () => ({
+            Instances,
+            Groups: [],
+            OwnerId: '123123123',
+          }),
+        };
       };
+
+      fake.calls = [];
+
+      return fake;
     },
 
     describeRegions: () => {
