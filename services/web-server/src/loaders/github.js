228472f1d32c9bf064d125d1261adcab38f2217f
Mustafa Jebara <moosejebara@gmail.com>
2019-12-05 21:24:52 -0500

    Handle Promise.all exceptions in github loader

--- services/web-server/src/loaders/github.js
+++ services/web-server/src/loaders/github.js
@@ -3,7 +3,13 @@ const DataLoader = require('dataloader');
 module.exports = ({ github }) => {
   const githubRepository = new DataLoader(queries =>
     Promise.all(
-      queries.map(({ owner, repo }) => github.repository(owner, repo)),
+      queries.map(({ owner, repo }) => { 
+        try {
+          return github.repository(owner, repo) 
+        } catch (err) {
+          return err
+        }
+      }),
     ),
   );
 
