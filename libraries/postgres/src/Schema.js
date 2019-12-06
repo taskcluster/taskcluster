d3f0eb3044003b6d090c4db923c149d91b27a52e
Hassan Ali <helfi92@gmail.com>
2019-12-06 16:04:12 -0500

    Add new methods, and todos to Schema and Database

--- libraries/postgres/src/Schema.js
+++ libraries/postgres/src/Schema.js
@@ -1,54 +1,68 @@
-const assert = require('assert');
+const fs = require('fs');
+const yaml = require('js-yaml');
+const path = require('path');
 
 class Schema{
-  /**
+  /**references
    * Create a new Schema
    *
    * script is a script to create the schema, suitable as an argument to
    * the Postgres DO statment; that is usually 'BEGIN stmt; stmt; .. END'.
    */
-  constructor({script}, {version, versions, methods} = {}) {
-    assert(script, 'script is required');
-    this.script = script;
-
-    this.version = version || 1;
-    this.versions = versions || [null];
-
-    assert.equal(this.versions.length, this.version);
-    this.versions.push(this);
-
-    this.methods = new Map(methods);
+  // TODO: Make sure that versions are contiguous
+  // TODO: Make sure that procedure argument values don't change
+  constructor(versions) {
+    this.versions = versions;
   }
 
-  /**
-   * Add a new version to this schema; version must be one more than the
-   * previous version.  A script must be provided to upgrade from the previous
-   * version. All defined methods are reset.
-   */
-  addVersion(version, script) {
-    assert(version === this.version + 1, 'versions must increment by one');
-    return new Schema(
-      {script},
-      {version, versions: this.versions});
+  static fromSerializable(serializable) {
+    return new Schema(serializable.versions);
   }
 
-  /**
-   * Add a new method to this schema version.  Calling the method on the
-   * resulting Database instance will invoke the given plpgsql script.  If the
-   * method was already defined in a previous version, then its args and
-   * returns must match the previous version exactly.
-   */
-  addMethod(method, rw, args, returns, script) {
-    assert(script, 'script is required');
-    if (this.methods.has(method)) {
-      const prev = this.methods.get(method);
-      assert.equal(args, prev.args,
-        'method args must match previous version');
-      assert.equal(returns, prev.returns,
-        'method returns must match previous version');
+  static fromDbDirectory(directory) {
+    const versions = new Map();
+
+    fs.readdirSync(directory).forEach(dentry => {
+      if (dentry.startsWith('.')) {
+        return;
+      }
+
+      const filename = path.join(directory, dentry);
+
+      if (fs.lstatSync(filename).isDirectory() || !/\.ya?ml/.test(filename)) {
+        throw new Error(`${filename} is a directory`);
+      }
+
+      const version = yaml.safeLoad(fs.readFileSync(filename));
+
+      versions.set(version.version, version);
+    });
+
+    return new Schema(versions);
+  }
+
+  getVersion(version) {
+    const v = this.versions.get(version);
+
+    if (!v) {
+      throw new Error(`Version ${version} not found in the schema`);
     }
-    this.methods.set(method, {rw, args, returns, script});
-    return this;
+
+    return v;
+  }
+
+  latestVersion() {
+    return this.versions.get(Math.max(...this.versions.keys()));
+  }
+
+  allMethods() {
+    return this.versions.values().reduce((acc, version) => {
+      Object.entries(version.methods).forEach(([name, { mode }]) => {
+        acc.add({ name, mode });
+      });
+
+      return acc;
+    }, new Set());
   }
 
   /** testing **/
