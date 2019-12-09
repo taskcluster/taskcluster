a027a02959b3272bec37162575a675c8615eb437
Dustin J. Mitchell <dustin@mozilla.com>
2019-12-09 16:58:38 +0000

    add tests for Schema class

--- libraries/postgres/README.md
+++ libraries/postgres/README.md
@@ -1,3 +1,8 @@
 # Postgres Library
 
 TBD
+
+// Overall plan is: access is only through plpgsql functions, and those must
+// have a consistent API (args + result).  Upgrades can redefine these
+// functions and add new functions, but not change API as existing software may
+// be using those functions concurrently.
--- libraries/postgres/src/index.js
+++ libraries/postgres/src/index.js
@@ -1,8 +1,5 @@
-// Overall plan is: access is only through plpgsql functions, and those must
-// have a consistent API (args + result).  Upgrades can redefine these
-// functions and add new functions, but not change API as existing software may
-// be using those functions concurrently.
-
-exports.Schema = require('./Schema');
-exports.Database = require('./Database');
-module.exports = {...exports, ...require('./constants')};
+module.exports = {
+  Schema: require('./Schema'),
+  Database: require('./Database'),
+  ...require('./constants'),
+};
