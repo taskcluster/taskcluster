805260766e570521e89bc6bdbc99323b5a4da9b4
Dustin J. Mitchell <dustin@mozilla.com>
2019-12-09 19:12:06 +0000

    Bug 1558532 - build windows and darwin binaries, too

--- release.sh
+++ release.sh
@@ -19,13 +19,8 @@ git add version.go README.md
 git commit -m "v$VERSION"
 git tag v$VERSION
 
-build() {
-    local output=start-worker-${1}-${2}
-    GOOS="${1}" GOARCH="${2}" CGO_ENABLED=0 go build -o $output ./cmd/start-worker
-    echo $output
-}
-echo "Building:"
-build linux amd64
+source ./build.sh
+
 echo "* Git push with --follow-tags to the upstream repo"
 echo "* Create a new release from the tag"
-echo "* Attach the above built binary to the release"
+echo "* Attach the above built binaries to the release"
