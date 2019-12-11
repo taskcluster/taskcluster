dfb16584282ab0031ce31a9c11fdf3d71260c39b
Hassan Ali <helfi92@gmail.com>
2019-12-11 13:15:12 -0500

    Revert speed dial button behavior
    Also rewrite SpeedDial and SpeedDialAction to use react hooks.
    Firefox doesn't trigger the focus event when a button is clicked
    so it was complicating things like handling the blur event when the user
    clicks on one of these menu items and when they click outside of it.

--- ui/src/components/SpeedDial/index.jsx
+++ ui/src/components/SpeedDial/index.jsx
@@ -1,71 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
 import { arrayOf, node, oneOfType } from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import MuiSpeedDial from '@material-ui/lab/SpeedDial';
 import SpeedDialIcon from '@material-ui/lab/SpeedDialIcon';
 import CloseIcon from 'mdi-react/CloseIcon';
 import DotsVerticalIcon from 'mdi-react/DotsVerticalIcon';
 
-@withStyles(theme => ({
+const useStyles = makeStyles(theme => ({
   speedDial: {
     ...theme.mixins.fab,
+    '& button:focus': {
+      background: 'red',
+    },
   },
-}))
+}));
+
 /**
  * Render a dynamically expanding set of floating action buttons.
  */
-export default class SpeedDial extends Component {
-  static propTypes = {
-    /**
-     * A set of `SpeedDialAction`s which will be rendered upon interaction
-     * with the base `SpeedDial` floating action button.
-     */
-    children: oneOfType([arrayOf(node), node]).isRequired,
-  };
+function SpeedDial(props) {
+  const classes = useStyles();
+  const { children, className, ...rest } = props;
+  const [open, setOpen] = useState(false);
 
-  state = {
-    open: false,
-  };
-
-  handleClose = (_, reason) => {
-    if (
-      reason === 'toggle' ||
-      reason === 'blur' ||
-      reason === 'escapeKeyDown'
-    ) {
-      this.setState({
-        open: false,
-      });
-    }
-  };
-
-  handleOpen = (_, reason) => {
-    if (reason === 'toggle') {
-      this.setState({
-        open: true,
-      });
-    }
-  };
-
-  render() {
-    const { classes, children, className, ...props } = this.props;
-    const { open } = this.state;
-
-    return (
-      <MuiSpeedDial
-        ariaLabel="speed-dial"
-        icon={
-          <SpeedDialIcon icon={<DotsVerticalIcon />} openIcon={<CloseIcon />} />
-        }
-        FabProps={{ color: 'secondary' }}
-        className={classNames(classes.speedDial, className)}
-        onOpen={this.handleOpen}
-        onClose={this.handleClose}
-        open={open}
-        {...props}>
-        {children}
-      </MuiSpeedDial>
-    );
+  function handleClose() {
+    setOpen(false);
   }
+
+  function handleOpen() {
+    setOpen(true);
+  }
+
+  return (
+    <MuiSpeedDial
+      ariaLabel="speed-dial"
+      icon={
+        <SpeedDialIcon icon={<DotsVerticalIcon />} openIcon={<CloseIcon />} />
+      }
+      FabProps={{ color: 'secondary' }}
+      className={classNames(classes.speedDial, className)}
+      onOpen={handleOpen}
+      onClose={handleClose}
+      open={open}
+      {...rest}>
+      {children}
+    </MuiSpeedDial>
+  );
 }
+
+SpeedDial.propTypes = {
+  /**
+   * A set of `SpeedDialAction`s which will be rendered upon interaction
+   * with the base `SpeedDial` floating action button.
+   */
+  children: oneOfType([arrayOf(node), node]).isRequired,
+};
+
+export default SpeedDial;
