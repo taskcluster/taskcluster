c8bc90f42e6b51224c2f64ff12d46253b823ac21
Dustin J. Mitchell <dustin@mozilla.com>
2019-12-13 02:22:07 +0000

    add DB tests

---
+++ db/README.md
@@ -0,0 +1,6 @@
+# Taskcluster Database
+
+This directory defines the Taskcluster database:
+
+* `versions/` -- the migrations that create the most-recent database schema
+* `test/` -- tests for the contents of this directory
---
+++ db/test/version_0001_test.js
@@ -0,0 +1,16 @@
+const assert = require('assert').strict;
+const helper = require('./helper');
+const testing = require('taskcluster-lib-testing');
+
+suite(testing.suiteName(), function() {
+  helper.withDb();
+
+  test('secrets table created', async function() {
+    await helper.upgradeTo(1);
+
+    await helper.withDbClient(async client => {
+      const res = await client.query('select * from secrets');
+      assert.deepEqual(res.rows, []);
+    });
+  });
+});
