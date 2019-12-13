5b0ca931d6c98ea358d402ac4aba4d63d95d181a
Dustin J. Mitchell <dustin@mozilla.com>
2019-12-13 19:42:26 +0000

    Introduce and test fake DB methods

--- db/test/helper.js
+++ db/test/helper.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const {Client} = require('pg');
 const {Schema, Database, WRITE} = require('taskcluster-lib-postgres');
+const {FakeDatabase} = require('taskcluster-db');
 
 exports.dbUrl = process.env.TEST_DB_URL;
 assert(exports.dbUrl, "TEST_DB_URL must be set to run db/ tests");
@@ -8,10 +9,11 @@ assert(exports.dbUrl, "TEST_DB_URL must be set to run db/ tests");
 exports.schema = Schema.fromDbDirectory();
 
 /**
- * Set helper.db to an empty database instance, using the schema from the repo.
- * Also sets up helper.withDbClient(fn) to call fn with a pg client.
- * Also sets up helper.upgradeTo(ver) to upgrade the DB to a specific
- * version.  Upgrade to the latest version with `helper.upgradeDb()`.
+ * Set
+ * - helper.db to an empty database instance, using the schema from the repo.
+ * - helper.withDbClient(fn) to call fn with a pg client.
+ * - helper.upgradeTo(ver) to upgrade the DB to a specific version
+ * - helper.upgradeDb9) to upgrade to the latest version
  *
  * The database is only reset at the beginning of the suite.  Test suites
  * should implement a `setup` method that sets state for all relevant tables
@@ -24,6 +26,11 @@ exports.withDb = function({ serviceName }) {
       schema: exports.schema,
       writeDbUrl: exports.dbUrl,
       readDbUrl: exports.dbUrl,
+      serviceName,
+    });
+
+    exports.fakeDb = await new FakeDatabase({
+      schema: exports.schema,
       serviceName,
     });
 
@@ -45,12 +52,26 @@ exports.withDb = function({ serviceName }) {
         serviceName,
       });
     };
+
     exports.withDbClient = fn => exports.db._withClient(WRITE, fn);
   });
 
   suiteTeardown('teardown database', async function() {
     await exports.db.close();
     delete exports.db;
+  });
+};
+
+/**
+ * helper.dbTest("description", async (db, isFake) => { .. }) runs the given
+ * test function both with a real and fake db.
+ */
+exports.dbTest = (description, testFn) => {
+  test(`${description} (real)`, async function() {
+    return testFn(exports.db, false);
+  });
+  test(`${description} (fake)`, async function() {
+    return testFn(exports.fakeDb, true);
   });
 };
 
