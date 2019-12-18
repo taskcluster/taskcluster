a11593ce4af78deb52b7bc14eb2176603c62cc70
Hassan Ali <helfi92@gmail.com>
2019-12-17 22:16:42 -0500

    Return { args, returns } in Schema's allMethods

--- libraries/postgres/test/schema_test.js
+++ libraries/postgres/test/schema_test.js
@@ -17,8 +17,8 @@ suite(path.basename(__filename), function() {
 
       assert.deepEqual([...sch.allMethods()].sort(),
         [
-          {name: 'get_secret', mode: READ, serviceName: 'secrets'},
-          {name: 'list_secrets', mode: READ, serviceName: 'secrets'},
+          {name: 'get_secret', mode: READ, serviceName: 'secrets', args: 'name text', returns: 'table (secret text)'},
+          {name: 'list_secrets', mode: READ, serviceName: 'secrets', args: '', returns: 'table (name text, expires timestamp)' },
         ]);
     });
 
@@ -125,8 +125,8 @@ suite(path.basename(__filename), function() {
     const sch = Schema.fromDbDirectory(path.join(__dirname, 'db-simple'));
     assert.deepEqual([...sch.allMethods()].sort(),
       [
-        {name: 'get_secret', mode: READ, serviceName: 'secrets'},
-        {name: 'list_secrets', mode: READ, serviceName: 'secrets'},
+        {name: 'get_secret', mode: READ, serviceName: 'secrets', args: 'name text', returns: 'table (secret text)'},
+        {name: 'list_secrets', mode: READ, serviceName: 'secrets', args: '', returns: 'table (name text, expires timestamp)' },
       ]);
   });
 
