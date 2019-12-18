718c4992a29ae3e81c0a294d09625c406809c369
Hassan Ali <helfi92@gmail.com>
2019-12-17 22:16:42 -0500

    Return { args, returns } in Schema's allMethods

--- libraries/postgres/src/Schema.js
+++ libraries/postgres/src/Schema.js
@@ -110,8 +110,8 @@ class Schema{
     const modes = {read: READ, write: WRITE};
 
     return this.versions.reduce((acc, version) => {
-      Object.entries(version.methods).forEach(([name, { mode, serviceName }]) => {
-        acc.add({ name, mode: modes[mode], serviceName });
+      Object.entries(version.methods).forEach(([name, { mode, serviceName, args, returns }]) => {
+        acc.add({ name, mode: modes[mode], serviceName, args, returns });
       });
 
       return acc;
