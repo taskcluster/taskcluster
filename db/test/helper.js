1042ce81164ad8cefc1f21faacc43582e79e826e
Dustin J. Mitchell <dustin@mozilla.com>
2019-12-19 22:18:38 +0000

    factor db/ tests into subdirectories, and be clearer about how they are written

--- db/test/helper.js
+++ db/test/helper.js
@@ -1,32 +1,82 @@
 const assert = require('assert');
-const {Client} = require('pg');
-const {Schema, WRITE} = require('taskcluster-lib-postgres');
+const {Client, Pool} = require('pg');
+const {WRITE} = require('taskcluster-lib-postgres');
 const tcdb = require('taskcluster-db');
+const debug = require('debug')('db-helper');
 
 exports.dbUrl = process.env.TEST_DB_URL;
 assert(exports.dbUrl, "TEST_DB_URL must be set to run db/ tests");
 
-exports.schema = Schema.fromDbDirectory();
-
 /**
+ * Set up to test a DB version.
+ *
  * Set
- * - helper.db to an empty database instance, using the schema from the repo.
  * - helper.withDbClient(fn) to call fn with a pg client.
- * - helper.upgradeDb() to upgrade to the given version (or latest version if omitted)
+ * - helper.upgradeTo(v) to upgrade to the given version.
  *
  * The database is only reset at the beginning of the suite.  Test suites
  * should implement a `setup` method that sets state for all relevant tables
  * before each test case.
- *
- * Pass serviceName when testing methods for a specific service.  If omitted,
- * it will be set to `dummy` and `helper.db.procs` will probably not do what
- * you want.
  */
-exports.withDb = function({ serviceName = "dummy" } = {}) {
-  suiteSetup('setup database', async function() {
-    await exports.clearDb();
+exports.withDbForVersion = function() {
+  let pool;
 
-    exports.db = await tcdb.setup({
+  suiteSetup('setup database', async function() {
+    pool = new Pool({connectionString: exports.dbUrl});
+    exports.withDbClient = async (cb) => {
+      const client = await pool.connect();
+      try {
+        try {
+          return await cb(client);
+        } catch (err) {
+          // show hints or details from this error in the debug log, to help
+          // debugging issues..
+          if (err.hint) {
+            debug(`HINT: ${err.hint}`);
+          }
+          if (err.detail) {
+            debug(`DETAIL: ${err.detail}`);
+          }
+          throw err;
+        }
+      } finally {
+        client.release();
+      }
+    };
+
+    exports.upgradeTo = async (toVersion) => {
+      await tcdb.upgrade({
+        adminDbUrl: exports.dbUrl,
+        toVersion,
+      });
+    };
+
+    await exports.withDbClient(client => clearDb(client));
+  });
+
+  suiteTeardown('teardown database', async function() {
+    if (pool) {
+      await pool.end();
+    }
+    pool = null;
+    delete exports.upgradeDb;
+    delete exports.withDbClient;
+  });
+};
+
+/**
+ * Set
+ * - helper.withDbClient(fn) to call fn with a pg client on the real DB.
+ * - helper.fakeDb to an instance of FakeDatabase
+ *
+ * The database is only reset at the beginning of the suite.  Test suites
+ * should implement a `setup` method that sets state for all relevant tables
+ * before each test case.
+ */
+exports.withDbForProcs = function({ serviceName }) {
+  let db;
+  suiteSetup('setup database', async function() {
+    db = await tcdb.setup({
       writeDbUrl: exports.dbUrl,
       readDbUrl: exports.dbUrl,
       serviceName,
@@ -36,45 +86,42 @@ exports.withDb = function({ serviceName = "dummy" } = {}) {
       serviceName,
     });
 
-    exports.upgradeDb = async (toVersion) => {
-      await tcdb.upgrade({
-        adminDbUrl: exports.dbUrl,
-        toVersion,
-      });
-    };
+    exports.withDbClient = fn => db._withClient(WRITE, fn);
 
-    exports.withDbClient = fn => exports.db._withClient(WRITE, fn);
+    // clear the DB and upgrade it to the latest version
+    await exports.withDbClient(client => clearDb(client));
+    await tcdb.upgrade({
+      adminDbUrl: exports.dbUrl,
+    });
   });
 
   suiteTeardown('teardown database', async function() {
-    await exports.db.close();
-    delete exports.db;
+    if (db) {
+      await db.close();
+    }
+    db = null;
+    delete exports.fakeDb;
+    delete exports.withDbClient;
   });
-};
 
-/**
- * helper.dbTest("description", async (db, isFake) => { .. }) runs the given
- * test function both with a real and fake db.
- */
-exports.dbTest = (description, testFn) => {
-  test(`${description} (real)`, async function() {
-    return testFn(exports.db, false);
-  });
-  test(`${description} (fake)`, async function() {
-    return testFn(exports.fakeDb, true);
-  });
+  /**
+   * helper.dbTest("description", async (db, isFake) => { .. }) runs the given
+   * test function both with a real and fake db.  This is used for testing procs.
+   */
+  exports.dbTest = (description, testFn) => {
+    test(`${description} (real)`, async function() {
+      return testFn(db, false);
+    });
+    test(`${description} (fake)`, async function() {
+      return testFn(exports.fakeDb, true);
+    });
+  };
 };
 
 /**
  * Clear the database, resetting it to its initial, empty state
  */
-exports.clearDb = async () => {
-  const client = new Client({connectionString: exports.dbUrl});
-  await client.connect();
-  try {
-    await client.query(`drop schema if exists public cascade`);
-    await client.query(`create schema public`);
-  } finally {
-    client.end();
-  }
+const clearDb = async client => {
+  await client.query(`drop schema if exists public cascade`);
+  await client.query(`create schema public`);
 };
--- db/test/versions/0001_test.js
+++ db/test/versions/0001_test.js
@@ -1,12 +1,12 @@
 const assert = require('assert').strict;
-const helper = require('./helper');
+const helper = require('../helper');
 const testing = require('taskcluster-lib-testing');
 
 suite(testing.suiteName(), function() {
-  helper.withDb();
+  helper.withDbForVersion();
 
   test('secrets table created', async function() {
-    await helper.upgradeDb(1);
+    await helper.upgradeTo(1);
 
     await helper.withDbClient(async client => {
       const res = await client.query('select * from secrets');
