240bd75118a748dd4a151b4f74df3d6921a445cf
Hassan Ali <helfi92@gmail.com>
2019-12-23 09:34:56 -0500

    Revamp warning component in docs

--- ui/src/views/Documentation/components/Warning/index.jsx
+++ ui/src/views/Documentation/components/Warning/index.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import { string } from 'prop-types';
-import ErrorPanel from '../../../../components/ErrorPanel';
+import { withStyles } from '@material-ui/core/styles';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import Paper from '@material-ui/core/Paper';
+import Markdown from '@mozilla-frontend-infra/components/Markdown';
 
-const Warning = ({ children }) => (
-  <ErrorPanel warning error={children} onClose={null} />
+const styles = withStyles(theme => ({
+  root: {
+    borderLeft: `5px solid ${theme.palette.warning.dark}`,
+    backgroundColor: fade(theme.palette.warning.main, 0.2),
+    padding: `${theme.spacing(1)}px ${theme.spacing(3)}px`,
+    margin: `${theme.spacing(3)} 0`,
+    '& a': {
+      ...theme.mixins.link,
+    },
+  },
+}));
+const Warning = ({ classes, children }) => (
+  <Paper square classes={{ root: classes.root }}>
+    <Markdown>{children}</Markdown>
+  </Paper>
 );
 
 Warning.propTypes = {
   children: string.isRequired,
 };
 
-export default Warning;
+export default styles(Warning);
