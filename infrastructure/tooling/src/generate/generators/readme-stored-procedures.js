8784fefdb705ed634f377a01247448b5c513dce9
Dustin J. Mitchell <dustin@mozilla.com>
2019-12-26 20:00:14 +0000

    Generate the list of required DB users automatically

--- infrastructure/tooling/src/generate/generators/readme-stored-procedures.js
+++ infrastructure/tooling/src/generate/generators/readme-stored-procedures.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const { Schema } = require('taskcluster-lib-postgres');
-const {readRepoFile, writeRepoFile, REPO_ROOT} = require('../../utils');
+const {readRepoFile, writeRepoFile} = require('../../utils');
 
 exports.tasks = [{
   title: 'README Stored Procedures',
+  requires: ['db-schema-serializable'],
   provides: ['readme-stored-procedures'],
   run: async (requirements, utils) => {
-    const schema = Schema.fromDbDirectory(path.join(REPO_ROOT, 'db'));
+    const schema = Schema.fromSerializable(requirements['db-schema-serializable']);
     const methods = schema.allMethods();
     const serviceNames = [...new Set([...methods].map(({ serviceName }) => serviceName).sort())];
     const services = new Map();
