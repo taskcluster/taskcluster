88c2e9be6a1f53e2048696c06ea66de190a63745
Hassan Ali <helfi92@gmail.com>
2020-01-02 12:15:27 -0500

    Add tc-lib-entities skeleton

---
+++ libraries/entities/src/index.js
@@ -0,0 +1,36 @@
+class Entity {
+  constructor(options) {
+    this.partitionKey = options.partitionKey;
+    this.rowKey = options.rowKey;
+    this.properties = options.properties;
+    this.tableName = null;
+    this.db = null;
+    this.serviceName = null;
+  }
+
+  setup(options) {
+    const { tableName, db, serviceName } = options;
+
+    this.tableName = tableName;
+    this.serviceName = serviceName;
+    this.db = db;
+  }
+
+  // TODO: Fix this. This is totally wrong :-)
+  calculateId(properties) {
+    return `${properties[this.partitionKey]}${properties[this.rowKey]}`;
+  }
+
+  create(properties, overwrite) {
+    const documentId = this.calculateId(properties);
+    this.db.procs[this.serviceName][`${this.tableName}_create`](documentId, properties, overwrite, 1);
+  }
+
+  static configure(options) {
+    return new Entity(options);
+  }
+}
+
+module.exports = {
+  Entity,
+};
