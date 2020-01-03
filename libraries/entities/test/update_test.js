b95d4ef126cc15d873da8e216345b4082b1d3124
Hassan Ali <helfi92@gmail.com>
2020-01-03 13:57:54 -0500

    Add entity update method

---
+++ libraries/entities/test/update_test.js
@@ -0,0 +1,67 @@
+const helper = require('./helper');
+const { Schema } = require('taskcluster-lib-postgres');
+const { Entity } = require('taskcluster-lib-entities');
+const path = require('path');
+const assert = require('assert').strict;
+
+helper.dbSuite(path.basename(__filename), function() {
+  let db;
+
+  teardown(async function() {
+    if (db) {
+      try {
+        await db.close();
+      } finally {
+        db = null;
+      }
+    }
+  });
+
+  suite('update', function() {
+    test('update entry', async function() {
+      const schema = Schema.fromDbDirectory(path.join(__dirname, 'db'));
+      const serviceName = 'test-entities';
+
+      db = await helper.withDb({ schema, serviceName });
+
+      const properties = {
+        taskId: 'string',
+        provisionerId: 'string',
+        workerType: 'string',
+      };
+      const entity = Entity.configure({
+        partitionKey: 'taskId',
+        rowKey: 'task',
+        properties,
+      });
+      const entry = {
+        taskId: 'taskId',
+        provisionerId: 'provisionerId',
+        workerType: 'string',
+      };
+      const documentId = entity.calculateId(entry);
+
+      entity.setup({ tableName: 'test_entities', db, serviceName });
+
+      await entity.create(entry);
+
+      let result = await entity.load(entry);
+
+      assert.equal(result.length, 1);
+      assert.equal(result[0].id, documentId);
+
+      const updatedEntry =  {
+        ...entry,
+        workerType: 'update',
+      };
+
+      await entity.update(updatedEntry);
+
+      result = await entity.load(updatedEntry);
+
+      assert.equal(result.length, 1);
+      assert.equal(result[0].id, documentId);
+      assert.deepEqual(result[0].value, updatedEntry);
+    });
+  });
+});
