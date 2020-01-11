2d43e43c6f186c19868b113a055e188cc155bce1
Brian Stack <bstack@mozilla.com>
2020-01-10 16:26:18 -0800

    Add azure customdata windows stub

---
+++ provider/azure/metadata_windows.go
@@ -0,0 +1,11 @@
+// +build windows
+
+package azure
+
+import (
+	"errors"
+)
+
+func (mds *realMetadataService) loadCustomData() ([]byte, error) {
+	return []byte{}, errors.New("Azure customdata not implemented for Windows")
+}
