4d608348813971c620f200d3d5620fb99087a2cd
Hassan Ali <helfi92@gmail.com>
2020-02-18 15:31:54 -0500

    Fix Op.greatherThanOrEqual symbol

--- libraries/entities/src/entityops.js
+++ libraries/entities/src/entityops.js
@@ -8,7 +8,7 @@ class Op {
 Op.equal = Op.eq = Op['=='] = x => new Op({ operator: "=", operand: x });
 Op.notEqual = Op.ne = Op['!='] = x => new Op({ operator: "<>", operand: x });
 Op.greaterThan = Op.gt = Op['>'] = x => new Op({ operator: ">", operand: x });
-Op.greaterThanOrEqual = Op.gte = Op['<='] = x => new Op({ operator: ">=", operand: x });
+Op.greaterThanOrEqual = Op.gte = Op['>='] = x => new Op({ operator: ">=", operand: x });
 Op.lessThan = Op.lt = Op['<'] = x => new Op({ operator: "<", operand: x });
 Op.lessThanOrEqual = Op.lte = Op['<='] = x => new Op({ operator: "<=", operand: x });
 
