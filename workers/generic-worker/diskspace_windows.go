7043bae940f7c5892828e0e459322085f154fc9e
Pete Moore <pmoore@mozilla.com>
2020-02-20 19:26:03 +0100

    Bug 1616214 - Delete User Profiles of old task users on Windows (multiuser), and move GetDiskFreeSpaceExW into win32 package

--- workers/generic-worker/diskspace_windows.go
+++ workers/generic-worker/diskspace_windows.go
@@ -3,20 +3,20 @@ package main
 import (
 	"log"
 	"syscall"
-	"unsafe"
+
+	"github.com/taskcluster/taskcluster/v25/workers/generic-worker/win32"
 )
 
 func freeDiskSpaceBytes(dir string) (uint64, error) {
-	h := syscall.MustLoadDLL("kernel32.dll")
-	c := h.MustFindProc("GetDiskFreeSpaceExW")
-	var freeBytes int64
-	var x, y uintptr
-	_, _, err := c.Call(uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(dir))),
-		uintptr(unsafe.Pointer(&freeBytes)), x, y)
-	if err != syscall.Errno(0) {
+	path, err := syscall.UTF16PtrFromString(".")
+	if err != nil {
 		return 0, err
 	}
-	b := uint64(freeBytes)
-	log.Printf("Disk available: %v bytes", b)
-	return b, nil
+	var freeBytes uint64
+	err = win32.GetDiskFreeSpace(path, &freeBytes, nil, nil)
+	if err != nil {
+		return 0, err
+	}
+	log.Printf("Disk available: %v bytes", freeBytes)
+	return freeBytes, nil
 }
