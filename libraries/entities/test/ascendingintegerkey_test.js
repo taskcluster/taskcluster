341261969197fbc1975073c72b073e2628d532a2
Hassan Ali <helfi92@gmail.com>
2020-02-21 10:49:02 -0500

    Make sure error throws with correct message

--- libraries/entities/test/ascendingintegerkey_test.js
+++ libraries/entities/test/ascendingintegerkey_test.js
@@ -70,12 +70,11 @@ helper.dbSuite(path.basename(__filename), function() {
       });
       const createdEntity = await TestTable.create(entry);
 
-      // TODO: Fix this
       await assert.rejects(async () => {
         await createdEntity.modify(item => {
           item.rev = 1;
         });
-      });
+      }, /can't modify element/);
     });
     test('can\'t use negative numbers', async function() {
       db = await helper.withDb({ schema, serviceName });
