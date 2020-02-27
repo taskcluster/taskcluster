d4f2c6be1df78b31233b00b41159d144fb112d5b
Hassan Ali <helfi92@gmail.com>
2020-02-27 17:34:55 -0500

    Fix typos

--- db/test-setup.sh
+++ db/test-setup.sh
@@ -27,7 +27,7 @@ CREATE USER test_notify;
 CREATE USER test_purge_cache;
 CREATE USER test_queue;
 CREATE USER test_secrets;
-CREATE USER test_web-server;
+CREATE USER test_web_server;
 CREATE USER test_worker_manager;
 -- END CREATE USERS --
 EOF
