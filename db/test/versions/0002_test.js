43e99dc46e8843df9ea682269aa41264d7d2be31
Hassan Ali <helfi92@gmail.com>
2020-03-02 16:24:13 -0500

    Yarn lint

--- db/test/versions/0002_test.js
+++ db/test/versions/0002_test.js
@@ -5,63 +5,63 @@ const testing = require('taskcluster-lib-testing');
 const {UNDEFINED_TABLE} = require('taskcluster-lib-postgres');
 
 suite(testing.suiteName(), function() {
-    helper.withDbForVersion();
-    const azureTableNames = [
-        'Clients',
-        // roles is actually stored in a azure container but we're putting everything in a table now
-        'Roles',
-        'TaskclusterGithubBuilds',
-        'TaskclusterIntergrationOwners',
-        'TaskclusterChecksToTasks',
-        'TaskclusterCheckRuns',
-        'Hooks',
-        'Queues',
-        'LastFire3',
-        'IndexedTasks',
-        'Namespaces',
-        'DenylistedNotification',
-        'CachePurges',
-        'QueueTasks',
-        'QueueArtifacts',
-        'QueueTaskGroups',
-        'QueueTaskGroupMembers',
-        'QueueTaskGroupActiveSets',
-        'QueueTaskRequirement',
-        'QueueTaskDependency',
-        'QueueWorker',
-        'QueueWorkerType',
-        'QueueProvisioner',
-        'Secrets',
-        'AuthorizationCodesTable',
-        'AccessTokenTable',
-        'SessionStorageTable',
-        'GithubAccessTokenTable',
-        'WMWorkers',
-        'WMWorkerPools',
-        'WMWorkerPoolErrors'
-    ];
+  helper.withDbForVersion();
+  const azureTableNames = [
+    'Clients',
+    // roles is actually stored in a azure container but we're putting everything in a table now
+    'Roles',
+    'TaskclusterGithubBuilds',
+    'TaskclusterIntergrationOwners',
+    'TaskclusterChecksToTasks',
+    'TaskclusterCheckRuns',
+    'Hooks',
+    'Queues',
+    'LastFire3',
+    'IndexedTasks',
+    'Namespaces',
+    'DenylistedNotification',
+    'CachePurges',
+    'QueueTasks',
+    'QueueArtifacts',
+    'QueueTaskGroups',
+    'QueueTaskGroupMembers',
+    'QueueTaskGroupActiveSets',
+    'QueueTaskRequirement',
+    'QueueTaskDependency',
+    'QueueWorker',
+    'QueueWorkerType',
+    'QueueProvisioner',
+    'Secrets',
+    'AuthorizationCodesTable',
+    'AccessTokenTable',
+    'SessionStorageTable',
+    'GithubAccessTokenTable',
+    'WMWorkers',
+    'WMWorkerPools',
+    'WMWorkerPoolErrors',
+  ];
 
-    azureTableNames.forEach(azureTableName => {
-        const postgresTableName = `${snakeCase(azureTableName)}_entities`;
+  azureTableNames.forEach(azureTableName => {
+    const postgresTableName = `${snakeCase(azureTableName)}_entities`;
 
-        test(`${postgresTableName} table initially not created`, async function () {
-            await helper.withDbClient(async client => {
-                await assert.rejects(
-                    () => client.query(`select * from ${postgresTableName}`),
-                    err => err.code === UNDEFINED_TABLE);
-            });
-        });
+    test(`${postgresTableName} table initially not created`, async function () {
+      await helper.withDbClient(async client => {
+        await assert.rejects(
+          () => client.query(`select * from ${postgresTableName}`),
+          err => err.code === UNDEFINED_TABLE);
+      });
     });
-    azureTableNames.forEach(azureTableName => {
-        const postgresTableName = `${snakeCase(azureTableName)}_entities`;
+  });
+  azureTableNames.forEach(azureTableName => {
+    const postgresTableName = `${snakeCase(azureTableName)}_entities`;
 
-        test(`${postgresTableName} table created`, async function () {
-            await helper.upgradeTo(2);
+    test(`${postgresTableName} table created`, async function () {
+      await helper.upgradeTo(2);
 
-            await helper.withDbClient(async client => {
-                const res = await client.query(`select * from ${postgresTableName}`);
-                assert.deepEqual(res.rows, []);
-            });
-        });
+      await helper.withDbClient(async client => {
+        const res = await client.query(`select * from ${postgresTableName}`);
+        assert.deepEqual(res.rows, []);
+      });
     });
+  });
 });
