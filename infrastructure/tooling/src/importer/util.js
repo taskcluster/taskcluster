ace4e322173c9058b1e27c28e392725c21826652
Hassan Ali <helfi92@gmail.com>
2020-03-10 14:33:03 -0400

    Account for undefined/null in writeToPostgres

--- infrastructure/tooling/src/importer/util.js
+++ infrastructure/tooling/src/importer/util.js
@@ -84,13 +84,15 @@ exports.writeToPostgres = async (tableName, entities, db, allowedTables = [], mo
     await client.query(`truncate ${postgresTableName}`);
   });
 
-  for (let entity of entities) {
-    await db._withClient(mode, async client => {
-      await client.query(
-        `insert into ${postgresTableName}(partition_key, row_key, value, version, etag) values ($1, $2, $3, 1, public.gen_random_uuid())`,
-        [entity.PartitionKey, entity.RowKey, entity],
-      );
-    });
+  if (entities) {
+    for (let entity of entities) {
+      await db._withClient(mode, async client => {
+        await client.query(
+          `insert into ${postgresTableName}(partition_key, row_key, value, version, etag) values ($1, $2, $3, 1, public.gen_random_uuid())`,
+          [entity.PartitionKey, entity.RowKey, entity],
+        );
+      });
+    }
   }
 };
 
