d61036c94b653bb725c3efbf5766bb70ebfbadd8
owlishDeveloper <bugzeeeeee@gmail.com>
2020-04-06 14:18:08 -0700

    v28.2.2

--- clients/client-shell/cmds/version/version.go
+++ clients/client-shell/cmds/version/version.go
@@ -44,7 +44,7 @@ var (
 
 	// VersionNumber is a formatted string with the version information. This is
 	// filled in by `yarn release`
-	VersionNumber = "28.2.1"
+	VersionNumber = "28.2.2"
 )
 
 func init() {
--- workers/generic-worker/main.go
+++ workers/generic-worker/main.go
@@ -65,7 +65,7 @@ var (
 	logName = "public/logs/live_backing.log"
 	logPath = filepath.Join("generic-worker", "live_backing.log")
 
-	version  = "28.2.1"
+	version  = "28.2.2"
 	revision = "" // this is set during build with `-ldflags "-X main.revision=$(git rev-parse HEAD)"`
 )
 
