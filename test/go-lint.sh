b61ae31aa5213f1d8f93df76cdcb8ce2c27fa074
Dustin J. Mitchell <dustin@mozilla.com>
2020-04-08 18:41:09 +0000

    Run go lint at the top level, and embed golangci-lint in the image

---
+++ test/go-lint.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+echo "Running gofmt.."
+unformatted_files=$(git ls-files | grep '\.go$' | xargs gofmt -l)
+if [ -n "${unformatted_files}" ]; then
+    echo 'Go files not formatted with gofmt:'
+    echo "${unformatted_files}"
+    exit 1;
+fi
+
+echo "Running golangci-lint.."
+golangci-lint run --build-tags simple
