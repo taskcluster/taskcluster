b0f9bd29d7699b1c5fca60ed01c9443decf8f024
Dustin J. Mitchell <dustin@mozilla.com>
2020-04-15 19:46:30 +0000

    Bug 1630124 - [livelog] avoid deadlock from locking an already-locked mutex

--- tools/livelog/writer/stream.go
+++ tools/livelog/writer/stream.go
@@ -158,11 +158,14 @@ func (self *Stream) Consume() error {
 				continue
 			}
 
+			// If this handle is backed up, drop it..
 			pendingWrites := len(handle.events)
 			if pendingWrites >= EVENT_BUFFER_SIZE-1 {
 				log.Printf("Removing handle with %d pending writes\n", pendingWrites)
-				// Remove the handle from any future event writes...
-				self.Unobserve(handle)
+				// Remove the handle from any future event writes.  We can't use
+				// Unobserve here as it locks self.mutex, which we have already
+				// locked.
+				delete(self.handles, handle)
 				close(handle.events)
 				continue
 			}
