1999f37542325e175538f35f426c5c1d281e50de
Pete Moore <pmoore@mozilla.com>
2020-04-22 15:47:08 +0200

    Don't call log.Fatal during test

--- workers/generic-worker/win32/merge_test.go
+++ workers/generic-worker/win32/merge_test.go
@@ -1,7 +1,6 @@
 package win32_test
 
 import (
-	"log"
 	"testing"
 
 	"github.com/taskcluster/taskcluster/v29/workers/generic-worker/win32"
@@ -10,7 +9,7 @@ import (
 func TestMergeNilListsFirstNil(t *testing.T) {
 	res, err := win32.MergeEnvLists(nil, &[]string{"FOO=bar"})
 	if err != nil {
-		log.Fatalf("Hit error: %v", err)
+		t.Fatalf("Hit error: %v", err)
 	}
 	if res == nil || len(*res) != 1 || (*res)[0] != "FOO=bar" {
 		t.Fatalf("Did not merge correctly; got %#v", res)
@@ -20,7 +19,7 @@ func TestMergeNilListsFirstNil(t *testing.T) {
 func TestMergeNilListsSecondNil(t *testing.T) {
 	res, err := win32.MergeEnvLists(&[]string{"FOO=bar"}, nil)
 	if err != nil {
-		log.Fatalf("Hit error: %v", err)
+		t.Fatalf("Hit error: %v", err)
 	}
 	if res == nil || len(*res) != 1 || (*res)[0] != "FOO=bar" {
 		t.Fatalf("Did not merge correctly; got %#v", res)
