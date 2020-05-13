d084d827052adce2a9e2fbe921ed9e24c00a0e8c
vnzongzna <vnzongzna@gmail.com>
2020-05-14 00:18:32 +0530

    adding logger in root/command.go
    Signed-off-by: vnzongzna <vnzongzna@gmail.com>

--- clients/client-shell/cmds/root/command.go
+++ clients/client-shell/cmds/root/command.go
@@ -1,13 +1,42 @@
 // Package root defines the root of the application command tree.
 package root
 
-import "github.com/spf13/cobra"
+import (
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 var (
 	// Command is the root of the command tree.
-	Command = &cobra.Command{
+	Command = setUpRootCmd()
+	// Logger is the default log.Logger of the commands
+	Logger = log.New(ioutil.Discard, "INFO: ", log.Lshortfile)
+)
+
+// Setup presistent flags and pre-run and return root command
+func setUpRootCmd() *cobra.Command {
+	rootCmd := &cobra.Command{
 		Use:   "taskcluster",
 		Short: "Taskcluster Shell client.",
 		Long:  "A shell interface to Taskcluster",
 	}
-)
+
+	verbose := rootCmd.PersistentFlags().BoolP("verbose", "v", false, "verbose output")
+
+	// function to run before every subcommand
+	rootCmd.PersistentPreRun = func(cmd *cobra.Command, args []string) {
+		setUpLogs(*verbose)
+	}
+
+	return rootCmd
+}
+
+// setup log output based of --verbose flag
+func setUpLogs(enable bool) {
+	if enable {
+		Logger.SetOutput(os.Stdout)
+	}
+}
