6be83c64eb5a4ae040e7f4493048f8a536210778
Brian Stack <bstack@mozilla.com>
2020-05-13 14:39:01 -0700

    v29.5.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@
 <!-- `yarn release` will insert the existing changelog snippets here: -->
 <!-- NEXT RELEASE HERE -->
 
+## v29.5.1
+
+No changes
+
 ## v29.5.0
 
 ### GENERAL
--- clients/client-shell/cmds/version/version.go
+++ clients/client-shell/cmds/version/version.go
@@ -44,7 +44,7 @@ var (
 
 	// VersionNumber is a formatted string with the version information. This is
 	// filled in by `yarn release`
-	VersionNumber = "29.5.0"
+	VersionNumber = "29.5.1"
 )
 
 func init() {
