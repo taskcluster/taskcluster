654f9d251a004eb4540ce457ecf49b7778c9e89d
Pete Moore <pmoore@mozilla.com>
2020-05-14 21:46:59 +0200

    Fixed intermittent test 'docker image with manifest.json file'
    (cherry picked from commit ec62cbc73a0de4a94cc88a20ae1f8b6295f8ad85)

--- workers/docker-worker/test/integration/helper/wait_task_completion.js
+++
@@ -1,9 +0,0 @@
-module.exports = (queue, taskId) => {
-  return new Promise(async accept => {
-    let status;
-    do {
-      status = await queue.status(taskId);
-    } while (status.status.state === 'running');
-    accept(status.status);
-  });
-};
