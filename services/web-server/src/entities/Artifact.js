74d84587dc292713e87f4d66a5ea6a6310ee5ecf
Aki Sasaki <aki@mozilla.com>
2020-05-15 12:20:55 -0700

    address review comments

--- services/web-server/src/entities/Artifact.js
+++ services/web-server/src/entities/Artifact.js
@@ -4,7 +4,7 @@ module.exports = class Artifact {
     this.taskId = taskId;
     this.isPublic = /^public\//.test(this.name);
     // bug 1638047 - limit log viewer to public text/plain *.log files
-    this.isLog = /^text\/plain.*/.test(this.contentType) && /\.log$/.test(this.name) && this.isPublic;
+    this.isLog = /^text\/plain.*/.test(this.contentType) && /\.log$/.test(this.name);
 
     if (runId) {
       this.runId = runId;
