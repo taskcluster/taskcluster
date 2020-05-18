4b30da99609e26a166aeef6d1fe3bd9c1820a831
Brian Stack <bstack@mozilla.com>
2020-05-18 15:29:50 -0700

    Auto-generate image names in taskgraph

--- taskcluster/src/__init__.py
+++ taskcluster/src/__init__.py
@@ -3,3 +3,4 @@ from importlib import import_module
 
 def register(graph_config):
     import_module('.job', package=__name__)
+    import_module('.transforms', package=__name__)
---
+++ taskcluster/src/transforms/__init__.py
@@ -0,0 +1,20 @@
+import json
+
+from taskgraph.transforms.base import TransformSequence
+
+transforms = TransformSequence()
+
+
+@transforms.add
+def taskcluster_images(config, jobs):
+    with open('package.json', 'r') as pkg:
+        with open('.go-version', 'r') as goversion:
+            node_version = json.load(pkg)["engines"]["node"]
+            go_version = goversion.read()
+            for job in jobs:
+                image = job["worker"]["docker-image"]
+                if isinstance(image, dict) and image.keys()[0] == "taskcluster":
+                    repo = image["taskcluster"]
+                    if (repo == "node-and-go"):
+                        job["worker"]["docker-image"] = "taskcluster/node-and-go:node{}-{}".format(node_version, go_version).strip()
+                yield job
