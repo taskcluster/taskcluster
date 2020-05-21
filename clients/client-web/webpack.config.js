91995a4f089e14ad8841da54a23169eace5a9196
Hassan Ali <helfi92@gmail.com>
2020-05-21 12:16:07 -0400

    Only generate a umd module when publishing

--- clients/client-web/webpack.config.js
+++ clients/client-web/webpack.config.js
@@ -1,16 +1,3 @@
 const neutrino = require('neutrino');
 
-const config = neutrino().webpack();
-const output = config.output;
-
-module.exports = [
-  config,
-  {
-    ...config,
-    output: {
-      ...output,
-      path: `${output.path}/esm`,
-      libraryTarget: 'var',
-    },
-  },
-];
+module.exports = neutrino().webpack();
