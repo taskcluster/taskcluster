b81f81642581321788ace79860aaccae8e51fac8
Brian Stack <bstack@mozilla.com>
2020-05-22 00:05:12 -0700

    Fix generic-worker command generation

--- taskcluster/src/job.py
+++ taskcluster/src/job.py
@@ -1,53 +1,62 @@
 from taskgraph.transforms.job import run_job_using
 from taskgraph.util.schema import Schema
 
-from voluptuous import Required, Optional, Extra
+from voluptuous import Required, Optional, Extra, Any
 
 bare_schema = Schema({
     Required("using"): "bare",
-    Required("command"): basestring,
-    Optional("install"): basestring,
-    Optional("pre-clone"): basestring,
-    Optional("skip-clone"): bool,
+    Required("command"): Any(basestring, [basestring]),
+    Optional("install"): Any(basestring, [basestring]),
+    Optional("clone"): bool,
     Extra: object
     })
 
+bare_defaults = {
+    "clone": True,
+}
 
-@run_job_using("docker-worker", "bare", schema=bare_schema)
+
+@run_job_using("docker-worker", "bare", schema=bare_schema, defaults=bare_defaults)
 def bare_docker_worker(config, job, taskdesc):
     run = job["run"]
     worker = taskdesc['worker'] = job['worker']
 
-    pre_clone = run.get("pre-clone", "")
-    if pre_clone:
-        pre_clone += " && "
-    install_command = run.get("install", "")
-    if install_command:
-        install_command += " && "
-    run_command = run["command"]
+    params = config.params
+    command = []
+    if run.get("clone"):
+        command.extend([
+          "git clone --quiet --depth=20 --no-single-branch {} taskcluster && ".format(params["head_repository"]),
+          "cd taskcluster && ",
+          "git checkout {} && ".format(params["head_rev"]),
+        ])
+    if run.get("install"):
+        command.append(run.get("install").format(**params) + " && ")
+    command.append(run["command"].format(**params))
 
-    if run.get("skip-clone"):
-        worker["command"] = ["{}{}{}".format(pre_clone, install_command, run_command)]
-    else:
-        worker["command"] = [
-                "/bin/bash",
-                "-c",
-                "".join([
-                    "{pre_clone}",
-                    "git clone --quiet --depth=20 --no-single-branch {head_repository} taskcluster && ",
-                    "cd taskcluster && ",
-                    "git checkout {head_rev} && ",
-                    "{install_command}",
-                    "{run_command}",
-                    ]).format(
-                        install_command=install_command,
-                        run_command=run_command,
-                        pre_clone=pre_clone,
-                        **config.params
-                        ),
-                    ]
+    worker["command"] = ["/bin/bash", "-ec", "".join(command)]
 
 
-@run_job_using("generic-worker", "bare", schema=bare_schema)
+@run_job_using("generic-worker", "bare", schema=bare_schema, defaults=bare_defaults)
 def bare_generic_worker(config, job, taskdesc):
-    return [bare_docker_worker(config, job, taskdesc)]
+    run = job['run']
+    worker = taskdesc['worker'] = job['worker']
+    is_win = worker['os'] == 'windows'
+
+    params = config.params
+    command = []
+    install = run.get("install")
+    if install:
+        if isinstance(install, list):
+            command.extend([c.format(**params) for c in install])
+        else:
+            command.append(install.format(**params))
+    if isinstance(run["command"], list):
+        command.extend([c.format(**params) for c in run["command"]])
+    else:
+        command.append(run["command"].format(**params))
+
+    if is_win:
+        worker["command"] = command
+    else:
+        worker["command"] = [["/bin/bash", "-ec", "\n".join(command)]]
+
