92c5f310a3a2ae5dedfec64a4476060558ff11cf
Hassan Ali <helfi92@gmail.com>
2020-06-09 13:49:29 -0400

    Add implementation of reregister endpoint

--- services/worker-manager/src/util.js
+++ services/worker-manager/src/util.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const taskcluster = require('taskcluster-client');
 
 /**
  * We consider a "workerPoolId" to be a string of the shape "<provisionerId>/<workerType>".
@@ -23,3 +24,24 @@ const joinWorkerPoolId = (provisionerId, workerType) => {
 };
 exports.joinWorkerPoolId = joinWorkerPoolId;
 exports.MAX_MODIFY_ATTEMPTS = 5;
+
+const createCredentials = (worker, expires, cfg) => {
+  return taskcluster.createTemporaryCredentials({
+    clientId: `worker/${worker.providerId}/${worker.workerPoolId}/${worker.workerGroup}/${worker.workerId}`,
+    scopes: [
+      `assume:worker-type:${worker.workerPoolId}`, // deprecated role
+      `assume:worker-pool:${worker.workerPoolId}`,
+      `assume:worker-id:${worker.workerGroup}/${worker.workerId}`,
+      `queue:worker-id:${worker.workerGroup}/${worker.workerId}`,
+      `secrets:get:worker-type:${worker.workerPoolId}`, // deprecated secret name
+      `secrets:get:worker-pool:${worker.workerPoolId}`,
+      `queue:claim-work:${worker.workerPoolId}`,
+      `worker-manager:remove-worker:${worker.workerPoolId}/${worker.workerGroup}/${worker.workerId}`,
+      `worker-manager:reregister-worker:${worker.workerPoolId}/${worker.workerGroup}/${worker.workerId}`,
+    ],
+    start: taskcluster.fromNow('-15 minutes'),
+    expiry: expires,
+    credentials: cfg.taskcluster.credentials,
+  });
+};
+exports.createCredentials = createCredentials;
