149db204d5c8b1604e75783a1af543dae7f89ec6
Dustin J. Mitchell <dustin@mozilla.com>
2020-06-18 18:20:45 +0000

    Drop test case with inaccurate assertion
    If the worker does not advertise the error-report capability, there's
    nothing in place to prevent it from sending error-report messages
    anyway.  This test passed only by luck of timing, as suggested by the
    comment regarding timing.

--- tools/worker-runner/errorreport/errorreport_test.go
+++ tools/worker-runner/errorreport/errorreport_test.go
@@ -21,81 +21,50 @@ func TestHandleMessage(t *testing.T) {
 	kind := "severe"
 	title := "test error"
 
-	t.Run("with capability", func(t *testing.T) {
-		wkr := ptesting.NewFakeWorkerWithCapabilities("error-report")
-		defer wkr.Close()
+	wkr := ptesting.NewFakeWorkerWithCapabilities("error-report")
+	defer wkr.Close()
 
-		state := run.State{}
-		er := new(&state, tc.FakeWorkerManagerClientFactory)
+	state := run.State{}
+	er := new(&state, tc.FakeWorkerManagerClientFactory)
 
-		er.SetProtocol(wkr.RunnerProtocol)
-		wkr.RunnerProtocol.Start(false)
-		wkr.RunnerProtocol.WaitUntilInitialized()
-		wkr.WorkerProtocol.Start(true)
-		wkr.WorkerProtocol.Send(workerproto.Message{
-			Type: "error-report",
-			Properties: map[string]interface{}{
-				"description": description,
-				"extra":       extra,
-				"kind":        kind,
-				"title":       title,
-			},
-		})
+	er.SetProtocol(wkr.RunnerProtocol)
+	wkr.RunnerProtocol.Start(false)
+	wkr.RunnerProtocol.WaitUntilInitialized()
+	wkr.WorkerProtocol.Start(true)
+	wkr.WorkerProtocol.Send(workerproto.Message{
+		Type: "error-report",
+		Properties: map[string]interface{}{
+			"description": description,
+			"extra":       extra,
+			"kind":        kind,
+			"title":       title,
+		},
+	})
 
-		var reports []*tcworkermanager.WorkerErrorReport
-		var err error
-		func() {
-			for i := 0; i < 200; i++ {
-				reports, err = tc.FakeWorkerManagerWorkerErrorReports()
-				if len(reports) == 1 {
-					return
-				}
-				time.Sleep(10 * time.Millisecond)
+	var reports []*tcworkermanager.WorkerErrorReport
+	var err error
+	func() {
+		for i := 0; i < 200; i++ {
+			reports, err = tc.FakeWorkerManagerWorkerErrorReports()
+			if len(reports) == 1 {
+				return
 			}
-			t.Fatalf("worker error report not received")
-		}()
-		require.NoError(t, err)
-		require.Len(t, reports, 1)
+			time.Sleep(10 * time.Millisecond)
+		}
+		t.Fatalf("worker error report not received")
+	}()
+	require.NoError(t, err)
+	require.Len(t, reports, 1)
 
-		require.Equal(t, description, reports[0].Description)
-		require.Equal(t, kind, reports[0].Kind)
-		require.Equal(t, title, reports[0].Title)
+	require.Equal(t, description, reports[0].Description)
+	require.Equal(t, kind, reports[0].Kind)
+	require.Equal(t, title, reports[0].Title)
 
-		receivedExtra, err := reports[0].Extra.MarshalJSON()
-		require.NoError(t, err)
-		sentExtra, err := json.Marshal(extra)
-		require.NoError(t, err)
-		require.Equal(t, sentExtra, receivedExtra)
+	receivedExtra, err := reports[0].Extra.MarshalJSON()
+	require.NoError(t, err)
+	sentExtra, err := json.Marshal(extra)
+	require.NoError(t, err)
+	require.Equal(t, sentExtra, receivedExtra)
 
-		require.True(t, true)
-	})
-
-	t.Run("without capability", func(t *testing.T) {
-		wkr := ptesting.NewFakeWorkerWithCapabilities()
-		defer wkr.Close()
-
-		state := run.State{}
-		er := new(&state, tc.FakeWorkerManagerClientFactory)
-
-		er.SetProtocol(wkr.RunnerProtocol)
-		wkr.RunnerProtocol.Start(false)
-		wkr.RunnerProtocol.WaitUntilInitialized()
-		wkr.WorkerProtocol.Start(true)
-		wkr.WorkerProtocol.Send(workerproto.Message{
-			Type: "error-report",
-			Properties: map[string]interface{}{
-				"description": description,
-				"extra":       extra,
-				"kind":        kind,
-				"title":       title,
-			},
-		})
-
-		// note that without a delay we may not have waited
-		// long enough for potential calls to finish
-
-		reports, err := tc.FakeWorkerManagerWorkerErrorReports()
-		require.Errorf(t, err, "No reportWorkerError calls")
-		require.Len(t, reports, 0)
-	})
+	require.True(t, true)
 }
