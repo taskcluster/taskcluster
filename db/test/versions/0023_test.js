8a34929fca2c058ce4d8f65cd5cd04d323d0beb5
Brian Stack <bstack@mozilla.com>
2020-06-26 14:14:38 -0700

    Improve version testing

--- db/test/versions/0023_test.js
+++ db/test/versions/0023_test.js
@@ -22,79 +22,9 @@ const Builds = Entity.configure({
     state: Entity.types.String,
     created: Entity.types.Date,
     updated: Entity.types.Date,
-  },
-}).configure({
-  version: 2,
-  properties: {
-    organization: Entity.types.String,
-    repository: Entity.types.String,
-    sha: Entity.types.String,
-    taskGroupId: Entity.types.String,
-    state: Entity.types.String,
-    created: Entity.types.Date,
-    updated: Entity.types.Date,
-    // GitHub installation ID that comes from the webhook
-    // Needed for authentication in statusHandler
-    installationId: Entity.types.Number,
-  },
-  migrate: function(item) {
-    item.installationId = 0;
-    return item;
-  },
-}).configure({
-  version: 3,
-  properties: {
-    organization: Entity.types.String,
-    repository: Entity.types.String,
-    sha: Entity.types.String,
-    taskGroupId: Entity.types.String,
-    state: Entity.types.String,
-    created: Entity.types.Date,
-    updated: Entity.types.Date,
-    installationId: Entity.types.Number,
-    eventType: Entity.types.String,
-  },
-  migrate: function(item) {
-    item.eventType = 'Unknown Event';
-    return item;
-  },
-}).configure({
-  version: 4,
-  properties: {
-    organization: Entity.types.String,
-    repository: Entity.types.String,
-    sha: Entity.types.String,
-    taskGroupId: Entity.types.String,
-    state: Entity.types.String,
-    created: Entity.types.Date,
-    updated: Entity.types.Date,
     installationId: Entity.types.Number,
     eventType: Entity.types.String,
     eventId: Entity.types.String,
-  },
-  migrate: function(item) {
-    item.Id = 'Unknown';
-    return item;
-  },
-}).configure({
-  version: 5,
-  properties: {
-    organization: Entity.types.String,
-    repository: Entity.types.String,
-    sha: Entity.types.String,
-    taskGroupId: Entity.types.String,
-    state: Entity.types.String,
-    created: Entity.types.Date,
-    updated: Entity.types.Date,
-    installationId: Entity.types.Number,
-    eventType: Entity.types.String,
-    eventId: Entity.types.String,
-  },
-  migrate: function(item) {
-    // Delete Id because it was mistakenly set in the last migration
-    delete item.Id; // Just in case the migrations are chained
-    item.eventId = item.eventId || 'Unknown';
-    return item;
   },
 });
 
@@ -149,9 +79,11 @@ suite(testing.suiteName(), function() {
     },
     loadConditions: [
       {condition: {taskGroupId: 'EpvHX_OSSj2WSBs1SLhFYA'}, expectedSample: 'simple1'},
+      {condition: {taskGroupId: 'other_OSSj2WSBs1SLhFYA'}, expectedSample: 'simple2'},
     ],
     scanConditions: [
       {condition: {}, expectedSamples: ['simple1', 'simple2']},
+      {condition: null, expectedSamples: ['simple1', 'simple2']},
     ],
     notFoundConditions: [
       {condition: {taskGroupId: 'doesntexist'}},
