608e73e7884d5ee190a89d4f6308c711cc1b58e2
vnzongzna <vnzongzna@gmail.com>
2020-08-01 04:22:01 +0530

    [client-shell] validate-json accepts relative path
    Signed-off-by: vnzongzna <vnzongzna@gmail.com>

--- clients/client-shell/cmds/validate-json/validate.go
+++ clients/client-shell/cmds/validate-json/validate.go
@@ -2,8 +2,11 @@
 package validateJson
 
 import (
+	"path/filepath"
+	"strings"
+
 	"github.com/spf13/cobra"
-	"github.com/taskcluster/taskcluster/v29/clients/client-shell/cmds/root"
+	"github.com/taskcluster/taskcluster/v35/clients/client-shell/cmds/root"
 	js "github.com/xeipuuv/gojsonschema"
 )
 
@@ -21,11 +24,14 @@ func init() {
 	root.Command.AddCommand(Command)
 }
 
+// Takes json-schema & json as input which should be in the following 2 formats
+// 1. https://community-tc.services.mozilla.com/references/schemas/<some-schema>.json
+// 2. file:///home/user/<some-schema>.json
 func validate(cmd *cobra.Command, args []string) {
-	schemaLoader := js.NewReferenceLoader(args[0])
-	documentLoader := js.NewReferenceLoader(args[1])
+	schema := schemaLoader(args[0])
+	document := schemaLoader(args[1])
 
-	result, err := js.Validate(schemaLoader, documentLoader)
+	result, err := js.Validate(schema, document)
 
 	if err != nil {
 		log.Panic(err.Error())
@@ -39,4 +45,24 @@ func validate(cmd *cobra.Command, args []string) {
 			log.Infof("- %s\n", desc)
 		}
 	}
+}
+
+// In case user don't provide absolute path, it should be generate & return a JSONLoader
+// Input: path to json schema
+// Return: JSON loader interface
+func schemaLoader(path string) js.JSONLoader {
+	if strings.HasPrefix(path, "http") || strings.HasPrefix(path, "file://") {
+		return js.NewReferenceLoader(path)
+	} else if filepath.IsAbs(path) {
+		path = "file://" + path
+	} else if !filepath.IsAbs(path) {
+		full_path, err := filepath.Abs(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		path = "file://" + full_path
+	} else {
+		log.Panic("Input isn't supported in the format")
+	}
+	return js.NewReferenceLoader(path)
 }
---
+++ clients/client-shell/cmds/validate-json/validate_test.go
@@ -0,0 +1,13 @@
+package validateJson
+
+import (
+	"testing"
+
+	_ "github.com/spf13/cobra"
+	_ "github.com/spf13/pflag"
+	assert "github.com/stretchr/testify/require"
+)
+
+func TestValidateJson(t *testing.T) {
+	_ = assert.New(t)
+}
