b1dd7a46fae47454690cdef8c0ae20a479dde91c
Dustin J. Mitchell <dustin@mozilla.com>
2020-08-18 17:52:37 +0000

    check for minimal yarn.lock files

---
+++ infrastructure/tooling/src/generate/generators/yarn-minify.js
@@ -0,0 +1,24 @@
+const minify = require('yarn-minify');
+const { gitLsFiles } = require('../../utils');
+
+// Ignore packages while we slowly whittle away the requirements
+const IGNORE = {
+  'clients/client/yarn.lock': pkg => false,
+  'clients/client-web/yarn.lock': pkg => true,
+  'yarn.lock': pkg => true,
+  'ui/yarn.lock': pkg => true,
+  'workers/docker-worker/yarn.lock': pkg => true,
+};
+
+exports.tasks = [{
+  title: 'Minify yarn.locks',
+  provides: ['target-yarn-minify'],
+  run: async (requirements, utils) => {
+    let yarnlocks = (await gitLsFiles())
+      .filter(file => file === 'yarn.lock' || file.endsWith('/yarn.lock'));
+
+    for (let filename of yarnlocks) {
+      minify(filename, { ignore: IGNORE[filename] });
+    }
+  },
+}];
