c535525278c571d62ef63b3c82f89ab3fca61799
Hassan Ali <helfi92@gmail.com>
2020-09-15 13:54:30 -0400

    Add empty db test file

---
+++ db/test/versions/0047_test.js
@@ -0,0 +1,6 @@
+const testing = require('taskcluster-lib-testing');
+
+suite(testing.suiteName(), function() {
+  // this version only creates a method, which is tested in unit tests,
+  // so there is nothing to test on upgrade
+});
