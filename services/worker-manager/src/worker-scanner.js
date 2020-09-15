27844d084b7d829d80a33d61b592997abe17e056
Hassan Ali <helfi92@gmail.com>
2020-09-15 13:32:20 -0400

    Remove unecessary if statement

--- services/worker-manager/src/worker-scanner.js
+++ services/worker-manager/src/worker-scanner.js
@@ -52,28 +52,26 @@ class WorkerScanner {
     const fetch = async (size, offset) => await this.db.fns.get_non_stopped_workers(null, null, null, size, offset);
     for await (let row of paginatedIterator({ fetch })) {
       const worker = Worker.fromDb(row);
-      if (worker.state !== Worker.states.STOPPED) {
-        const provider = this.providers.get(worker.providerId);
-        if (provider) {
-          try {
-            await provider.checkWorker({ worker });
-          } catch (err) {
-            this.monitor.reportError(err); // Just report it and move on so this doesn't block other providers
-          }
-        } else {
-          this.monitor.info(
-            `Worker ${worker.workerGroup}/${worker.workerId} has unknown providerId ${worker.providerId} (ignoring)`);
+      const provider = this.providers.get(worker.providerId);
+      if (provider) {
+        try {
+          await provider.checkWorker({ worker });
+        } catch (err) {
+          this.monitor.reportError(err); // Just report it and move on so this doesn't block other providers
         }
+      } else {
+        this.monitor.info(
+          `Worker ${worker.workerGroup}/${worker.workerId} has unknown providerId ${worker.providerId} (ignoring)`);
+      }
 
-        // If the worker will be expired soon but it still exists,
-        // update it to stick around a while longer. If this doesn't happen,
-        // long-lived instances become orphaned from the provider. We don't update
-        // this on every loop just to avoid the extra work when not needed
-        if (worker.expires < taskcluster.fromNow('1 week')) {
-          await worker.update(this.db, worker => {
-            worker.expires = taskcluster.fromNow('8 days');
-          });
-        }
+      // If the worker will be expired soon but it still exists,
+      // update it to stick around a while longer. If this doesn't happen,
+      // long-lived instances become orphaned from the provider. We don't update
+      // this on every loop just to avoid the extra work when not needed
+      if (worker.expires < taskcluster.fromNow('1 week')) {
+        await worker.update(this.db, worker => {
+          worker.expires = taskcluster.fromNow('8 days');
+        });
       }
     }
 
