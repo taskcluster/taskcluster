8d6c848ba629157a81d9f72acf80a441c92c1752
Hassan Ali <helfi92@gmail.com>
2020-09-16 09:49:50 -0400

    Add db method get_workers_without_provider_data

--- db/test/versions/0048_test.js
+++ db/test/versions/0048_test.js
@@ -1,6 +1,6 @@
 const testing = require('taskcluster-lib-testing');
 
 suite(testing.suiteName(), function() {
-  // this version only creates an index,
+  // this version only creates a method and an index, which is tested in unit tests,
   // so there is nothing to test on upgrade
 });
