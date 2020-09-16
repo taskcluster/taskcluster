c7bfe6832394ea5f45feff3f06e5bed9e93b1979
Hassan Ali <helfi92@gmail.com>
2020-09-16 09:34:42 -0400

    Add index on workers table state field

---
+++ db/test/versions/0048_test.js
@@ -0,0 +1,6 @@
+const testing = require('taskcluster-lib-testing');
+
+suite(testing.suiteName(), function() {
+  // this version only creates an index,
+  // so there is nothing to test on upgrade
+});
