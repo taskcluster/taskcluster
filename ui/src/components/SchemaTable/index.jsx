e6526c0e33ad40ca94aa1a0c2dfaa051140f7b8a
Dustin J. Mitchell <dustin@mozilla.com>
2020-09-23 22:23:45 +0000

    Upgrade schema viewer to show full descriptions

--- ui/src/components/SchemaTable/index.jsx
+++ ui/src/components/SchemaTable/index.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { string } from 'prop-types';
 import Spinner from '@mozilla-frontend-infra/components/Spinner';
-import Table from 'material-ui-json-schema-viewer/build/SchemaViewer';
+import SchemaViewer from 'material-ui-json-schema-viewer/build/SchemaViewer';
 import jsonSchemaDraft06 from 'ajv/lib/refs/json-schema-draft-06.json';
 import jsonSchemaDraft07 from 'ajv/lib/refs/json-schema-draft-07.json';
 import ErrorPanel from '../ErrorPanel';
@@ -96,7 +96,7 @@ export default class SchemaTable extends Component {
     }
 
     return schema ? (
-      <Table schema={schema} references={references} />
+      <SchemaViewer schema={schema} references={references} />
     ) : (
       <Spinner loading />
     );
