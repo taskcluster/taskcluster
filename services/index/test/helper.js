a4bb1758b0fbe8daebed0fee619331f1c24abbaf
Pete Moore <pmoore@mozilla.com>
2020-10-17 15:45:31 +0200

    RFC 165: update index service tests to work with anonymous scopes

--- services/index/test/helper.js
+++ services/index/test/helper.js
@@ -70,7 +70,16 @@ exports.withServer = (mock, skipping) => {
     // a local rootUrl to test the API, including mocking auth on that
     // rootUrl.
     exports.load.cfg('taskcluster.rootUrl', helper.rootUrl);
-    fakeauth.start({ 'test-client': ['*'] }, { rootUrl: helper.rootUrl });
+    fakeauth.start({
+      'test-client': ['*'],
+    }, {
+      rootUrl: helper.rootUrl,
+      anonymousScopes: [
+        'queue:get-artifact:public/*',
+        'index:list-tasks',
+        'index:list-namespaces',
+      ],
+    });
 
     helper.Index = taskcluster.createClient(builder.reference());
 
