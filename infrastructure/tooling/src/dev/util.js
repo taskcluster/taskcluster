f5df88c14abb6bcc34fcab8af57f34410a0b0881
Hassan Ali <helfi92@gmail.com>
2020-10-20 14:15:36 -0400

    Remove ssl=1
    As of node-pg 8, that tries to verify the certs but for cloud SQL that
    doesn't make sense.

--- infrastructure/tooling/src/dev/util.js
+++ infrastructure/tooling/src/dev/util.js
@@ -6,8 +6,5 @@ exports.makePgUrl = ({ hostname, username, password, dbname }) => {
   u.username = username;
   u.password = password;
   u.pathname = `/${dbname}`;
-  // enable SSL without any identity checking (so basically encryption but no
-  // prevention of MITM attacks)
-  u.search = 'ssl=1';
   return u.toString();
 };
