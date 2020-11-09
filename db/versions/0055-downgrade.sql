388ca5cd7a6a14fcce90d73086ea8d836b11b8b6
Hassan Ali <helfi92@gmail.com>
2020-11-09 16:26:31 -0500

    Nit

--- db/versions/0055-downgrade.sql
+++ db/versions/0055-downgrade.sql
@@ -1,4 +1,4 @@
 begin
-    alter table objects drop column backend_id text;
-    alter table objects drop column project_id text;
+    alter table objects drop column backend_id;
+    alter table objects drop column project_id;
 end
