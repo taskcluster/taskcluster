2e9783f84823c092912b0bf1afdac88d3d26673d
Alex López <alex.lopez.zorzano@gmail.com>
2020-12-04 07:24:57 +0100

    Attempt to implement db version with online downgrade

--- db/test/versions/0056_test.js
+++ db/test/versions/0056_test.js
@@ -9,13 +9,16 @@ suite(testing.suiteName(), function() {
 
   // A helper to make it easier to create tasks with dummy values within queries
   const makeFieldsForCreation = (opts) => {
-    let result = `${opts.taskId || `'tid'`}, ${opts.provisionerId || `'pp'`}, ${opts.workerType || `'wt'`},
+    const pp = opts.provisionerId || `'pp'`;
+    const wt = opts.workerType || `'wt'`;
+    let result = `${opts.taskId || `'tid'`}, ${opts.provisionerId || `'pp'`}, ${wt},
             'sid', 'tgid', jsonb_object('{}'), 'all-completed',
             jsonb_object('{}'), 'normal', 0, now(), now(), now(), jsonb_object('{}'),
             jsonb_object('{}'), jsonb_object('{}'), jsonb_object('{}'),
             jsonb_object('{}')`;
     if (opts.withDefaults) {
-      result += `, 0, jsonb_build_array(), null, false`;
+      const tqid = `${pp} || '/' || ${wt}`;
+      result += `, 0, jsonb_build_array(), null, false, ${tqid}`;
     }
     return result;
   };
@@ -24,9 +27,12 @@ suite(testing.suiteName(), function() {
 
   helper.dbVersionTest({
     version: THIS_VERSION,
-    onlineMigration: true,
-    onlineDowngrade: false,
+    onlineMigration: false,
+    onlineDowngrade: true,
     createData: async client => {
+      // create the data including a task_queue_id that must be the
+      // combined identifier of provisioner_id/worker_type.
+      // this is a safe assumption as tested for the previous version.
       await client.query(`
         with gen as (
           select generate_series(1, 99) as i
@@ -52,32 +58,31 @@ suite(testing.suiteName(), function() {
                            retries_left,
                            runs,
                            taken_until,
-                           ever_resolved)
+                           ever_resolved,
+                           task_queue_id)
         select ${makeFieldsForCreation({ taskId: `'tid-' || gen.i`, workerType: `'wt-' || gen.i`, withDefaults: true })}
         from gen`);
     },
     startCheck: async client => {
-      // basic check
-      const res = await client.query('select * from tasks');
-      const taskCount = res.rows.length;
-      assert(taskCount >= 99, 'data was not created properly');
+      const res = await client.query('select task_id, task_queue_id from tasks');
+      const nextTaskId = res.rows.length + 1;
+      assert(nextTaskId >= 100, 'data was not created properly');
+      const tqids = res.rows.map(({ task_queue_id }) => task_queue_id).sort();
+      assert.deepEqual(tqids, _.range(1, nextTaskId).map(i => `pp/wt-${i}`).sort());
 
       // check the schema
-      await helper.assertTableColumn('tasks', 'provisioner_id');
-      await helper.assertTableColumn('tasks', 'worker_type');
-      await helper.assertNoTableColumn('tasks', 'task_queue_id');
+      await helper.assertTableColumn('tasks', 'task_queue_id');
     },
     concurrentCheck: async client => {
       // check that the inserted data looks as expected
-      const res = await client.query('select task_id, provisioner_id, worker_type from tasks');
+      const res = await client.query('select task_id, task_queue_id from tasks');
       const nextTaskId = res.rows.length + 1;
-      const pps = res.rows.map(({ provisioner_id }) => provisioner_id);
-      assert.deepEqual(new Set(pps), new Set(['pp']));
-      const wts = res.rows.map(({ worker_type }) => worker_type).sort();
-      assert.deepEqual(wts, _.range(1, nextTaskId).map(i => `wt-${i}`).sort());
       const tids = res.rows.map(({ task_id }) => task_id).sort();
       assert.deepEqual(tids, _.range(1, nextTaskId).map(i => `tid-${i}`).sort());
+      const tqids = res.rows.map(({ task_queue_id }) => task_queue_id).sort();
+      assert.deepEqual(tqids, _.range(1, nextTaskId).map(i => `pp/wt-${i}`).sort());
 
+      // check functions that use provisioner_id and worker_type (which should still work during an online downgrade)
       // check that create_task works as expected
       const taskOpts = {
         taskId: `'tid-${nextTaskId}'`,
@@ -99,16 +104,18 @@ suite(testing.suiteName(), function() {
         'the last task created with create_task could not be retrieved with get_task');
     },
     finishedCheck: async client => {
-      // check that all tasks have a task_queue_id that is not null
-      // and equal to the expected combination of old identifiers
-      const res = await client.query('select provisioner_id, worker_type, task_queue_id from tasks');
-      res.rows.forEach(({ provisioner_id, worker_type, task_queue_id }) => {
-        assert.equal(task_queue_id, `${provisioner_id}/${worker_type}`);
-      });
+      // check that task_queue_id values are as expected
+      // this may be somewhat redundant with what's checked in the concurrentCheck
+      // but it doesn't hurt
+      const res = await client.query('select task_queue_id from tasks');
+      const nextTaskId = res.rows.length + 1;
+      const tqids = res.rows.map(({ task_queue_id }) => task_queue_id).sort();
+      assert.deepEqual(tqids, _.range(1, nextTaskId).map(i => `pp/wt-${i}`).sort());
 
       // check the schema
-      await helper.assertTableColumn('tasks', 'provisioner_id');
-      await helper.assertTableColumn('tasks', 'worker_type');
+      // we expect provisioner_id and worker_type to have been dropped
+      await helper.assertNoTableColumn('tasks', 'provisioner_id');
+      await helper.assertNoTableColumn('tasks', 'worker_type');
       await helper.assertTableColumn('tasks', 'task_queue_id');
     },
   });
