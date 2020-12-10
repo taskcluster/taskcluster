194b78885ba2bf647d99eec4c1e18b3cfdff72d2
Alex López <alex.lopez.zorzano@gmail.com>
2020-12-10 17:42:21 +0100

    Temporarily rename db versions to merge main

--- db/test/versions/0057_test.js
+++
@@ -1,6 +0,0 @@
-const testing = require('taskcluster-lib-testing');
-
-suite(testing.suiteName(), function() {
-  // this version only adds an index and some methods,
-  // so there is nothing to test on upgrade
-});
