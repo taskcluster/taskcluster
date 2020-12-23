30045120061d44326a3531b28ff5ed08e1919f8e
Dustin J. Mitchell <dustin@mozilla.com>
2020-12-23 20:22:49 +0000

    Consolidate object-service deployment docs in one place
    This also adds more step-by-step docs for test account/bucket setup.  In
    particular, the updated service account is not a project owner,
    drastically reducing its permissions.

--- services/object/test/helper/google.js
+++ services/object/test/helper/google.js
@@ -1,20 +1,21 @@
 /**
- * The google test bucket was set up manually.
- * The taskcluster-dev project was manually configured to be the default project.
- * The test bucket has bucket-level permissions.
- * There is no retention policy, since tests need to delete objects almost immediately.
- *
- * Console view:
- *   https://console.cloud.google.com/storage/browser/taskcluster-test
- *
- * Access control
- *   Uniform: No object-level ACLs enabled
- *
- * Permissions
- *   allUsers: Storage Object Viewer
- *   Editors of project taskcluster-dev: [Storage Legacy Bucket Owner, Storage Legacy Object Owner]
- *   Owners of project taskcluster-dev: [Storage Legacy Bucket Owner, Storage Legacy Object Owner]
- *   Viewers of project taskcluster-dev: [Storage Legacy Bucket Reader, Storage Legacy Object Reader]
+ * The google test bucket was set up manually:
+ *  * Create a service account, `object-service-test@taskcluster-dev.iam.gserviceaccount.com`.
+ *    Do not grant it any roles.
+ *  * Create a GCS bucket, `taskcluster-test`
+ *    * "Region" location type (choice of region does not matter)
+ *    * Standard storeage class
+ *    * "Uniform" access control
+ *    * No "Advanced Settings"
+ *  * Under the bucket's "permissions" tab
+ *    * Click add, paste the full name of the service account (this does not auto-complete)
+ *      * Add roles "Storage Legacy Bucket Owner" and "Storage Legacy Object Owner" both under "Cloud Storage Legacy"
+ *    * Click add, type "allUsers"
+ *      * Add roles "Storage Object Viewer" under "Cloud Storage"
+ *  * Under the bucket's "lifecycle" tab
+ *    * Add a rule to delete objects after 1 day (this cleans up any leftovers from test runs)
+ *  * In the Google Cloud Storage Settings view, "Interoperability" tab, create HMAC credentials for the service account
+ *    and put those in the secret with GOOGLE_ACCESS_KEY_ID and GOOGLE_SECRET_ACCESS_KEY.
  */
 exports.secret = [
   { env: 'GOOGLE_ACCESS_KEY_ID', name: 'accessKeyId' },
