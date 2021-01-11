5f7a31ac6ede9335b1b174586eca208c40ae5a70
Dustin J. Mitchell <dustin@mozilla.com>
2021-01-11 21:42:55 +0000

    v40.0.3

--- clients/client-shell/cmds/version/version.go
+++ clients/client-shell/cmds/version/version.go
@@ -44,7 +44,7 @@ var (
 
 	// VersionNumber is a formatted string with the version information. This is
 	// filled in by `yarn release`
-	VersionNumber = "40.0.2"
+	VersionNumber = "40.0.3"
 )
 
 var log = root.Logger
