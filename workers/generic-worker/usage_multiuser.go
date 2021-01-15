b7a6ef911b99f09562372477872634a1233df7ea
Pete Moore <pmoore@mozilla.com>
2021-01-15 16:39:55 +0100

    wip

--- workers/generic-worker/usage_multiuser.go
+++ workers/generic-worker/usage_multiuser.go
@@ -6,6 +6,14 @@ const (
 	CANT_SECURE_CONFIG ExitCode = 77
 )
 
+func headlessTasksUsage() string {
+	return `
+          headlessTasks                     If true, no dedicated graphical session will be available to tasks.
+                                            There will also be no reboots between tasks and multiple workers
+                                            can be run on the same host. Useful for tasks that don't require
+                                            a graphical session, such as software builds. [default: false]`
+}
+
 func runTasksAsCurrentUserUsage() string {
 	return `
           runTasksAsCurrentUser             If true, users will still be created for tasks, but
