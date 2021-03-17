248a552cf68682e70fc484f365469c9dd42e2cce
Dustin J. Mitchell <dustin@mozilla.com>
2021-03-17 21:55:21 +0000

    Update tests to expect new Ajv error syntax

--- libraries/references/test/validate_test.js
+++ libraries/references/test/validate_test.js
@@ -174,7 +174,7 @@ suite(testing.suiteName(), function() {
       })
       .end();
     assertProblems(references, [
-      'test-schema.yml: schema.properties[\'abc\'] should be object,boolean',
+      'test-schema.yml: schema/properties/abc should be object,boolean',
     ]);
   });
 
@@ -186,7 +186,7 @@ suite(testing.suiteName(), function() {
       })
       .end();
     assertProblems(references, [
-      'test-schema.yml: schema.metadata should have required property \'name\'',
+      'test-schema.yml: schema/metadata should have required property \'name\'',
     ]);
   });
 
@@ -218,7 +218,7 @@ suite(testing.suiteName(), function() {
       .apiref({ serviceName: true })
       .end();
     assertProblems(references, [
-      'test-api-ref.yml: reference.serviceName should be string',
+      'test-api-ref.yml: reference/serviceName should be string',
     ]);
   });
 
@@ -228,7 +228,7 @@ suite(testing.suiteName(), function() {
       .exchangesref({ title: false })
       .end();
     assertProblems(references, [
-      'test-exch-ref.yml: reference.title should be string',
+      'test-exch-ref.yml: reference/title should be string',
     ]);
   });
 
