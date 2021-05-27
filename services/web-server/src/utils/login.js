83a471b409721dcc2d4fcac4e9e83b7951d8fdca
Brian Stack <bstack@mozilla.com>
2021-05-27 13:56:22 -0700

    Encode postmessage content to avoid issues

--- services/web-server/src/utils/login.js
+++ services/web-server/src/utils/login.js
@@ -1,5 +1,15 @@
+const assert = require('assert');
+const _ = require('lodash');
+
 // A function that returns a middleware that renders callback.ejs
 module.exports = (publicUrl) => async (request, response) => {
+  assert(request.user, 'Must have a user');
+  assert(request.user.profile);
+  assert(request.user.identityProviderId);
+  assert(request.user.providerExpires);
+
+  request.user.encodedProfile = new Buffer.from(JSON.stringify(request.user.profile)).toString('base64');
+
   response.render('callback', {
     user: request.user,
     publicUrl,
