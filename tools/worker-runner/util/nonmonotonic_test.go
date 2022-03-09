1e133a1eb6785e6fe8d054d2f748ad0891b25723
Dustin J. Mitchell <dustin@v.igoro.us>
2022-03-08 20:03:14 -0500

    Renew worker creds on time, even after hibernation

---
+++ tools/worker-runner/util/nonmonotonic_test.go
@@ -0,0 +1,22 @@
+package util
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSleepUntilWallClockTime(t *testing.T) {
+	until := time.Now().Add(time.Millisecond * 10)
+	require.True(t, SleepUntilWallClock(until, context.Background()))
+	require.GreaterOrEqual(t, time.Now().UnixMilli(), until.UnixMilli())
+}
+
+func TestSleepUntilWallClockTimeCancel(t *testing.T) {
+	until := time.Now().Add(time.Second * 10)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Millisecond)
+	require.False(t, SleepUntilWallClock(until, ctx))
+	require.Less(t, time.Now().UnixMilli(), until.UnixMilli())
+}
