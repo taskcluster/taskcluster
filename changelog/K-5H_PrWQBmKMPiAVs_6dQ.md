ef257a872f39a87e8af3eceb77e3de2774ab3cf6
Yaraslau Kurmyza <yarik@mozilla.com>
2022-07-04 10:03:48 +0200

    refactor(ui): Remove node-fetch dependency
    It was used in a helper script to fetch and cache graphql types.

---
+++ changelog/K-5H_PrWQBmKMPiAVs_6dQ.md
@@ -0,0 +1,5 @@
+audience: developers
+level: patch
+---
+
+Remove node-fetch dependency from ui/ as it was only used in abandoned queryServer.js script to cache possible graphql types.
--- ui/src/scripts/queryServer.js
+++ ui/src/scripts/queryServer.js
@@ -1,51 +1,70 @@
 /**
  * Query server / schema to obtain the necessary information
  * about unions and interfaces and write it to a file.
+ *
+ * Run against local web-server:
+ * GRAPHQL_ENDPOINT=http://localhost:3050/graphql yarn run create:fragment-matcher
+ *
  */
-const fetch = require('node-fetch');
 const fs = require('fs');
 const assert = require('assert');
+const http = require('http');
+const https = require('https');
+const { URL } = require('url');
+
+const endpoint =
+  typeof window !== 'undefined'
+    ? window?.env?.GRAPHQL_ENDPOINT
+    : process?.env?.GRAPHQL_ENDPOINT;
 
 assert(
-  window.env.GRAPHQL_ENDPOINT,
-  'GRAPHQL_ENDPOINT is requird to obtain information about unions and interfaces.'
+  endpoint,
+  'GRAPHQL_ENDPOINT is required to obtain information about unions and interfaces.'
 );
 
-fetch(window.env.GRAPHQL_ENDPOINT, {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({
-    variables: {},
-    operationName: '',
-    query: `
-      {
-        __schema {
-          types {
-            kind
+const parsed = new URL(endpoint);
+const data = JSON.stringify({
+  variables: {},
+  query: `
+    {
+      __schema {
+        types {
+          kind
+          name
+          possibleTypes {
             name
-            possibleTypes {
-              name
-            }
           }
         }
       }
-    `,
-  }),
-})
-  .then(result => result.json())
-  .then(result => {
+    }
+  `,
+});
+const options = {
+  hostname: parsed.hostname,
+  port: parsed.port,
+  path: parsed.pathname,
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length,
+  },
+};
+const executor = parsed.protocol === 'https:' ? https : http;
+const req = executor.request(options, res => {
+  res.on('data', result => {
+    const response = JSON.parse(Buffer.from(result).toString());
     // here we're filtering out any type information unrelated
     // to unions or interfaces
     // eslint-disable-next-line no-underscore-dangle
-    const filteredData = result.data.__schema.types.filter(
+    const filteredData = response.data.__schema.types.filter(
       type => type.possibleTypes !== null
     );
 
     // eslint-disable-next-line no-param-reassign, no-underscore-dangle
-    result.data.__schema.types = filteredData;
+    response.data.__schema.types = filteredData;
     fs.writeFile(
       './src/fragments/fragmentTypes.json',
-      `${JSON.stringify(result.data)}\n`,
+      `${JSON.stringify(response.data)}\n`,
       err => {
         if (err) {
           // eslint-disable-next-line no-console
@@ -57,3 +76,11 @@ fetch(window.env.GRAPHQL_ENDPOINT, {
       }
     );
   });
+});
+
+req.on('error', error => {
+  // eslint-disable-next-line no-console
+  console.error(error);
+});
+req.write(data);
+req.end();
