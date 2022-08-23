f8bc010b92e5b5cec488d76210e7fc6b40e518f4
Pete Moore <pmoore@mozilla.com>
2022-08-23 12:14:47 +0200

    wip

--- infrastructure/tooling/src/release/tasks.js
+++ infrastructure/tooling/src/release/tasks.js
@@ -201,6 +201,7 @@ module.exports = ({ tasks, cmdOptions, credentials }) => {
         'clients/client-shell/**',
         'internal/**',
         'tools/**',
+        'ui/docs/reference/workers/generic-worker/installing.mdx',
         'ui/docs/reference/workers/websocktunnel.mdx',
         // Provide explicit list of allowed file extensions so that
         // workers/generic-worker/testdata/*.zip files are not modified.
