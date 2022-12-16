452f7fc13b14448028363bb90fb26ce0372b23a9
Yaraslau Kurmyza <yarik@mozilla.com>
2022-12-16 18:21:22 +0100

    feat(w-m): include workerGroup to join condition
    improves #5891 changes

--- db/test/versions/0080_test.js
+++ db/test/versions/0080_test.js
@@ -28,13 +28,13 @@ suite(testing.suiteName(), function () {
     const wmDb = await helper.setupDb('worker_manager');
 
     for (let i = 0; i < 10; i++) {
-      await create(queueDb, { taskQueueId: `prov/w/${i}`, workerId: `worker-${i}` });
+      await create(queueDb, { taskQueueId: `prov/w/${i}`, workerId: `worker-${i}`, workerGroup: `wg-${i}` });
     }
     // create same worker with multiple queue ids
-    const workerPoolIds = ['prov/w/1', 'some/other/pool'];
-    await Promise.all(workerPoolIds.map(id => wmDb.fns.create_worker(
-      id,
-      'w/group',
+    const workerPoolIds = ['prov/w/1', 'some/other/pool', 'extra/pool'];
+    await Promise.all(workerPoolIds.map((pool, idx) => wmDb.fns.create_worker(
+      pool,
+      `wg-${idx}`,
       'worker-1',
       'static',
       new Date(),
@@ -49,7 +49,7 @@ suite(testing.suiteName(), function () {
     await helper.upgradeTo(PREV_VERSION);
     const res = await queueDb.fns.get_queue_workers_with_wm_join('prov/w/1', null, null, null);
     // older implementation will include same worker that exists in `workers` table with two different `worker_pool_id`
-    assert.equal(res.length, 2);
+    assert.equal(res.length, 3);
 
     await helper.upgradeTo(THIS_VERSION);
     const res2 = await queueDb.fns.get_queue_workers_with_wm_join('prov/w/1', null, null, null);
