dd9549baedba50ecfe18347ba7e1e26ad87faacd
Matt Boris <mboris@mozilla.com>
2023-04-01 22:02:56 -0400

    refactor!: drop support for python 3.6

--- clients/client-py/setup.py
+++ clients/client-py/setup.py
@@ -58,8 +58,8 @@ class Tox(TestCommand):
         errno = tox.cmdline(args=args)
         sys.exit(errno)
 
-if sys.version_info[0] == 3 and sys.version_info[:2] < (3, 5):
-    raise Exception('This library does not support Python 3 versions below 3.5')
+if sys.version_info[0] == 3 and sys.version_info[:2] < (3, 6):
+    raise Exception('This library does not support Python 3 versions below 3.6')
 
 with open('README.md', encoding='utf8') as f:
     long_description = f.read()
@@ -89,9 +89,10 @@ if __name__ == '__main__':
         cmdclass={'test': Tox},
         zip_safe=False,
         classifiers=[
-            'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
         ],
     )
