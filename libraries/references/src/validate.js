f5e6a663c803194713c09e32a66f582959a9fc53
Pete Moore <pmoore@mozilla.com>
2023-05-19 09:42:58 +0200

    try this

--- libraries/references/src/validate.js
+++ libraries/references/src/validate.js
@@ -27,7 +27,6 @@ const UNREFERENCED_SCHEMAS = [
   { service: 'generic-worker', schema: 'simple_posix.json#' },
   { service: 'generic-worker', schema: 'multiuser_windows.json#' },
   { service: 'generic-worker', schema: 'multiuser_posix.json#' },
-  { service: 'generic-worker', schema: 'docker_posix.json#' },
   { service: 'docker-worker', schema: 'v1/payload.json#' },
 ];
 
