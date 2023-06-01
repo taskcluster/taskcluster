ddbeb8ba123813255139903fb5c468ff67967261
Matt Boris <mboris@mozilla.com>
2023-06-01 09:57:42 -0400

    feat: validate input from stdin

--- cmd/d2g/main.go
+++ cmd/d2g/main.go
@@ -3,32 +3,85 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/mcuadros/go-defaults"
 	"github.com/taskcluster/d2g"
 	"github.com/taskcluster/d2g/dockerworker"
+	"github.com/xeipuuv/gojsonschema"
 )
 
 func main() {
 	log.SetFlags(0)
 	log.SetPrefix("d2g: ")
+
+	// Read the JSON input from standard input
+	input, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatal("Failed to read input:", err)
+	}
+
+	// Define the path to the JSON schema
+	schemaPath := "schemas/docker_worker_payload.json"
+
+	// Validate the JSON input against the schema
+	err = validateJSON(schemaPath, input)
+	if err != nil {
+		log.Fatalf("Input validation failed: %v", err)
+	}
+
+	// Convert the validated JSON input
 	dwPayload := new(dockerworker.DockerWorkerPayload)
 	defaults.SetDefaults(dwPayload)
-	decoder := json.NewDecoder(os.Stdin)
-	decoder.DisallowUnknownFields()
-	err := decoder.Decode(&dwPayload)
+	err = json.Unmarshal(input, &dwPayload)
 	if err != nil {
 		log.Fatalf("Failed to convert input to a docker worker payload definition: %v", err)
 	}
+
 	gwPayload, err := d2g.Convert(dwPayload)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	formattedActualGWPayload, err := json.MarshalIndent(*gwPayload, "", "  ")
 	if err != nil {
 		log.Fatalf("Cannot convert Generic Worker payload %#v to JSON: %s", *gwPayload, err)
 	}
+
 	fmt.Println(string(formattedActualGWPayload))
+}
+
+func validateJSON(schemaPath string, input []byte) error {
+	// Read the JSON schema from file
+	schemaBytes, err := os.ReadFile(schemaPath)
+	if err != nil {
+		return fmt.Errorf("failed to read JSON schema: %v", err)
+	}
+
+	// Parse the JSON schema
+	schemaLoader := gojsonschema.NewBytesLoader(schemaBytes)
+	documentLoader := gojsonschema.NewBytesLoader(input)
+
+	// Perform the validation
+	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+	if err != nil {
+		return err
+	}
+
+	// Check if the validation failed
+	if !result.Valid() {
+		// Collect validation errors
+		var errors []string
+		for _, desc := range result.Errors() {
+			errors = append(errors, desc.String())
+		}
+
+		// Return the validation errors as an error message
+		return fmt.Errorf("validation failed:\n%s", strings.Join(errors, "\n"))
+	}
+
+	return nil
 }
