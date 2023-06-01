1e5616c90fe5ccf6635119658b460416a99cd8e3
Matt Boris <mboris@mozilla.com>
2023-06-01 10:18:14 -0400

    Fetch schema from TASKCLUSTER_ROOT_URL so cli doesn't depend on being run from repo root

--- cmd/d2g/main.go
+++ cmd/d2g/main.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
+	"path"
 	"strings"
 
 	"github.com/mcuadros/go-defaults"
@@ -25,10 +28,19 @@ func main() {
 	}
 
 	// Define the path to the JSON schema
-	schemaPath := "schemas/docker_worker_payload.json"
+	rootURL := os.Getenv("TASKCLUSTER_ROOT_URL")
+	if rootURL == "" {
+		log.Fatal("TASKCLUSTER_ROOT_URL environment variable is not set")
+	}
+	u, err := url.Parse(rootURL)
+	if err != nil {
+		log.Fatalf("Failed to parse TASKCLUSTER_ROOT_URL: %v", err)
+	}
+	u.Path = path.Join(u.Path, "schemas/docker-worker/v1/payload.json")
+	schemaURL := u.String()
 
 	// Validate the JSON input against the schema
-	err = validateJSON(schemaPath, input)
+	err = validateJSON(schemaURL, input)
 	if err != nil {
 		log.Fatalf("Input validation failed: %v", err)
 	}
@@ -54,9 +66,16 @@ func main() {
 	fmt.Println(string(formattedActualGWPayload))
 }
 
-func validateJSON(schemaPath string, input []byte) error {
-	// Read the JSON schema from file
-	schemaBytes, err := os.ReadFile(schemaPath)
+func validateJSON(schemaURL string, input []byte) error {
+	// Fetch the JSON schema from the URL
+	response, err := http.Get(schemaURL)
+	if err != nil {
+		return fmt.Errorf("failed to fetch JSON schema: %v", err)
+	}
+	defer response.Body.Close()
+
+	// Read the schema from the response body
+	schemaBytes, err := io.ReadAll(response.Body)
 	if err != nil {
 		return fmt.Errorf("failed to read JSON schema: %v", err)
 	}
