65af32b861bd58581515d8a0419a50100293eb9b
Pete Moore <pmoore@mozilla.com>
2023-06-22 18:40:20 +0200

    wip

--- workers/generic-worker/helper_test.go
+++ workers/generic-worker/helper_test.go
@@ -599,7 +599,7 @@ func getArtifactContentWithResponses(t *testing.T, taskID string, artifact strin
 	if err != nil {
 		t.Fatalf("Error creating GET request for url %v", url)
 	}
-	req.Header.Set("Accept-Encoding", "gzip,identity")
+	req.Header.Set("Accept-Encoding", "gzip")
 	client := &http.Client{Transport: tr}
 	rawResp, _, err := httpbackoff.ClientDo(client, req)
 	if err != nil {
