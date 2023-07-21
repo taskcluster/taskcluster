4938bb2669f34711f07f82e7448fcac407ea6718
Yaraslau Kurmyza <yarik@mozilla.com>
2023-07-21 12:49:32 +0200

    feat(queue): Expire artifacts don't report errors on each missing object
    Instead, debug level is used and total errors count is added to the
    resulting log message.
    
    This is because GCS throws errors unlike AWS when deleted object is
    missing, which results in millions of reported objects (which were
    removed manually before migration)

---
+++ changelog/issue-6405.md
@@ -0,0 +1,6 @@
+audience: admins
+level: patch
+reference: issue 6405
+---
+
+Expire artifacts job no longer logs errors for each missing artifact. Instead it reports the number of missing artifacts at the end of the job.
--- services/queue/src/utils.js
+++ services/queue/src/utils.js
@@ -44,6 +44,7 @@ const artifactUtils = {
   async expire({ db, publicBucket, privateBucket, ignoreError, monitor,
     expires, useBulkDelete, expireArtifactsBatchSize }) {
     let count = 0;
+    let errorsCount = 0;
 
     assert(!useBulkDelete || expireArtifactsBatchSize <= 1000, 'expireArtifactsBatchSize must be <= 1000 when useBulkDelete is true');
 
@@ -93,6 +94,7 @@ const artifactUtils = {
                 message: obj.Message,
                 prefix: obj.Key,
               })));
+              errorsCount += response.Errors.length;
 
               // this will likely be a soft error, so we'll just log it
               const err = new Error('Failed to delete s3 objects');
@@ -117,15 +119,16 @@ const artifactUtils = {
         try {
           return await bucket.deleteObject(entry.details.prefix);
         } catch (err) {
+          errorsCount++;
           // Some S3-compatible storage providers might throw an error when file is missing
           // where AWS S3 would return 204 response without body
           // GCS: https://cloud.google.com/storage/docs/xml-api/delete-object
           // S3: https://docs.aws.amazon.com/AmazonS3/latest/API/API_DeleteObject.html
           if (`${err.code} ${err.name} ${err.message}`.includes('NoSuchKey')) {
-            const reported = new Error('Failed to delete missing S3 object');
-            reported.prefix = entry.details.prefix;
-            reported.bucket = bucket.bucket;
-            monitor.reportError(reported);
+            monitor.debug(
+              'WARNING: Failed to delete missing S3 object: %s:%s %j',
+              bucket.bucket, entry.details.prefix, err,
+            );
           } else {
             throw err;
           }
@@ -178,7 +181,7 @@ const artifactUtils = {
       }
     }
 
-    return count;
+    return { count, errorsCount };
   },
 };
 
