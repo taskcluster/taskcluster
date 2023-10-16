2ca2c64464843b8b50b72b556543edbe0a166aa9
Pete Moore <pmoore@mozilla.com>
2023-10-16 18:26:28 +0200

    no biggie

--- workers/generic-worker/helper_test.go
+++ workers/generic-worker/helper_test.go
@@ -162,18 +162,19 @@ func ensureResolution(t *testing.T, taskID, state, reason string) {
 	if err != nil {
 		t.Fatal("Error retrieving status from queue")
 	}
+	// This extra space is *super-useful* for breaking up the output since
+	// this shows a task log embedded inside a different task log
+	t.Log("Task logs:")
+	t.Log("")
+	t.Log("")
+	t.Log("")
+	t.Log(LogText(t))
+	t.Log("")
+	t.Log("")
+	t.Log("")
+
 	if status.Status.Runs[0].State != state || status.Status.Runs[0].ReasonResolved != reason {
 		t.Logf("Expected task %v to resolve as '%v/%v' but resolved as '%v/%v'", taskID, state, reason, status.Status.Runs[0].State, status.Status.Runs[0].ReasonResolved)
-		t.Log("Task logs:")
-		// This extra space is *super-useful* for breaking up the output since
-		// this shows a task log embedded inside a different task log
-		t.Log("")
-		t.Log("")
-		t.Log("")
-		t.Fatal(LogText(t))
-		t.Log("")
-		t.Log("")
-		t.Log("")
 	} else {
 		t.Logf("Task %v resolved as %v/%v as required.", taskID, status.Status.Runs[0].State, status.Status.Runs[0].ReasonResolved)
 	}
