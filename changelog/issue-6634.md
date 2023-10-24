bc678bff12b5c36094c711d92afcba44f5c5046c
Yaraslau Kurmyza <yarik@mozilla.com>
2023-10-24 15:29:38 +0200

    feat(queue): Add HintPoller tests

---
+++ changelog/issue-6634.md
@@ -0,0 +1,6 @@
+audience: general
+level: patch
+reference: issue 6634
+---
+
+Fixes `queue.claimWork` endpoint returning `500` in some rare conditions.
---
+++ services/queue/test/hintpoller_test.js
@@ -0,0 +1,120 @@
+import assume from 'assume';
+import HintPoller from '../src/hintpoller.js';
+import helper from './helper.js';
+import testing from 'taskcluster-lib-testing';
+
+helper.secrets.mockSuite(testing.suiteName(), [], function(mock, skipping) {
+  test('calls pollPendingQueue', async () => {
+    const monitor = await helper.load('monitor');
+
+    let pollCalls = 0;
+    const pollPendingQueue = async (count) => {
+      pollCalls += count;
+      return [count];
+    };
+
+    const hintPoller = new HintPoller('taskQueue/Id', {
+      monitor,
+      pollPendingQueue,
+      onError: err => console.error(err),
+      onDestroy: () => {},
+    });
+
+    const aborted = new Promise(resolve => setTimeout(resolve, 100));
+    const hints = await hintPoller.requestClaim(1, aborted);
+
+    assume(hints).deep.equals([1]);
+    assume(pollCalls).equals(1);
+  });
+
+  test('cannot request claim after destroy', async () => {
+    const monitor = await helper.load('monitor');
+
+    let pollCalls = 0;
+    const pollPendingQueue = async (count) => {
+      pollCalls += count;
+      return [count];
+    };
+
+    let destroyCalled = false;
+
+    const hintPoller = new HintPoller('taskQueue/Id', {
+      monitor,
+      pollPendingQueue,
+      onError: err => console.error(err),
+      onDestroy: () => destroyCalled = true,
+    });
+
+    const aborted = new Promise(resolve => setTimeout(resolve, 1000));
+
+    hintPoller.destroy();
+    try {
+      await hintPoller.requestClaim(1, aborted);
+      assume(true).equals(false); // this should not happen
+    } catch (err) {
+      assume(err.message).equals('requestClaim() called after destroy()');
+    }
+    assume(pollCalls).equals(0);
+    assume(destroyCalled).equals(true);
+  });
+
+  test('multiple listeners', async () => {
+    const monitor = await helper.load('monitor');
+
+    let pollCalls = 0;
+    const pollPendingQueue = async (count) => {
+      pollCalls += 1;
+      return [count];
+    };
+
+    const hintPoller = new HintPoller('taskQueue/Id', {
+      monitor,
+      pollPendingQueue,
+      onError: err => console.error(err),
+      onDestroy: () => {},
+    });
+
+    const aborted = new Promise(resolve => setTimeout(resolve, 100));
+    const hints1 = await hintPoller.requestClaim(1, aborted);
+    const hints2 = await hintPoller.requestClaim(1, aborted);
+
+    assume(hints1).deep.equals([1]);
+    assume(hints2).deep.equals([1]);
+    assume(pollCalls).equals(2);
+  });
+
+  test('releases unused hints', async () => {
+    const monitor = await helper.load('monitor');
+
+    let pollCalls = 0;
+    let released = false;
+    const pollPendingQueue = async (count) => {
+      pollCalls += 1;
+      return [{
+        count,
+        // first hint will be claimed
+      }, {
+        count,
+        // second hint will be released
+        release: () => released = true,
+      }];
+    };
+
+    const hintPoller = new HintPoller('taskQueue/Id', {
+      monitor,
+      pollPendingQueue,
+      onError: err => console.error(err),
+      onDestroy: () => {},
+    });
+
+    const aborted = new Promise(resolve => setTimeout(resolve, 100));
+    await hintPoller.requestClaim(1, aborted);
+    assume(pollCalls).equals(1);
+    assume(released).equals(true);
+
+    released = false;
+    await hintPoller.requestClaim(2, aborted);
+    assume(pollCalls).equals(2);
+    assume(released).equals(false); // since 2 were requested
+  });
+});
