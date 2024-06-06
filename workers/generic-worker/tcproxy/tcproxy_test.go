28d1b79b02fe2a3f8bc4dbac7b2e3b137cb666d6
Pete Moore <pmoore@mozilla.com>
2024-06-06 11:57:36 +0200

    Use unique port for tcproxy test to fix conflicts

--- workers/generic-worker/tcproxy/tcproxy_test.go
+++ workers/generic-worker/tcproxy/tcproxy_test.go
@@ -28,7 +28,7 @@ func TestTcProxy(t *testing.T) {
 		Certificate:      certificate,
 		AuthorizedScopes: []string{"queue:get-artifact:SampleArtifacts/_/X.txt"},
 	}
-	ll, err := New(executable, 34569, rootURL, creds)
+	ll, err := New(executable, 34570, rootURL, creds)
 	// Do defer before checking err since err could be a different error and
 	// process may have already started up.
 	defer func() {
@@ -40,7 +40,7 @@ func TestTcProxy(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Could not initiate taskcluster-proxy process:\n%s", err)
 	}
-	res, err := http.Get("http://localhost:34569/auth/v1/scopes/current")
+	res, err := http.Get("http://localhost:34570/auth/v1/scopes/current")
 	if err != nil {
 		t.Fatalf("Could not hit url to download artifact using taskcluster-proxy: %v", err)
 	}
