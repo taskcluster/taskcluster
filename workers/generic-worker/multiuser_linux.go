1a58d29fe5d5866a83c731d6a181608da895d235
Pete Moore <pmoore@mozilla.com>
2024-07-31 20:29:57 +0200

    fix(generic-worker)!: only chown writable directory cache files owned by previous task user to new task user

--- workers/generic-worker/multiuser_linux.go
+++ workers/generic-worker/multiuser_linux.go
@@ -2,6 +2,14 @@
 
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/taskcluster/taskcluster/v77/workers/generic-worker/host"
+	gwruntime "github.com/taskcluster/taskcluster/v77/workers/generic-worker/runtime"
+)
+
 func defaultTasksDir() string {
 	return "/home"
 }
@@ -16,4 +24,14 @@ func platformFeatures() []Feature {
 		// checks as late as possible
 		&ChainOfTrustFeature{},
 	}
+}
+
+func makeDirUnreadableForUser(dir string, user *gwruntime.OSUser) error {
+	// Note, only need to set top directory, not recursively, since without
+	// access to top directory, nothing inside can be read anyway
+	err := host.Run("/bin/chown", "0:0", dir)
+	if err != nil {
+		return fmt.Errorf("[mounts] Not able to make directory %v owned by root/root in order to prevent %v from having access: %v", dir, user.Name, err)
+	}
+	return os.Chmod(dir, 0700)
 }
