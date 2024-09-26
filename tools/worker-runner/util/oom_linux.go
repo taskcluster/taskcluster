f6858b38a80dad9b61f27cab3477699f89dd16eb
Matt Boris <mboris@mozilla.com>
2024-09-26 14:52:23 -0400

    fix(worker-runner): replace deprecated oom adjuster

--- tools/worker-runner/util/oom_linux.go
+++ tools/worker-runner/util/oom_linux.go
@@ -8,8 +8,10 @@ import (
 // DisableOOM disables oom killer for the given process
 func DisableOOM(pid int) error {
 	return os.WriteFile(
-		fmt.Sprintf("/proc/%d/oom_adj", pid),
-		[]byte("-17"),
+		fmt.Sprintf("/proc/%d/oom_score_adj", pid),
+		// Use -1000 to completely disable OOM-killing
+		// https://man7.org/linux/man-pages/man5/proc_pid_oom_adj.5.html
+		[]byte("-1000"),
 		0600,
 	)
 }
