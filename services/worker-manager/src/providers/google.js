80d50a9a2c6da29e80f5be2328e495bf287be89a
Matt Boris <mboris@mozilla.com>
2024-10-10 15:59:04 -0400

    Remove variable shadowing

--- services/worker-manager/src/providers/google.js
+++ services/worker-manager/src/providers/google.js
@@ -163,10 +163,10 @@ export class GoogleProvider extends Provider {
   }
 
   async removeWorker({ worker, reason }) {
-    await worker.update(this.db, worker => {
-      if ([Worker.states.REQUESTED, Worker.states.RUNNING].includes(worker.state)) {
-        worker.lastModified = new Date();
-        worker.state = Worker.states.STOPPING;
+    await worker.update(this.db, w => {
+      if ([Worker.states.REQUESTED, Worker.states.RUNNING].includes(w.state)) {
+        w.lastModified = new Date();
+        w.state = Worker.states.STOPPING;
       }
     });
     await this.onWorkerRemoved({ worker, reason });
