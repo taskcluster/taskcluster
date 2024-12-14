661c727490ff4c02514eba747200c8a4c87ac09e
Ujas Thakkar <42888663+thakkarujas5@users.noreply.github.com>
2024-12-14 16:42:12 +0530

    feat(db): Add audit history

---
+++ db/versions/0105-migration.sql
@@ -0,0 +1,13 @@
+begin
+
+  CREATE TABLE audit_history (
+    entity_id text not null,
+    entity_type text not null,
+    client_id text not null,
+    entity_history jsonb not null,
+    created_at timestamptz not null
+  );
+
+  ALTER TABLE audit_history add primary key (entity_id, entity_type);
+
+end
