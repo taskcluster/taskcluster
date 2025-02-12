875b908ca0e742e3b27a93004e8d04fa1a5a4f41
Matt Boris <mboris@mozilla.com>
2025-02-12 14:23:37 -0500

    build(deps): upgrade to go1.24.0

--- workers/generic-worker/build.sh
+++ workers/generic-worker/build.sh
@@ -9,9 +9,9 @@ cd "$(dirname "${0}")"
 #
 # DO NOT CHANGE HERE!
 ####################################################################
-# Support go 1.23 or higher.
+# Support go 1.24 or higher.
 GO_MAJOR_VERSION=1
-MIN_GO_MINOR_VERSION=23
+MIN_GO_MINOR_VERSION=24
 
 unset CGO_ENABLED
 unset GOOS
