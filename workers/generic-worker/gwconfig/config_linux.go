4c78297d2218f5500bd332a74456d5be8854ece5
Matt Boris <mboris@mozilla.com>
2025-09-18 15:49:47 -0400

    chore(d2g): create config struct type

--- workers/generic-worker/gwconfig/config_linux.go
+++ workers/generic-worker/gwconfig/config_linux.go
@@ -1,33 +1,37 @@
 package gwconfig
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/taskcluster/taskcluster/v90/tools/d2g"
+)
 
 type PublicPlatformConfig struct {
-	D2GConfig                 map[string]any `json:"d2gConfig"`
-	DisableNativePayloads     bool           `json:"disableNativePayloads"`
-	EnableLoopbackAudio       bool           `json:"enableLoopbackAudio"`
-	EnableLoopbackVideo       bool           `json:"enableLoopbackVideo"`
-	LoopbackAudioDeviceNumber uint8          `json:"loopbackAudioDeviceNumber"`
-	LoopbackVideoDeviceNumber uint8          `json:"loopbackVideoDeviceNumber"`
+	D2GConfig                 d2g.Config `json:"d2gConfig"`
+	DisableNativePayloads     bool       `json:"disableNativePayloads"`
+	EnableLoopbackAudio       bool       `json:"enableLoopbackAudio"`
+	EnableLoopbackVideo       bool       `json:"enableLoopbackVideo"`
+	LoopbackAudioDeviceNumber uint8      `json:"loopbackAudioDeviceNumber"`
+	LoopbackVideoDeviceNumber uint8      `json:"loopbackVideoDeviceNumber"`
 }
 
 func DefaultPublicPlatformConfig() *PublicPlatformConfig {
 	return &PublicPlatformConfig{
-		D2GConfig: map[string]any{
-			"enableD2G":             false,
-			"allowChainOfTrust":     true,
-			"allowDisableSeccomp":   true,
-			"allowGPUs":             false,
-			"allowHostSharedMemory": true,
-			"allowInteractive":      true,
-			"allowKVM":              true,
-			"allowLoopbackAudio":    true,
-			"allowLoopbackVideo":    true,
-			"allowPrivileged":       true,
-			"allowPtrace":           true,
-			"allowTaskclusterProxy": true,
-			"gpus":                  "all",
-			"logTranslation":        true,
+		D2GConfig: d2g.Config{
+			EnableD2G:             false,
+			AllowChainOfTrust:     true,
+			AllowDisableSeccomp:   true,
+			AllowGPUs:             false,
+			AllowHostSharedMemory: true,
+			AllowInteractive:      true,
+			AllowKVM:              true,
+			AllowLoopbackAudio:    true,
+			AllowLoopbackVideo:    true,
+			AllowPrivileged:       true,
+			AllowPtrace:           true,
+			AllowTaskclusterProxy: true,
+			GPUs:                  "all",
+			LogTranslation:        true,
 		},
 		DisableNativePayloads:     false,
 		EnableLoopbackAudio:       true,
@@ -38,14 +42,14 @@ func DefaultPublicPlatformConfig() *PublicPlatformConfig {
 }
 
 func (c *PublicPlatformConfig) D2GEnabled() bool {
-	return c.D2GConfig["enableD2G"].(bool)
+	return c.D2GConfig.EnableD2G
 }
 
 // Helper method used to enable D2G
 // during testing.
 func (c *PublicPlatformConfig) EnableD2G(t *testing.T) {
 	t.Helper()
-	c.D2GConfig["enableD2G"] = true
+	c.D2GConfig.EnableD2G = true
 }
 
 func (c *PublicPlatformConfig) NativePayloadsDisabled() bool {
