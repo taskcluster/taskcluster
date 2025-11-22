taskcluster/taskcluster

commit cd4a7ae7f82244955018b4d5e6dbac4c871c26aa

Step 2 - remove reliance on the Task-related entities

services/queue/src/dependencytracker.js
@@ -1,5 +1,5 @@
 let assert = require('assert');
-let Entity = require('taskcluster-lib-entities');
+const {Task} = require('./data');
 
 /**
  * DependencyTracker tracks dependencies between tasks and ensure that dependent
@@ -8,33 +8,23 @@ let Entity = require('taskcluster-lib-entities');
  *
  * Options:
  * {
- *   Task:               data.Task instance
  *   publisher:          publisher from exchanges
  *   queueService:       QueueService instance
- *   TaskDependency:     data.TaskDependency instance
- *   TaskRequirement:    data.TaskRequirement instance
- *   TaskGroupActiveSet: data.TaskGroupMember instance
  * }
  */
 class DependencyTracker {
   constructor(options = {}) {
     // Validate options
     assert(options, 'options are required');
-    assert(options.Task, 'Expected options.Task');
+    assert(options.db, 'Expected options.db');
     assert(options.publisher, 'Expected options.publisher');
     assert(options.queueService, 'Expected options.queueService');
-    assert(options.TaskDependency, 'Expected options.TaskDependency');
-    assert(options.TaskRequirement, 'Expected options.TaskRequirement');
-    assert(options.TaskGroupActiveSet, 'Expected options.TaskGroupActiveSet');
     assert(options.monitor, 'Expected options.monitor');
 
     // Store options on this object
-    this.Task = options.Task;
+    this.db = options.db;
     this.publisher = options.publisher;
     this.queueService = options.queueService;
-    this.TaskDependency = options.TaskDependency;
-    this.TaskRequirement = options.TaskRequirement;
-    this.TaskGroupActiveSet = options.TaskGroupActiveSet;
     this.monitor = options.monitor;
   }
 
@@ -45,32 +35,9 @@ class DependencyTracker {
    * This will return {message, details} if there is an error.
    */
   async trackDependencies(task) {
-
-    // Create TaskRequirement entries, each entry implies that taskId is blocked
-    // by requiredTaskId. This relation is used to track if a taskId is blocked.
-    await Promise.all(task.dependencies.map(requiredTaskId => {
-      return this.TaskRequirement.create({
-        taskId: task.taskId,
-        requiredTaskId,
-        expires: task.expires,
-      }, true);
-    }));
-
-    // Create TaskDependency entries, each entry implies that taskId is required
-    // by dependentTaskId. This relation is used so taskId can find dependent
-    // tasks when it is resolved.
-    let require = 'completed';
-    if (task.requires === 'all-resolved') {
-      require = 'resolved';
+    for (let requiredTaskId of task.dependencies) {
+      await this.db.fns.add_task_dependency(task.taskId, requiredTaskId, task.requires, task.expires);
     }
-    await Promise.all(task.dependencies.map(requiredTaskId => {
-      return this.TaskDependency.create({
-        taskId: requiredTaskId,
-        dependentTaskId: task.taskId,
-        expires: task.expires,
-        require,
-      }, true);
-    }));
 
     // Load all task dependencies to see if they have been resolved.
     // We will also check for missing and expiring dependencies.
@@ -78,7 +45,7 @@ class DependencyTracker {
     let expiring = []; // Dependencies that expire before deadline
     let anySatisfied = false; // Track if any dependencies were satisfied
     await Promise.all(task.dependencies.map(async (requiredTaskId) => {
-      let requiredTask = await this.Task.load({taskId: requiredTaskId}, true);
+      let requiredTask = await Task.get(this.db, requiredTaskId);
 
       // If task is missing, we should report and error
       if (!requiredTask) {
@@ -94,11 +61,7 @@ class DependencyTracker {
       let state = requiredTask.state();
       if (state === 'completed' || task.requires === 'all-resolved' &&
           (state === 'exception' || state === 'failed')) {
-        // If a dependency is satisfied we delete the TaskRequirement entry
-        await this.TaskRequirement.remove({
-          taskId: task.taskId,
-          requiredTaskId,
-        }, true);
+        await this.db.fns.satisfy_task_dependency(task.taskId, requiredTaskId);
         // Track that we've deleted something, now we must check if any are left
         // afterward (using isBlocked)
         anySatisfied = true;
@@ -162,27 +125,13 @@ class DependencyTracker {
       // only see this error while developing their automation, so the fact
       // there is a small risk the task won't be deleted isn't an attack vector.
 
-      // First remove task and TaskDependency entries
-      await Promise.all([
-        task.remove(true),
-        Promise.all(task.dependencies.map(requiredTaskId => {
-          return this.TaskDependency.remove({
-            taskId: requiredTaskId,
-            dependentTaskId: task.taskId,
-          }, true);
-        })),
-      ]);
+      // First remove task
+      await this.db.fns.remove_task(task.taskId);
 
-      // Then remove TaskRequirement entries, because removing these makes it
+      // Then remove depdencies, because removing these makes it
       // easier to trigger the task. So we remove them after removing the task.
-      await Promise.all([
-        Promise.all(task.dependencies.map(requiredTaskId => {
-          return this.TaskRequirement.remove({
-            taskId: task.taskId,
-            requiredTaskId,
-          }, true);
-        })),
-      ]);
+      await Promise.all(task.dependencies.map(requiredTaskId =>
+        this.db.fns.remove_task_dependency(task.taskId, requiredTaskId)));
 
       return {
         message: msg,
@@ -199,19 +148,8 @@ class DependencyTracker {
     if (anySatisfied && !await this.isBlocked(task.taskId) ||
         task.dependencies.length === 0) {
 
-      await task.modify(task => {
-        // Don't modify if there already is a run
-        if (task.runs.length > 0) {
-          return;
-        }
-
-        // Add initial run (runId = 0)
-        task.runs.push({
-          state: 'pending',
-          reasonCreated: 'scheduled',
-          scheduled: new Date().toJSON(),
-        });
-      });
+      task.updateStatusWith(
+        await this.db.fns.schedule_task(task.taskId, 'scheduled'));
     }
 
     // We don't have any error
@@ -224,61 +162,46 @@ class DependencyTracker {
          resolution === 'exception',
     'resolution must be completed, failed or exception');
 
-    // Create query condition
-    let condition = {
-      taskId: Entity.op.equal(taskId),
-    };
+    // iterate through the dependencies in such a way that deletion of
+    // a task does not cause a dependency to be skipped (which might occur
+    // if using offset and limit)
+    let tasksAfter = null;
+    while (true) {
+      const deps = await this.db.fns.get_dependent_tasks(taskId, null, tasksAfter, 100, null);
 
-    // We only support conditions on dates, as they cannot
-    // be used to inject SQL -- `Date.toJSON` always produces a simple string
-    // with no SQL metacharacters.
-    //
-    // Previously with azure, we added the `require` field in the scan method
-    // (i.e., this.TaskRequirement.scan({ taskId, require }, ...))
-    await this.TaskDependency.query(condition, {
-      limit: 250,
-      handler: async (dep) => {
+      for (let dep of deps) {
+        tasksAfter = dep.dependent_task_id;
+
         if (resolution !== 'completed') {
-          // If the resolution wasn't 'completed', we can only remove
-          // TaskRequirement entries if the 'require' relation is 'resolved'.
-          if (dep.require !== 'resolved') {
+          // If the resolution wasn't 'completed', we can only remove mark this
+          // dependency as satisified if the 'require' relation is
+          // 'all-resolved'.
+          if (dep.requires !== 'all-resolved') {
             return;
           }
         }
-        // Remove the requirement that is blocking
-        await this.TaskRequirement.remove({
-          taskId: dep.dependentTaskId,
-          requiredTaskId: taskId,
-        }, true);
 
-        if (!await this.isBlocked(dep.dependentTaskId)) {
-          await this.scheduleTask(dep.dependentTaskId);
+        // Remove the requirement that is blocking
+        await this.db.fns.satisfy_task_dependency(dep.dependent_task_id, taskId);
+
+        if (!await this.isBlocked(dep.dependent_task_id)) {
+          await this.scheduleTask(dep.dependent_task_id);
         }
-      },
-    });
+      }
+
+      // when we've seen all of the dependencies, we're done
+      if (deps.length === 0) {
+        break;
+      }
+    }
 
     await this.updateTaskGroupActiveSet(taskId, taskGroupId, schedulerId);
   }
 
   /** Returns true, if some task requirement is blocking the task */
   async isBlocked(taskId) {
-    let result = await this.TaskRequirement.query({taskId}, {limit: 1});
-
-    // Ensure that we can in-fact make emptiness in a single request. It seems
-    // logical that we can. But Microsoft Azure documentation is sketchy, so
-    // we better not make assumptions about their APIs being sane. But since
-    // we're not filtering here I fully expect that we should able to get the
-    // first entry. Just we could if we specified both partitionKey and rowKey.
-    if (result.entries.length === 0 && result.continuation) {
-      let err = new Error('Single request emptiness check invariant failed. ' +
-                          'This is a flawed assumption in isBlocked()');
-      err.taskId = taskId;
-      err.result = result;
-      throw err;
-    }
-
-    // If we have any entries the taskId is blocked!
-    return result.entries.length > 0;
+    const res = await this.db.fns.is_task_blocked(taskId);
+    return res[0].is_task_blocked;
   }
 
   /**
@@ -288,23 +211,11 @@ class DependencyTracker {
    * that the group is "resolved" for the time being.
    */
   async updateTaskGroupActiveSet(taskId, taskGroupId, schedulerId) {
-    await this.TaskGroupActiveSet.remove({taskId, taskGroupId}, true);
+    await this.db.fns.mark_task_ever_resolved(taskId);
 
-    // check for emptiness of the partition
-    let result = await this.TaskGroupActiveSet.query({taskGroupId}, {limit: 1});
+    const res = await this.db.fns.is_task_group_active(taskGroupId);
 
-    // We assume this generally won't happen, see comment in isBlocked(...)
-    // which also uses a query operation with limit = 1 to check for partition emptiness.
-    if (result.entries.length === 0 && result.continuation) {
-      let err = new Error('Single request emptiness check invariant failed. ' +
-                          'This is a flawed assumption in resolveTask()');
-      err.taskId = taskId;
-      err.taskGroupId = taskGroupId;
-      err.result = result;
-      throw err;
-    }
-
-    if (result.entries.length === 0) {
+    if (!res[0].is_task_group_active) {
       await this.publisher.taskGroupResolved({
         taskGroupId,
         schedulerId,
@@ -322,7 +233,7 @@ class DependencyTracker {
     // Load task, if not already loaded
     let task = taskOrTaskId;
     if (typeof task === 'string') {
-      task = await this.Task.load({taskId: taskOrTaskId}, true);
+      task = await Task.get(this.db, taskOrTaskId);
 
       if (!task) {
         // This happens if we fail half-way through a createTask call.
@@ -340,19 +251,8 @@ class DependencyTracker {
     }
 
     // Ensure that we have an initial run
-    await task.modify(task => {
-      // Don't modify if there already is a run
-      if (task.runs.length > 0) {
-        return;
-      }
-
-      // Add initial run (runId = 0)
-      task.runs.push({
-        state: 'pending',
-        reasonCreated: 'scheduled',
-        scheduled: new Date().toJSON(),
-      });
-    });
+    task.updateStatusWith(
+      await this.db.fns.schedule_task(task.taskId, 'scheduled'));
 
     // Construct status structure
     let status = task.status();

services/queue/test/dependency_test.js
@@ -569,27 +569,25 @@ helper.secrets.mockSuite(testing.suiteName(), ['aws'], function(mock, skipping) 
     helper.assertNoPulseMessage('task-pending');
     helper.clearPulseMessages();
 
-    debug('### Get new data wrappers');
-    const TaskDependency = helper.TaskDependency;
-    const TaskRequirement = helper.TaskRequirement;
+    const db = await helper.load('db');
 
-    debug('### Load relations to ensure they are present');
-    const r3 = await TaskDependency.load({taskId: taskIdA, dependentTaskId: taskIdA}, true);
-    const r4 = await TaskRequirement.load({taskId: taskIdA, requiredTaskId: taskIdA}, true);
-    assert(r3, 'Expected TaskDependency');
-    assert(r4, 'Expected TaskRequirement');
+    debug('### Load dependencies to ensure they are present');
+    assert.deepEqual(
+      await db.fns.get_dependent_tasks(taskIdA, null, null, null, null),
+      [{dependent_task_id: taskIdA, requires: 'all-completed', satisfied: false}]);
+    assert.deepEqual(
+      await db.fns.get_dependent_tasks(taskIdB, null, null, null, null),
+      [{dependent_task_id: taskIdB, requires: 'all-completed', satisfied: false}]);
 
-    debug('### expire task-requirement/task-dependency');
-    await helper.runExpiration('expire-task-requirement'); // no longer does anything..
+    debug('### expire task-dependency');
     await helper.runExpiration('expire-task-dependency');
-    const r7 = await TaskDependency.load({taskId: taskIdA, dependentTaskId: taskIdA}, true);
-    const r8 = await TaskRequirement.load({taskId: taskIdA, requiredTaskId: taskIdA}, true);
-    assert(!r7, 'Did not expect TaskDependency');
-    assert(!r8, 'Did not expect TaskRequirement');
 
-    const r9 = await TaskDependency.load({taskId: taskIdB, dependentTaskId: taskIdB}, true);
-    const r10 = await TaskRequirement.load({taskId: taskIdB, requiredTaskId: taskIdB}, true);
-    assert(r9, 'Expected TaskDependency');
-    assert(r10, 'Expected TaskRequirement');
+    debug('### Load dependencies to ensure taskA is gone');
+    assert.deepEqual(
+      await db.fns.get_dependent_tasks(taskIdA, null, null, null, null),
+      []);
+    assert.deepEqual(
+      await db.fns.get_dependent_tasks(taskIdB, null, null, null, null),
+      [{dependent_task_id: taskIdB, requires: 'all-completed', satisfied: false}]);
   });
 });

db/test/versions/0030_test.js
@@ -1,0 +1,6 @@
+const testing = require('taskcluster-lib-testing');
+
+suite(testing.suiteName(), function() {
+  // this version only adds/redefines methods, which is tested in unit tests,
+  // so there is nothing to test on upgrade
+});

dev-docs/dev-config-example.yml
@@ -185,13 +185,7 @@ queue:
       cpu: 10m
     expireTaskGroups:
       cpu: 10m
-    expireTaskGroupMembers:
-      cpu: 10m
-    expireTaskGroupSizes:
-      cpu: 10m
     expireTaskDependency:
-      cpu: 10m
-    expireTaskRequirement:
       cpu: 10m
     expireQueueMessages:
       cpu: 10m

entrypoint
@@ -37,10 +37,7 @@ queue/dependencyResolver) exec node services/queue/src/main dependency-resolver;
 queue/expireArtifacts) exec node services/queue/src/main expire-artifacts;;
 queue/expireTask) exec node services/queue/src/main expire-tasks;;
 queue/expireTaskGroups) exec node services/queue/src/main expire-task-groups;;
-queue/expireTaskGroupMembers) exec node services/queue/src/main expire-task-group-members;;
-queue/expireTaskGroupSizes) exec node services/queue/src/main expire-task-group-sizes;;
 queue/expireTaskDependency) exec node services/queue/src/main expire-task-dependency;;
-queue/expireTaskRequirement) exec node services/queue/src/main expire-task-requirement;;
 queue/expireQueueMessages) exec node services/queue/src/main expire-queue-messages;;
 queue/expireWorkerInfo) exec node services/queue/src/main expire-worker-info;;
 secrets/web) exec node services/secrets/src/main.js server;;

infrastructure/k8s/templates/taskcluster-queue-cron-expireTaskGroupMembers.yaml
@@ -1,42 +1,0 @@
-apiVersion: batch/v1beta1
-kind: CronJob
-metadata:
-  name: taskcluster-queue-expiretaskgroupmembers
-  labels: &ref_0
-    app.kubernetes.io/name: taskcluster-queue
-    app.kubernetes.io/instance: '{{ .Release.Name }}'
-    app.kubernetes.io/component: taskcluster-queue-expiretaskgroupmembers
-    app.kubernetes.io/part-of: taskcluster
-spec:
-  concurrencyPolicy: Forbid
-  schedule: 0 0 * * *
-  jobTemplate:
-    metadata:
-      labels: *ref_0
-    spec:
-      activeDeadlineSeconds: 86340
-      template:
-        metadata:
-          labels: *ref_0
-        spec:
-          restartPolicy: OnFailure
-          containers:
-            - name: taskcluster-queue-expiretaskgroupmembers
-              image: '{{ .Values.dockerImage }}'
-              imagePullPolicy: Always
-              args:
-                - queue/expireTaskGroupMembers
-              resources:
-                requests:
-                  cpu: '{{ .Values.queue.procs.expireTaskGroupMembers.cpu }}'
-                  memory: '{{ .Values.queue.procs.expireTaskGroupMembers.memory }}'
-              env:
-                - name: TASKCLUSTER_ROOT_URL
-                  value: '{{ .Values.rootUrl }}'
-                - name: USE_KUBERNETES_DNS_SERVICE_DISCOVERY
-                  value: '{{ .Values.useKubernetesDnsServiceDiscovery }}'
-                - name: NODE_ENV
-                  value: production
-              envFrom:
-                - secretRef:
-                    name: taskcluster-queue

commit f2978a88ed43c44d2c79165db9e7fbf5aea4a2a5

Fix early return in resolveTask sometimes skipping all-resolved dependents
This fixes a regression from commit cd4a7ae7f82 where the code was
changed to a for loop (from a .query()), which means that `return`
changed semantic from skipping one dependent to skipping all remaining
dependents.

This bug is very easily reproducible with the following graph:

- task A - fails
- task B - all-completed depends on A
- task C - all-resolved depends on A
- task B's ID is lexicographically ordered before C's
  (`get_dependent_tasks` returns task ordered by ID)

In that case, neither B or C would get scheduled even though C should
be.

Fixes #7829

services/queue/src/dependencytracker.js
@@ -184,7 +184,7 @@ class DependencyTracker {
           // dependency as satisfied if the 'require' relation is
           // 'all-resolved'.
           if (dep.requires !== 'all-resolved') {
-            return;
+            continue;
           }
         }
 

services/queue/test/dependency_test.js
@@ -550,6 +550,57 @@ helper.secrets.mockSuite(testing.suiteName(), ['aws'], function(mock, skipping) 
     await helper.stopPollingService();
   });
 
+  // https://github.com/taskcluster/taskcluster/issues/7829
+  test('taskA <- taskB (all-completed) + taskC (all-resolved)', async () => {
+    let taskIdA = slugid.v4();
+    // Ensure taskIdB < taskIdC lexicographically so B is processed first.
+    // The bug only happened if the all-completed task was processed first
+    let taskIdB = 'A' + slugid.v4().slice(1);
+    let taskIdC = 'B' + slugid.v4().slice(1);
+
+    let taskA = taskDef();
+    let taskB = _.defaults({
+      dependencies: [taskIdA],
+      requires: 'all-completed',
+    }, taskDef());
+    let taskC = _.defaults({
+      dependencies: [taskIdA],
+      requires: 'all-resolved',
+    }, taskDef());
+
+    await helper.startPollingService('dependency-resolver');
+
+    debug('### Create taskA, taskB, and taskC');
+    let r1 = await helper.queue.createTask(taskIdA, taskA);
+    let r2 = await helper.queue.createTask(taskIdB, taskB);
+    let r3 = await helper.queue.createTask(taskIdC, taskC);
+    assume(r1.status.state).equals('pending');
+    assume(r2.status.state).equals('unscheduled');
+    assume(r3.status.state).equals('unscheduled');
+    helper.clearPulseMessages();
+
+    debug('### Claim and fail taskA');
+    await helper.queue.claimTask(taskIdA, 0, {
+      workerGroup: 'my-worker-group-extended-extended',
+      workerId: 'my-worker-extended-extended',
+    });
+    await helper.queue.reportFailed(taskIdA, 0);
+    helper.assertPulseMessage('task-failed', m => m.payload.status.taskId === taskIdA);
+    helper.clearPulseMessages();
+
+    debug('### Wait for taskC (all-resolved) to be pending');
+    await testing.poll(
+      async () => helper.assertPulseMessage('task-pending', m => m.payload.status.taskId === taskIdC),
+      200, 250);
+
+    debug('### Verify taskB (all-completed) stays unscheduled');
+    let statusB = helper.checkDates(await helper.queue.status(taskIdB));
+    assume(statusB.status.state).equals('unscheduled');
+
+    helper.clearPulseMessages();
+    await helper.stopPollingService();
+  });
+
   test('expiration of relationships', async () => {
     const taskIdA = slugid.v4();
     const taskA = _.defaults({