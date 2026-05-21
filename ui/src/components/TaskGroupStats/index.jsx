ae6534ca1c07790b50c9fe32e91aec44d5de1755
Bastien Orivel <borivel@mozilla.com>
2026-05-21 16:36:23 +0200

    Make clickable div in `TaskGroupStats` an actual button
    This fixes 2 lints regarding accessibility on this element,
    `a11y/useSemanticElements` and `a11y/useKeyWithClickEvents`. The
    result looks exactly the same and now works with keyboard navigation

--- ui/src/components/TaskGroupStats/index.jsx
+++ ui/src/components/TaskGroupStats/index.jsx
@@ -50,6 +50,10 @@ import { clearAllCaches } from '../../utils/memoize';
     right: theme.spacing(2),
     cursor: 'pointer',
     color: theme.palette.info.main,
+    background: 'none',
+    border: 'none',
+    padding: 0,
+    font: 'inherit',
   },
   chart: {
     color: THEME.PRIMARY_TEXT_DARK,
@@ -227,15 +231,14 @@ export default class TaskGroupStats extends Component {
               </div>
             )}
             {tasks.length > maxTasksInGraph && (
-              <div
+              <button
+                type="button"
                 className={classes.sampleSwitch}
-                role="button"
-                tabIndex={0}
                 onClick={() => this.toggleGraphAll()}>
                 {graphAll
                   ? `Show sample (${maxInSample})`
                   : `Show all (${tasks.length})`}
-              </div>
+              </button>
             )}
             <svg
               className={classes.chart}
