715823ea2bc3da5afb4c130a38d86d655f925d5c
Nitish Agarwal <1592163+nitishagar@users.noreply.github.com>
2026-06-12 21:02:07 +0530

    feat(ui): show metadata for recently viewed tasks and task groups (#8711)

--- ui/src/utils/db.js
+++ ui/src/utils/db.js
@@ -9,6 +9,13 @@ db.version(1).stores({
   taskDefinitions: 'metadata.name, created',
 });
 
+db.version(2).stores({
+  taskIdsHistory: 'taskId, viewedAt',
+  taskGroupIdsHistory: 'taskGroupId, viewedAt',
+  userPreferences: '',
+  taskDefinitions: 'metadata.name, created',
+});
+
 /**
  * A wrapper around indexDB. If you're not dealing with collections,
  * use localForage or localStorage instead.
