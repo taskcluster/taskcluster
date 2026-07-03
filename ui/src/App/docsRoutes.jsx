0f9086b9a1e9486b0fc33b4b8ec75125c811119e
Bastien Orivel <borivel@mozilla.com>
2026-07-03 15:38:09 +0200

    Cleanup dead `webpackChunkName` comments
    Those are ignored by rollup

--- ui/src/App/docsRoutes.jsx
+++ ui/src/App/docsRoutes.jsx
@@ -2,18 +2,9 @@ import { join } from 'path';
 import { DOCS_PATH_PREFIX } from '../utils/constants';
 import lazy from '../utils/lazy';
 
-const Documentation = lazy(
-  () => import(/* webpackChunkName: 'Documentation' */ '../views/Documentation')
-);
-const Profile = lazy(
-  () => import(/* webpackChunkName: 'Profile' */ '../views/Profile')
-);
-const SwitchEntryPoint = lazy(
-  () =>
-    import(
-      /* webpackChunkName: 'SwitchEntryPoint' */ '../views/SwitchEntryPoint'
-    )
-);
+const Documentation = lazy(() => import('../views/Documentation'));
+const Profile = lazy(() => import('../views/Profile'));
+const SwitchEntryPoint = lazy(() => import('../views/SwitchEntryPoint'));
 
 export default [
   {
