taskcluster/taskcluster

commit 28fb725c885b34346ea70392b88fe8280ff94fc4

review comments

ui/src/components/WMErrorsTable/index.jsx
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from 'react';
-import { withRouter } from 'react-router-dom';
 import { isEmpty, map, pipe, sort as rSort } from 'ramda';
 import { withStyles } from '@material-ui/core/styles';
 import { camelCase } from 'change-case';
@@ -8,10 +7,12 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { shape, arrayOf, string, func } from 'prop-types';
 import ContentCopyIcon from 'mdi-react/ContentCopyIcon';
 import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from 'mdi-react/CloseIcon';
 import InformationVariantIcon from 'mdi-react/InformationVariantIcon';
 import TableRow from '@material-ui/core/TableRow/TableRow';
 import Drawer from '@material-ui/core/Drawer';
 import TableCell from '@material-ui/core/TableCell/TableCell';
+import Code from '@mozilla-frontend-infra/components/Code';
 import List from '@material-ui/core/List';
 import ListItemText from '@material-ui/core/ListItemText/ListItemText';
 import ListItem from '@material-ui/core/ListItem';
@@ -23,7 +24,6 @@ import DateDistance from '../DateDistance';
 import sort from '../../utils/sort';
 import { pageInfo, WMError } from '../../utils/prop-types';
 
-@withRouter
 @withStyles(theme => ({
   errorDescription: {
     marginRight: theme.spacing.unit,
@@ -48,15 +48,26 @@ import { pageInfo, WMError } from '../../utils/prop-types';
     paddingBottom: theme.spacing.double,
     width: 400,
   },
+  drawerPaper: {
+    width: '40vw',
+    [theme.breakpoints.down('sm')]: {
+      width: '90vw',
+    },
+  },
+  drawerCloseIcon: {
+    position: 'absolute',
+    top: theme.spacing.unit,
+    right: theme.spacing.unit,
+  },
 }))
 export default class WorkerManagerErrorsTable extends Component {
   static propTypes = {
     onPageChange: func.isRequired,
     searchTerm: string,
     errorsConnection: shape({
-      edges: arrayOf(WMError),
-      pageInfo,
-    }),
+      edges: arrayOf(shape({node: WMError.isRequred}).isRequired).isRequired,
+      pageInfo: pageInfo.isRequired,
+    }).isRequired,
   };
 
   static defaultProps = {
@@ -148,17 +159,17 @@ export default class WorkerManagerErrorsTable extends Component {
     return (
       <TableRow key={errorId}>
         <TableCell>
+          <TableCellItem>
+            <ListItemText disableTypography primary={title} />
+          </TableCellItem>
+        </TableCell>
+        <TableCell>
           <IconButton
             className={classes.infoButton}
             name={errorId}
             onClick={this.handleDrawerOpen}>
             <InformationVariantIcon size={iconSize} />
           </IconButton>
-          <TableCellItem>
-            <ListItemText disableTypography primary={title} />
-          </TableCellItem>
-        </TableCell>
-        <TableCell>
           <Typography className={classes.errorDescription}>
             {description}
           </Typography>
@@ -209,38 +220,49 @@ export default class WorkerManagerErrorsTable extends Component {
         <Drawer
           anchor="right"
           open={Boolean(drawerError)}
-          onClose={this.handleDrawerClose}>
+          onClose={this.handleDrawerClose}
+          classes={{paper: classes.drawerPaper}}>
           {drawerError && (
-            <div className={classes.metadataContainer}>
-              <Typography variant="h5" className={classes.headline}>
-                {drawerError.errorId}
-              </Typography>
-              <List>
-                <ListItem>
-                  <ListItemText primary="Title" secondary={drawerError.title} />
-                </ListItem>
-                <ListItem>
-                  <ListItemText
-                    primary="Description"
-                    secondary={drawerError.description}
-                  />
-                </ListItem>
-                <ListItem>
-                  <ListItemText
-                    primary="Reported"
-                    secondary={drawerError.reported}
-                  />
-                </ListItem>
-                <ListItem>
-                  <ListItemText
-                    primary="Extra"
-                    secondary={
-                      <pre>{JSON.stringify(drawerError.extra, null, 2)}</pre>
-                    }
-                  />
-                </ListItem>
-              </List>
-            </div>
+            <Fragment>
+              <IconButton
+                onClick={this.handleDrawerClose}
+                className={classes.drawerCloseIcon}>
+                <CloseIcon />
+              </IconButton>
+              <div className={classes.metadataContainer}>
+                <Typography variant="h5" className={classes.headline}>
+                  {drawerError.errorId}
+                </Typography>
+                <List>
+                  <ListItem>
+                    <ListItemText primary="Title" secondary={drawerError.title} />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemText
+                      primary="Description"
+                      secondary={drawerError.description}
+                    />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemText
+                      primary="Reported"
+                      secondary={drawerError.reported}
+                    />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemText
+                      primary="Extra"
+                      secondaryTypographyProps={{
+                        component: 'div',
+                      }}
+                      secondary={
+                        <Code language="json">{JSON.stringify(drawerError.extra, null, 2)}</Code>
+                      }
+                    />
+                  </ListItem>
+                </List>
+              </div>
+            </Fragment>
           )}
         </Drawer>
       </Fragment>

ui/src/views/WorkerManager/WMViewErrors/index.jsx
@@ -2,7 +2,6 @@ import { hot } from 'react-hot-loader';
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import dotProp from 'dot-prop-immutable';
-import { withRouter } from 'react-router-dom';
 import Spinner from '@mozilla-frontend-infra/components/Spinner';
 import Dashboard from '../../../components/Dashboard';
 import ErrorPanel from '../../../components/ErrorPanel';
@@ -12,7 +11,6 @@ import errorsQuery from './errors.graphql';
 import Search from '../../../components/Search';
 
 @hot(module)
-@withRouter
 @graphql(errorsQuery, {
   options: props => ({
     variables: {
@@ -82,7 +80,7 @@ export default class WMViewErrors extends Component {
             placeholder="Title, description, or error ID"
           />
         }>
-        <ErrorPanel fixed error={this.state.error || error} />
+        <ErrorPanel fixed error={error} />
 
         {loading && <Spinner loading />}
 

services/web-server/src/graphql/WorkerManager.graphql
@@ -9,12 +9,13 @@ type WorkerManagerWorkerPoolSummary {
 }
 
 type WorkerManagerError {
+    workerPoolId: String!
     errorId: ID!
     kind: String!
     title: String!
     description: String!
-    reported: DateTime
-    extra: JSON
+    reported: DateTime!
+    extra: JSON!
 }
 
 type WorkerManagerErrorsConnection implements Connection {

ui/src/utils/prop-types.js
@@ -181,10 +181,11 @@ export const workers = shape({
 });
 
 export const WMError = shape({
-  title: string,
-  description: string,
-  reported: date,
-  extra: object,
+  errorId: string.isRequired,
+  title: string.isRequired,
+  description: string.isRequired,
+  reported: date.isRequired,
+  extra: object.isRequired,
 });
 
 export const WMWorker = shape({

commit 20fbc2ddb29c5c182454f9a55c698118c226bb81

Fix a typo in WMErrorsTable propTypes
Introduced in 28fb725c885b34346ea70392b88fe8280ff94fc4, has been
throwing warnings like this since then:

```
Warning: Failed prop type: WorkerManagerErrorsTable: prop type `errorsConnection.edges[0].node` is invalid; it must be a function, usually from the `prop-types` package, but received `undefined`.
    in WorkerManagerErrorsTable (created by WithStyles(WorkerManagerErrorsTable))
    in WithStyles(WorkerManagerErrorsTable) (at WMViewErrors/index.jsx:178)
    in ErrorBoundary (at Dashboard/index.jsx:427)
```

ui/src/components/WMErrorsTable/index.jsx
@@ -70,7 +70,7 @@ export default class WorkerManagerErrorsTable extends Component {
     searchTerm: string,
     workerPoolId: string,
     errorsConnection: shape({
-      edges: arrayOf(shape({ node: WMError.isRequred }).isRequired).isRequired,
+      edges: arrayOf(shape({ node: WMError.isRequired }).isRequired).isRequired,
       pageInfo: pageInfo.isRequired,
     }).isRequired,
   };