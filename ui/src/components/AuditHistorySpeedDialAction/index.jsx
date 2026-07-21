e0c27bbef4acf62a55b6e5c99523f784bdef1862
Ujas Thakkar <42888663+thakkarujas5@users.noreply.github.com>
2026-07-22 02:13:19 +0530

    fix AuditHistorySpeedDialAction component

--- ui/src/components/AuditHistorySpeedDialAction/index.jsx
+++ ui/src/components/AuditHistorySpeedDialAction/index.jsx
@@ -1,43 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { bool, object, string } from 'prop-types';
-import { useHistory } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import HistoryIcon from 'mdi-react/HistoryIcon';
 import SpeedDialAction from '../SpeedDialAction';
 
-function AuditHistorySpeedDialAction({
-  disabled,
-  entityId,
-  entityName,
-  FabProps,
-  ...props
-}) {
-  const history = useHistory();
+@withRouter
+/**
+ * A speed dial action that links to an entity's audit history.
+ */
+export default class AuditHistorySpeedDialAction extends Component {
+  static propTypes = {
+    /** If true, the audit history action is disabled. */
+    disabled: bool,
+    /** The ID of the entity whose audit history should be displayed. */
+    entityId: string.isRequired,
+    /** The type of entity whose audit history should be displayed. */
+    entityName: string.isRequired,
+    /** Props applied to the underlying floating action button. */
+    FabProps: object,
+  };
 
-  return (
-    <SpeedDialAction
-      {...props}
-      requiresAuth
-      tooltipOpen
-      icon={<HistoryIcon />}
-      onClick={() => {
-        history.push(`/audit/${entityName}/${encodeURIComponent(entityId)}`);
-      }}
-      tooltipTitle="View Audit History"
-      FabProps={{ ...FabProps, disabled }}
-    />
-  );
+  static defaultProps = {
+    disabled: false,
+    FabProps: {},
+  };
+
+  render() {
+    const {
+      disabled,
+      entityId,
+      entityName,
+      FabProps,
+      history,
+      location: _location,
+      match: _match,
+      staticContext: _staticContext,
+      ...props
+    } = this.props;
+
+    return (
+      <SpeedDialAction
+        {...props}
+        requiresAuth
+        tooltipOpen
+        icon={<HistoryIcon />}
+        onClick={() => {
+          history.push(`/audit/${entityName}/${encodeURIComponent(entityId)}`);
+        }}
+        tooltipTitle="View Audit History"
+        FabProps={{ ...FabProps, disabled }}
+      />
+    );
+  }
 }
-
-AuditHistorySpeedDialAction.propTypes = {
-  disabled: bool,
-  entityId: string.isRequired,
-  entityName: string.isRequired,
-  FabProps: object,
-};
-
-AuditHistorySpeedDialAction.defaultProps = {
-  disabled: false,
-  FabProps: {},
-};
-
-export default AuditHistorySpeedDialAction;
