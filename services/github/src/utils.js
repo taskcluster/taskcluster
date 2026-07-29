2da1571a444e34aac0b12d598e0b18485a607b21
Yaraslau Kurmyza <yarik@mozilla.com>
2026-07-29 16:12:19 +0200

    Remove unused now throttleRequest and its tests

--- services/github/src/utils.js
+++ services/github/src/utils.js
@@ -1,12 +1,8 @@
 import crypto from 'node:crypto';
-import request from 'superagent';
-import util from 'node:util';
 import { PassThrough } from 'node:stream';
 import { downloadManagedArtifact } from '@taskcluster/client';
 
 import { ISSUE_COMMENT_ACTIONS } from './constants.js';
-
-const setTimeoutPromise = util.promisify(setTimeout);
 
 /**
  * Download a task artifact with a queue client limited to reading that one artifact.
@@ -83,51 +79,6 @@ export const downloadArtifactAsStream = async ({ queueClient, taskId, runId, art
 
   return await consumed;
 };
-
-/**
- * Retry a request call with the given URL and method.
- *
- * Responses with status 5xx are retried, and if 5 retries are exceeded then the last
- * response is returned.
- *
- * All other HTTP responses, including 4xx-series responses, are returned (not thrown).
- *
- * All other errors from superagent are thrown.
- */
-export const throttleRequest = async ({ url, method, response = { status: 0 }, attempt = 1, delay = 100 }) => {
-  if (attempt > 5) {
-    return response;
-  }
-
-  let res;
-  try {
-    res = await throttleRequest.request(method, url);
-  } catch (e) {
-    if (e.status >= 400 && e.status < 500) {
-      return e;
-    }
-
-    if (e.status >= 500) {
-      const newDelay = 2 ** attempt * delay;
-      return await setTimeoutPromise(
-        newDelay,
-        throttleRequest({
-          url,
-          method,
-          response: e,
-          attempt: attempt + 1,
-          delay,
-        })
-      );
-    }
-
-    throw e;
-  }
-  return res;
-};
-
-// for overriding in testing..
-throttleRequest.request = request;
 
 export const ciSkipRegexp = /\[(skip ci|ci skip)\]/i;
 
@@ -375,7 +326,6 @@ export const checkGithubSignature = (secret, payload, signature) => {
 };
 
 export default {
-  throttleRequest,
   downloadArtifactAsText,
   downloadArtifactAsStream,
   shouldSkipCommit,
--- services/github/test/utils_test.js
+++ services/github/test/utils_test.js
@@ -4,7 +4,6 @@ import { Readable } from 'node:stream';
 import testing from '@taskcluster/lib-testing';
 
 import {
-  throttleRequest,
   downloadArtifactAsText,
   downloadArtifactAsStream,
   shouldSkipCommit,
@@ -21,90 +20,6 @@ import {
 const toStream = str => Readable.from([Buffer.from(str)]);
 
 suite(testing.suiteName(), () => {
-  suite('throttleRequest', () => {
-    let oldRequest;
-
-    suiteSetup(() => {
-      oldRequest = throttleRequest.request;
-    });
-
-    teardown(() => {
-      throttleRequest.request = oldRequest;
-    });
-
-    test('calls with the given method and url and returns a success result immediately', async () => {
-      throttleRequest.request = async (method, url) => {
-        assert.equal(method, 'GET');
-        assert.equal(url, 'https://foo');
-        return { result: true };
-      };
-
-      const res = await throttleRequest({ url: 'https://foo', method: 'GET' });
-      assert.deepEqual(res, { result: true });
-    });
-
-    test('4xx statuses are returned (not thrown) immediately', async () => {
-      let calls = 0;
-
-      throttleRequest.request = async (_method, _url) => {
-        calls++;
-        const err = new Error('uhoh');
-        err.status = 424;
-        throw err;
-      };
-
-      const res = await throttleRequest({ url: 'https://foo', method: 'GET' });
-      assert.equal(res.status, 424);
-      assert.equal(calls, 1); // didn't retry
-    });
-
-    test('5xx statuses are retried', async () => {
-      let calls = 0;
-
-      throttleRequest.request = async (_method, _url) => {
-        calls++;
-        const err = new Error('uhoh');
-        err.status = 543;
-        throw err;
-      };
-
-      // (set delay=10 to retry more quickly than usual)
-      const res = await throttleRequest({ url: 'https://foo', method: 'GET', delay: 10 });
-      assert.equal(res.status, 543);
-      assert.equal(calls, 5);
-    });
-
-    test('5xx status retried once returns successful result', async () => {
-      let calls = 0;
-
-      throttleRequest.request = async (_method, _url) => {
-        calls++;
-        if (calls >= 1) {
-          return { status: 200 };
-        }
-        const err = new Error('uhoh');
-        err.status = 543;
-        throw err;
-      };
-
-      const res = await throttleRequest({ url: 'https://foo', method: 'GET', delay: 10 });
-      assert.equal(res.status, 200);
-      assert.equal(calls, 1);
-    });
-
-    test('connection errors are thrown directly', async () => {
-      throttleRequest.request = async (_method, _url) => {
-        const err = new Error('uhoh');
-        err.code = 'ECONNREFUSED';
-        throw err;
-      };
-
-      await assert.rejects(
-        () => throttleRequest({ url: 'https://foo', method: 'GET', delay: 10 }),
-        err => err.code === 'ECONNREFUSED'
-      );
-    });
-  });
   suite('shouldSkipCommit', () => {
     const skipMessages = [
       '[CI Skip] this is not ready',
